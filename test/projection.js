14ea9bf61a5501e5ac2e0cbb94cc728a5e2292be
Chris Chua <chua@uber.com>
2016-06-08 19:42:48 -0700

    simple test case

---
+++ test/projection.js
@@ -0,0 +1,26 @@
+import test from 'tape-catch';
+import project from '../src/projection';
+
+test('Basic functionality', t => {
+  const mapViewState = {
+    altitude: 1.5,
+    bearing: 0,
+    height: 389,
+    latitude: 37.751537058389985,
+    longitude: -122.42694203247012,
+    pitch: 0,
+    width: 1821,
+    zoom: 11.5
+  };
+
+  const expectedResult = [
+    0.0016474464579901227, 0, 0, 0, 0,
+    -0.0077120822622108315, 0, 0, 0, 0,
+    -0.0029379360998898277, -0.002570694087403599,
+    -390.6996450744531, 4421.318644674351, 1.5, 1.5
+  ];
+
+  t.deepEqual(project(mapViewState), expectedResult, 'Gets expected result');
+  t.end();
+});
+
