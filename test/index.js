3c1603033d3af9d8a04eb58a70645e9e6be930da
Ib Green <ibgreen@users.noreply.github.com>
2016-09-06 16:22:57 -0700

    Add layer construction test cases, including empty and null data.  (#66)
    * Add layer construction test cases, including empty and null data. More robust layerName debug helper.
    
    * Fix unit tests

--- test/index.js
+++ test/index.js
@@ -20,3 +20,5 @@
 
 import './layer-spec';
 import './viewport-spec';
+
+import './layers-spec';
--- test/layers-spec.js
+++ test/layers-spec.js
@@ -22,140 +22,62 @@ import test from 'tape-catch';
 /* eslint-disable func-style */
 /* eslint-disable no-console */
 
-import {Mat4} from 'luma.gl';
+import 'luma.gl/headless';
+import {Mat4, Scene, createGLContext} from 'luma.gl';
 
 import {
+  Layer,
+
   // DeckGLOverlay,
   // HexagonLayer,
   ChoroplethLayer,
-  // ScatterplotLayer,
-  // ArcLayer,
+  ScatterplotLayer,
+  ArcLayer,
   GridLayer
 } from '../src';
+
+// Import private method to test that layers can successfully be updated
+import {updateLayers} from '../src/layer-manager';
 
 import CHOROPLETHS from '../example/data/sf.zip.geo.json';
 // const HEXAGONS_FILE = './example/data/hexagons.csv';
 // const POINTS_FILE = './example/data/sf.bike.parking.csv';
 
+const gl = createGLContext();
+
 const FIXTURE = {
-  window: {
-    innerWidth: 800,
-    innerHeight: 640
+
+  layerState: {
+    oldLayers: [],
+    gl,
+    scene: new Scene(gl)
   },
-  mapViewState: {
+
+  mapSize: {
+    width: 800,
+    height: 640
+  },
+
+  mapState: {
     latitude: 37.751537058389985,
     longitude: -122.42694203247012,
-    zoom: 11.5,
-    projectionMatrix: new Mat4()
+    zoom: 11.5
   },
-  choropleths: CHOROPLETHS,
-  hexagons: null,
-  points: [[100, 100]],
-  arcs: null,
-  arcs2: null,
-  arcStrokeWidth: 1
+
+  choropleths: [], // CHOROPLETHS,
+  hexagons: [],
+  points: [{position: {x: 100, y: 100}, color:[255, 0, 0]}],
+  arcs: [{position: {x0: 0, y0: 0, x1: 1, y1: 3}, color:[255, 0, 0]}]
+
 };
 
-// function reducer(state = INITIAL_STATE, action) {
-//   switch (action.type) {
-//   case 'UPDATE_MAP':
-//     return {...state, mapViewState: action.mapViewState};
-//   case 'LOAD_CHOROPLETHS':
-//     return {...state, choropleths: action.choropleths};
-//   case 'LOAD_HEXAGONS':
-//     const {hexagons} = action;
-//     const hexData = processHexagons(hexagons);
-//     return {...state, hexagons, hexData};
-//   case 'LOAD_POINTS': {
-//     const points = action.points.map(point => {
-//       const coordString = point.COORDINATES;
-//       const p0 = coordString.indexOf('(') + 1;
-//       const p1 = coordString.indexOf(')');
-//       const coords = coordString.slice(p0, p1).split(',');
-//       return {
-//         position: {
-//           x: Number(coords[1]),
-//           y: Number(coords[0]),
-//           z: 0
-//         },
-//         color: [88, 9, 124]
-//       };
-//     });
-
-//     const arcs = pointsToArcs(points);
-//     const arcs1 = arcs.slice(0, arcs.length / 2);
-//     const arcs2 = arcs.slice(arcs.length / 2);
-//     return {...state, points, arcs: arcs1, arcs2};
-//   }
-
-//   default:
-//     return state;
-//   }
-// }
-
-// ---- Helpers ---- //
-// function processHexagons(hexagons) {
-//   const values = hexagons.map(hexagon => Number(hexagon.value));
-//   const maxValue = Math.max(...values);
-
-//   const data = hexagons.map(hexagon => ({
-//     centroid: {
-//       x: Number(hexagon['centroid.x']),
-//       y: Number(hexagon['centroid.y'])
-//     },
-//     vertices: [
-//       [Number(hexagon['v0.x']), Number(hexagon['v0.y'])],
-//       [Number(hexagon['v1.x']), Number(hexagon['v1.y'])],
-//       [Number(hexagon['v2.x']), Number(hexagon['v2.y'])],
-//       [Number(hexagon['v3.x']), Number(hexagon['v3.y'])],
-//       [Number(hexagon['v4.x']), Number(hexagon['v4.y'])],
-//       [Number(hexagon['v5.x']), Number(hexagon['v5.y'])]
-//     ],
-//     color: [
-//       Number(hexagon.value) / maxValue * 255,
-//       Number(hexagon.value) / maxValue * 128,
-//       Number(hexagon.value) / maxValue * 64
-//     ],
-//     elevation: Number(hexagon.value) / maxValue * 100
-
-//   }));
-//   return data;
-// }
-
-// function pointsToArcs(points) {
-//   return points.map((point, i) => {
-//     if (i === points.length - 1) {
-//       return {
-//         position: {x0: 0, y0: 0, x1: 0, y1: 0},
-//         color: [35, 81, 128]
-//       };
-//     }
-
-//     const source = point;
-//     const target = points[i + 1];
-
-//     return {
-//       position: {
-//         x0: source.position.x, y0: source.position.y,
-//         x1: target.position.x, y1: target.position.y
-//       },
-//       colors: {
-//         c0: [255, 0, 0], c1: [0, 0, 255]
-//       }
-//     };
-//   });
-// }
-
 test('GridLayer#constructor', t => {
-  const {window, mapViewState, points} = FIXTURE;
+  const {mapSize, mapState, points} = FIXTURE;
 
   const layer = new GridLayer({
     id: 'gridLayer',
-    width: window.innerWidth,
-    height: window.innerHeight,
-    latitude: mapViewState.latitude,
-    longitude: mapViewState.longitude,
-    zoom: mapViewState.zoom,
+    ...mapSize,
+    ...mapState,
     isPickable: false,
     opacity: 0.06,
     data: points
@@ -166,15 +88,12 @@ test('GridLayer#constructor', t => {
 });
 
 test('ChoroplethLayer#constructor', t => {
-  const {window, mapViewState, choropleths} = FIXTURE;
+  const {mapSize, mapState, choropleths} = FIXTURE;
   const layer = new ChoroplethLayer({
     id: 'choroplethLayer',
-    width: window.innerWidth,
-    height: window.innerHeight,
-    latitude: mapViewState.latitude,
-    longitude: mapViewState.longitude,
-    zoom: mapViewState.zoom,
-    data: choropleths,
+    ...mapSize,
+    ...mapState,
+    data: [],
     opacity: 0.8,
     isPickable: false,
     drawContour: true
@@ -184,79 +103,76 @@ test('ChoroplethLayer#constructor', t => {
   t.end();
 });
 
-  // _renderHexagonLayer() {
-  //   const {mapViewState, hexData} = this.props;
+test('ScatterplotLayer#constructor', t => {
+  const {mapSize, mapState, points} = FIXTURE;
 
-  //   return new HexagonLayer({
-  //     id: 'hexagonLayer',
-  //     width: window.innerWidth,
-  //     height: window.innerHeight,
-  //     latitude: mapViewState.latitude,
-  //     longitude: mapViewState.longitude,
-  //     zoom: mapViewState.zoom,
-  //     data: hexData,
-  //     opacity: 0.5,
-  //     elevation: 10,
-  //     isPickable: true,
-  //     onHover: this._handleHexagonHovered,
-  //     onClick: this._handleHexagonClicked
-  //   });
-  // }
+  const layer = new ScatterplotLayer({
+    id: 'scatterplotLayer',
+    ...mapSize,
+    ...mapState,
+    data: points,
+    isPickable: true
+  });
+  t.ok(layer instanceof ScatterplotLayer, 'ScatterplotLayer created');
 
-  // _renderHexagonSelectionLayer() {
-  //   const {mapViewState} = this.props;
-  //   const {selectedHexagons} = this.state;
+  const emptyLayer = new ScatterplotLayer({
+    id: 'scatterplotLayer',
+    ...mapSize,
+    ...mapState,
+    data: [],
+    isPickable: true
+  });
+  t.ok(emptyLayer instanceof ScatterplotLayer, 'Empty ScatterplotLayer created');
 
-  //   return new HexagonLayer({
-  //     id: 'hexagonSelectionLayer',
-  //     width: window.innerWidth,
-  //     height: window.innerHeight,
-  //     latitude: mapViewState.latitude,
-  //     longitude: mapViewState.longitude,
-  //     zoom: mapViewState.zoom,
-  //     data: selectedHexagons,
-  //     opacity: 0.1,
-  //     elevation: 10,
-  //     isPickable: true,
-  //     onHover: this._handleHexagonHovered,
-  //     onClick: this._handleHexagonClicked
-  //   });
-  // }
+  const {layerState} = FIXTURE;
+  t.doesNotThrow(
+    () => updateLayers({...layerState, newLayers: [layer, emptyLayer]}),
+    undefined,
+    'ScatterplotLayer update does not throw');
 
-  // _renderScatterplotLayer() {
-  //   const {mapViewState, points} = this.props;
+  t.end();
+});
 
-  //   return new ScatterplotLayer({
-  //     id: 'scatterplotLayer',
-  //     width: window.innerWidth,
-  //     height: window.innerHeight,
-  //     latitude: mapViewState.latitude,
-  //     longitude: mapViewState.longitude,
-  //     zoom: mapViewState.zoom,
-  //     data: points,
-  //     isPickable: true,
-  //     onHover: this._handleScatterplotHovered,
-  //     onClick: this._handleScatterplotClicked
-  //   });
-  // }
+test('ArcLayer#constructor', t => {
+  const {mapSize, mapState, arcs} = FIXTURE;
 
-  // _renderArcLayer() {
-  //   const {mapViewState, arcs} = this.props;
+  const layer = new ArcLayer({
+    id: 'arcLayer',
+    ...mapSize,
+    ...mapState,
+    data: arcs,
+    isPickable: true
+  });
+  t.ok(layer instanceof ArcLayer, 'ArcLayer created');
 
-  //   return new ArcLayer({
-  //     id: 'arcLayer',
-  //     width: window.innerWidth,
-  //     height: window.innerHeight,
-  //     latitude: mapViewState.latitude,
-  //     longitude: mapViewState.longitude,
-  //     zoom: mapViewState.zoom,
-  //     data: arcs,
-  //     strokeWidth: this.state.arcStrokeWidth || 1,
-  //     color0: [0, 0, 255],
-  //     color1: [0, 0, 255],
-  //     isPickable: true,
-  //     onHover: this._handleArcHovered,
-  //     onClick: this._handleArcClicked
-  //   });
-  // }
+  const emptyLayer = new ArcLayer({
+    id: 'arcLayer',
+    ...mapSize,
+    ...mapState,
+    data: [],
+    isPickable: true
+  });
+  t.ok(emptyLayer instanceof ArcLayer, 'Empty ArcLayer created');
 
+  t.throws(
+    () => new ArcLayer({
+      id: 'arcLayer',
+      ...mapSize,
+      ...mapState,
+      data: null,
+      isPickable: true
+    }),
+    'Null ArcLayer threw exception'
+  );
+
+  const {layerState} = FIXTURE;
+  t.doesNotThrow(
+    () => updateLayers({
+      ...layerState,
+      newLayers: [layer, emptyLayer]
+    }),
+    undefined,
+    'ArcLayer update does not throw');
+
+  t.end();
+});
