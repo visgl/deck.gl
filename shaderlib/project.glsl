1b405bb89e41c60e89add94a39713b84bb53bcd6
Yang Wang <gnavvy@uber.com>
2016-09-06 19:09:32 -0700

    fix bool uniform that webgl in certain environment handles it differently

--- shaderlib/project.glsl
+++ shaderlib/project.glsl
@@ -2,7 +2,7 @@ const float TILE_SIZE = 512.0;
 const float PI = 3.1415926536;
 const float WORLD_SCALE = TILE_SIZE / (PI * 2.0);
 
-uniform bool disableMercatorProjector;
+uniform float disableMercatorProjector;
 uniform float mercatorScale;
 
 // non-linear projection: lnglats => unit tile [0-1, 0-1]
@@ -14,7 +14,7 @@ vec2 mercatorProject(vec2 lnglat) {
 }
 
 vec2 project(vec2 position) {
-  if (disableMercatorProjector) {
+  if (disableMercatorProjector == 1.0) {
     return (position + vec2(TILE_SIZE / 2.0)) * mercatorScale;
   } else {
     return mercatorProject(position) * WORLD_SCALE * mercatorScale;
