1908875cf6c8a4f12adecfc50983634357f2c487
Ib Green <ib@uber.com>
2016-10-31 13:33:26 -0700

    Address review feedback

--- src/viewport/index.js
+++ src/viewport/index.js
@@ -1,6 +1,10 @@
+// Note: The base viewport class will be moved to a separate module
+// viewport-mercator-project
 export * from './viewport';
 export {default as BaseViewport} from './viewport';
+
+// The WebGL Viewport is a subclass of the BaseViewport that generates
+// WebGL Matrices and Uniforms for deck.gl
 export * from './webgl-viewport';
-export {default as WebGLViewport} from './webgl-viewport';
 export {default as Viewport} from './webgl-viewport';
 export {default as default} from './webgl-viewport';
