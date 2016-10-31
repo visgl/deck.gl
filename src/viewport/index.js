ee350177ad909c6cff87cdd5d02df013627b05ae
Ib Green <ib@uber.com>
2016-10-31 11:09:34 -0700

    Split the viewport into GL and non-GL related classes. Clean up interface.

--- src/viewport/index.js
+++ src/viewport/index.js
@@ -1,2 +1,6 @@
-export {default as default} from './viewport';
-export {default as Viewport} from './viewport';
+export * from './viewport';
+export {default as BaseViewport} from './viewport';
+export * from './webgl-viewport';
+export {default as WebGLViewport} from './webgl-viewport';
+export {default as Viewport} from './webgl-viewport';
+export {default as default} from './webgl-viewport';
