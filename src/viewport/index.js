faf27f0da0becfca6567511b68235dcf6cdd1e89
Ib Green <ib@uber.com>
2016-11-03 11:43:31 -0700

    Import viewport from viewport-mercator-project

--- src/viewport/index.js
+++ src/viewport/index.js
@@ -1,10 +1,3 @@
-// Note: The base viewport class will be moved to a separate module
-// viewport-mercator-project
 export * from './viewport';
-export {default as BaseViewport} from './viewport';
-
-// The WebGL Viewport is a subclass of the BaseViewport that generates
-// WebGL Matrices and Uniforms for deck.gl
-export * from './webgl-viewport';
-export {default as Viewport} from './webgl-viewport';
-export {default as default} from './webgl-viewport';
+export {default as Viewport} from './viewport';
+export {default as default} from './viewport';
