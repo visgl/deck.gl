d824f534fc743243a3aa173cb7155e3e476c4b68
Ib Green <ibgreen@users.noreply.github.com>
2017-01-05 17:33:33 -0800

    Use buble compiler in example for even shorter package.json (#275)

--- example/webpack2.buble.config.js
+++ example/webpack2.buble.config.js
@@ -8,12 +8,31 @@ module.exports = {
 
   devtool: 'source-maps',
 
+  resolve: {
+    alias: {
+      // mapbox-gl config
+      webworkify: 'webworkify-webpack-dropin',
+      'gl-matrix': resolve('./node_modules/gl-matrix/dist/gl-matrix.js'),
+      // Work against base library
+      'deck.gl': resolve('../dist'),
+      // Using our dependencies
+      'luma.gl': resolve('./node_modules/luma.gl'),
+      'viewport-mercator-project': resolve('./node_modules/viewport-mercator-project'),
+      react: resolve('./node_modules/react'),
+      'autobind-decorator': resolve('./node_modules/autobind-decorator'),
+      brfs: resolve('./node_modules/brfs'),
+      earcut: resolve('./node_modules/earcut'),
+      'geojson-normalize': resolve('./node_modules/geojson-normalize'),
+      'lodash.flattendeep': resolve('./node_modules/lodash.flattendeep')
+    }
+  },
+
   module: {
     rules: [{
       // Compile ES2015 using buble
       test: /\.js$/,
       loader: 'buble-loader',
-      include: resolve('./app.js'),
+      exclude: [/node_modules/, /dist/],
       options: {
         objectAssign: 'Object.assign',
         transforms: {
@@ -26,18 +45,10 @@ module.exports = {
       loader: 'json-loader'
     }, {
       // Mapbox has some unresolved fs calls
-      include: /node_modules\/mapbox-gl/,
+      include: [resolve('../dist'), /node_modules\/mapbox-gl/],
       loader: 'transform-loader',
       options: 'brfs'
     }]
-  },
-
-  resolve: {
-    alias: {
-      webworkify: 'webworkify-webpack-dropin',
-      // Uncommenting this, the build still works but is 400K bigger...
-      'gl-matrix': resolve('./node_modules/gl-matrix/dist/gl-matrix.js')
-    }
   },
 
   node: {
@@ -47,7 +58,6 @@ module.exports = {
   // Allow setting mapbox token using environment variables
   plugins: [
     new webpack.EnvironmentPlugin(['MAPBOX_ACCESS_TOKEN', 'MapboxAccessToken']),
-    // TODO - doesn't eseem to have any effect
-    new webpack.LoaderOptionsPlugin({minimize: true, debug: false})
+    // new webpack.LoaderOptionsPlugin({minimize: true, debug: false})
   ]
 };
