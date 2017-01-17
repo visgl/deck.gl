939ddeb27d3be21973dcb2bf52257be9a2d29a75
Shaojing Li <shaojing@uber.com>
2017-01-17 14:28:24 -0800

    Make 64-bit ChoroplethLayer subclass of 32-bit ChoroplethLayer

--- src/layers/fp64/choropleth-layer/choropleth-layer-vertex.glsl
+++ src/layers/fp64/choropleth-layer/choropleth-layer-vertex.glsl
@@ -20,10 +20,8 @@
 
 #define SHADER_NAME choropleth-layer-64-vertex-shader
 
-attribute vec4 positionsFP64;
-attribute vec2 heightsFP64;
-
-attribute vec3 positions;
+attribute vec4 positions64;
+attribute vec2 heights64;
 attribute vec4 colors;
 attribute vec3 pickingColors;
 
@@ -31,34 +29,35 @@ uniform float opacity;
 uniform float renderPickingBuffer;
 uniform vec3 selectedPickingColor;
 
-varying vec4 vColor;
-
-vec4 getColor(vec4 color, float opacity, vec3 pickingColor, float renderPickingBuffer) {
-  vec4 color4 = vec4(color.xyz / 255., color.w / 255. * opacity);
-  vec4 pickingColor4 = vec4(pickingColor / 255., 1.);
-  return mix(color4, pickingColor4, renderPickingBuffer);
+uniform float pickingEnabled;
+varying vec4 vPickingColor;
+void picking_setPickColor(vec3 pickingColor) {
+  vPickingColor = vec4(pickingColor,  1.);
+}
+vec4 picking_setNormalAndPickColors(vec4 color, vec3 pickingColor) {
+  vec4 pickingColor4 = vec4(pickingColor.rgb, 1.);
+  vPickingColor = mix(color, pickingColor4, pickingEnabled);
+  return vPickingColor;
 }
 
 void main(void) {
   // For some reason, need to add one to elevation to show up in untilted mode
   vec2 projectedCoord[2];
-  project_position_fp64(positionsFP64, projectedCoord);
+  project_position_fp64(positions64, projectedCoord);
 
   vec2 vertex_pos_modelspace[4];
 
   vertex_pos_modelspace[0] = projectedCoord[0];
   vertex_pos_modelspace[1] = projectedCoord[1];
-  vertex_pos_modelspace[2] = heightsFP64;
+  vertex_pos_modelspace[2] = heights64;
   vertex_pos_modelspace[3] = vec2(1.0, 0.0);
 
   gl_Position = project_to_clipspace_fp64(vertex_pos_modelspace);
 
   vec4 color = vec4(colors.rgb, colors.a * opacity) / 255.;
-  vec4 pickingColor = vec4(pickingColors / 255., 1.);
 
-  vColor = mix(
+  picking_setNormalAndPickColors(
     color,
-    pickingColor,
-    renderPickingBuffer
+    pickingColors / 255.
   );
 }
