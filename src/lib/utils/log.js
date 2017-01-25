fcecab64f008bdbd26bc8cd9bc13b8c87bd77648
Ib Green <ibgreen@users.noreply.github.com>
2017-01-25 14:17:18 -0800

    Fix to defaultProps handling (#316)

--- src/lib/utils/log.js
+++ src/lib/utils/log.js
@@ -5,17 +5,22 @@ import assert from 'assert';
 export default function log(priority, ...args) {
   assert(Number.isFinite(priority), 'log priority must be a number');
   if (priority <= log.priority) {
-    console.debug(...args);
+    // Node doesn't have console.debug, but looks better in browser consoles
+    if (console.debug) {
+      console.debug(...args);
+    } else {
+      console.info(...args);
+    }
   }
 }
 
 const cache = {};
 
 function once(priority, arg, ...args) {
-  if (!cache[arg]) {
-    log(priority, arg, ...args);
+  if (!cache[arg] && priority <= log.priority) {
+    console.warn(...[arg, ...args]);
+    cache[arg] = true;
   }
-  cache[arg] = true;
 }
 
 log.priority = 0;
