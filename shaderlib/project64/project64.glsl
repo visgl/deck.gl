d89ab03a1e2a54f5e75a802df1e323dc01e7aa09
Shaojing Li <shaojing@uber.com>
2017-01-25 13:57:53 -0800

    Fix a bug in the project_fp64 that only shows up in linux + Nvidia; Added unit test for radians_fp64

--- shaderlib/project64/project64.glsl
+++ shaderlib/project64/project64.glsl
@@ -5,7 +5,11 @@ uniform vec2 projectionFP64[16];
 
 void mercatorProject_fp64(vec4 lnglat_fp64, out vec2 out_val[2]) { //longitude: lnglat_fp64.xy; latitude: lnglat_fp64.zw
 
+#if defined(NVIDIA_FP64_WORKAROUND)
+  out_val[0] = sum_fp64(radians_fp64(lnglat_fp64.xy), PI_FP64 * ONE);
+#else
   out_val[0] = sum_fp64(radians_fp64(lnglat_fp64.xy), PI_FP64);
+#endif
   out_val[1] = sub_fp64(PI_FP64, log_fp64(tan_fp64(sum_fp64(PI_4_FP64, radians_fp64(lnglat_fp64.zw) / 2.0))));
   return;
 }
