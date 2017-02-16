cdd0d5399a5fa232f94f05463f5875ce083f0e58
Shaojing Li <shaojing@uber.com>
2017-02-16 12:59:35 -0800

    Blinn-Phong lighting model

--- src/layers/core/hexagon-layer/hexagon-layer-vertex.glsl
+++ src/layers/core/hexagon-layer/hexagon-layer-vertex.glsl
@@ -12,11 +12,6 @@ attribute vec3 instancePickingColors;
 uniform float renderPickingBuffer;
 uniform vec3 selectedPickingColor;
 
-// Projection uniforms
-uniform mat4 worldMatrix;
-uniform mat4 viewMatrix;
-uniform mat4 worldInverseTransposeMatrix;
-
 // Custom uniforms
 uniform float opacity;
 uniform float radius;
@@ -24,19 +19,51 @@ uniform float angle;
 uniform float extruded;
 uniform float radiusScale;
 
-// Lighting constants
-const vec3 ambientColor = vec3(1., 1., 1.);
-const vec3 pointLocation = vec3(-1., 3., -1.);
-const vec3 pointColor = vec3(1., 1., 1.);
-const vec3 pointSpecularColor = vec3(0.6, 0.6, 0.6);
-const float shininess = 3.;
-const float pointLightAmbientCoefficient = 0.8;
+// light uniforms
+#define NUM_OF_LIGHTS 2
+uniform mat4 modelViewMatrix;
+uniform mat4 normalMatrix; // inverse transpose of modelViewMatrix
+uniform vec3 cameraPos;
+uniform vec3 lightsPosition[16];
+uniform vec2 lightsStrength[16];
+uniform float ambientRatio;
+uniform float diffuseRatio;
+uniform float specularRatio;
 
 // Result
 varying vec4 vColor;
 
 // A magic number to scale elevation so that 1 unit approximate to 100 meter
 #define ELEVATION_SCALE 80.
+
+// Without specular lighting
+float getLightWeight(vec4 position_worldspace, vec3 normals_worldspace) {
+  float lightWeight = 0.0;
+
+  vec3 position_worldspace_vec3 = position_worldspace.xyz / position_worldspace.w;
+  vec3 normals_worldspace_vec3 = normals_worldspace.xzy;
+
+  vec3 camera_pos_worldspace = cameraPos;
+  vec3 view_direction = normalize(camera_pos_worldspace - position_worldspace_vec3);
+
+  for (int i = 0; i < NUM_OF_LIGHTS; i++) {
+    vec3 light_position_worldspace = project_position(lightsPosition[i]);
+    vec3 light_direction = normalize(light_position_worldspace - position_worldspace_vec3);
+
+    vec3 halfway_direction = normalize(light_direction + view_direction);
+    float lambertian = dot(light_direction, normals_worldspace_vec3);
+    float specular = 0.0;
+    if (lambertian > 0.0) {
+      float specular_angle = max(dot(normals_worldspace_vec3, halfway_direction), 0.0);
+      specular = pow(specular_angle, 32.0);
+    }
+    lambertian = max(lambertian, 0.0);
+    lightWeight += (ambientRatio + lambertian * diffuseRatio + specular * specularRatio) * lightsStrength[i].x;
+
+  }
+
+  return lightWeight;
+}
 
 void main(void) {
 
@@ -57,33 +84,35 @@ void main(void) {
   // project center of hexagon
   vec4 centroidPosition = vec4(project_position(instancePositions.xy), elevation, 0.0);
 
-  gl_Position = project_to_clipspace(centroidPosition + vec4(vec2(rotatedPositions.xz * dotRadius), 0., 1.));
+  vec4 position_worldspace = centroidPosition + vec4(vec2(rotatedPositions.xz * dotRadius), 0., 1.);
 
-  // check whether hexagon is currently picked
-  float selected = isPicked(instancePickingColors, selectedPickingColor);
+  gl_Position = project_to_clipspace(position_worldspace);
 
-  // calculate lighting
-  vec3 lightWeighting = getLightWeight(
-    viewMatrix,
-    worldMatrix,
-    worldInverseTransposeMatrix,
-    rotatedPositions,
-    rotatedNormals,
-    selected,
-    ambientColor,
-    pointLocation,
-    pointColor,
-    pointSpecularColor,
-    pointLightAmbientCoefficient,
-    shininess
-  );
+  // render display
+  if (renderPickingBuffer < 0.5) {
+    // check whether hexagon is currently picked
+    float selected = isPicked(instancePickingColors, selectedPickingColor);
 
-  vec3 lightWeightedColor = mix(vec3(1), lightWeighting, extruded) * (instanceColors.rgb / 255.0);
+    // Light calculations
+    // Worldspace is the linear space after Mercator projection
 
-  // Color: Either opacity-multiplied instance color, or picking color
-  vec4 color = vec4(lightWeightedColor, opacity * instanceColors.a / 255.);
+    vec3 normals_worldspace = rotatedNormals;
+    float lightWeighting = getLightWeight(
+      position_worldspace,
+      normals_worldspace
+    );
 
-  vec4 pickingColor = vec4(instancePickingColors / 255., 1.);
+    vec3 lightWeightedColor = mix(1.0, lightWeighting, extruded) * (instanceColors.rgb / 255.0);
 
-  vColor = mix(color, pickingColor, renderPickingBuffer);
+    // Color: Either opacity-multiplied instance color, or picking color
+    vec4 color = vec4(lightWeightedColor, opacity * instanceColors.a / 255.);
+
+    vColor = color;
+
+  } else {
+
+    vec4 pickingColor = vec4(instancePickingColors / 255., 1.);
+    vColor = pickingColor;
+
+  }
 }
