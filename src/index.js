19886d9caa8c9e094dc8ef27d0f5a39d4b9b5c8d
Shaojing Li <shaojing@uber.com>
2017-02-23 10:47:39 -0800

    cleanup the folder structure for all layers and import syntax

--- src/index.js
+++ src/index.js
@@ -41,15 +41,15 @@ export {default as PolygonLayer} from './layers/core/polygon-layer/polygon-layer
 export {default as GeoJsonLayer} from './layers/core/geojson-layer/geojson-layer';
 
 // 64-bit Layers
-export {default as ScatterplotLayer64} from './layers/fp64/scatterplot-layer';
-export {default as ArcLayer64} from './layers/fp64/arc-layer';
-export {default as LineLayer64} from './layers/fp64/line-layer';
+export {default as ScatterplotLayer64} from './layers/fp64/scatterplot-layer/scatterplot-layer-64';
+export {default as ArcLayer64} from './layers/fp64/arc-layer/arc-layer-64';
+export {default as LineLayer64} from './layers/fp64/line-layer/line-layer-64';
 export {default as GeoJsonLayer64} from './layers/fp64/geojson-layer/geojson-layer-64';
 
 // Deprecated Layers
-export {default as ChoroplethLayer} from './layers/deprecated/choropleth-layer';
-export {default as ChoroplethLayer64} from './layers/deprecated/choropleth-layer-64';
-export {default as ExtrudedChoroplethLayer64} from './layers/deprecated/extruded-choropleth-layer-64';
+export {default as ChoroplethLayer} from './layers/deprecated/choropleth-layer/choropleth-layer';
+export {default as ChoroplethLayer64} from './layers/deprecated/choropleth-layer-64/choropleth-layer-64';
+export {default as ExtrudedChoroplethLayer64} from './layers/deprecated/extruded-choropleth-layer-64/extruded-choropleth-layer-64';
 
 // React exports
 export {default as DeckGL} from './react/deckgl';
