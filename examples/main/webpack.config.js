971485b927a0d2f81c51d27c92ce9354d2ea6168
Xiaoji Chen <xiaoji@uber.com>
2017-02-28 11:00:16 -0800

    add sample layers to main example

--- examples/main/webpack.config.js
+++ examples/main/webpack.config.js
@@ -40,6 +40,12 @@ module.exports = {
         test: /\.json$/,
         loader: 'json-loader',
         exclude: [/node_modules/]
+      },
+      {
+        // Needed to inline deck.gl GLSL shaders
+        include: [resolve(__dirname, '../sample-layers')],
+        loader: 'transform-loader',
+        options: 'brfs-babel'
       }
     ]
   },
