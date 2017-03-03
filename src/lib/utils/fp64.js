9d593895fda691c45f475759cb55835a70220317
Shaojing Li <shaojing@uber.com>
2017-03-03 10:44:40 -0800

    correct the linter error

--- src/lib/utils/fp64.js
+++ src/lib/utils/fp64.js
@@ -12,12 +12,10 @@ export function enable64bitSupport(props) {
   if (props.fp64) {
     if (props.projectionMode === COORDINATE_SYSTEM.LNG_LAT) {
       return true;
-    } else {
-      log.once(0, `64-bit mode only works with projectionMode set to
-       COORDINATE_SYSTEM.LNG_LAT. Rendering in 32-bit mode instead`);
-      return false;
     }
-  } else {
-    return false;
+    log.once(0, `64-bit mode only works with projectionMode set to
+      COORDINATE_SYSTEM.LNG_LAT. Rendering in 32-bit mode instead`);
   }
+
+  return false;
 }
