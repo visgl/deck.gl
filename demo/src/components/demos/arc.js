4978147e52f5388886337af2c241720de74c987e
Xiaoji Chen <xiaoji@uber.com>
2017-03-27 17:51:02 -0700

    move arc example to standalone app

--- demo/src/components/demos/arc.js
+++ demo/src/components/demos/arc.js
@@ -1,30 +1,8 @@
 import React, {Component} from 'react';
-import {scaleQuantile} from 'd3-scale';
 
-import DeckGL, {GeoJsonLayer, ArcLayer} from 'deck.gl';
-
-import {readableInteger} from '../../utils/format-utils';
 import {MAPBOX_STYLES} from '../../constants/defaults';
-
-const inFlowColors = [
-  [255, 255, 204],
-  [199, 233, 180],
-  [127, 205, 187],
-  [65, 182, 196],
-  [29, 145, 192],
-  [34, 94, 168],
-  [12, 44, 132]
-];
-
-const outFlowColors = [
-  [255, 255, 178],
-  [254, 217, 118],
-  [254, 178, 76],
-  [253, 141, 60],
-  [252, 78, 42],
-  [227, 26, 28],
-  [177, 0, 38]
-];
+import {readableInteger} from '../../utils/format-utils';
+import ArcOverlay, {inFlowColors, outFlowColors} from '../../../../examples/arc/deckgl-overlay';
 
 const colorRamp = inFlowColors.slice().reverse().concat(outFlowColors)
   .map(color => `rgb(${color.join(',')})`);
@@ -46,13 +24,8 @@ export default class ArcDemo extends Component {
 
   static get viewport() {
     return {
-      mapStyle: MAPBOX_STYLES.LIGHT,
-      longitude: -100,
-      latitude: 40.7,
-      zoom: 3,
-      maxZoom: 15,
-      pitch: 30,
-      bearing: 30
+      ...ArcOverlay.defaultViewport,
+      mapStyle: MAPBOX_STYLES.LIGHT
     };
   }
 
@@ -90,92 +63,38 @@ export default class ArcDemo extends Component {
 
   constructor(props) {
     super(props);
-    this.state = this._updateFlows(props);
+    this.state = {
+      selectedCounty: props.data ? props.data[362] : null
+    };
   }
 
   componentWillReceiveProps(nextProps) {
-    const {data} = nextProps;
-    if (data && data !== this.props.data) {
-      this.setState(this._updateFlows(nextProps));
+    if (nextProps.data !== this.props.data) {
+      this.setState({
+        selectedCounty: nextProps.data[362]
+      });
     }
   }
 
-  _updateFlows(props, selectedFeature) {
-    const {data} = props;
-
-    if (!data || !data.length) {
-      return {};
-    }
-    const {features} = data[0];
-    selectedFeature = selectedFeature || features[362];
-
-    const {flows, centroid, name} = selectedFeature.properties;
-
-    const arcs = Object.keys(flows).map(toId => {
-      const f = features[toId];
-      return {
-        source: centroid,
-        target: f.properties.centroid,
-        value: flows[toId]
-      };
-    });
-
-    const scale = scaleQuantile()
-      .domain(arcs.map(a => Math.abs(a.value)))
-      .range(inFlowColors.map((c, i) => i));
-
-    arcs.forEach(a => {
-      a.gain = Math.sign(a.value);
-      a.quantile = scale(Math.abs(a.value));
-    });
-
-    this.props.onStateChange({sourceName: name});
-
-    return {arcs, scale, selectedFeature};
-  }
-
-  _initialize(gl) {
-    gl.enable(gl.DEPTH_TEST);
-    gl.depthFunc(gl.LEQUAL);
-  }
-
-  _onClickFeature({object}) {
-    if (this.state.selectedFeature !== object) {
-      this.setState(this._updateFlows(this.props, object));
-    }
+  _onSelectCounty(feature) {
+    this.setState({selectedCounty: feature});
+    this.props.onStateChange({sourceName: feature.properties.name});
   }
 
   render() {
     const {viewport, params, data} = this.props;
-    const {arcs} = this.state;
+    const {selectedCounty} = this.state;
 
     if (!data) {
       return null;
     }
 
-    const layers = [
-      new GeoJsonLayer({
-        id: 'choropleth',
-        data: data[0],
-        stroked: false,
-        filled: true,
-        getFillColor: () => [0, 0, 0, 0],
-        onClick: this._onClickFeature.bind(this),
-        pickable: true
-      }),
-      new ArcLayer({
-        id: 'arc',
-        data: arcs,
-        getSourcePosition: d => d.source,
-        getTargetPosition: d => d.target,
-        getSourceColor: d => (d.gain > 0 ? inFlowColors : outFlowColors)[d.quantile],
-        getTargetColor: d => (d.gain > 0 ? outFlowColors : inFlowColors)[d.quantile],
-        strokeWidth: params.lineWidth.value
-      })
-    ];
-
     return (
-      <DeckGL {...viewport} layers={ layers } onWebGLInitialized={this._initialize} />
+      <ArcOverlay viewport={viewport}
+        data={data}
+        selectedFeature={selectedCounty}
+        strokeWidth={params.lineWidth.value}
+        onClickFeature={this._onSelectCounty.bind(this)} />
     );
   }
 }
