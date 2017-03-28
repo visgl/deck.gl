a91f9b97a6e44fa9c6ce4e3f5790c2ecc76418e1
Xiaoji Chen <xiaoji@uber.com>
2017-03-27 20:00:02 -0700

    add icon example to standalone apps

--- demo/src/components/demos/icon.js
+++ demo/src/components/demos/icon.js
@@ -1,26 +1,8 @@
 /* global window */
 import React, {Component} from 'react';
-import DeckGL, {IconLayer, WebMercatorViewport} from 'deck.gl';
-import autobind from 'autobind-decorator';
-import rbush from 'rbush';
-
 import {MAPBOX_STYLES} from '../../constants/defaults';
 import {readableInteger} from '../../utils/format-utils';
-
-const ICON_SIZE = 80;
-
-function getIconName(size) {
-  if (size === 0) {
-    return '';
-  }
-  if (size < 10) {
-    return `marker-${size}`;
-  }
-  if (size < 100) {
-    return `marker-${Math.floor(size / 10)}0`;
-  }
-  return 'marker-100';
-}
+import IconOverlay from '../../../../examples/icon/deckgl-overlay';
 
 function stopPropagation(evt) {
   evt.stopPropagation();
@@ -48,13 +30,8 @@ export default class IconDemo extends Component {
 
   static get viewport() {
     return {
-      mapStyle: MAPBOX_STYLES.DARK,
-      longitude: -89.4,
-      latitude: 36.7,
-      zoom: 3,
-      maxZoom: 20,
-      pitch: 0,
-      bearing: 0
+      ...IconOverlay.defaultViewport,
+      mapStyle: MAPBOX_STYLES.DARK
     };
   }
 
@@ -79,8 +56,6 @@ export default class IconDemo extends Component {
   constructor(props) {
     super(props);
 
-    // build spatial index
-    this._tree = rbush(9, ['.x', '.y', '.x', '.y']);
     this.state = {
       x: 0,
       y: 0,
@@ -89,76 +64,7 @@ export default class IconDemo extends Component {
     };
   }
 
-  componentWillReceiveProps(nextProps) {
-    const points = nextProps.data && nextProps.data[0];
-    const oldPoints = this.props.data && this.props.data[0];
-    const {viewport} = nextProps;
-    const oldViewport = this.props.viewport;
-
-    if (points && (
-      points !== oldPoints ||
-      viewport.width !== oldViewport.width ||
-      viewport.height !== oldViewport.height)) {
-      this._updateCluster(points, nextProps.viewport);
-    }
-  }
-
-  // Compute icon clusters
-  // We use the projected positions instead of longitude and latitude to build
-  // the spatial index, because this particular dataset is distributed all over
-  // the world, we can't use some fixed deltaLon and deltaLat
-  _updateCluster(points, viewport) {
-    const tree = this._tree;
-
-    const transform = new WebMercatorViewport({
-      ...viewport,
-      zoom: 0
-    });
-
-    points.forEach(p => {
-      const screenCoords = transform.project(p.coordinates);
-      p.x = screenCoords[0];
-      p.y = screenCoords[1];
-      p.zoomLevels = [];
-    });
-
-    tree.clear();
-    tree.load(points);
-
-    for (let z = 0; z <= 20; z++) {
-      const radius = ICON_SIZE / window.devicePixelRatio / 2 / Math.pow(2, z);
-
-      points.forEach(p => {
-        if (p.zoomLevels[z] === undefined) {
-          // this point does not belong to a cluster
-          const {x, y} = p;
-
-          // find all points within radius that do not belong to a cluster
-          const neighbors = tree.search({
-            minX: x - radius,
-            minY: y - radius,
-            maxX: x + radius,
-            maxY: y + radius
-          })
-          .filter(neighbor => neighbor.zoomLevels[z] === undefined);
-
-          // only show the center point at this zoom level
-          neighbors.forEach(neighbor => {
-            if (neighbor === p) {
-              p.zoomLevels[z] = {
-                icon: getIconName(neighbors.length),
-                points: neighbors
-              };
-            } else {
-              neighbor.zoomLevels[z] = null;
-            }
-          });
-        }
-      });
-    }
-  }
-
-  @autobind _onHover({x, y, object}) {
+  _onHover({x, y, object}) {
     const {viewport, params} = this.props;
     const z = Math.floor(viewport.zoom);
     const showCluster = params.cluster.value;
@@ -185,11 +91,11 @@ export default class IconDemo extends Component {
     this.setState({x, y, hoveredItems, expanded: false});
   }
 
-  @autobind _onClick() {
+  _onClick() {
     this.setState({expanded: true});
   }
 
-  _renderhoveredItems(z, showCluster) {
+  _renderhoveredItems() {
     const {x, y, hoveredItems, expanded} = this.state;
 
     if (!hoveredItems) {
@@ -219,39 +125,21 @@ export default class IconDemo extends Component {
     const {viewport, params, data} = this.props;
     const {hoveredItems} = this.state;
 
-    if (!data || !data[0] || !data[1]) {
+    if (!data) {
       return null;
     }
 
-    const z = Math.floor(viewport.zoom);
-    const showCluster = params.cluster.value;
-    const size = showCluster ? 1 : Math.min(Math.pow(1.5, viewport.zoom - 10), 1);
-
-    const layer = new IconLayer({
-      id: 'icon',
-      data: data[0],
-      pickable: true,
-      iconAtlas: 'images/location-icon-atlas.png',
-      iconMapping: data[1],
-      sizeScale: ICON_SIZE * size,
-      getPosition: d => d.coordinates,
-      getIcon: d => showCluster ? (d.zoomLevels[z] && d.zoomLevels[z].icon) : 'marker-1',
-      getColor: d => [255, 180, 0, 160],
-      getSize: d => 1,
-      onHover: this._onHover,
-      onClick: this._onClick,
-      updateTriggers: {
-        getIcon: {
-          z: z * showCluster
-        }
-      }
-    });
-
     return (
       <div className={`icon-demo ${hoveredItems ? 'clickable' : ''}`}>
-        <DeckGL {...viewport} layers={ [layer] } />
+        <IconOverlay viewport={viewport}
+          data={data[0]}
+          iconAtlas="images/location-icon-atlas.png"
+          iconMapping={data[1]}
+          showCluster={params.cluster.value}
+          onHover={this._onHover.bind(this)}
+          onClick={this._onClick.bind(this)} />
 
-        { this._renderhoveredItems(z, showCluster) }
+        { this._renderhoveredItems() }
       </div>
     );
   }
