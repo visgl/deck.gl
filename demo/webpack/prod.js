bf4f2c23ed0592a938ee9b0d5e4cf20c7a09ea47
Xiaoji Chen <xiaoji@uber.com>
2017-03-29 13:07:05 -0700

    rollback to react-map-gl 2 and fixing demo build

--- demo/webpack/prod.js
+++ demo/webpack/prod.js
@@ -1,5 +1,12 @@
 const config = require('./config');
 
+// transpile mapbox-gl utils imported by react-map-gl@2
+// these files are ES6 and break UglifyJS
+config.module.rules.push({
+  test: /node_modules\/mapbox-gl\/js.*\.js$/,
+  loader: 'babel-loader'
+});
+
 module.exports = Object.assign(config, {
 
   output: {
