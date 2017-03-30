f9b9bbe3e0b3d788b3dc1020ccf5b8e2bf32177d
Xiaoji Chen <xiaoji@uber.com>
2017-03-29 17:42:24 -0700

    clean up

--- demo/src/static/workers/heatmap-data-decoder.js
+++ demo/src/static/workers/heatmap-data-decoder.js
@@ -4,15 +4,14 @@ let total = 0;
 onmessage = function(e) {
 
   const lines = e.data.text.split('\n');
-  let result = [];
-
-  lines.forEach(function(line) {
-    if (!line) {
-      return;
+  
+  const result = lines.reduce(function(acc, line) {
+    if (line) {
+      const pts = decodePolyline(line);
+      return acc.concat(pts);
     }
-    const pts = decodePolyline(line);
-    result = result.concat(pts);
-  });
+    return acc;
+  }, []);
 
   total += result.length;
 
