visgl/deck.gl

commit 08b8d0560a67baa711f841c7f19650ec624adf8e

Polygon layer fix (#424)
1) Update the data file and default values for the GeoJson layer sample
2) Update the accessor names of GeoJson layer and Polygon layer
3) Change the behavior of "extruded", "wireframe" and "stroked" props. Now "wireframe" only affects the extruded polygon and "stroked" only affects the non-extruded polygon
5) Add calls to super.updateState() to all layers so that data change leads to attribute update
6) Added a polygonWireframeLayer to GeoJSON layer and Polygon layer
7) Fixed other bugs mentioned by the review
8) Move uniform settings to the draw function from the updateState function in SolidPolygon layer

src/layers/core/solid-polygon-layer/solid-polygon-layer.js
@@ -83,7 +83,7 @@ export default class PolygonLayer extends Layer {
     /* eslint-disable max-len */
     attributeManager.add({
       indices: {size: 1, isIndexed: true, update: this.calculateIndices, noAlloc},
-      positions: {size: 3, accessor: 'getHeight', update: this.calculatePositions, noAlloc},
+      positions: {size: 3, accessor: 'getElevation', update: this.calculatePositions, noAlloc},
       normals: {size: 3, update: this.calculateNormals, noAlloc},
       colors: {size: 4, type: GL.UNSIGNED_BYTE, accessor: 'getColor', update: this.calculateColors, noAlloc},
       pickingColors: {size: 3, type: GL.UNSIGNED_BYTE, update: this.calculatePickingColors, noAlloc}
@@ -108,27 +108,35 @@ export default class PolygonLayer extends Layer {
     }
   }
 
-  updateState({props, oldProps, changeFlags}) {
-    this.updateGeometry({props, oldProps, changeFlags});
-    this.updateModel({props, oldProps, changeFlags});
-    this.updateAttribute({props, oldProps, changeFlags});
+  draw({uniforms}) {
+    const {extruded, lightSettings} = this.props;
 
-    const {opacity, extruded, lightSettings} = props;
-
-    this.setUniforms(Object.assign({}, {
-      extruded: extruded ? 1.0 : 0.0,
-      opacity
+    this.state.model.render(Object.assign({}, uniforms, {
+      extruded: extruded ? 1.0 : 0.0
     },
     lightSettings));
   }
 
+  updateState({props, oldProps, changeFlags}) {
+    super.updateState({props, oldProps, changeFlags});
+
+    const geometryChanged = this.updateGeometry({props, oldProps, changeFlags});
+
+    // Re-generate model if geometry changed
+    if (geometryChanged) {
+      const {gl} = this.context;
+      this.setState({model: this._getModel(gl)});
+    }
+    this.updateAttribute({props, oldProps, changeFlags});
+  }
+
   updateGeometry({props, oldProps, changeFlags}) {
-    const geometryChanged =
+    const regenerateModel = changeFlags.dataChanged ||
       props.extruded !== oldProps.extruded ||
       props.wireframe !== oldProps.wireframe || props.fp64 !== oldProps.fp64;
 
-    if (changeFlags.dataChanged || geometryChanged) {
-      const {getPolygon, extruded, wireframe, getHeight} = props;
+    if (regenerateModel) {
+      const {getPolygon, extruded, wireframe, getElevation} = props;
 
       // TODO - avoid creating a temporary array here: let the tesselator iterate
       const polygons = props.data.map(getPolygon);
@@ -137,13 +145,15 @@ export default class PolygonLayer extends Layer {
         polygonTesselator: !extruded ?
           new PolygonTesselator({polygons, fp64: this.props.fp64}) :
           new PolygonTesselatorExtruded({polygons, wireframe,
-            getHeight: polygonIndex => getHeight(this.props.data[polygonIndex]),
+            getHeight: polygonIndex => getElevation(this.props.data[polygonIndex]),
             fp64: this.props.fp64
           })
       });
 
       this.state.attributeManager.invalidateAll();
     }
+
+    return regenerateModel;
   }
 
   _getModel(gl) {

examples/layer-browser/src/examples/core-layers.js
@@ -24,9 +24,9 @@ import dataSamples from '../immutable-data-samples';
 import {parseColor, setOpacity} from '../utils/color';
 
 const MARKER_SIZE_MAP = {
-  small: 2,
-  medium: 5,
-  large: 10
+  small: 200,
+  medium: 500,
+  large: 1000
 };
 
 const LIGHT_SETTINGS = {
@@ -84,7 +84,7 @@ const GeoJsonLayerExample = {
       return setOpacity(color, opacity);
     },
     getWidth: f => f.properties['stroke-width'],
-    getHeight: f => Math.random() * 1000,
+    getElevation: f => 500,
     widthScale: 10,
     widthMinPixels: 1,
     pickable: true
@@ -96,7 +96,7 @@ const GeoJsonLayerExtrudedExample = {
   getData: () => dataSamples.choropleths,
   props: {
     id: 'geojsonLayer-extruded',
-    getHeight: f => get(f, 'properties.ZIP_CODE') * 10 % 127 * 10,
+    getElevation: f => get(f, 'properties.ZIP_CODE') * 10 % 127 * 10,
     getFillColor: f => [0, 255, get(f, 'properties.ZIP_CODE') * 23 % 100 + 155],
     getColor: f => [200, 0, 80],
     drawPolygons: true,

src/layers/core/arc-layer/arc-layer.js
@@ -89,7 +89,12 @@ export default class ArcLayer extends Layer {
   }
 
   updateState({props, oldProps, changeFlags}) {
-    this.updateModel({props, oldProps, changeFlags});
+    super.updateState({props, oldProps, changeFlags});
+    // Re-generate model if geometry changed
+    if (props.fp64 !== oldProps.fp64) {
+      const {gl} = this.context;
+      this.setState({model: this._getModel(gl)});
+    }
     this.updateAttribute({props, oldProps, changeFlags});
   }
 

src/layers/core/geojson-layer/geojson-layer.js
@@ -86,49 +86,54 @@ export default class GeoJsonLayer extends CompositeLayer {
     let {} = this.props;
     const drawPoints = pointFeatures && pointFeatures.length > 0;
     const drawLines = lineFeatures && lineFeatures.length > 0;
-    const drawPolygons = stroked && polygonOutlineFeatures && polygonOutlineFeatures.length > 0;
-    const fillPolygons = filled && polygonFeatures && polygonFeatures.length > 0;
+    const hasPolygonOutline = polygonOutlineFeatures && polygonOutlineFeatures.length > 0;
+    const hasPolygon = polygonFeatures && polygonFeatures.length > 0;
 
     const onHover = this._onHoverSubLayer.bind(this);
     const onClick = this._onClickSubLayer.bind(this);
 
     // Filled Polygon Layer
-    const polygonFillLayer = fillPolygons && new SolidPolygonLayer(Object.assign({}, this.props, {
-      id: `${id}-polygon-fill`,
-      data: polygonFeatures,
-      extruded,
-      wireframe: false,
-      getPolygon: getCoordinates,
-      getElevation,
-      getColor: getFillColor,
-      updateTriggers: {
-        getElevation: updateTriggers.getElevation,
-        getColor: updateTriggers.getFillColor
-      },
-      onHover,
-      onClick
-    }));
+    const polygonFillLayer = filled &&
+      hasPolygon &&
+      new SolidPolygonLayer(Object.assign({}, this.props, {
+        id: `${id}-polygon-fill`,
+        data: polygonFeatures,
+        extruded,
+        wireframe: false,
+        getPolygon: getCoordinates,
+        getElevation,
+        getColor: getFillColor,
+        updateTriggers: {
+          getElevation: updateTriggers.getElevation,
+          getColor: updateTriggers.getFillColor
+        },
+        onHover,
+        onClick
+      }));
 
-    // Polygon outline or wireframe
-    let polygonOutlineLayer = null;
-    if (drawPolygons && extruded && wireframe) {
-      polygonOutlineLayer = new SolidPolygonLayer(Object.assign({}, this.props, {
+    const polygonWireframeLayer = wireframe &&
+      extruded &&
+      hasPolygon &&
+      new SolidPolygonLayer(Object.assign({}, this.props, {
         id: `${id}-polygon-wireframe`,
         data: polygonFeatures,
-        extruded: true,
+        extruded,
         wireframe: true,
         getPolygon: getCoordinates,
         getElevation,
         getColor,
         updateTriggers: {
           getElevation: updateTriggers.getElevation,
-          getColor: updateTriggers.getColor
+          getColor: updateTriggers.getFillColor
         },
         onHover,
         onClick
       }));
-    } else if (drawPolygons) {
-      polygonOutlineLayer = new PathLayer(Object.assign({}, this.props, {
+
+    const polygonOutlineLayer = !extruded &&
+      stroked &&
+      hasPolygonOutline &&
+      new PathLayer(Object.assign({}, this.props, {
         id: `${id}-polygon-outline`,
         data: polygonOutlineFeatures,
         getPath: getCoordinates,
@@ -141,7 +146,6 @@ export default class GeoJsonLayer extends CompositeLayer {
         onHover,
         onClick
       }));
-    }
 
     const lineLayer = drawLines && new PathLayer(Object.assign({}, this.props, {
       id: `${id}-line-paths`,
@@ -173,6 +177,7 @@ export default class GeoJsonLayer extends CompositeLayer {
 
     return [
       polygonFillLayer,
+      polygonWireframeLayer,
       polygonOutlineLayer,
       lineLayer,
       pointLayer

src/layers/core/grid-cell-layer/grid-cell-layer.js
@@ -116,7 +116,11 @@ export default class GridCellLayer extends Layer {
 
   updateState({props, oldProps, changeFlags}) {
     super.updateState({props, oldProps, changeFlags});
-    this.updateModel({props, oldProps, changeFlags});
+    // Re-generate model if geometry changed
+    if (props.fp64 !== oldProps.fp64) {
+      const {gl} = this.context;
+      this.setState({model: this._getModel(gl)});
+    }
     this.updateAttribute({props, oldProps, changeFlags});
     this.updateUniforms();
   }

src/layers/core/hexagon-cell-layer/hexagon-cell-layer.js
@@ -143,7 +143,10 @@ export default class HexagonCellLayer extends Layer {
 
   updateState({props, oldProps, changeFlags}) {
     super.updateState({props, oldProps, changeFlags});
-    this.updateModel({props, oldProps, changeFlags});
+    if (props.fp64 !== oldProps.fp64) {
+      const {gl} = this.context;
+      this.setState({model: this._getModel(gl)});
+    }
     this.updateAttribute({props, oldProps, changeFlags});
 
     const viewportChanged = changeFlags.viewportChanged;

src/layers/core/icon-layer/icon-layer.js
@@ -103,6 +103,8 @@ export default class IconLayer extends Layer {
   }
 
   updateState({oldProps, props, changeFlags}) {
+    super.updateState({props, oldProps, changeFlags});
+
     const {iconAtlas} = props;
 
     if (oldProps.iconAtlas !== iconAtlas) {
@@ -121,7 +123,10 @@ export default class IconLayer extends Layer {
       }
     }
 
-    this.updateModel({props, oldProps, changeFlags});
+    if (props.fp64 !== oldProps.fp64) {
+      const {gl} = this.context;
+      this.setState({model: this._getModel(gl)});
+    }
     this.updateAttribute({props, oldProps, changeFlags});
 
   }

src/layers/core/line-layer/line-layer.js
@@ -88,7 +88,12 @@ export default class LineLayer extends Layer {
   }
 
   updateState({props, oldProps, changeFlags}) {
-    this.updateModel({props, oldProps, changeFlags});
+    super.updateState({props, oldProps, changeFlags});
+
+    if (props.fp64 !== oldProps.fp64) {
+      const {gl} = this.context;
+      this.setState({model: this._getModel(gl)});
+    }
     this.updateAttribute({props, oldProps, changeFlags});
   }
 

commit ceba67c1e226e5ef3e0ce38637c4d71791035cb2

Fix the model regeneration bug introduced by #424

src/layers/core/solid-polygon-layer/solid-polygon-layer.js
@@ -117,10 +117,9 @@ export default class SolidPolygonLayer extends Layer {
   updateState({props, oldProps, changeFlags}) {
     super.updateState({props, oldProps, changeFlags});
 
-    const geometryChanged = this.updateGeometry({props, oldProps, changeFlags});
+    const regenerateModel = this.updateGeometry({props, oldProps, changeFlags});
 
-    // Re-generate model if geometry changed
-    if (geometryChanged) {
+    if (regenerateModel) {
       const {gl} = this.context;
       this.setState({model: this._getModel(gl)});
     }
@@ -128,11 +127,12 @@ export default class SolidPolygonLayer extends Layer {
   }
 
   updateGeometry({props, oldProps, changeFlags}) {
-    const regenerateModel = changeFlags.dataChanged ||
-      props.extruded !== oldProps.extruded ||
+    const geometryConfigChanged = props.extruded !== oldProps.extruded ||
       props.wireframe !== oldProps.wireframe || props.fp64 !== oldProps.fp64;
 
-    if (regenerateModel) {
+     // When the geometry config  or the data is changed,
+     // tessellator needs to be invoked
+    if (changeFlags.dataChanged || geometryConfigChanged) {
       const {getPolygon, extruded, wireframe, getElevation} = props;
 
       // TODO - avoid creating a temporary array here: let the tesselator iterate
@@ -150,7 +150,7 @@ export default class SolidPolygonLayer extends Layer {
       this.state.attributeManager.invalidateAll();
     }
 
-    return regenerateModel;
+    return geometryConfigChanged;
   }
 
   _getModel(gl) {