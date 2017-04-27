23fec81b180fa5bf1845c5dd30e3a8f6c71af555
Sam Hogg <sam@samhogg.com>
2017-04-27 15:24:00 -0500

    Migrate from React.PropTypes to `prop-types` module (#583)

--- demo/src/components/page.js
+++ demo/src/components/page.js
@@ -1,5 +1,6 @@
 /* global window */
-import React, {Component, PropTypes} from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import autobind from 'autobind-decorator';
 
