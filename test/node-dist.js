a24e1b834d162680e6555b3bb0e7aaef5fb318a8
Shaojing Li <shaojing@uber.com>
2017-05-24 14:25:58 -0700

    Remove JSDom to align the behavior between node.js and node-dist.js (#662)
    * Remove JSDom to align the behavior between node.js and node-dist.js
    * Replace sinon with a much simpler spy
    * Address review comments

--- test/node-dist.js
+++ test/node-dist.js
@@ -21,12 +21,6 @@
 // Enables ES2015 import/export in Node.js
 require('reify');
 
-// Enables import of glsl
-const fs = require('fs');
-require.extensions['.glsl'] = function readShader(module, filename) {
-  module.exports = fs.readFileSync(filename, 'utf8');
-};
-
 // Registers an alias for this module
 const path = require('path');
 const moduleAlias = require('module-alias');
--- test/node.js
+++ test/node.js
@@ -21,18 +21,6 @@
 // Enables ES2015 import/export in Node.js
 require('reify');
 
-// setup for jsDom
-require('./setup-jsdom-env');
-// Mock addEventListener on window, required for seer
-const {window} = require('../src/lib/utils/globals');
-window.addEventListener = () => {};
-
-// Enables import of glsl
-const fs = require('fs');
-require.extensions['.glsl'] = function readShader(module, filename) {
-  module.exports = fs.readFileSync(filename, 'utf8');
-};
-
 // Registers an alias for this module
 const path = require('path');
 const moduleAlias = require('module-alias');
