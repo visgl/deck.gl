cc7cc926b52eb977edbc7d6535e42320d98cf55b
Ib Green <ibgreen@users.noreply.github.com>
2017-07-06 06:48:55 -0700

    Add links to other frameworks in suite (#753)

--- website/src/components/header.js
+++ website/src/components/header.js
@@ -1,23 +1,52 @@
+
+
 import React, {Component} from 'react';
 import {Link} from 'react-router';
 
+import {FRAMEWORK_NAME, FRAMEWORK_GITHUB_URL} from '../../contents/framework';
+import FRAMEWORK_LINKS from '../../contents/links';
+
 export default class Header extends Component {
+
+  _renderLinks() {
+    const links = Object.keys(FRAMEWORK_LINKS).filter(name => name !== FRAMEWORK_NAME);
+    return (
+      <div className="site-links">
+        <div className="site-link" key={FRAMEWORK_NAME}>
+          <a href="#">{FRAMEWORK_NAME}</a>
+        </div>
+        {
+          links.map(name =>
+            <div className="site-link" key={name}>
+              <a href={FRAMEWORK_LINKS[name]}>{name}</a>
+            </div>
+          )
+        }
+      </div>
+    );
+  }
 
   render() {
     const {isMenuOpen, opacity, toggleMenu} = this.props;
 
     return (
-      <header className={ isMenuOpen ? 'open' : '' }>
+      <header className={isMenuOpen ? 'open' : ''}>
         <div className="bg" style={{opacity}} />
         <div className="container">
-          <a className="logo" href="#">deck.gl</a>
+          <a className="logo" href="#">
+            {FRAMEWORK_NAME}
+          </a>
+          { this._renderLinks() }
           <div className="menu-toggle" onClick={ () => toggleMenu(!isMenuOpen) }>
             <i className={`icon icon-${isMenuOpen ? 'close' : 'menu'}`} />
           </div>
           <div className="links">
-            <Link activeClassName="active" to="examples">Gallery</Link>
+            <Link activeClassName="active" to="examples">Examples</Link>
             <Link activeClassName="active" to="documentation">Documentation</Link>
             <a href="http://uber.github.io/deck.gl/blog/latest">Blog</a>
+            <Link activeClassName="active" href={FRAMEWORK_GITHUB_URL}>
+              Github<i className="icon icon-github" />
+            </Link>
           </div>
         </div>
       </header>
