9934912aafeef5a0f755c8a5360de32a9b64a373
Xiaoji Chen <xiaoji@uber.com>
2017-07-25 19:30:04 -0700

    remove redundant configs

--- website/webpack.config.js
+++ website/webpack.config.js
@@ -96,12 +96,6 @@ const addDevConfig = config => {
 };
 
 const addProdConfig = config => {
-  // transpile mapbox-gl utils imported by react-map-gl@2
-  // these files are ES6 and break UglifyJS
-  config.module.rules.push({
-    test: /node_modules\/mapbox-gl\/js.*\.js$/,
-    loader: 'babel-loader'
-  });
 
   return Object.assign(config, {
     output: {
