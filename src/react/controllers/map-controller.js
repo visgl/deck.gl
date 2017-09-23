fb8c15e951beac8fe24bf548a665dc3d1ddac609
Ib Green <ib@uber.com>
2017-09-23 09:29:58 -0700

    React map controller now thin wrapper over MapControllerJS

--- src/react/controllers/map-controller.js
+++ src/react/controllers/map-controller.js
@@ -1,135 +1,33 @@
 import {PureComponent, createElement} from 'react';
-import PropTypes from 'prop-types';
-
-import {EventManager} from 'mjolnir.js';
-import Controls from '../../controllers/controls';
-import MapState from '../../controllers/map-state';
-import {MAPBOX_LIMITS} from '../../controllers/map-state';
-import CURSOR from './cursors';
-
-const propTypes = {
-  /** The width of the map. */
-  width: PropTypes.number.isRequired,
-  /** The height of the map. */
-  height: PropTypes.number.isRequired,
-  /** The longitude of the center of the map. */
-  longitude: PropTypes.number.isRequired,
-  /** The latitude of the center of the map. */
-  latitude: PropTypes.number.isRequired,
-  /** The tile zoom level of the map. */
-  zoom: PropTypes.number.isRequired,
-  /** Specify the bearing of the viewport */
-  bearing: PropTypes.number,
-  /** Specify the pitch of the viewport */
-  pitch: PropTypes.number,
-  /** Altitude of the viewport camera. Default 1.5 "screen heights" */
-  // Note: Non-public API, see https://github.com/mapbox/mapbox-gl-js/issues/1137
-  altitude: PropTypes.number,
-
-  /** Viewport constraints */
-  // Max zoom level
-  maxZoom: PropTypes.number,
-  // Min zoom level
-  minZoom: PropTypes.number,
-  // Max pitch in degrees
-  maxPitch: PropTypes.number,
-  // Min pitch in degrees
-  minPitch: PropTypes.number,
-
-  /**
-   * `onViewportChange` callback is fired when the user interacted with the
-   * map. The object passed to the callback contains viewport properties
-   * such as `longitude`, `latitude`, `zoom` etc.
-   */
-  onViewportChange: PropTypes.func,
-
-  /** Enables control event handling */
-  // Scroll to zoom
-  scrollZoom: PropTypes.bool,
-  // Drag to pan
-  dragPan: PropTypes.bool,
-  // Drag to rotate
-  dragRotate: PropTypes.bool,
-  // Double click to zoom
-  doubleClickZoom: PropTypes.bool,
-  // Pinch to zoom / rotate
-  touchZoomRotate: PropTypes.bool,
-
-  /** Accessor that returns a cursor style to show interactive state */
-  getCursor: PropTypes.func,
-
-  // A map control instance to replace the default map controls
-  // The object must expose one property: `events` as an array of subscribed
-  // event names; and two methods: `setState(state)` and `handle(event)`
-  controls: PropTypes.shape({
-    events: PropTypes.arrayOf(PropTypes.string),
-    handleEvent: PropTypes.func
-  })
-};
-
-const getDefaultCursor = ({isDragging}) => isDragging ? CURSOR.GRABBING : CURSOR.GRAB;
-
-const defaultProps = Object.assign({}, MAPBOX_LIMITS, {
-  onViewportChange: null,
-
-  scrollZoom: true,
-  dragPan: true,
-  dragRotate: true,
-  doubleClickZoom: true,
-  touchZoomRotate: true,
-
-  getCursor: getDefaultCursor
-});
+import MapControllerJS from '../../pure-js/map-controller';
 
 export default class MapController extends PureComponent {
 
   constructor(props) {
     super(props);
-
-    this.state = {
-      isDragging: false      // Whether the cursor is down
-
-    };
+    this.controller = null;
   }
 
   componentDidMount() {
     const {eventCanvas} = this.refs;
-
-    const eventManager = new EventManager(eventCanvas);
-
-    this._eventManager = eventManager;
-
-    // If props.controls is not provided, fallback to default MapState Controls instance
-    // Cannot use defaultProps here because it needs to be per map instance
-    this._controls = this.props.controls || new Controls(MapState);
-    this._controls.setOptions(Object.assign({}, this.props, {
-      onStateChange: this._onInteractiveStateChange.bind(this),
-      eventManager
-    }));
+    this.controller = new MapControllerJS(Object.assign({}, this.props, {canvas: eventCanvas}));
   }
 
   componentWillUpdate(nextProps) {
-    this._controls.setOptions(nextProps);
+    this.controller.setProps(nextProps);
   }
 
   componentWillUnmount() {
-    this._eventManager.destroy();
-  }
-
-  _onInteractiveStateChange({isDragging = false}) {
-    if (isDragging !== this.state.isDragging) {
-      this.setState({isDragging});
-    }
+    this.controller.finalize();
   }
 
   render() {
-    const {width, height, getCursor} = this.props;
+    const {width, height} = this.props;
 
     const eventCanvasStyle = {
       width,
       height,
-      position: 'relative',
-      cursor: getCursor(this.state)
+      position: 'relative'
     };
 
     return (
@@ -145,5 +43,5 @@ export default class MapController extends PureComponent {
 }
 
 MapController.displayName = 'MapController';
-MapController.propTypes = propTypes;
-MapController.defaultProps = defaultProps;
+MapController.propTypes = MapControllerJS.propTypes;
+MapController.defaultProps = MapControllerJS.defaultProps;
