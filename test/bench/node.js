2dece57210060f2ce4fd931b0958288cddd241e9
Ib Green <ib@uber.com>
2017-10-04 08:01:07 -0700

    Fix npm run bench

--- test/bench/node.js
+++ test/bench/node.js
@@ -30,6 +30,7 @@ require.extensions['.glsl'] = function readShader(module, filename) {
 // Registers an alias for this module
 const path = require('path');
 const moduleAlias = require('module-alias');
+moduleAlias.addAlias('deck.gl/test', path.resolve('./test'));
 moduleAlias.addAlias('deck.gl', path.resolve('./src'));
 
 require('babel-polyfill');
