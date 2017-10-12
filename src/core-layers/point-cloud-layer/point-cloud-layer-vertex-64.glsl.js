1c121a6b6c2eed439cc1a1c887bc3fbd0200fb93
Ib Green <ib@uber.com>
2017-10-12 08:27:12 -0700

    Add new project64 module function that removes raw fp64 math from layer shaders

--- src/core-layers/point-cloud-layer/point-cloud-layer-vertex-64.glsl.js
+++ src/core-layers/point-cloud-layer/point-cloud-layer-vertex-64.glsl.js
@@ -39,29 +39,23 @@ void main(void) {
   // position on the containing square in [-1, 1] space
   unitPosition = positions.xy;
 
-  vec4 instancePositions64xy = vec4(
-    instancePositions.x, instancePositions64xyLow.x,
-    instancePositions.y, instancePositions64xyLow.y);
+  vec4 position_worldspace;
 
-  vec2 projected_coord_xy[2];
-  project_position_fp64(instancePositions64xy, projected_coord_xy);
+  vec3 vertex = vec3(positions.xy * radiusPixels, 0.);
 
-  vec2 vertex_pos_modelspace[4];
-  vertex_pos_modelspace[0] = projected_coord_xy[0];
-  vertex_pos_modelspace[1] = projected_coord_xy[1];
-  vertex_pos_modelspace[2] = vec2(project_scale(instancePositions.z), 0.0);
-  vertex_pos_modelspace[3] = vec2(1.0, 0.0);
+  gl_Position = project_model_position_and_offset_to_clipspace_fp64(
+    instancePositions,
+    instancePositions64xyLow,
+    vertex,
+    // out parameter
+    position_worldspace
+  );
 
-  gl_Position = project_to_clipspace_fp64(vertex_pos_modelspace);
   gl_Position += project_pixel_to_clipspace(positions.xy * radiusPixels);
 
-  vec4 position_worldspace = vec4(
-    projected_coord_xy[0].x, projected_coord_xy[1].x,
-    project_scale(instancePositions.z), 1.0);
 
   // Apply lighting
-  float lightWeight = getLightWeight(position_worldspace.xyz, // the w component is always 1.0
-    instanceNormals);
+  float lightWeight = getLightWeight(position_worldspace.xyz, instanceNormals); // w is always 1.0
 
   // Apply opacity to instance color, or return instance picking color
   vColor = vec4(lightWeight * instanceColors.rgb, instanceColors.a * opacity) / 255.;
