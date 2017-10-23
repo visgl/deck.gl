6f05ec053dd71e6bc88479d0e9549e8a702254aa
Ib Green <ib@uber.com>
2017-10-23 16:26:21 -0700

    Use project64utils shader module

--- src/experimental-layers/src/mesh-layer/mesh-layer-vertex-64.glsl.js
+++ src/experimental-layers/src/mesh-layer/mesh-layer-vertex-64.glsl.js
@@ -39,30 +39,6 @@ attribute vec3 instancePickingColors;
 varying vec2 vTexCoord;
 varying float vLightWeight;
 
-vec4 getPosition(vec3 position64xyzHi, vec2 position64xyLow, vec3 offset) {
-  // This is the local offset to the instance position
-  vec2 offset64[4];
-  vec4_fp64(vec4(offset, 0.0), offset64);
-
-  // The 64 bit xy vertex position
-  vec4 position64xy = vec4(
-    position64xyzHi.x, position64xyLow.x,
-    position64xyzHi.y, position64xyLow.y
-  );
-
-  // Apply web mercator projection (depends on coordinate system imn use)
-  vec2 projectedPosition64xy[2];
-  project_position_fp64(position64xy, projectedPosition64xy);
-
-  vec2 worldPosition64[4];
-  worldPosition64[0] = sum_fp64(offset64[0], projectedPosition64xy[0]);
-  worldPosition64[1] = sum_fp64(offset64[1], projectedPosition64xy[1]);
-  worldPosition64[2] = sum_fp64(offset64[2], vec2(project_scale(position64xyzHi.z), 0.0));
-  worldPosition64[3] = vec2(1.0, 0.0);
-
-  return project_to_clipspace_fp64(worldPosition64);
-}
-
 void main(void) {
   // Calculate arrow offset (from instance position)
   float angle = instanceAngles;
@@ -72,10 +48,9 @@ void main(void) {
   offset = project_scale(offset * sizeScale);
   offset = vec3(rotationMatrix * offset.xy, offset.z);
 
-  gl_Position = getPosition(instancePositions, instancePositions64xy, offset);
-  // gl_Position = project_position_and_offset_to_clipspace_fp64(
-  //   instancePositions, instancePositions64xy, offset
-  // );
+  gl_Position = project_position_and_offset_to_clipspace_fp64(
+    instancePositions, instancePositions64xy, offset
+  );
 
   picking_setPickingColor(instancePickingColors);
 
