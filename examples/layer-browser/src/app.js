01f12be28448721f21008a4700bd144636e89f96
Shan He <shan@uber.com>
2017-11-21 15:01:31 -0800

    remove emply line

--- examples/layer-browser/src/app.js
+++ examples/layer-browser/src/app.js
@@ -123,7 +123,6 @@ class App extends PureComponent {
 
   _renderExampleLayer(example, settings, index) {
     const {layer: Layer, props, getData} = example;
-
     const layerProps = Object.assign({}, props, settings);
 
     if (getData) {
