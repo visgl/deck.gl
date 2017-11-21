c61249fc45f2213efb8bfa132bd7ef29f118c234
Xiaoji Chen <xiaoji@uber.com>
2017-11-21 12:08:22 -0800

    upgrade viewport-mercator-project

--- src/core/viewports/web-mercator-viewport.js
+++ src/core/viewports/web-mercator-viewport.js
@@ -25,8 +25,9 @@ import Viewport from './viewport';
 import {
   projectFlat,
   unprojectFlat,
-  makeProjectionMatrixFromMercatorParams,
-  makeUncenteredViewMatrixFromMercatorParams
+  getProjectionMatrix,
+  getUncenteredViewMatrix,
+  fitBounds
 } from 'viewport-mercator-project';
 
 // TODO - import from viewport-mercator-project
@@ -93,7 +94,7 @@ export default class WebMercatorViewport extends Viewport {
     // TODO - just throw an Error instead?
     altitude = Math.max(0.75, altitude);
 
-    const projectionMatrix = makeProjectionMatrixFromMercatorParams({
+    const projectionMatrix = getProjectionMatrix({
       width,
       height,
       pitch,
@@ -106,7 +107,7 @@ export default class WebMercatorViewport extends Viewport {
     // shader (cheap) which gives a coordinate system that has its center in
     // the layer's center position. This makes rotations and other modelMatrx
     // transforms much more useful.
-    const viewMatrixUncentered = makeUncenteredViewMatrixFromMercatorParams({
+    const viewMatrixUncentered = getUncenteredViewMatrix({
       width,
       height,
       longitude,
@@ -278,62 +279,6 @@ export default class WebMercatorViewport extends Viewport {
     return pitch <= (MAPBOX_LIMITS.pitch + EPSILON) &&
       zoom <= (MAPBOX_LIMITS.zoom + EPSILON);
   }
-}
-
-// TODO - investigate if we can move this viewport-mercator-project
-
-/**
- * Returns map settings {latitude, longitude, zoom}
- * that will contain the provided corners within the provided width.
- * Only supports non-perspective mode.
- * @param {Number} width - viewport width
- * @param {Number} height - viewport height
- * @param {Array} bounds - [[lon, lat], [lon, lat]]
- * @param {Number} [padding] - The amount of padding in pixels to add to the given bounds.
- * @param {Array} [offset] - The center of the given bounds relative to the map's center,
- *    [x, y] measured in pixels.
- * @returns {Object} - latitude, longitude and zoom
- */
-export function fitBounds({
-  width,
-  height,
-  bounds,
-  // options
-  padding = 0,
-  offset = [0, 0]
-}) {
-  const [[west, south], [east, north]] = bounds;
-
-  const viewport = new WebMercatorViewport({
-    width,
-    height,
-    longitude: 0,
-    latitude: 0,
-    zoom: 0
-  });
-
-  const nw = viewport.project([west, north]);
-  const se = viewport.project([east, south]);
-  const size = [
-    Math.abs(se[0] - nw[0]),
-    Math.abs(se[1] - nw[1])
-  ];
-  const center = [
-    (se[0] + nw[0]) / 2,
-    (se[1] + nw[1]) / 2
-  ];
-
-  const scaleX = (width - padding * 2 - Math.abs(offset[0]) * 2) / size[0];
-  const scaleY = (height - padding * 2 - Math.abs(offset[1]) * 2) / size[1];
-
-  const centerLngLat = viewport.unproject(center);
-  const zoom = viewport.zoom + Math.log2(Math.abs(Math.min(scaleX, scaleY)));
-
-  return {
-    longitude: centerLngLat[0],
-    latitude: centerLngLat[1],
-    zoom
-  };
 }
 
 WebMercatorViewport.displayName = 'WebMercatorViewport';
