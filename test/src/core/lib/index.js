b9a05d3ffabfc64690730366e088d0df29513ff7
Xiaoji Chen <xiaoji@uber.com>
2017-11-21 23:36:29 -0800

    viewport transition tests

--- test/src/core/lib/index.js
+++ test/src/core/lib/index.js
@@ -18,6 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+import './transition';
 import './utils';
 import './props.spec';
 import './attribute-manager.spec';
---
+++ test/src/core/lib/transition/index.js
@@ -0,0 +1,3 @@
+import './transition-interpolator.spec';
+import './linear-interpolator.spec';
+import './viewport-fly-to-interpolator.spec';
