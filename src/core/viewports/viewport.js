43fd968bd54a9da5af19f470c053666dd083e462
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2017-12-21 13:03:25 -0800

    More precise offset projection (#1265)

--- src/core/viewports/viewport.js
+++ src/core/viewports/viewport.js
@@ -303,7 +303,15 @@ export default class Viewport {
     return false;
   }
 
-  getDistanceScales() {
+  getDistanceScales(coordinateOrigin = null) {
+    if (coordinateOrigin) {
+      return getDistanceScales({
+        longitude: coordinateOrigin[0],
+        latitude: coordinateOrigin[1],
+        scale: this.scale,
+        highPrecision: true
+      });
+    }
     return this.distanceScales;
   }
 
