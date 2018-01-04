d165deea27220e0c5e69d255e2454c6a83be1341
Jian Huang <jianh@uber.com>
2018-01-04 14:22:52 -0800

    Fix broken rendering test

--- src/test-utils/src/index.js
+++ src/test-utils/src/index.js
@@ -3,3 +3,4 @@ export {toLowPrecision} from './precision';
 export {default as spy} from './spy';
 export {default as makeSpy} from './spy';
 export {default as gl} from './setup-gl';
+export * from '../luma.gl/gpgpu';
