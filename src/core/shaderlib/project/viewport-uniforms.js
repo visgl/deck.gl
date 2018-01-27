08f1a2830255ea46bb3d90c56d4f28f317e8fa1f
Jian Huang <jianh@uber.com>
2018-01-26 17:26:10 -0800

    Bump luma.gl to 5.1.0-beta.1 and fix unit tests

--- src/core/shaderlib/project/viewport-uniforms.js
+++ src/core/shaderlib/project/viewport-uniforms.js
@@ -164,7 +164,8 @@ export function getUniformsFromViewport({
   const distanceScales = viewport.getDistanceScales();
 
   // TODO - does this depend on useDevicePixels?
-  const devicePixelRatio = (window && window.devicePixelRatio) || 1;
+  const devicePixelRatio =
+    (typeof window !== 'undefined' && window && window.devicePixelRatio) || 1;
   const viewportSize = [viewport.width * devicePixelRatio, viewport.height * devicePixelRatio];
 
   const glModelMatrix = modelMatrix || IDENTITY_MATRIX;
