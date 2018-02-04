0df02cf3b614b4249d7f95c88c0f683e988d8001
Xiaoji Chen <xiaoji@uber.com>
2018-02-04 10:15:22 -0800

    add comments

--- src/core/shaderlib/memoize.js
+++ src/core/shaderlib/memoize.js
@@ -3,6 +3,8 @@ function isEqual(a, b) {
     return true;
   }
   if (Array.isArray(a)) {
+    // Special treatment for arrays: compare 1-level deep
+    // This is to support equality of matrix/coordinate props
     const len = a.length;
     if (!b || b.length !== len) {
       return false;
@@ -18,6 +20,11 @@ function isEqual(a, b) {
   return false;
 }
 
+/**
+ * Speed up consecutive function calls by caching the result of calls with identical input
+ * https://en.wikipedia.org/wiki/Memoization
+ * @param {function} compute - the function to be memoized
+ */
 export default function memoize(compute) {
   let cachedArgs = {};
   let cachedResult;
