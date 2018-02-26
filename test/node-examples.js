f41d7e0b126a1f3452451cad07db4bfa3b60564b
Jian Huang <jianh@uber.com>
2018-02-26 15:15:09 -0800

    Add more testing cases and polish code

--- test/node-examples.js
+++ test/node-examples.js
@@ -29,25 +29,28 @@ const fs = require('fs');
 const PNG = require('pngjs').PNG;
 const pixelmatch = require('pixelmatch');
 
-async function validateWithWaitingTime(child, waitingTime, threshold) {
+function printResult(diffRatio, threshold) {
+  return diffRatio <= threshold
+    ? console.log('\x1b[32m%s\x1b[0m', 'Rendering test Passed!')
+    : console.log('\x1b[31m%s\x1b[0m', 'Rendering test failed!');
+}
+
+async function validateWithWaitingTime(child, folder, waitingTime, threshold) {
   const browser = await puppeteer.launch({
     headless: false,
     executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
   });
   const page = await browser.newPage();
-  await page.waitFor(1000);
+  await page.waitFor(2000);
   await page.goto('http://localhost:8080');
   await page.setViewport({width: 1000, height: 800});
   await page.waitFor(waitingTime);
-
   await page.screenshot({path: 'new.png'});
 
-  const goldImagedata = fs.readFileSync('gold.png');
-  const goldImage = PNG.sync.read(goldImagedata);
-
+  const goldImageData = fs.readFileSync(`../test/golden-images/${folder}.png`);
+  const goldImage = PNG.sync.read(goldImageData);
   const newImageData = fs.readFileSync('new.png');
   const newImage = PNG.sync.read(newImageData);
-
   const diffImage = new PNG({width: goldImage.width, height: goldImage.height});
 
   const pixelDiffSize = pixelmatch(
@@ -56,25 +59,23 @@ async function validateWithWaitingTime(child, waitingTime, threshold) {
     diffImage.data,
     goldImage.width,
     goldImage.height,
-    {threshold: 0.068, includeAA: true}
+    {threshold: 0.105, includeAA: true}
   );
-
   const pixelDiffRatio = pixelDiffSize / (goldImage.width * goldImage.height);
+
   console.log(`Mismatched pixel number: ${pixelDiffSize}`);
   console.log(`Mismatched pixel ratio: ${pixelDiffRatio}`);
 
-  diffImage.pack().pipe(fs.createWriteStream('diff.png'));
+  const diffImageData = PNG.sync.write(diffImage, {colorType: 6});
+  fs.writeFileSync('diff.png', diffImageData);
+  fs.unlinkSync('new.png');
+  fs.unlinkSync('diff.png');
 
   child.kill();
   await page.waitFor(1000);
   await browser.close();
-
-  if (pixelDiffRatio <= threshold) {
-    console.log('\x1b[32m%s\x1b[0m', 'Rendering test Passed!');
-    return true;
-  }
-  console.log('\x1b[31m%s\x1b[0m', 'Rendering test failed!');
-  return false;
+  printResult(pixelDiffRatio, threshold);
+  return pixelDiffRatio <= threshold;
 }
 
 async function yarnAndLaunchWebpack() {
@@ -82,100 +83,50 @@ async function yarnAndLaunchWebpack() {
   // console.log(output.toString('utf8'));
   const child = execFile(
     './node_modules/.bin/webpack-dev-server',
-    [' --env.local', '--config', 'webpack.config.js', '--progress'],
+    ['--env.local', '--config', 'webpack.config.js', '--progress', '--hot'],
     {
       maxBuffer: 5000 * 1024
     },
-    (err, stdout, stderr) => {
+    err => {
       if (err) {
-        // console.error(err);
+        console.error(err);
         return;
       }
-      // console.log(stdout);
     }
   );
   return child;
 }
 
-process.chdir('./examples');
+function changeFolder(folder) {
+  console.log('--------------------------');
+  console.log(`Begin to test ${folder}`);
+  process.chdir(`./${folder}`);
+}
 
-// eslint-disable-next-line
+async function TestExample(folder) {
+  changeFolder(folder);
+  const child = await yarnAndLaunchWebpack();
+  const valid = await validateWithWaitingTime(child, folder, 5000, 0.01);
+  process.chdir('../');
+  if (!valid) {
+    process.exit(1); //eslint-disable-line
+  }
+}
+
 (async () => {
-  let child;
-  let valid = true;
-  let finalResult = true;
+  process.chdir('./examples');
 
-  console.log('--------------------------');
-  console.log('Begin to test plot');
-  process.chdir('./plot');
-  child = await yarnAndLaunchWebpack();
-  valid = await validateWithWaitingTime(child, 5000, 0.01);
-  finalResult = finalResult && valid;
-  process.chdir('../');
-
-  console.log('--------------------------');
-  console.log('Begin to test 3d-heatmap');
-  process.chdir('./3d-heatmap');
-  child = await yarnAndLaunchWebpack();
-  valid = await validateWithWaitingTime(child, 5000, 0.01);
-  finalResult = finalResult && valid;
-  process.chdir('../');
-
-  console.log('--------------------------');
-  console.log('Begin to test arc');
-  process.chdir('./arc');
-  child = await yarnAndLaunchWebpack();
-  valid = await validateWithWaitingTime(child, 5000, 0.001);
-  finalResult = finalResult && valid;
-  process.chdir('../');
-
-  console.log('--------------------------');
-  console.log('Begin to test bezier');
-  process.chdir('./bezier');
-  child = await yarnAndLaunchWebpack();
-  valid = await validateWithWaitingTime(child, 5000, 0.001);
-  finalResult = finalResult && valid;
-  process.chdir('../');
-
-  console.log('--------------------------');
-  console.log('Begin to test brushing');
-  process.chdir('./brushing');
-  child = await yarnAndLaunchWebpack();
-  valid = valid && (await validateWithWaitingTime(child, 5000, 0.001));
-  finalResult = finalResult && valid;
-  process.chdir('../');
-
-  console.log('--------------------------');
-  console.log('Begin to test geojson');
-  process.chdir('./geojson');
-  child = await yarnAndLaunchWebpack();
-  valid = await validateWithWaitingTime(child, 5000, 0.01);
-  finalResult = finalResult && valid;
-  process.chdir('../');
-
-  console.log('--------------------------');
-  console.log('Begin to test graph');
-  process.chdir('./graph');
-  child = await yarnAndLaunchWebpack();
-  valid = await validateWithWaitingTime(child, 10000, 0.01);
-  finalResult = finalResult && valid;
-  process.chdir('../');
-
-  console.log('--------------------------');
-  console.log('Begin to test icon');
-  process.chdir('./icon');
-  child = await yarnAndLaunchWebpack();
-  valid = await validateWithWaitingTime(child, 5000, 0.001);
-  finalResult = finalResult && valid;
-  process.chdir('../');
-
-  console.log('--------------------------');
-  console.log('Begin to test line');
-  process.chdir('./line');
-  child = await yarnAndLaunchWebpack();
-  valid = await validateWithWaitingTime(child, 5000, 0.001);
-  finalResult = finalResult && valid;
-  process.chdir('../');
-
-  process.exit(finalResult ? 0 : 1); //eslint-disable-line
+  await TestExample('3d-heatmap');
+  await TestExample('arc');
+  await TestExample('bezier');
+  await TestExample('brushing');
+  await TestExample('geojson');
+  await TestExample('graph');
+  await TestExample('icon');
+  await TestExample('line');
+  await TestExample('plot');
+  await TestExample('scatterplot');
+  await TestExample('screen-grid');
+  await TestExample('tagmap');
+  await TestExample('without-map');
 })();
