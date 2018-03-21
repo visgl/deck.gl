fb45f0e66fe82869e779823950815a3293627ceb
Ravi Akkenapally <chandu@uber.com>
2018-03-21 12:32:57 -0700

    Address comments, increase padding, set texture WRAP modes

--- src/experimental-layers/src/text-layer/font-atlas.js
+++ src/experimental-layers/src/text-layer/font-atlas.js
@@ -1,9 +1,12 @@
 /* global document */
 import {Texture2D} from 'luma.gl';
 
+const GL_TEXTURE_WRAP_S = 0x2802;
+const GL_TEXTURE_WRAP_T = 0x2803;
+const GL_CLAMP_TO_EDGE = 0x812f;
 const MAX_CANVAS_WIDTH = 1024;
 const fontSize = 64;
-const padding = 2;
+const padding = 4;
 
 const charList = [];
 for (let i = 32; i < 128; i++) {
@@ -54,9 +57,13 @@ export function makeFontAtlas(gl, fontFamily) {
   return {
     mapping,
     texture: new Texture2D(gl, {
-      pixels: canvas
-      // no need to specify texture filter here
-      // icon layer use the most accurate mode by default
+      pixels: canvas,
+      // padding is added only between the characters but not for borders
+      // enforce CLAMP_TO_EDGE to avoid any artifacts.
+      parameters: {
+        [GL_TEXTURE_WRAP_S]: GL_CLAMP_TO_EDGE,
+        [GL_TEXTURE_WRAP_T]: GL_CLAMP_TO_EDGE
+      }
     })
   };
 }
