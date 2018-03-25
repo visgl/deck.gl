bb74678106faa1f523c20fc3482ee4896cc9c8fe
Chris Gervang <chr@uber.com>
2018-03-25 15:32:56 -0700

    move floating polygon to experimental

--- src/experimental-layers/src/index.js
+++ src/experimental-layers/src/index.js
@@ -7,6 +7,7 @@ export {default as PathMarkerLayer} from './path-marker-layer/path-marker-layer'
 export {default as PathOutlineLayer} from './path-outline-layer/path-outline-layer';
 
 export {default as SolidPolygonLayer} from './solid-polygon-layer/solid-polygon-layer';
+export {default as FloatingPolygonLayer} from './floating-polygon-layer/floating-polygon-layer';
 
 export {default as Arrow2DGeometry} from './path-marker-layer/arrow-2d-geometry';
 export {default as TextLayer} from './text-layer/text-layer';
