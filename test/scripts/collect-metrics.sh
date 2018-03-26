7f907018f3d392896ff8c61e2fd3005622566f2f
Ib Green <ib@uber.com>
2018-03-26 12:43:42 -0700

    Update test script

--- test/scripts/collect-metrics.sh
+++ test/scripts/collect-metrics.sh
@@ -3,43 +3,46 @@
 
 export PATH=$PATH:node_modules/.bin
 
-version=$(jq '.version' ./package.json)
+# Get name from package.json
+module=$(jq '.name' ./package.json)
+# Get version from packag.json and remove quotes
+version=$(jq '.version' ./package.json | awk '{ gsub(/"/,"",$1); printf "%-14s", $1 }')
 
 echo
-echo "\033[1mAutomatically collecting metrics for $(jq '.name' ./package.json)"
+echo "\033[1mAutomatically collecting metrics for $module"
 echo
 
-echo "| Version   | Dist | Bundle | Compressed |"
-echo "| ---       | ---  | ---    | ---        |"
+echo "| Version        | Dist | Bundle         | Compressed     |"
+echo "| ---            | ---  | ---            | ---            |"
 
 
 NODE_ENV=production webpack --config test/webpack.config.js --hide-modules --env.import-nothing --env.bundle > /dev/null
 
-# cp ./dist/bundle*.js /tmp/bundle.js
+# cp ./dist-es6/bundle*.js /tmp/bundle.js
 # Size it
-size=$(du -k /tmp/bundle.js | awk '{ print $1 }')
+size=$(wc -c /tmp/bundle.js | awk '{ print int($1 / 1024) "KB (" $1 ")" }')
 # Zip it
 gzip -9f /tmp/bundle.js
 # Size it again
-zipsize=$(du -k /tmp/bundle.js.gz | awk '{ print $1 }')
+zipsize=$(wc -c /tmp/bundle.js.gz | awk '{ print int($1 / 1024) "KB (" $1 ")" }')
 # Remove our copy
 rm /tmp/bundle.js.gz
 # Print version, size, compressed size with markdown
 
-echo "| $version   | ESM  | $size KB | $zipsize KB     |"
+echo "| $version | ESM  | $size | $zipsize |"
 
 
 NODE_ENV=production webpack --config test/webpack.config.js --hide-modules --env.import-nothing --env.bundle --env.es5 > /dev/null
 
 # cp ./dist/bundle*.js /tmp/bundle.js
 # Size it
-size=$(du -k /tmp/bundle.js | awk '{ print $1 }')
+size=$(wc -c /tmp/bundle.js | awk '{ print int($1 / 1024) "KB (" $1 ")" }')
 # Zip it
 gzip -9f /tmp/bundle.js
 # Size it again
-zipsize=$(du -k /tmp/bundle.js.gz | awk '{ print $1 }')
+zipsize=$(wc -c /tmp/bundle.js.gz | awk '{ print int($1 / 1024) "KB (" $1 ")" }')
 # Remove our copy
 rm /tmp/bundle.js.gz
 # Print version, size, compressed size with markdown
 
-echo "| $version   | ES5  | $size KB | $zipsize KB     |"
+echo "| $version | ES5  | $size | $zipsize |"
