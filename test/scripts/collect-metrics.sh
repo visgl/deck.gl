e5a041a9f1a2738d0262249c71ca65e1c44fe5f6
Ib Green <ib@uber.com>
2018-03-28 05:35:07 -0700

    Upgrade to webpack 4

--- test/scripts/collect-metrics.sh
+++ test/scripts/collect-metrics.sh
@@ -7,41 +7,48 @@ export PATH=$PATH:node_modules/.bin
 version=$(jq '.version' ./package.json | awk '{ gsub(/"/,"",$1); print }')
 # Get name from package.json
 module=$(jq '.name' ./package.json)
+
+# Helper functions
+
+print_size_header() {
+  echo "| Version       | Dist | Bundle | Compressed |"
+  echo "| ---           | ---  | ---    | ---        |"
+}
+
+print_size() {
+  DIST=$1
+  # Size it
+  size=$(du -k /tmp/bundle.js | awk '{ print $1 }')
+  # Zip it
+  gzip -9f /tmp/bundle.js
+  # Size it again
+  zipsize=$(du -k /tmp/bundle.js.gz | awk '{ print $1 }')
+  # Remove our copy
+  rm /tmp/bundle.js.gz
+  # Print version, size, compressed size with markdown
+
+  echo "| $version | $DIST  | $size KB | $zipsize KB     |"
+}
+
+build_bundle() {
+  ENV=$1
+  NODE_ENV=production webpack --config test/webpack.config.js --hide-modules --env.import-nothing --env.bundle --env.$ENV > /dev/null
+}
+
+# Main Script
+
 echo
 echo "\033[1mAutomatically collecting metrics for $module"
 echo
 
-echo "| Version       | Dist | Bundle | Compressed |"
-echo "| ---           | ---  | ---    | ---        |"
+print_size_header
 
+build_bundle es6
+print_size ES6
 
-NODE_ENV=production webpack --config test/webpack.config.js --hide-modules --env.import-nothing --env.bundle --env.es6 > /dev/null
+build_bundle esm
+print_size ESM
 
-# cp ./dist/bundle*.js /tmp/bundle.js
-# Size it
-size=$(du -k /tmp/bundle.js | awk '{ print $1 }')
-# Zip it
-gzip -9f /tmp/bundle.js
-# Size it again
-zipsize=$(du -k /tmp/bundle.js.gz | awk '{ print $1 }')
-# Remove our copy
-rm /tmp/bundle.js.gz
-# Print version, size, compressed size with markdown
+build_bundle es5
+print_size ES5
 
-echo "| $version | ES6  | $size KB | $zipsize KB     |"
-
-
-NODE_ENV=production webpack --config test/webpack.config.js --hide-modules --env.import-nothing --env.bundle > /dev/null
-
-# cp ./dist/bundle*.js /tmp/bundle.js
-# Size it
-size=$(du -k /tmp/bundle.js | awk '{ print $1 }')
-# Zip it
-gzip -9f /tmp/bundle.js
-# Size it again
-zipsize=$(du -k /tmp/bundle.js.gz | awk '{ print $1 }')
-# Remove our copy
-rm /tmp/bundle.js.gz
-# Print version, size, compressed size with markdown
-
-echo "| $version | ESM  | $size KB | $zipsize KB     |"
