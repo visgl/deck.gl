0d3b0d50ea34a3a08628457721460c645ca715f7
Ib Green <ib@uber.com>
2018-03-31 11:14:50 -0700

    Fix tests

--- test/src/core/views/view.spec.js
+++ test/src/core/views/view.spec.js
@@ -1,6 +1,5 @@
 import test from 'tape-catch';
-import {experimental} from 'deck.gl';
-const {View} = experimental;
+import {View} from 'deck.gl';
 
 test('View#imports', t => {
   t.ok(View, 'View import ok');
