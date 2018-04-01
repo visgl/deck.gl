7e38a62c583ad0cf291ff19cbb7539effeaeee81
Ib Green <ibgreen@users.noreply.github.com>
2018-04-01 08:54:41 -0700

    View improvements (#1613)

--- src/core/views/orthographic-view.js
+++ src/core/views/orthographic-view.js
@@ -4,39 +4,40 @@ import Viewport from '../viewports/viewport';
 import mat4_lookAt from 'gl-mat4/lookAt';
 import mat4_ortho from 'gl-mat4/ortho';
 
+function ORTHO_PROJECTION({left, right, bottom, top, near, far, width, height}) {
+  right = Number.isFinite(right) ? right : left + width;
+  bottom = Number.isFinite(bottom) ? bottom : top + height;
+
+  return mat4_ortho([], left, right, bottom, top, near, far);
+}
+
 export default class OrthographicView extends View {
+  constructor(props = {}) {
+    super(
+      Object.assign(
+        {
+          projection: props.projection || ORTHO_PROJECTION
+        },
+        props
+      )
+    );
+  }
+
   _getViewport({x, y, width, height, viewState}) {
     const {
       // view matrix arguments
       eye = [0, 0, 1], // Defines eye position
       lookAt = [0, 0, 0], // Which point is camera looking at, default origin
-      up = [0, 1, 0], // Defines up direction, default positive y axis
-
-      // projection matrix arguments
-      left, // Left bound of the frustum
-      top // Top bound of the frustum
+      up = [0, 1, 0] // Defines up direction, default positive y axis
     } = viewState;
 
-    let {
-      // automatically calculated
-      right = null, // Right bound of the frustum
-      bottom = null // Bottom bound of the frustum
-    } = viewState;
-
-    const {
-      near = 1, // Distance of near clipping plane
-      far = 100 // Distance of far clipping plane
-    } = this.props;
-
-    right = Number.isFinite(right) ? right : left + width;
-    bottom = Number.isFinite(bottom) ? bottom : top + height;
     return new Viewport({
       x,
       y,
       width,
       height,
       viewMatrix: mat4_lookAt([], eye, lookAt, up),
-      projectionMatrix: mat4_ortho([], left, right, bottom, top, near, far)
+      projectionMatrix: this._getProjectionMatrix({width, height})
     });
   }
 }
