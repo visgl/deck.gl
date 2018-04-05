8d8024c8acc35074af0fbe5662bbd2f2fa39405e
Xiaoji Chen <xiaoji@uber.com>
2018-04-05 17:23:37 -0600

    address comments

--- modules/lite/src/index.js
+++ modules/lite/src/index.js
@@ -1,7 +1,6 @@
 /* global window */
 import * as lumaGL from 'luma.gl';
 import * as deckGLCore from '@deck.gl/core';
-// import * as deckGLLayers from '@deck.gl/core-layers';
 
 import DeckGL from './deckgl';
 
