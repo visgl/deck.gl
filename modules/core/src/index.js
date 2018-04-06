ac5234e8812bb25542fd1c1b01676c56fdc42edd
Xiaoji Chen <xiaoji@uber.com>
2018-04-05 18:08:02 -0600

    explicit exports in @deck.gl/core

--- modules/core/src/index.js
+++ modules/core/src/index.js
@@ -1,2 +1,40 @@
-export * from './core';
-export * from './core-layers';
+export {
+  COORDINATE_SYSTEM,
+  Deck,
+  LayerManager,
+  AttributeManager,
+  Layer,
+  CompositeLayer,
+  Viewport,
+  WebMercatorViewport,
+  PerspectiveViewport,
+  OrthographicViewport,
+  project,
+  project64,
+  lighting,
+  View,
+  MapView,
+  FirstPersonView,
+  ThirdPersonView,
+  OrbitView,
+  PerspectiveView,
+  OrthographicView,
+  experimental
+} from './core';
+
+export {
+  ArcLayer,
+  IconLayer,
+  LineLayer,
+  PointCloudLayer,
+  ScatterplotLayer,
+  ScreenGridLayer,
+  GridLayer,
+  GridCellLayer,
+  HexagonLayer,
+  HexagonCellLayer,
+  PathLayer,
+  PolygonLayer,
+  GeoJsonLayer,
+  TextLayer
+} from './core-layers';
