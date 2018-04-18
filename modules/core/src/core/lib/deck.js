29bd7eef7ba30e1b00a3eeea3e83d0cfbdfbd9ae
Jian Huang <jianhuang01@hotmail.com>
2018-04-18 14:30:33 -0700

    handle review feedback for useDevicePixels (#1713)

--- modules/core/src/core/lib/deck.js
+++ modules/core/src/core/lib/deck.js
@@ -304,8 +304,7 @@ export default class Deck {
     // Note: avoid React setState due GL animation loop / setState timing issue
     this.layerManager = new LayerManager(gl, {
       eventManager: new EventManager(canvas),
-      stats: this.stats,
-      useDevicePixels: this.props.useDevicePixels
+      stats: this.stats
     });
 
     this.effectManager = new EffectManager({gl, layerManager: this.layerManager});
