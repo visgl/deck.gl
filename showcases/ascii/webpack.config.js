3507e9aa9576448ab185e0e86415a9c4959ffb14
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2018-04-20 14:00:48 -0700

    Move wind and graph examples to showcases (#1724)

--- showcases/ascii/webpack.config.js
+++ showcases/ascii/webpack.config.js
@@ -10,17 +10,12 @@ const CONFIG = {
   },
 
   devtool: 'source-map',
-  output: {
-    path: resolve('./dist'),
-    filename: 'bundle.js'
-  },
 
   module: {
     rules: [
       {
         // Compile ES2015 using buble
         test: /\.js$/,
-        // test: /^((?!graph-layer).)*\.js$/,
         loader: 'buble-loader',
         include: [resolve('.')],
         exclude: [/node_modules/],
@@ -33,4 +28,4 @@ const CONFIG = {
 };
 
 // This line enables bundling against src in this repo rather than installed deck.gl module
-module.exports = env => (env ? require('../../webpack.config.local')(CONFIG)(env) : CONFIG);
+module.exports = env => (env ? require('../../examples/webpack.config.local')(CONFIG)(env) : CONFIG);
--- showcases/graph/webpack.config.js
+++ showcases/graph/webpack.config.js
@@ -10,12 +10,17 @@ const CONFIG = {
   },
 
   devtool: 'source-map',
+  output: {
+    path: resolve('./dist'),
+    filename: 'bundle.js'
+  },
 
   module: {
     rules: [
       {
         // Compile ES2015 using buble
         test: /\.js$/,
+        // test: /^((?!graph-layer).)*\.js$/,
         loader: 'buble-loader',
         include: [resolve('.')],
         exclude: [/node_modules/],
@@ -28,4 +33,4 @@ const CONFIG = {
 };
 
 // This line enables bundling against src in this repo rather than installed deck.gl module
-module.exports = env => (env ? require('../../webpack.config.local')(CONFIG)(env) : CONFIG);
+module.exports = env => (env ? require('../../examples/webpack.config.local')(CONFIG)(env) : CONFIG);
