c594844fdff14be02e75e475f6376522b7981db9
Ib Green <ib@uber.com>
2018-05-18 12:21:39 -0700

    linter

--- test/render/test-cases.js
+++ test/render/test-cases.js
@@ -1,7 +1,7 @@
 import * as dataSamples from '../../examples/layer-browser/src/data-samples';
 import {parseColor, setOpacity} from '../../examples/layer-browser/src/utils/color';
 // TODO: remove hard path once @deck.gl/experimental-layers published with GPUScreenGridLayer
-import {GPUScreenGridLayer} from '@deck.gl/experimental-layers';
+// import {GPUScreenGridLayer} from '@deck.gl/experimental-layers';
 import {GL} from 'luma.gl';
 import {OrbitView, OrthographicView} from 'deck.gl';
 
