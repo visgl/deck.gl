1d8b0fb7ad72d642524089f6c101dd7e38772420
Xiaoji Chen <xiaoji@uber.com>
2018-05-18 17:08:54 -0700

    fix arc/line layer prop override

--- modules/core-layers/src/arc-layer/arc-layer.js
+++ modules/core-layers/src/arc-layer/arc-layer.js
@@ -41,11 +41,14 @@ const defaultProps = {
 
 export default class ArcLayer extends Layer {
   constructor(props) {
+    let overrideProps = null;
     if (Number.isFinite(props.strokeWidth)) {
       log.deprecated('ArcLayer: `strokeWidth`', '`getStrokeWidth`');
-      props.getStrokeWidth = props.strokeWidth;
+      overrideProps = {
+        getStrokeWidth: props.strokeWidth
+      };
     }
-    super(props);
+    super(props, overrideProps);
   }
 
   getShaders() {
