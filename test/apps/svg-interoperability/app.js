5a6c50205eabaf038a3b9beb648819a9796d3446
Ravi Akkenapally <chandu@uber.com>
2018-07-05 10:35:28 -0700

    Contours with Orthographic View: Add render test and update infoViz demo

--- test/apps/svg-interoperability/app.js
+++ test/apps/svg-interoperability/app.js
@@ -3,9 +3,10 @@
 import React, {PureComponent} from 'react';
 import {render} from 'react-dom';
 import DeckGL, {COORDINATE_SYSTEM, ScatterplotLayer, OrthographicView} from 'deck.gl';
+import {ContourLayer} from '@deck.gl/experimental-layers';
 
 const DEGREE_TO_RADIAN = Math.PI / 180;
-const NUM_POINTS = 2000;
+const NUM_POINTS = 20000;
 const VIEW_MODE = {
   WEBGL: 0,
   SVG: 1,
@@ -19,19 +20,22 @@ class Root extends PureComponent {
     this._onResize = this._onResize.bind(this);
     this._onClick = this._onClick.bind(this);
     this._update = this._update.bind(this);
+    this._onContourToggle = this._onContourToggle.bind(this);
 
-    this.state = {
-      width: 0,
-      height: 0,
-      viewMode: VIEW_MODE.WEBGL
-    };
-
-    this.points = Array.from(Array(NUM_POINTS)).map((_, i) => {
+    const points = Array.from(Array(NUM_POINTS)).map((_, i) => {
       return {
         radius: Math.random(),
         theta: Math.random() * 360
       };
     });
+
+    this.state = {
+      width: 0,
+      height: 0,
+      viewMode: VIEW_MODE.WEBGL,
+      points
+    };
+
   }
 
   componentDidMount() {
@@ -69,25 +73,32 @@ class Root extends PureComponent {
     this.setState({viewMode: nextViewMode});
   }
 
+  _onContourToggle() {
+    const renderContours = !this.state.renderContours;
+    this.setState({renderContours});
+  }
+
   _update() {
-    this.points.forEach(point => {
+    const {points} = this.state;
+    const newPoints = points.map(point => {
       point.theta += Math.sqrt(point.radius);
+      return point;
     });
-    this.points._lastUpdate = Date.now();
+    this.setState({points: newPoints});
 
     this.forceUpdate();
     window.requestAnimationFrame(this._update);
   }
 
   _renderSVGPoints() {
-    const {width, height} = this.state;
+    const {width, height, points} = this.state;
     const size = Math.min(width, height) / 2;
 
     return (
       <g transform={`translate(${size}, ${size})`}>
-        {this.points &&
-          this.points.length &&
-          this.points.map((p, i) => (
+        {points &&
+          points.length &&
+          points.map((p, i) => (
             <circle
               key={i}
               r={2}
@@ -101,29 +112,49 @@ class Root extends PureComponent {
   }
 
   _renderScatterplotLayer() {
-    const {width, height} = this.state;
+    const {width, height, points} = this.state;
     const size = Math.min(width, height) / 2;
 
     return new ScatterplotLayer({
       id: 'scatterplot-layer',
-      data: this.points,
+      data: points,
       getPosition: p => [
         p.radius * Math.cos(p.theta * DEGREE_TO_RADIAN) * size,
         p.radius * Math.sin(p.theta * DEGREE_TO_RADIAN) * size
       ],
       getRadius: p => 2,
       getColor: p => [255, 0, 128, 196],
-      updateTriggers: {
-        getPosition: this.points._lastUpdate
-      },
       coordinateSystem: COORDINATE_SYSTEM.IDENTITY,
       // there's a bug that the radius calculated with project_scale
       radiusMinPixels: 2
     });
   }
 
+  _renderContourLayer() {
+    const {width, height, points} = this.state;
+    const size = Math.min(width, height) / 2;
+
+    return new ContourLayer({
+      id: 'contour-layer',
+      data: points,
+      getPosition: p => [
+        p.radius * Math.cos(p.theta * DEGREE_TO_RADIAN) * size,
+        p.radius * Math.sin(p.theta * DEGREE_TO_RADIAN) * size
+      ],
+      coordinateSystem: COORDINATE_SYSTEM.IDENTITY,
+      cellSize: 20,
+      getStrokeWidth: 6,
+      contours: [
+        {threshold: 2, color: [0, 250, 250]},
+        {threshold: 25, color: [0, 150, 150]},
+        {threshold: 50, color: [0, 100, 100]}
+      ],
+      gpuAggregation: true
+    });
+  }
+
   render() {
-    const {width, height, viewMode} = this.state;
+    const {width, height, viewMode, renderContours} = this.state;
     const left = -Math.min(width, height) / 2;
     const top = -Math.min(width, height) / 2;
     const view = new OrthographicView({width, height, left, top});
@@ -141,11 +172,14 @@ class Root extends PureComponent {
               height={height}
               views={view}
               style={{position: 'absolute', top: '0px', left: '0px'}}
-              layers={[this._renderScatterplotLayer()]}
+              layers={[this._renderScatterplotLayer(), renderContours && this._renderContourLayer()]}
             />
           )}
           <button style={{position: 'absolute', top: '8px', left: '8px'}} onClick={this._onClick}>
             switch
+          </button>
+          <button style={{position: 'absolute', top: '8px', left: '100px'}} onClick={this._onContourToggle}>
+            Toggle Contours
           </button>
         </div>
       )
