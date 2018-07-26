97531f6adf0e89bd5574896c1abb2a5288eabf57
Ravi Akkenapally <chandu@uber.com>
2018-07-26 13:27:58 -0700

    GPUAggregator : move shader code into seperate files

---
+++ modules/core/src/experimental/utils/gpu-grid-aggregation/aggregate-all-fs.glsl.js
@@ -0,0 +1,35 @@
+// Copyright (c) 2015 - 2018 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+export default `\
+#define SHADER_NAME gpu-aggregation-all-fs
+
+#version 300 es
+precision highp float;
+
+in vec2 vTextureCoord;
+uniform sampler2D uSampler;
+out vec4 fragColor;
+void main(void) {
+  vec4 textureColor = texture(uSampler, vec2(vTextureCoord.s, vTextureCoord.t));
+  // Red: total count, Green: total weight, Alpha: maximum wieght
+  fragColor = vec4(textureColor.r, textureColor.g, 0., textureColor.g);
+}
+`;
