118ab362074cd5442113c309c188315955011109
Ib Green <ib@uber.com>
2018-07-31 12:13:17 -0700

    Minimal integration with mapbox custom layer API

---
+++ test/apps/mapbox-custom-layers/README.md
@@ -0,0 +1,5 @@
+Experimental integration with mapbox-layer API
+
+```js
+yarn start-local
+```
---
+++ test/apps/mapbox-custom-layers/deck-layer.js
@@ -0,0 +1,50 @@
+import {Deck, MapView} from '@deck.gl/core';
+
+export default class DeckLayer {
+  constructor({id = 'deck-layer', layers}) {
+    this.id = id;
+    this.type = 'custom';
+
+    this.deck = null;
+
+    this.layers = layers;
+  }
+
+  _getViewState() {
+    const {lng, lat} = this.map.getCenter();
+    return {
+      longitude: lng,
+      latitude: lat,
+      zoom: this.map.getZoom(),
+      bearing: this.map.getBearing(),
+      pitch: this.map.getPitch()
+    };
+  }
+
+  onAdd(map, gl) {
+    // console.log('onAdd', map, gl);
+
+    this.map = map;
+    this.deck = new Deck({
+      // TODO - is this needed?
+      canvas: 'deck-canvas',
+      width: '100%',
+      height: '100%',
+      controller: false,
+      customRender: true,
+      viewState: this._getViewState(),
+      views: [new MapView({farZmultiplier: .101})]
+    });
+    this.deck._setGLContext(gl);
+    this.deck.setProps({layers: this.layers});
+  }
+
+  render3D(gl, matrix) {
+    const viewState = this._getViewState();
+    // console.log('render3D', viewState, matrix);
+
+    this.deck.setProps({viewState});
+    this.deck._drawLayers();
+    this.map.triggerRepaint();
+  }
+}
