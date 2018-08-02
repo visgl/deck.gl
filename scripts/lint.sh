e0ee50f389f98770723f68dd2478a003ba222a96
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2018-08-02 10:16:58 -0700

    Improve linting script (#2148)

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,11 +1,38 @@
 #!/bin/sh
 # Script to check code styles
-
 set -e
 
-prettier-check "{modules,examples,test}/**/*.js" || echo "Running prettier." && prettier --write "{modules,examples,test}/**/*.js" --loglevel warn
+MODE=$1
 
-eslint modules test examples
+case $MODE in
+  "pre-commit")
+    echo "Running prettier & eslint..."
+
+    # only check changed files
+    set +e
+    FILES=`git diff HEAD --name-only | grep .js$`
+    set -e
+
+    if [ ! -z "${FILES}" ]; then
+      for f in $FILES
+        do
+          prettier --write $f --loglevel warn
+          eslint $f
+      done
+    fi
+
+    # add changes to commit
+    git add .
+    break;;
+
+  *)
+    echo "Checking prettier code styles..."
+    prettier-check "{modules,examples,test}/**/*.js" || echo "Running prettier." && prettier --write "{modules,examples,test}/**/*.js" --loglevel warn
+
+    echo "Running eslint..."
+    eslint modules test examples
+    ;;
+  esac
 
 # check if yarn.lock contains private registery information
 !(grep -q unpm.u yarn.lock) && echo 'Lockfile valid.' || (echo 'Please rebuild yarn file using public npmrc' && false)
