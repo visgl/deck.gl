5c68ad905cf7897e7bf79ab374d76fa78215c1aa
Ib Green <ibgreen@users.noreply.github.com>
2018-08-27 10:18:20 -0700

    Example of minimap in cloned trips layer (#2205)

---
+++ test/apps/minimap/app.js
@@ -0,0 +1,161 @@
+/* global window */
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import {StaticMap} from 'react-map-gl';
+import DeckGL from '@deck.gl/react';
+import {View, MapView} from '@deck.gl/core';
+import {PolygonLayer} from '@deck.gl/layers';
+import {TripsLayer} from '@deck.gl/experimental-layers';
+
+// Set your mapbox token here
+const MAPBOX_TOKEN = process.env.MapboxAccessToken; // eslint-disable-line
+
+// Source data CSV
+const DATA_URL = {
+  BUILDINGS:
+    'https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/trips/buildings.json', // eslint-disable-line
+  TRIPS:
+    'https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/trips/trips.json' // eslint-disable-line
+};
+
+const LIGHT_SETTINGS = {
+  lightsPosition: [-74.05, 40.7, 8000, -73.5, 41, 5000],
+  ambientRatio: 0.05,
+  diffuseRatio: 0.6,
+  specularRatio: 0.8,
+  lightsStrength: [2.0, 0.0, 0.0, 0.0],
+  numberOfLights: 2
+};
+
+export const INITIAL_VIEW_STATE = {
+  longitude: -74,
+  latitude: 40.72,
+  zoom: 13,
+  maxZoom: 16,
+  pitch: 45,
+  bearing: 0
+};
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: 0
+    };
+  }
+
+  componentDidMount() {
+    this._animate();
+  }
+
+  componentWillUnmount() {
+    if (this._animationFrame) {
+      window.cancelAnimationFrame(this._animationFrame);
+    }
+  }
+
+  _animate() {
+    const {
+      loopLength = 1800, // unit corresponds to the timestamp in source data
+      animationSpeed = 30 // unit time per second
+    } = this.props;
+    const timestamp = Date.now() / 1000;
+    const loopTime = loopLength / animationSpeed;
+
+    this.setState({
+      time: ((timestamp % loopTime) / loopTime) * loopLength
+    });
+    this._animationFrame = window.requestAnimationFrame(this._animate.bind(this));
+  }
+
+  _renderLayers() {
+    const {buildings = DATA_URL.BUILDINGS, trips = DATA_URL.TRIPS, trailLength = 180} = this.props;
+
+    return [
+      new TripsLayer({
+        id: 'trips',
+        data: trips,
+        getPath: d => d.segments,
+        getColor: d => (d.vendor === 0 ? [253, 128, 93] : [23, 184, 190]),
+        opacity: 0.3,
+        strokeWidth: 2,
+        trailLength,
+        currentTime: this.state.time
+      }),
+      new PolygonLayer({
+        id: 'buildings',
+        data: buildings,
+        extruded: true,
+        wireframe: false,
+        fp64: true,
+        opacity: 0.5,
+        getPolygon: f => f.polygon,
+        getElevation: f => f.height,
+        getFillColor: [74, 80, 87],
+        lightSettings: LIGHT_SETTINGS
+      })
+    ];
+  }
+
+  render() {
+    const {controller = true, baseMap = true} = this.props;
+
+    return (
+      <DeckGL
+        layers={this._renderLayers()}
+        initialViewState={INITIAL_VIEW_STATE}
+        views={[
+          new MapView({id: 'main'}),
+          new MapView({
+            id: 'minimap',
+
+            // Position on top of main map
+            x: '65%',
+            y: '5%',
+            width: '30%',
+            height: '30%',
+
+            // Minimap is overlaid on top of an existing view, so need to clear the background
+            clear: true,
+
+            viewState: {
+              // Share the view state id of the main map
+              id: 'main',
+
+              // view state overrides: stay zoomed out and looking straight down, north up
+              zoom: 11,
+              pitch: 0,
+              bearing: 0
+            },
+
+            // No controller in the minimap in this first version
+            controller: false
+          })
+        ]}
+        controller={controller}
+      >
+        {baseMap && (
+          <StaticMap
+            reuseMaps
+            mapStyle="mapbox://styles/mapbox/dark-v9"
+            preventStyleDiffing={true}
+            mapboxApiAccessToken={MAPBOX_TOKEN}
+          />
+        )}
+        {baseMap && (
+          <View id="minimap">
+            <StaticMap
+              reuseMaps
+              mapStyle="mapbox://styles/mapbox/light-v9"
+              preventStyleDiffing={true}
+              mapboxApiAccessToken={MAPBOX_TOKEN}
+            />
+          </View>
+        )}
+      </DeckGL>
+    );
+  }
+}
+
+/* global document */
+render(<App />, document.body.appendChild(document.createElement('div')));
