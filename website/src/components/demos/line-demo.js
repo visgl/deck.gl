3028969af468da41a1605c908850e8f8a72f363a
Ravi Akkenapally <chandu@uber.com>
2018-08-29 14:22:20 -0700

    Fix ContourLayer for WebGL2

--- website/src/components/demos/line-demo.js
+++ website/src/components/demos/line-demo.js
@@ -6,7 +6,6 @@ import {App, INITIAL_VIEW_STATE} from 'website-examples/line/app';
 const EMPTY_ARRAY = [];
 
 export default class LineDemo extends Component {
-
   static get data() {
     return [
       {
@@ -21,8 +20,14 @@ export default class LineDemo extends Component {
 
   static get parameters() {
     return {
-      strokeWidth: {displayName: 'Stroke Width',
-        type: 'range', value: 3, step: 0.1, min: 0, max: 10}
+      strokeWidth: {
+        displayName: 'Stroke Width',
+        type: 'range',
+        value: 3,
+        step: 0.1,
+        min: 0,
+        max: 10
+      }
     };
   }
 
@@ -40,12 +45,16 @@ export default class LineDemo extends Component {
         <h3>Flights To And From London Heathrow Airport</h3>
         <p>Flight paths in a 6-hour window</p>
         <p>From 08:32:43 GMT to 14:32:43 GMT on March 28th, 2017</p>
-        <p>Flight path data source:
-          <a href="https://opensky-network.org/"> The OpenSky Network</a><br />
+        <p>
+          Flight path data source:
+          <a href="https://opensky-network.org/"> The OpenSky Network</a>
+          <br />
           Airport location data source:
           <a href="http://www.naturalearthdata.com/"> Natural Earth</a>
         </p>
-        <div className="stat">No. of Line Segments<b>{ readableInteger(meta.count || 0) }</b></div>
+        <div className="stat">
+          No. of Line Segments<b>{readableInteger(meta.count || 0)}</b>
+        </div>
       </div>
     );
   }
@@ -56,9 +65,10 @@ export default class LineDemo extends Component {
     return (
       <App
         {...otherProps}
-        flightPaths={data && data[0] || EMPTY_ARRAY}
-        airports={data && data[1] || EMPTY_ARRAY}
-        strokeWidth={params.strokeWidth.value} />
+        flightPaths={(data && data[0]) || EMPTY_ARRAY}
+        airports={(data && data[1]) || EMPTY_ARRAY}
+        getStrokeWidth={params.strokeWidth.value}
+      />
     );
   }
 }
