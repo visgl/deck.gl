c4f491be8080a4e3b6b4797d7ccafec607302aa6
Xiaoji Chen <xiaoji@uber.com>
2018-09-19 14:16:33 -0700

    address comments

--- modules/mapbox-layers/src/deck-layer.js
+++ modules/mapbox-layers/src/deck-layer.js
@@ -1,23 +1,60 @@
 import {Deck} from '@deck.gl/core';
 
-const deckInstances = new Map();
-
 function updateDeck(deck) {
   const layers = [];
-  deck.props._mapboxLayers.forEach(deckLayer => {
-    layers.push(deckLayer.layers);
+  deck.props.userData.mapboxLayers.forEach(deckLayer => {
+    const LayerType = deckLayer.props.type;
+    const layer = new LayerType(deckLayer.props);
+    layers.push(layer);
   });
   deck.setProps({layers});
 }
 
 export default class DeckLayer {
-  constructor({id = 'deck-layer', layers}) {
-    this.id = id;
+  constructor(props) {
+    if (!props.id) {
+      throw new Error('Layer must have an unique id');
+    }
+
+    this.id = props.id;
     this.type = 'custom';
     this.renderingMode = '3d';
     this.deck = null;
-    this.layers = layers;
+    this.props = props;
   }
+
+  onAdd(map, gl) {
+    this.map = map;
+    this.deck = this._getDeckInstance(map, gl);
+    updateDeck(this.deck);
+  }
+
+  onRemove() {
+    const {mapboxLayers} = this.deck.props.userData;
+    mapboxLayers.delete(this);
+    updateDeck(this.deck);
+  }
+
+  setProps(props) {
+    // id cannot be changed
+    Object.assign(this.props, props, {id: this.id});
+    updateDeck(this.deck);
+    this.map.triggerRepaint();
+  }
+
+  render(gl, matrix) {
+    const viewState = this._getViewState();
+
+    this.deck.setProps({
+      viewState,
+      layerFilter: ({layer}) => this.id === layer.id
+    });
+    this.deck._drawLayers();
+
+    // this.map.triggerRepaint();
+  }
+
+  /* Private API */
 
   _getViewState() {
     const {map, deck} = this;
@@ -33,9 +70,9 @@ export default class DeckLayer {
     };
   }
 
-  _getDeckInstance(gl) {
+  _getDeckInstance(map, gl) {
     // Only create one deck instance per context
-    let deck = deckInstances.get(gl);
+    let deck = map.__deck;
 
     if (!deck) {
       deck = new Deck({
@@ -45,54 +82,17 @@ export default class DeckLayer {
         height: '100%',
         controller: false,
         _customRender: true,
-        _mapboxLayers: new Set(),
+        userData: {
+          mapboxLayers: new Set()
+        },
         viewState: this._getViewState()
       });
       deck._setGLContext(gl);
-      deckInstances.set(gl, deck);
+      map.__deck = deck;
     }
 
-    deck.props._mapboxLayers.add(this);
+    deck.props.userData.mapboxLayers.add(this);
 
     return deck;
-  }
-
-  onAdd(map, gl) {
-    this.map = map;
-    this.deck = this._getDeckInstance(gl);
-    updateDeck(this.deck);
-  }
-
-  onRemove() {
-    const mapboxLayers = this.deck.props._mapboxLayers;
-    mapboxLayers.delete(this);
-
-    if (mapboxLayers.size === 0) {
-      // this deck instance is now empty
-      deckInstances.delete(this.map);
-      this.deck.finalize();
-    } else {
-      updateDeck(this.deck);
-    }
-  }
-
-  setProps(props) {
-    if ('layers' in props) {
-      this.layers = props.layers;
-      updateDeck(this.deck);
-      this.map.triggerRepaint();
-    }
-  }
-
-  render(gl, matrix) {
-    const viewState = this._getViewState();
-
-    this.deck.setProps({
-      viewState,
-      layerFilter: ({layer}) => this.layers.includes(layer)
-    });
-    this.deck._drawLayers();
-
-    // this.map.triggerRepaint();
   }
 }
