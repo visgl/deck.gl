63f57849aacdf385c167f551bb15304c14bde20a
Xiaoji Chen <xiaoji@uber.com>
2018-09-19 18:38:33 -0700

    add tests for view.equals

--- test/modules/core/views/view.spec.js
+++ test/modules/core/views/view.spec.js
@@ -1,7 +1,44 @@
 import test from 'tape-catch';
-import {View} from 'deck.gl';
+import {View, MapView} from 'deck.gl';
 
 test('View#imports', t => {
   t.ok(View, 'View import ok');
+  t.end();
+});
+
+test('View#equals', t => {
+  const mapView1 = new MapView({
+    id: 'default-view',
+    latitude: 0,
+    longitude: 0,
+    zoom: 11,
+    position: [0, 0]
+  });
+  const mapView2 = new MapView({
+    id: 'default-view',
+    latitude: 0,
+    longitude: 0,
+    zoom: 11,
+    position: [0, 0]
+  });
+  const mapView3 = new MapView({
+    id: 'default-view',
+    latitude: 0,
+    longitude: 0,
+    zoom: 11,
+    position: [0, 1]
+  });
+  const mapView4 = new View({
+    id: 'default-view',
+    latitude: 0,
+    longitude: 0,
+    zoom: 11,
+    position: [0, 0]
+  });
+
+  t.ok(mapView1.equals(mapView2), 'Identical view props');
+  t.notOk(mapView1.equals(mapView3), 'Different view props');
+  t.notOk(mapView1.equals(mapView4), 'Different type');
+
   t.end();
 });
