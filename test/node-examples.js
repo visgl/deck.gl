07d251d0755edff263ffd36f91ef401106b79670
Xiaokai Li <xiaokai@uber.com>
2018-10-08 21:15:23 -0700

    modified functions that handle file directories

--- test/node-examples.js
+++ test/node-examples.js
@@ -41,7 +41,7 @@ function printResult(diffRatio, threshold) {
     : console.log('\x1b[31m%s\x1b[0m', 'Rendering test failed!');
 }
 
-async function validateWithWaitingTime(child, folder, subfolder, waitingTime, threshold) {
+async function validateWithWaitingTime(child, folder, waitingTime, threshold) {
   const browser = await puppeteer.launch({
     headless: false,
     args: [`--window-size=${1000},${800}`]
@@ -57,7 +57,7 @@ async function validateWithWaitingTime(child, folder, subfolder, waitingTime, th
     path.resolve(
       LIB_DIR,
       'test/render/golden-images/examples/',
-      `${subfolder ? `${folder}_${subfolder}` : folder}.png`
+      `${folder.replace(/\//g, '_')}.png`
     )
   );
   const goldImage = PNG.sync.read(goldImageData);
@@ -122,10 +122,10 @@ function changeFolder(folder) {
   process.chdir(path.resolve(exampleDir, folder));
 }
 
-async function runTestExample(folder, subfolder = undefined) {
-  changeFolder(subfolder ? `${folder}/${subfolder}` : folder);
+async function runTestExample(folder) {
+  changeFolder(folder);
   const child = await yarnAndLaunchWebpack();
-  const valid = await validateWithWaitingTime(child, folder, subfolder, 5000, 0.01);
+  const valid = await validateWithWaitingTime(child, folder, 5000, 0.01);
   if (!valid) {
     process.exit(1); //eslint-disable-line
   }
@@ -135,25 +135,25 @@ async function runTestExample(folder, subfolder = undefined) {
   checkMapboxToken();
 
   exampleDir = EXAMPLES_DIR;
-  await runTestExample('experimental', 'bezier');
-  await runTestExample('experimental', 'pure-js-app');
+  await runTestExample('experimental/bezier');
+  await runTestExample('experimental/json-pure-js');
 
-  await runTestExample('get-started', 'pure-js');
-  await runTestExample('get-started', 'pure-js-without-map');
-  await runTestExample('get-started', 'react-webpack-2');
-  await runTestExample('get-started', 'react-without-map');
+  await runTestExample('get-started/pure-js');
+  await runTestExample('get-started/pure-js-without-map');
+  await runTestExample('get-started/react-webpack-2');
+  await runTestExample('get-started/react-without-map');
 
   await runTestExample('layer-browser');
 
-  await runTestExample('website', '3d-heatmap');
-  await runTestExample('website', 'arc');
-  await runTestExample('website', 'brushing');
-  await runTestExample('website', 'geojson');
-  await runTestExample('website', 'highway');
-  await runTestExample('website', 'icon');
-  await runTestExample('website', 'line');
-  await runTestExample('website', 'plot');
-  await runTestExample('website', 'scatterplot');
-  await runTestExample('website', 'screen-grid');
-  await runTestExample('website', 'tagmap');
+  await runTestExample('website/3d-heatmap');
+  await runTestExample('website/arc');
+  await runTestExample('website/brushing');
+  await runTestExample('website/geojson');
+  await runTestExample('website/highway');
+  await runTestExample('website/icon');
+  await runTestExample('website/line');
+  await runTestExample('website/plot');
+  await runTestExample('website/scatterplot');
+  await runTestExample('website/screen-grid');
+  await runTestExample('website/tagmap');
 })();
