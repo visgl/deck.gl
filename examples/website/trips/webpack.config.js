8d350b047d1fd166766b5d86eb01be6b7c9c92ae
Xiaoji Chen <xiaoji@uber.com>
2018-10-13 23:17:05 -0700

    bump example dependencies

--- examples/website/trips/webpack.config.js
+++ examples/website/trips/webpack.config.js
@@ -32,13 +32,6 @@ const CONFIG = {
     ]
   },
 
-  resolve: {
-    alias: {
-      // From mapbox-gl-js README. Required for non-browserify bundlers (e.g. webpack):
-      'mapbox-gl$': resolve('./node_modules/mapbox-gl/dist/mapbox-gl.js')
-    }
-  },
-
   // Optional: Enables reading mapbox token from environment variable
   plugins: [
     new HtmlWebpackPlugin({template: '../template.html'}),
