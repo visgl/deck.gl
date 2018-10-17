477bbf76bd4fb6d21bccadaf2395c0e184bffb48
Cynthia Zhang <cynthia.zhang@uber.com>
2018-10-16 17:40:04 -0700

    Add JSDoc to TileCache

--- modules/experimental-layers/src/tile-layer/utils/tile-cache.js
+++ modules/experimental-layers/src/tile-layer/utils/tile-cache.js
@@ -2,22 +2,33 @@ import Tile from './tile';
 import {getTileIndices} from './viewport-util';
 
 /**
- * Manages loading and purging of tiles data
+ * Manages loading and purging of tiles data. This class caches recently visited tiles
+ * and only create new tiles if they are present.
  */
 
 export default class TileCache {
-  // TODO: Instead of hardcode size, we should calculate how much memory left
+  /**
+   * Takes in a function that returns tile data, and a cache size (default to 10).
+   */
   constructor({getTileData, size = 10}) {
+    // TODO: Instead of hardcode size, we should calculate how much memory left
     this.getTileData = getTileData;
     this.size = size;
-
     this.cache = [];
   }
 
+  /**
+   * Clear the current cache
+   */
   finalize() {
     this.cache = null;
   }
 
+  /**
+   * Update the cache with the given viewport and triggers callback onUpdate.
+   * @param {*} viewport
+   * @param {*} onUpdate
+   */
   update(viewport, onUpdate) {
     const {cache, size, getTileData} = this;
     const tiles = getTileIndices(viewport).map(({x, y, z}) => {
@@ -46,10 +57,20 @@ export default class TileCache {
     onUpdate(tiles);
   }
 
+  /**
+   * Return whether a tile with x, y, z exists in the cache.
+   * @param {*} x
+   * @param {*} y
+   * @param {*} z
+   */
   _find(x, y, z) {
     return this.cache.find(t => t.x === x && t.y === y && t.z === z);
   }
 
+  /**
+   * Add tile to cache
+   * @param {*} tile
+   */
   _push(tile) {
     this.cache.push(tile);
   }
