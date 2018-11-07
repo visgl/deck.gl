3085a5b27f866a0603a41cc5097dc08ac51f0a3c
Goszczular <goszczynskip@gmail.com>
2018-11-07 21:26:36 +0100

    fix userdata is not present on initial render (#2418)

--- modules/core/src/lib/layer-manager.js
+++ modules/core/src/lib/layer-manager.js
@@ -160,6 +160,15 @@ export default class LayerManager {
    */
   /* eslint-disable complexity, max-statements */
   setProps(props) {
+    // A way for apps to add data to context that can be accessed in layers
+    if ('userData' in props) {
+      this.context.userData = props.userData;
+    }
+
+    if ('useDevicePixels' in props) {
+      this.context.useDevicePixels = props.useDevicePixels;
+    }
+
     // TODO - For now we set layers before viewports to preserve changeFlags
     if ('layers' in props) {
       this.setLayers(props.layers);
@@ -177,15 +186,6 @@ export default class LayerManager {
         this.drawPickingColors = props.drawPickingColors;
         this.setNeedsRedraw('drawPickingColors changed');
       }
-    }
-
-    // A way for apps to add data to context that can be accessed in layers
-    if ('userData' in props) {
-      this.context.userData = props.userData;
-    }
-
-    if ('useDevicePixels' in props) {
-      this.context.useDevicePixels = props.useDevicePixels;
     }
   }
   /* eslint-enable complexity, max-statements */
