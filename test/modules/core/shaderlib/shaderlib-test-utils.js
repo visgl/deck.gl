3867601c50ac76a1917f145e512d45eedd7c280a
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2018-12-17 11:25:06 -0800

    Fix fp64 shader tests (#2506)

--- test/modules/core/shaderlib/shaderlib-test-utils.js
+++ test/modules/core/shaderlib/shaderlib-test-utils.js
@@ -1,28 +1,12 @@
 // TODO - move to @luma.gl/debug ?
 import Compiler from 'glsl-transpiler';
 
-const normalizeSource = source =>
-  source
-    // prepr (GLSL preprocessor) does not like #define without value
-    .replace(/^(#define \w+) *$/gm, ($0, $1) => `${$1} 1`)
-    // remove comments due to glsl-transpiler issue #39
-    .replace(/(\/\*(.|\s)*?\*\/)|(\/\/.*)/g, '')
-    // remove unecessary exponential notation due to glsl-transpiler issue #41
-    .replace(/e\+00/g, '')
-    // work around glsl-transpiler issue #40
-    .replace(
-      /(\w+\[\d\])\.x = (\S+);\s*(\w+\[\d\])\.y = (\S+);/g,
-      ($0, $1, $2, $3, $4) => `${$1} = vec2(${$3}, ${$4});`
-    );
-
 const compileVS = Compiler({
   uniform: name => `uniforms.${name}`
 });
 
 // @returns JavaScript function of the transpiled shader
 export function compileVertexShader(source) {
-  source = normalizeSource(source);
-
   const compiledSource = compileVS(source);
   const {compiler} = compileVS;
   const {functions} = compiler;
