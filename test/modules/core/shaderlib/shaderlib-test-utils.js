702314e55c80efa99afe1d2cb464d6bc7a082be0
Xiaoji Chen <xiaoji@uber.com>
2018-12-17 10:11:12 -0800

    upgrade to 1.8.3 and remove hacks

--- test/modules/core/shaderlib/shaderlib-test-utils.js
+++ test/modules/core/shaderlib/shaderlib-test-utils.js
@@ -1,21 +1,12 @@
 // TODO - move to @luma.gl/debug ?
 import Compiler from 'glsl-transpiler';
 
-const normalizeSource = source =>
-  source
-    // prepr (GLSL preprocessor) does not like #define without value
-    .replace(/^(#define \w+) *$/gm, ($0, $1) => `${$1} 1`)
-    // replace negate due to glsl-transpiler issue #44
-    .replace(/(= |\()-([a-z])/g, ($0, $1, $2) => `${$1}-1.0 * ${$2}`);
-
 const compileVS = Compiler({
   uniform: name => `uniforms.${name}`
 });
 
 // @returns JavaScript function of the transpiled shader
 export function compileVertexShader(source) {
-  source = normalizeSource(source);
-
   const compiledSource = compileVS(source);
   const {compiler} = compileVS;
   const {functions} = compiler;
