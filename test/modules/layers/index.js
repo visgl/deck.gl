206d36b1057f7faf8a823e6bcf70cbb2e4e870c7
1chandu <chandu@uber.com>
2018-12-17 21:03:45 -0800

    UnitTests: for path layer shaders (#2503)
    * UnitTests: for path layer shaders
    * Bump luma.gl to 7.0.0-alpha.2

--- test/modules/layers/index.js
+++ test/modules/layers/index.js
@@ -31,3 +31,4 @@ import './hexagon-layer.spec';
 import './hexagon-aggregator.spec';
 import './contour-layer/marching-squares.spec';
 import './contour-layer/contour-layer.spec';
+import './path-layer/path-layer-vertex.spec';
