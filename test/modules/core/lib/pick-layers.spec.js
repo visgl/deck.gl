6e6471b7bf5c1348736787ea1d19a75a9086f12c
XiaokaiUber <42359372+XiaokaiUber@users.noreply.github.com>
2018-12-20 12:32:51 -0800

    add picking test (#2514)

---
+++ test/modules/core/lib/pick-layers.spec.js
@@ -0,0 +1,424 @@
+// Copyright (c) 2015 - 2017 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+/* eslint-disable dot-notation, max-statements, no-unused-vars */
+
+import test from 'tape-catch';
+import {MapView, ScatterplotLayer, Deck, PolygonLayer, PathLayer} from 'deck.gl';
+import * as DATA from '../../../../examples/layer-browser/src/data-samples';
+
+const VIEW_STATE = {
+  latitude: 37.751537058389985,
+  longitude: -122.42694203247012,
+  zoom: 11.5,
+  pitch: 0,
+  bearing: 0
+};
+
+const TEST_CASES = [
+  {
+    id: 'scatterplotLayer',
+    props: {
+      width: 500,
+      height: 550,
+      layers: [
+        new ScatterplotLayer({
+          data: DATA.points,
+          getPosition: d => d.COORDINATES,
+          getRadius: d => d.SPACES,
+          pickable: true,
+          radiusScale: 30,
+          radiusMinPixels: 1,
+          radiusMaxPixels: 30
+        })
+      ],
+      views: [new MapView()],
+      viewState: VIEW_STATE,
+      useDevicePixels: false
+    },
+    pickingMethods: {
+      pickObject: [
+        {
+          parameters: {
+            x: 60,
+            y: 160
+          },
+          results: {
+            count: 1
+          }
+        },
+        {
+          parameters: {
+            x: 90,
+            y: 350
+          },
+          results: {
+            count: 0
+          }
+        }
+      ],
+      pickObjects: [
+        {
+          parameters: {
+            x: 300,
+            y: 300,
+            width: 100,
+            height: 100
+          },
+          results: {
+            count: 33
+          }
+        },
+        {
+          parameters: {
+            x: 50,
+            y: 50,
+            width: 10,
+            height: 10
+          },
+          results: {
+            count: 0
+          }
+        }
+      ],
+      pickMultipleObjects: [
+        {
+          parameters: {
+            x: 250,
+            y: 273
+          },
+          results: {
+            count: 2
+          }
+        },
+        {
+          parameters: {
+            x: 300,
+            y: 300
+          },
+          results: {
+            count: 0
+          }
+        }
+      ]
+    }
+  },
+  {
+    id: 'polygonLayer',
+    props: {
+      width: 500,
+      height: 550,
+      layers: [
+        new PolygonLayer({
+          data: DATA.polygons,
+          getPolygon: f => f,
+          pickable: true
+        })
+      ],
+      views: [new MapView()],
+      viewState: VIEW_STATE,
+      useDevicePixels: false
+    },
+    pickingMethods: {
+      pickObject: [
+        {
+          parameters: {
+            x: 60,
+            y: 160
+          },
+          results: {
+            count: 1
+          }
+        },
+        {
+          parameters: {
+            x: 10,
+            y: 10
+          },
+          results: {
+            count: 0
+          }
+        }
+      ],
+      pickObjects: [
+        {
+          parameters: {
+            x: 300,
+            y: 300,
+            width: 100,
+            height: 100
+          },
+          results: {
+            count: 3
+          }
+        },
+        {
+          parameters: {
+            x: 10,
+            y: 10,
+            width: 10,
+            height: 10
+          },
+          results: {
+            count: 0
+          }
+        }
+      ],
+      pickMultipleObjects: [
+        {
+          parameters: {
+            x: 250,
+            y: 273
+          },
+          results: {
+            count: 1
+          }
+        },
+        {
+          parameters: {
+            x: 10,
+            y: 10
+          },
+          results: {
+            count: 0
+          }
+        }
+      ]
+    }
+  },
+  {
+    id: 'pathLayer',
+    props: {
+      width: 500,
+      height: 550,
+      layers: [
+        new PathLayer({
+          data: DATA.zigzag,
+          getPath: f => f.path,
+          getWidth: 10,
+          widthMinPixels: 1,
+          pickable: true
+        })
+      ],
+      views: [new MapView()],
+      viewState: VIEW_STATE,
+      useDevicePixels: false
+    },
+    pickingMethods: {
+      pickObject: [
+        {
+          parameters: {
+            x: 260,
+            y: 300
+          },
+          results: {
+            count: 1
+          }
+        },
+        {
+          parameters: {
+            x: 10,
+            y: 10
+          },
+          results: {
+            count: 0
+          }
+        }
+      ],
+      pickObjects: [
+        {
+          parameters: {
+            x: 0,
+            y: 0,
+            width: 400,
+            height: 400
+          },
+          results: {
+            count: 3
+          }
+        },
+        {
+          parameters: {
+            x: 10,
+            y: 10,
+            width: 10,
+            height: 10
+          },
+          results: {
+            count: 0
+          }
+        }
+      ],
+      pickMultipleObjects: [
+        // {
+        //   parameters: {
+        //     x: 260,
+        //     y: 300
+        //   },
+        //   results: {
+        //     count: 1
+        //   }
+        // },
+        {
+          parameters: {
+            x: 10,
+            y: 10
+          },
+          results: {
+            count: 0
+          }
+        }
+      ]
+    }
+  },
+  {
+    id: 'multiLayers',
+    props: {
+      width: 500,
+      height: 550,
+      layers: [
+        new ScatterplotLayer({
+          data: DATA.points,
+          getPosition: d => d.COORDINATES,
+          getRadius: d => d.SPACES,
+          pickable: true,
+          radiusScale: 30,
+          radiusMinPixels: 1,
+          radiusMaxPixels: 30
+        }),
+        new PolygonLayer({
+          data: DATA.polygons,
+          getPolygon: f => f,
+          pickable: true
+        }),
+        new PathLayer({
+          data: DATA.zigzag,
+          getPath: f => f.path,
+          getWidth: 10,
+          widthMinPixels: 1,
+          pickable: true
+        })
+      ],
+      views: [new MapView()],
+      viewState: VIEW_STATE,
+      useDevicePixels: false
+    },
+    pickingMethods: {
+      pickObject: [
+        {
+          parameters: {
+            x: 260,
+            y: 300
+          },
+          results: {
+            count: 1
+          }
+        },
+        {
+          parameters: {
+            x: 10,
+            y: 10
+          },
+          results: {
+            count: 0
+          }
+        }
+      ],
+      pickObjects: [
+        {
+          parameters: {
+            x: 0,
+            y: 0,
+            width: 400,
+            height: 400
+          },
+          results: {
+            count: 32
+          }
+        },
+        {
+          parameters: {
+            x: 10,
+            y: 10,
+            width: 10,
+            height: 10
+          },
+          results: {
+            count: 0
+          }
+        }
+      ],
+      pickMultipleObjects: [
+        // {
+        //   parameters: {
+        //     x: 260,
+        //     y: 300
+        //   },
+        //   results: {
+        //     count: 1
+        //   }
+        // },
+        {
+          parameters: {
+            x: 10,
+            y: 10
+          },
+          results: {
+            count: 0
+          }
+        }
+      ]
+    }
+  }
+];
+
+test(`pickingTest`, t => {
+  const deck = new Deck();
+  t.ok(deck, 'Deck should be constructed');
+
+  const len = TEST_CASES.length;
+  let index = 0;
+  let testCase;
+
+  function runTests() {
+    testCase = TEST_CASES[index++];
+    const pickingMethods = testCase.pickingMethods;
+
+    let pickInfos;
+    for (const pickingMethod in pickingMethods) {
+      for (const pickingCase of pickingMethods[pickingMethod]) {
+        pickInfos = deck[pickingMethod](pickingCase.parameters);
+        t.equal(
+          !pickInfos ? 0 : !Array.isArray(pickInfos) ? 1 : pickInfos.length,
+          pickingCase.results.count,
+          `${testCase.id}: ${pickingMethod} should find expected number of objects`
+        );
+      }
+    }
+    if (index === len) {
+      deck.finalize();
+      t.end();
+    } else {
+      deck.setProps(TEST_CASES[index].props);
+    }
+  }
+
+  deck.setProps(Object.assign({}, TEST_CASES[0].props, {onAfterRender: runTests}));
+});
