3424b1464c6d9e85ad8ffc97897e316864224067
1chandu <chandu@uber.com>
2019-01-08 13:28:09 -0800

    GPUAggregator: fixes for Min and Mean aggregation (#2554)
    * GPUAggregator: fixes for Min and Mean aggregation

--- modules/core/src/experimental/utils/gpu-grid-aggregation/transform-mean-vs.glsl.js
+++ modules/core/src/experimental/utils/gpu-grid-aggregation/transform-mean-vs.glsl.js
@@ -26,7 +26,7 @@ varying vec4 meanValues;
 void main()
 {
   // TODO: Use 64-bit division ?? not needed given this is aggregation ??
-  bool isCellValid = bool(aggregationValues.w);
+  bool isCellValid = bool(aggregationValues.w > 0.);
   // aggregationValues:  XYZ contain aggregated values, W contains count
   meanValues.xyz = isCellValid ? aggregationValues.xyz/aggregationValues.w : vec3(0, 0, 0);
   meanValues.w = aggregationValues.w;
