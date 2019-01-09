5ddb94c62669a4063c1271fc0a9dc58a85178748
1chandu <chandu@uber.com>
2019-01-08 16:30:07 -0800

    ScreenGridLayer: add support Min/Max/Mean aggregation (#2560)

--- modules/layers/src/screen-grid-layer/screen-grid-layer-fragment.glsl.js
+++ modules/layers/src/screen-grid-layer/screen-grid-layer-fragment.glsl.js
@@ -26,9 +26,13 @@ export default `\
 precision highp float;
 
 in vec4 vColor;
+in float vSampleCount;
 out vec4 fragColor;
 
 void main(void) {
+  if (vSampleCount <= 0.0) {
+    discard;
+  }
   fragColor = vColor;
 
   fragColor = picking_filterColor(fragColor);
