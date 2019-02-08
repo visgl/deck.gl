8d11f3fd9f4eb52b2761dbf5841ba1aeafa652b8
Jian Huang <jianhuang01@hotmail.com>
2019-02-08 15:50:25 -0800

    Refactor rendering workflow (1/3) (#2656)
    * new layer pass classes
    
    * code review changes
    
    * code review changes
    
    * rename pass classes
    
    * code review changes

---
+++ modules/core/src/lib/deck-renderer.js
@@ -0,0 +1,70 @@
+import log from '../utils/log';
+import DrawLayersPass from '../passes/draw-layers-pass';
+
+const LOG_PRIORITY_DRAW = 2;
+
+export default class DeckRenderer {
+  constructor(props) {
+    const {gl, layerManager, effectManager} = props;
+
+    this.gl = gl;
+    this.layerManager = layerManager;
+    this.effectManager = effectManager;
+    this.drawLayersPass = new DrawLayersPass(gl);
+    this.renderCount = 0;
+  }
+
+  renderLayers({
+    viewports,
+    views,
+    redrawReason = 'unknown reason',
+    customRender = false,
+    pass,
+    stats
+  }) {
+    // TODO: Remove LayerManager reference after clean up LayerManager & pick-layers.js
+    const {drawPickingColors, layers, layerFilter} = this.layerManager;
+    const {useDevicePixels} = this.layerManager.context;
+
+    this.drawLayersPass.setProps({
+      layers,
+      viewports,
+      views,
+      onViewportActive: this.layerManager.activateViewport,
+      useDevicePixels,
+      drawPickingColors,
+      layerFilter,
+      redrawReason,
+      customRender
+    });
+
+    const renderStats = this.drawLayersPass.render();
+    this.renderCount++;
+
+    if (log.priority >= LOG_PRIORITY_DRAW) {
+      renderStats.forEach(status => {
+        this.logRenderStats({status, pass, redrawReason, stats});
+      });
+    }
+  }
+
+  logRenderStats({renderStats, pass, redrawReason, stats}) {
+    const {totalCount, visibleCount, compositeCount, pickableCount} = renderStats;
+    const primitiveCount = totalCount - compositeCount;
+    const hiddenCount = primitiveCount - visibleCount;
+
+    let message = '';
+    message += `RENDER #${this.renderCount} \
+${visibleCount} (of ${totalCount} layers) to ${pass} because ${redrawReason} `;
+    if (log.priority > LOG_PRIORITY_DRAW) {
+      message += `\
+(${hiddenCount} hidden, ${compositeCount} composite ${pickableCount} pickable)`;
+    }
+
+    log.log(LOG_PRIORITY_DRAW, message)();
+
+    if (stats) {
+      stats.increment('redraw layers', visibleCount);
+    }
+  }
+}
