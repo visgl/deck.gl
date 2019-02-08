6a460578987d095f3403ee70d1cc11898b8279f5
Jian Huang <jianh@uber.com>
2019-02-08 14:53:50 -0800

    code review changes

--- modules/core/src/effects/effect-preparator.js
+++ modules/core/src/effects/effect-preparator.js
@@ -1,10 +1,14 @@
+// EffectPreparator prepares effects to generate effect props by feeding layers
+// or other resources, and sharing common rendering passes like depth map between them
+
 export default class EffectPreparator {
   constructor(props) {}
 
   prepareEffects({effects}) {
     const effectProps = {};
-    effects.forEach(effect => {
-      Object.assign(effectProps, effect.prepare());
-    });
+
+    for (const i in effects) {
+      Object.assign(effectProps, effects[i].prepare());
+    }
   }
 }
