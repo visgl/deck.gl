ae21c2aaac36fa8b27cc69a9159bd3223aa12726
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2019-03-06 11:20:05 -0800

    Build standalone bundle for each submodule (#2760)

---
+++ scripts/bundle.config.js
@@ -0,0 +1,85 @@
+/* global process */
+const {resolve} = require('path');
+const webpack = require('webpack');
+
+const getAliases = require('../aliases');
+
+const PACKAGE_ROOT = resolve('.');
+const PACKAGE_INFO = require(resolve(PACKAGE_ROOT, 'package.json'));
+
+/**
+ * peerDependencies are excluded using `externals`
+ * https://webpack.js.org/configuration/externals/
+ * e.g. @deck.gl/core is not bundled with @deck.gl/geo-layers
+ */
+function getExternals(packageInfo) {
+  let externals = {};
+  const {peerDependencies = {}} = packageInfo;
+
+  for (const depName in peerDependencies) {
+    if (depName.startsWith('@deck.gl')) {
+      // Instead of bundling the dependency, import from the global `deck` object
+      externals[depName] = 'deck';
+    }
+  }
+
+  if (externals['@deck.gl/core']) {
+    // Do not bundle luma.gl if `core` is peer dependency
+    externals = [
+      externals,
+      (context, request, callback) => {
+        if (/^@?luma\.gl/.test(request)) {
+          return callback(null, 'luma');
+        }
+        return callback();
+      }
+    ];
+  }
+
+  return externals;
+}
+
+const config = {
+  mode: 'production',
+
+  entry: {
+    main: resolve('./bundle')
+  },
+
+  output: {
+    libraryTarget: 'umd',
+    path: PACKAGE_ROOT,
+    filename: 'dist.min.js'
+  },
+
+  resolve: {
+    alias: Object.assign({}, getAliases('src'))
+  },
+
+  module: {
+    rules: [
+      {
+        // Compile ES2015 using babel
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: /src/
+      }
+    ]
+  },
+
+  externals: getExternals(PACKAGE_INFO),
+
+  plugins: [
+    // This is used to define the __VERSION__ constant in core/lib/init.js
+    // babel-plugin-version-inline uses the package version from the working directory
+    // Therefore we need to manually import the correct version from the core
+    // This is called in prepublishOnly, after lerna bumps the package versions
+    new webpack.DefinePlugin({
+      __VERSION__: JSON.stringify(PACKAGE_INFO.version)
+    })
+  ],
+
+  devtool: false
+};
+
+module.exports = config;
