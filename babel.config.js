33ecbbbc45808dc66ffef63976951542d366e4e7
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2019-03-08 13:34:03 -0800

    fix start-local commands (#2769)

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,14 @@
 const getBabelConfig = require('ocular-dev-tools/config/babel.config');
 
 module.exports = api => {
-  return getBabelConfig(api, {
-    plugins: [
-      'version-inline',
-      [
-        'remove-glsl-comments',
-        {
-          patterns: ['**/*.glsl.js']
-        }
-      ]
-    ]
-  });
+  const config = getBabelConfig(api);
+
+  config.plugins.push('version-inline', [
+    'remove-glsl-comments',
+    {
+      patterns: ['**/*.glsl.js']
+    }
+  ]);
+
+  return config;
 };
