971945fe3f1caa2f796b04345e80048732fba30a
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2019-03-07 16:43:06 -0800

    Replace dev scripts with ocular-dev-tools (#2764)
    For #2736

---
+++ ocular-dev-tools.config.js
@@ -0,0 +1,20 @@
+const {resolve} = require('path');
+
+module.exports = {
+  lint: {
+    paths: ['modules', 'examples', 'test'],
+    extensions: ['js']
+  },
+
+  aliases: {
+    'deck.gl/test': resolve(__dirname, './test')
+  },
+
+  entry: {
+    test: 'test/modules/index.js',
+    'test-browser': 'test/browser.js',
+    bench: 'test/bench/index.js',
+    'bench-browser': 'test/bench/browser.js',
+    size: 'test/size/import-nothing.js'
+  }
+};
