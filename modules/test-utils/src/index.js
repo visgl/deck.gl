79b054cf5b79c23d40b61210bdbf3944043f59b3
Xiaoji Chen <xiaoji@uber.com>
2019-03-08 23:17:25 -0800

    add tests for core layers

--- modules/test-utils/src/index.js
+++ modules/test-utils/src/index.js
@@ -2,10 +2,8 @@ export {toLowPrecision} from './utils/precision';
 export {default as gl} from './utils/setup-gl';
 
 // Utilities for update tests (lifecycle tests)
-export {testLayer} from './lifecycle-test';
-
-// Deprecated utilities for update tests (lifecycle tests)
-export {testInitializeLayer, testUpdateLayer, testDrawLayer} from './lifecycle-test-deprecated';
+export {testLayer, testInitializeLayer, testUpdateLayer, testDrawLayer} from './lifecycle-test';
+export {generateLayerTests} from './generate-layer-tests';
 
 // Basic utility for rendering multiple scenes (could go into "deck.gl/core")
 export {default as TestRunner} from './test-runner';
