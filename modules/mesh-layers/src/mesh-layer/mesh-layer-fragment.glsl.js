9a8a72239d592f3ee65146d363928294eddaef18
Georgios Karnas <georgios@uber.com>
2019-03-11 16:19:08 -0700

    Refactor mesh-layers model matrix (#2778)

--- modules/mesh-layers/src/mesh-layer/mesh-layer-fragment.glsl.js
+++ modules/mesh-layers/src/mesh-layer/mesh-layer-fragment.glsl.js
@@ -9,11 +9,10 @@ uniform vec4 color;
 
 varying vec2 vTexCoord;
 varying vec4 vColor;
-varying float vLightWeight;
 
 void main(void) {
   vec4 color = hasTexture ? texture2D(sampler, vTexCoord) : vColor / 255.;
-  gl_FragColor = vec4(color.rgb * vLightWeight, color.a);
+  gl_FragColor = vec4(color.rgb, color.a);
 
   // use highlight color if this fragment belongs to the selected object.
   gl_FragColor = picking_filterHighlightColor(gl_FragColor);
