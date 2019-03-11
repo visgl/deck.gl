279ebf0ce5c17291d6cec5f9e28a38025932485d
Georgios Karnas <georgios@uber.com>
2019-03-11 12:42:33 -0700

    Add mat4 instanceModelMatrix to ScenegraphLayer (#2767)

--- modules/mesh-layers/src/scenegraph-layer/scenegraph-layer.js
+++ modules/mesh-layers/src/scenegraph-layer/scenegraph-layer.js
@@ -19,7 +19,8 @@
 // THE SOFTWARE.
 
 import {Layer} from '@deck.gl/core';
-import {GLTFInstantiator, fp64} from 'luma.gl';
+import {createGLTFObjects, fp64} from 'luma.gl';
+import {Matrix4} from 'math.gl';
 
 const {fp64LowPart} = fp64;
 
@@ -28,6 +29,7 @@ const vs = `
   attribute vec3 instancePositions;
   attribute vec2 instancePositions64xy;
   attribute vec3 instancePickingColors;
+  attribute mat4 instanceModelMatrix;
 
   // Uniforms
   uniform float sizeScale;
@@ -45,7 +47,7 @@ const vs = `
       vTEXCOORD_0 = TEXCOORD_0;
     #endif
 
-    vec3 pos = POSITION.xyz;
+    vec3 pos = (instanceModelMatrix * POSITION).xyz;
     pos = project_scale(pos * sizeScale);
 
     vec4 worldPosition;
@@ -72,6 +74,7 @@ const fs = `
 `;
 
 const DEFAULT_COLOR = [0, 0, 0, 255];
+const RADIAN_PER_DEGREE = Math.PI / 180;
 
 const defaultProps = {
   sizeScale: {type: 'number', value: 1, min: 0},
@@ -83,7 +86,10 @@ const defaultProps = {
   // https://en.wikipedia.org/wiki/Euler_angles
   getYaw: {type: 'accessor', value: x => x.yaw || x.angle || 0},
   getPitch: {type: 'accessor', value: x => x.pitch || 0},
-  getRoll: {type: 'accessor', value: x => x.roll || 0}
+  getRoll: {type: 'accessor', value: x => x.roll || 0},
+  getScale: {type: 'accessor', value: x => x.scale || [1, 1, 1]},
+  getTranslation: {type: 'accessor', value: x => x.translate || [0, 0, 0]},
+  getMatrix: {type: 'accessor', value: x => x.matrix || null}
 };
 
 export default class ScenegraphLayer extends Layer {
@@ -98,6 +104,33 @@ export default class ScenegraphLayer extends Layer {
         size: 2,
         accessor: 'getPosition',
         update: this.calculateInstancePositions64xyLow
+      },
+      instanceModelMatrix: {
+        size: 16,
+        accessor: ['getYaw', 'getPitch', 'getRoll', 'getScale', 'getTranslation', 'getMatrix'],
+        shaderAttributes: {
+          instanceModelMatrix__0: {
+            size: 4,
+            stride: 64,
+            offset: 0
+          },
+          instanceModelMatrix__1: {
+            size: 4,
+            stride: 64,
+            offset: 16
+          },
+          instanceModelMatrix__2: {
+            size: 4,
+            stride: 64,
+            offset: 32
+          },
+          instanceModelMatrix__3: {
+            size: 4,
+            stride: 64,
+            offset: 48
+          }
+        },
+        update: this.calculateInstanceModelMatrix
       }
     });
   }
@@ -123,7 +156,7 @@ export default class ScenegraphLayer extends Layer {
 
   updateState({props, oldProps, changeFlags}) {
     if (changeFlags.propsChanged && props.gltf) {
-      const instantiator = new GLTFInstantiator(this.context.gl, {
+      const {scenes} = createGLTFObjects(this.context.gl, props.gltf, {
         modelOptions: {
           vs,
           fs,
@@ -131,8 +164,6 @@ export default class ScenegraphLayer extends Layer {
           isInstanced: true
         }
       });
-
-      const scenes = instantiator.instantiate(props.gltf);
       this.setState({
         sceneGraph: scenes[0]
       });
@@ -159,6 +190,76 @@ export default class ScenegraphLayer extends Layer {
         }
       });
     });
+  }
+
+  calculateInstanceModelMatrix(attribute) {
+    const {data, getYaw, getPitch, getRoll, getScale, getTranslation, getMatrix} = this.props;
+    let instanceModelMatrixData = attribute.value;
+    let rotationMatrix = new Float32Array(16);
+    let modelMatrix = new Matrix4();
+
+    let i = 0;
+    for (const object of data) {
+      let matrix = getMatrix(object);
+
+      if (!matrix) {
+        let roll = getRoll(object) * RADIAN_PER_DEGREE;
+        let pitch = getPitch(object) * RADIAN_PER_DEGREE;
+        let yaw = getYaw(object) * RADIAN_PER_DEGREE;
+        let scale = getScale(object);
+        let translate = getTranslation(object);
+
+        let sr = Math.sin(roll);
+        let sp = Math.sin(pitch);
+        let sw = Math.sin(yaw);
+
+        let cr = Math.cos(roll);
+        let cp = Math.cos(pitch);
+        let cw = Math.cos(yaw);
+
+        rotationMatrix[0] = cw * cp; // 0,0
+        rotationMatrix[1] = sw * cp; // 1,0
+        rotationMatrix[2] = -sp; // 2,0
+        rotationMatrix[3] = 0;
+        rotationMatrix[4] = -sw * cr + cw * sp * sr; // 0,1
+        rotationMatrix[5] = cw * cr + sw * sp * sr; // 1,1
+        rotationMatrix[6] = cp * sr; // 2,1
+        rotationMatrix[7] = 0;
+        rotationMatrix[8] = sw * sr + cw * sp * cr; // 0,2
+        rotationMatrix[9] = -cw * sr + sw * sp * cr; // 1,2
+        rotationMatrix[10] = cp * cr; // 2,2
+        rotationMatrix[11] = 0;
+        rotationMatrix[12] = 0;
+        rotationMatrix[13] = 0;
+        rotationMatrix[14] = 0;
+        rotationMatrix[15] = 1;
+
+        modelMatrix
+          .identity()
+          .translate(translate)
+          .multiplyRight(rotationMatrix)
+          .scale(scale);
+
+        matrix = modelMatrix;
+      }
+
+      instanceModelMatrixData[i++] = matrix[0];
+      instanceModelMatrixData[i++] = matrix[1];
+      instanceModelMatrixData[i++] = matrix[2];
+      instanceModelMatrixData[i++] = matrix[3];
+      instanceModelMatrixData[i++] = matrix[4];
+      instanceModelMatrixData[i++] = matrix[5];
+      instanceModelMatrixData[i++] = matrix[6];
+      instanceModelMatrixData[i++] = matrix[7];
+      instanceModelMatrixData[i++] = matrix[8];
+      instanceModelMatrixData[i++] = matrix[9];
+      instanceModelMatrixData[i++] = matrix[10];
+      instanceModelMatrixData[i++] = matrix[11];
+      instanceModelMatrixData[i++] = matrix[12];
+      instanceModelMatrixData[i++] = matrix[13];
+      instanceModelMatrixData[i++] = matrix[14];
+      instanceModelMatrixData[i++] = matrix[15];
+    }
   }
 }
 
