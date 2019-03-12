53c75c188b7f6937e899e53ecd773c575cf6d437
Ib Green <ib@uber.com>
2019-03-12 15:00:25 -0700

    Fix dev script

--- scripts/bundle-dev.config.js
+++ scripts/bundle-dev.config.js
@@ -1,9 +1,14 @@
-const config = module.require('./bundle-config');
+const config = module.require('./bundle.config');
 
 // Set development mode (no minification)
 config.mode = 'development';
 
+// Remove .min from the name
+config.output.filename = 'dist.js';
+
 // Disable transpilation
 config.module.rules = [];
+
+console.log(JSON.stringify(config, null, 2))
 
 module.exports = config;
