ef3f998dafbdec27526fafaedb6736b9b257a43d
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2019-03-20 11:29:49 -0700

    improve bundle size (#2813)

--- babel.config.js
+++ babel.config.js
@@ -3,12 +3,16 @@ const getBabelConfig = require('ocular-dev-tools/config/babel.config');
 module.exports = api => {
   const config = getBabelConfig(api);
 
-  config.plugins.push('version-inline', [
-    'remove-glsl-comments',
-    {
-      patterns: ['**/*.glsl.js']
-    }
-  ]);
+  config.plugins.push(
+    'version-inline',
+    'inline-webgl-constants',
+    [
+      'remove-glsl-comments',
+      {
+        patterns: ['**/*.glsl.js']
+      }
+    ]
+  );
 
   return config;
 };
