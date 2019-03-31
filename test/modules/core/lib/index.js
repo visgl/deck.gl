a2af4f708efc1c77eff5cf2409c9e0d078f461c4
Ib Green <ibgreen@users.noreply.github.com>
2019-03-31 09:08:50 -0700

    PROPOSAL: Move Attribute from luma to deck (#2867)

--- test/modules/core/lib/index.js
+++ test/modules/core/lib/index.js
@@ -18,6 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+import './base-attribute.spec';
 import './attribute.spec';
 import './attribute-manager.spec';
 import './attribute-transition-manager.spec';
