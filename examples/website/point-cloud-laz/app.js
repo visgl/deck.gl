fccc81458e5f112c1127439596dd078138161b93
Xiaoji Chen <xiaoji@uber.com>
2019-04-02 19:14:21 -0700

    switch to use loaders.gl in pointcloud example

--- examples/website/point-cloud-laz/app.js
+++ examples/website/point-cloud-laz/app.js
@@ -2,23 +2,22 @@ import React, {PureComponent} from 'react';
 import {render} from 'react-dom';
 import DeckGL, {COORDINATE_SYSTEM, PointCloudLayer, OrbitView, LinearInterpolator} from 'deck.gl';
 
-import GL from '@luma.gl/constants';
-import loadLazFile from './utils/laslaz-loader';
-import {normalize} from './utils/point-cloud-utils';
+import {LASLoader} from '@loaders.gl/las';
+import {loadFile} from '@loaders.gl/core';
 
 // Data source: kaarta.com
 const DATA_URL =
   'https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/point-cloud-laz/indoor.0.1.laz';
 
 const INITIAL_VIEW_STATE = {
-  lookAt: [0, 0, 0],
+  target: [0, 0, 0],
   rotationX: 0,
   rotationOrbit: 0,
   orbitAxis: 'Y',
   fov: 50,
-  minDistance: 0.5,
-  maxDistance: 3,
-  zoom: 9
+  minZoom: 0,
+  maxZoom: 10,
+  zoom: 1
 };
 
 const transitionInterpolator = new LinearInterpolator(['rotationOrbit']);
@@ -29,23 +28,15 @@ export class App extends PureComponent {
 
     this.state = {
       viewState: INITIAL_VIEW_STATE,
-      points: [],
-      progress: 0
+      data: this._loadData()
     };
 
-    this._onLoad = this._onLoad.bind(this);
     this._onViewStateChange = this._onViewStateChange.bind(this);
     this._rotateCamera = this._rotateCamera.bind(this);
-
-    this._loadData();
   }
 
   _onViewStateChange({viewState}) {
     this.setState({viewState});
-  }
-
-  _onLoad() {
-    this._rotateCamera();
   }
 
   _rotateCamera() {
@@ -61,46 +52,60 @@ export class App extends PureComponent {
     });
   }
 
+  _onMetadata(metadata) {
+    const {mins, maxs} = metadata;
+    const {viewState} = this.state;
+
+    this.setState({
+      viewState: {
+        ...viewState,
+        target: [
+          (mins[0] + maxs[0]) / 2,
+          (mins[1] + maxs[1]) / 2,
+          (mins[2] + maxs[2]) / 2
+        ],
+        /* global window */
+        zoom: Math.log2(window.innerWidth / (maxs[0] - mins[0])) - 1
+      }
+    }, this._rotateCamera);
+  }
+
   _loadData() {
-    const {points} = this.state;
     const skip = 1;
+    const {onLoad = () => {}} = this.props;
 
-    if (this.props.onLoad) {
-      this.props.onLoad({count: 0, progress: 0});
-    }
+    onLoad({count: 0, progress: 0});
 
-    loadLazFile(DATA_URL, skip, (decoder, progress) => {
-      for (let i = 0; i < decoder.pointsCount; i++) {
-        const {position} = decoder.getPoint(i);
-        points.push(position);
-      }
+    const onProgress = ({header, progress}) => {
+      onLoad({count: header.totalRead, progress});
+    };
 
-      if (this.props.onLoad) {
-        this.props.onLoad({count: points.length, progress});
-      }
-
-      if (progress >= 1) {
-        normalize(points);
-      }
-
-      this.setState({points, progress});
-    });
+    return loadFile(DATA_URL, LASLoader, {skip, onProgress})
+      .then(data => {
+        this._onMetadata(data.loaderData.header);
+        return {
+          length: data.header.vertexCount,
+          positions: data.attributes.POSITION.value
+        };
+      });
   }
 
   _renderLayers() {
-    const {points, progress} = this.state;
+    const {data} = this.state;
 
     return [
-      progress >= 1.0 &&
-        new PointCloudLayer({
-          id: 'laz-point-cloud-layer',
-          data: points,
-          coordinateSystem: COORDINATE_SYSTEM.IDENTITY,
-          getPosition: d => d,
-          getNormal: [0, 1, 0],
-          getColor: [255, 255, 255],
-          radiusPixels: 0.5
-        })
+      new PointCloudLayer({
+        id: 'laz-point-cloud-layer',
+        data,
+        coordinateSystem: COORDINATE_SYSTEM.IDENTITY,
+        getPosition: (_, {index, data}) => {
+          return data.positions.subarray(index * 3, index * 3 + 3)
+        },
+        getNormal: [0, 1, 0],
+        getColor: [255, 255, 255],
+        opacity: 0.5,
+        pointSize: 0.5
+      })
     ];
   }
 
@@ -112,12 +117,10 @@ export class App extends PureComponent {
         views={new OrbitView()}
         viewState={viewState}
         controller={true}
-        onLoad={this._onLoad}
         onViewStateChange={this._onViewStateChange}
         layers={this._renderLayers()}
         parameters={{
-          clearColor: [0.07, 0.14, 0.19, 1],
-          blendFunc: [GL.SRC_ALPHA, GL.ONE_MINUS_SRC_ALPHA]
+          clearColor: [0.07, 0.14, 0.19, 1]
         }}
       />
     );
