ed1ea5a8fb0eef2391e05c61ee2492cd2f6622b3
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2019-04-11 21:10:00 -0700

    Make point cloud example support generic formats (#2951)

--- website/src/components/demos/point-cloud-demo.js
+++ website/src/components/demos/point-cloud-demo.js
@@ -1,10 +1,9 @@
 import React, {Component} from 'react';
 import autobind from 'autobind-decorator';
 import {readableInteger} from '../../utils/format-utils';
-import {App} from 'website-examples/point-cloud-laz/app';
+import {App} from 'website-examples/point-cloud/app';
 
 export default class PointCloudDemo extends Component {
-
   static get data() {
     return [];
   }
@@ -22,9 +21,12 @@ export default class PointCloudDemo extends Component {
       <div>
         <h3>3D Indoor Scan</h3>
         <p>This demo may not work on mobile devices due to browser limitations.</p>
-        <p>Data source: <a href="https://kaarta.com">kaarta.com</a></p>
-        <div className="stat">No. of Points
-          <b>{ readableInteger(meta.count) }</b>
+        <p>
+          Data source: <a href="https://kaarta.com">kaarta.com</a>
+        </p>
+        <div className="stat">
+          No. of Points
+          <b>{readableInteger(meta.count)}</b>
         </div>
       </div>
     );
@@ -36,8 +38,6 @@ export default class PointCloudDemo extends Component {
   }
 
   render() {
-    return (
-      <App onLoad={this._onLoad} />
-    );
+    return <App onLoad={this._onLoad} />;
   }
 }
