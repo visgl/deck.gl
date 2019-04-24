e8541fdcb1378689bd76d06da6f8f01b19eff3ad
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2019-04-24 15:38:39 -0700

    Fix start-local for get-started examples and test apps (#3029)

--- examples/get-started/pure-js/google-maps/webpack.config.js
+++ examples/get-started/pure-js/google-maps/webpack.config.js
@@ -17,4 +17,4 @@ const CONFIG = {
 };
 
 // This line enables bundling against src in this repo rather than installed module
-module.exports = env => (env ? require('../../webpack.config.local')(CONFIG)(env) : CONFIG);
+module.exports = env => (env ? require('../../../webpack.config.local')(CONFIG)(env) : CONFIG);
