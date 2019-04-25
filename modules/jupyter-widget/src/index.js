98e8b50b898c87fa93dfe1048d875b8b32d16e96
Andrew Duberstein <duberstein@uber.com>
2019-04-25 13:24:59 -0700

    Add Jupyter notebook widget JS module

---
+++ modules/jupyter-widget/src/index.js
@@ -0,0 +1,4 @@
+// Copyright (c) Uber Technologies Inc.
+// Distributed under the terms of the Modified BSD License.
+export {MODULE_VERSION, MODULE_NAME} from './version';
+export {DeckGLModel, DeckGLView} from './widget';
