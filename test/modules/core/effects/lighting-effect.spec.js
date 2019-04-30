8a9ebdb480b27341454d05b361d5d737a7f8ffc7
Jian Huang <jianh@uber.com>
2019-04-30 11:17:15 -0700

    update internal props of point lights

--- test/modules/core/effects/lighting-effect.spec.js
+++ test/modules/core/effects/lighting-effect.spec.js
@@ -1,6 +1,6 @@
 import test from 'tape-catch';
 import LightingEffect from '@deck.gl/core/effects/lighting/lighting-effect';
-import {_CameraLight as CameraLight} from '@deck.gl/core';
+import {_CameraLight as CameraLight, PointLight} from '@deck.gl/core';
 
 import {MapView, PolygonLayer} from 'deck.gl';
 import * as FIXTURES from 'deck.gl-test/data';
@@ -34,5 +34,32 @@ test('LightingEffect#CameraLight', t => {
   const projectedLights = lightEffect.getProjectedPointLights(layer);
   t.ok(projectedLights[0], 'Camera light is ok');
   t.deepEqual(projectedLights[0].position, [0, 0, 150], 'Camera light projection is ok');
+  t.end();
+});
+
+test('LightingEffect#PointLight', t => {
+  const pointLight = new PointLight();
+  const lightEffect = new LightingEffect({pointLight});
+
+  const viewport = new MapView().makeViewport({
+    width: 100,
+    height: 100,
+    viewState: {longitude: -122, latitude: 37, zoom: 13}
+  });
+
+  const layer = new PolygonLayer({
+    data: FIXTURES.polygons.slice(0, 3),
+    getPolygon: f => f,
+    getFillColor: (f, {index}) => [index, 0, 0]
+  });
+
+  layer.context = {viewport};
+  pointLight.intensity = 2.0;
+  pointLight.color = [255, 0, 0];
+
+  const projectedLights = lightEffect.getProjectedPointLights(layer);
+  t.ok(projectedLights[0], 'point light is ok');
+  t.equal(projectedLights[0].intensity, 2.0, 'point light intensity is ok');
+  t.deepEqual(projectedLights[0].color, [255, 0, 0], 'point light color is ok');
   t.end();
 });
