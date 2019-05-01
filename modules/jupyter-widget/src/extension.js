e8379609fe8d6f8d07038ff1b6c919c5fec2cef8
Andrew Duberstein <duberstein@uber.com>
2019-04-30 21:27:22 -0700

    Add extension.js and notebook.js files

---
+++ modules/jupyter-widget/src/extension.js
@@ -0,0 +1,14 @@
+/* global window */
+if (window.require) {
+  window.require.config({
+    map: {
+      '*': {
+        pydeck: 'nbextensions/pydeck/index'
+      }
+    }
+  });
+}
+
+module.exports = {
+  load_ipython_extension() {}
+};
---
+++ modules/jupyter-widget/src/notebook.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+// Sets up notebook base URL
+__webpack_public_path__ =
+  document.querySelector('body').getAttribute('data-base-url') + 'nbextensions/pydeck/';
+
+module.exports = require('./index.js');
