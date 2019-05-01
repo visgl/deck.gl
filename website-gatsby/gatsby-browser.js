f479a8a2b8dd4ec2a2e8fc10c1358e3714ba9c75
Ib Green <ib@uber.com>
2019-04-30 21:29:27 -0700

    Fixes

--- website-gatsby/gatsby-browser.js
+++ website-gatsby/gatsby-browser.js
@@ -1,13 +1,1 @@
-// const {registerReactComponent} = require('ocular-gatsby/api');
-
-// const Hero = require('./src/components/hero').default;
-// const ExampleRunner = require('./src/components/example-runner').default;
-// const EXAMPLES = require('./src/components/examples');
-
 module.exports = require('ocular-gatsby/gatsby-browser');
-
-// Make sure to regsiter custom components after ocular registers defaults
-
-// registerReactComponent('Hero', Hero);
-// registerReactComponent('ExampleRunner', ExampleRunner);
-// registerReactComponent('EXAMPLES', EXAMPLES);
