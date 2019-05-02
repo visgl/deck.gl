5cd5c504793708bdf3940fe87be723cc68bce954
1chandu <chandu@uber.com>
2019-05-02 14:43:19 -0700

    GPUGridLayer PART-1: Fix world space aggregation (#3051)
    * GPU Grid Aggregation PART-1: Aggregator: fix world space aggregation

--- modules/aggregation-layers/src/utils/gpu-grid-aggregation/aggregate-all-vs-64.glsl.js
+++ modules/aggregation-layers/src/utils/gpu-grid-aggregation/aggregate-all-vs-64.glsl.js
@@ -23,24 +23,24 @@ export default `\
 #define SHADER_NAME gpu-aggregation-all-vs-64
 
 in vec2 position;
-uniform vec2 gridSize;
-
+uniform ivec2 gridSize;
 out vec2 vTextureCoord;
+
 void main(void) {
   // Map each position to single pixel
   vec2 pos = vec2(-1.0, -1.0);
 
   // Move to pixel center, pixel-size in screen sapce (2/gridSize) * 0.5 => 1/gridSize
-  vec2 offset = 1.0 / gridSize;
+  vec2 offset = 1.0 / vec2(gridSize);
   pos = pos + offset;
 
   gl_Position = vec4(pos, 0.0, 1.0);
 
-  float yIndex = floor(float(gl_InstanceID) / gridSize[0]);
-  float xIndex = float(gl_InstanceID) - (yIndex * gridSize[0]);
+  int yIndex = gl_InstanceID / gridSize[0];
+  int xIndex = gl_InstanceID - (yIndex * gridSize[0]);
 
-  vec2 yIndexFP64 = vec2(yIndex, 0.);
-  vec2 xIndexFP64 = vec2(xIndex, 0.);
+  vec2 yIndexFP64 = vec2(float(yIndex), 0.);
+  vec2 xIndexFP64 = vec2(float(xIndex), 0.);
   vec2 gridSizeYFP64 = vec2(gridSize[1], 0.);
   vec2 gridSizeXFP64 = vec2(gridSize[0], 0.);
 
