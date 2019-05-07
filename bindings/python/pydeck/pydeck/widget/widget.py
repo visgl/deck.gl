ce5ea39688d904605b08e6d2d7bb12e0414ec05a
Andrew Duberstein <duberstein@uber.com>
2019-05-07 16:53:18 -0700

    Update docstring

--- bindings/python/pydeck/pydeck/widget/widget.py
+++ bindings/python/pydeck/pydeck/widget/widget.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-"""
-DeckGLWidget renders JSON into a deck.gl plot
-You may set a Mapbox API key as an environment variable to use Mapbox maps in your visualization
-"""
 from __future__ import unicode_literals
 import os
 
@@ -15,15 +11,24 @@ from ._frontend import module_name, module_version
 
 
 class DeckGLWidget(widgets.DOMWidget):
+    """
+    DeckGLWidget accepts JSON encoded deck.gl layers and layer properties
+    and renders them based on provided view states and properties.
+
+    You may set a Mapbox API key as an environment variable to use Mapbox maps in your visualization
+
+    Attributes
+    ----------
+    mapbox_key (str) - Read on inititialization from the MAPBOX_API_KEY environment variable. Defaults to None if not set.
+    json_input (str) - JSON as a string meant for reading into deck.gl JSON API
+    """
     _model_name = Unicode('DeckGLModel').tag(sync=True)
     _model_module = Unicode(module_name).tag(sync=True)
     _model_module_version = Unicode(module_version).tag(sync=True)
     _view_name = Unicode('DeckGLView').tag(sync=True)
     _view_module = Unicode(module_name).tag(sync=True)
     _view_module_version = Unicode(module_version).tag(sync=True)
-    # Mapbox key, read on inititialization from the MAPBOX_API_KEY environment variable. Defaults to None if not set.
     mapbox_key = Unicode(os.getenv('MAPBOX_API_KEY')).tag(sync=True)
-    # JSON as a string meant for reading into deck.gl JSON API
     json_input = Unicode('').tag(sync=True)
     height = Int(500).tag(sync=True)
     width = Int(500).tag(sync=True)
