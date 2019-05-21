ec64883f42f3c2c91662fce0ea9abb659adea744
Adrian Darian <adarian@ucmerced.edu>
2019-05-21 16:03:31 -0700

    Update links.js
    Original link was broken

--- website/contents/links.js
+++ website/contents/links.js
@@ -3,7 +3,7 @@ export default {
   'deck.gl': 'https://uber.github.io/deck.gl',
   'luma.gl': 'https://uber.github.io/luma.gl',
   'react-map-gl': 'https://uber.github.io/react-map-gl',
-  'nebula.gl': 'https://uber.github.io/nebula.gl',
+  'nebula.gl': 'https://nebula.gl/',
   'react-vis': 'https://uber.github.io/react-vis',
   'vis.gl': 'http://vis.gl'
 };
