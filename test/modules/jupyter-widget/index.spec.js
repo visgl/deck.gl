faaed501173f963e17e6697ea59431f21520cbc0
Andrew Duberstein <duberstein@uber.com>
2019-05-25 17:00:40 +0300

    Raise jupyter-widget module test coverage

--- test/modules/jupyter-widget/index.spec.js
+++ test/modules/jupyter-widget/index.spec.js
@@ -1,10 +1,34 @@
 import '@jupyter-widgets/base';
 
-import {DeckGLView} from '@deck.gl/jupyter-widget';
+import {DeckGLModel, DeckGLView} from '@deck.gl/jupyter-widget';
+import {createTestModel} from './utils.spec';
+
 import test from 'tape-catch';
 
 test('DeckGLView should initialize', t => {
   const obj = new DeckGLView();
   t.ok(obj, 'DeckGLView initalized');
   t.end();
+});
+
+test('jupyter-widget should be createable', t => {
+  const model = createTestModel(DeckGLModel);
+  t.equal(typeof model, 'DeckGLModel');
+  t.equal(model.get('json_input'), '');
+  t.equal(model.get('mapbox_key'), '');
+  t.equal(model.get('width'), 500);
+  t.equal(model.get('height'), 500);
+});
+
+test('jupyter-widget should be creatable with a value', t => {
+  const state = {
+    mapbox_key: 'fake-key',
+    json_input: '{mock_input: 1}'
+  };
+  const model = createTestModel(DeckGLModel, state);
+  t.equal(typeof model, 'DeckGLModel');
+  t.equal(model.get('json_input'), state.json_input);
+  t.equal(model.get('mapbox_key'), state.mapbox_key);
+  t.equal(model.get('width'), 500);
+  t.equal(model.get('height'), 500);
 });
