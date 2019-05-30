c773601276fcc1f7bcc1617f58bc2e9a3b246062
Xintong Xia <xintong@uber.com>
2019-05-30 09:54:48 -0700

    Fix GridCellLayer website demo (#3158)

--- website/.eslintrc.js
+++ website/.eslintrc.js
@@ -1,6 +1,7 @@
 // prettier-ignore
 module.exports = {
   plugins: ['react'],
+  parser: 'babel-eslint',
   extends: ['uber-jsx', 'uber-es2015', 'prettier', 'prettier/react', 'plugin:import/errors'],
   overrides: {
     files: ['*.spec.js', 'webpack.config.js', '**/bundle/*.js'],
@@ -10,10 +11,7 @@ module.exports = {
   },
   settings: {
     'import/core-modules': [
-      '@luma.gl/core',
-      '@luma.gl/constants',
-      'math.gl',
-      'viewport-mercator-project'
+      'deck.gl'
     ]
   },
   rules: {
