8a30c6488b661c75df04c0142323f98b63a27859
Andrew Duberstein <duberstein@uber.com>
2019-06-03 10:58:02 -0700

    Change version

--- bindings/python/pydeck/pydeck/_version.py
+++ bindings/python/pydeck/pydeck/_version.py
@@ -2,5 +2,5 @@
 
 # Copyright (c) Uber Technologies, Inc.
 # Distributed under the terms of the Modified BSD License.
-version_info = (0, 0, 1, 'dev')
+version_info = (0, 1, 'dev1')
 __version__ = '.'.join(map(str, version_info))
