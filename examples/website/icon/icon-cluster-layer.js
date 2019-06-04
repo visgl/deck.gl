fc8bb7b65f69c072a79fe2bd232947633ba72489
Xiaoji Chen <xiaoji@uber.com>
2019-06-04 11:48:57 -0700

    use supercluster for IconLayer demo clustering

--- examples/website/icon/icon-cluster-layer.js
+++ examples/website/icon/icon-cluster-layer.js
@@ -1,5 +1,5 @@
-import {CompositeLayer, IconLayer, WebMercatorViewport} from 'deck.gl';
-import rbush from 'rbush';
+import {CompositeLayer, IconLayer} from 'deck.gl';
+import Supercluster from 'supercluster';
 
 function getIconName(size) {
   if (size === 0) {
@@ -19,43 +19,49 @@ function getIconSize(size) {
 }
 
 export default class IconClusterLayer extends CompositeLayer {
-  initializeState() {
-    this.setState({
-      // build spatial index
-      tree: rbush(9, ['.x', '.y', '.x', '.y']),
-      version: -1,
-      z: -1,
-      width: 0,
-      height: 0
-    });
-  }
-
   shouldUpdateState({changeFlags}) {
     return changeFlags.somethingChanged;
   }
 
   updateState({props, oldProps, changeFlags}) {
-    const {viewport} = this.context;
+    const rebuildIndex = changeFlags.dataChanged || props.sizeScale !== oldProps.sizeScale;
 
-    const {width, height} = viewport;
-
-    if (
-      changeFlags.dataChanged ||
-      props.sizeScale !== oldProps.sizeScale ||
-      this.state.width !== width ||
-      this.state.height !== height
-    ) {
-      const version = this._updateCluster(props, viewport);
-      this.setState({version, width, height});
+    if (rebuildIndex) {
+      const index = new Supercluster({maxZoom: 16, radius: props.sizeScale});
+      index.load(
+        props.data.map(d => ({
+          geometry: {coordinates: props.getPosition(d)},
+          properties: d
+        }))
+      );
+      this.setState({index});
     }
-    this.setState({
-      z: Math.floor(viewport.zoom)
-    });
+
+    const z = Math.floor(this.context.viewport.zoom);
+    if (rebuildIndex || z !== this.state.z) {
+      this.setState({
+        data: this.state.index.getClusters([-180, -85, 180, 85], z),
+        z
+      });
+    }
+  }
+
+  getPickingInfo({info, mode}) {
+    const pickedObject = info.object && info.object.properties;
+    if (pickedObject) {
+      if (pickedObject.cluster && mode !== 'hover') {
+        info.objects = this.state.index
+          .getLeaves(pickedObject.cluster_id, 25)
+          .map(f => f.properties);
+      }
+      info.object = pickedObject;
+    }
+    return info;
   }
 
   renderLayers() {
-    const {z, version} = this.state;
-    const {data, iconAtlas, iconMapping, sizeScale, getPosition, onHover, onClick} = this.props;
+    const {data} = this.state;
+    const {iconAtlas, iconMapping, sizeScale} = this.props;
 
     return new IconLayer(
       this.getSubLayerProps({
@@ -64,79 +70,10 @@ export default class IconClusterLayer extends CompositeLayer {
         iconAtlas,
         iconMapping,
         sizeScale,
-        getPosition,
-        getIcon: d => d.zoomLevels[z] && d.zoomLevels[z].icon,
-        getSize: d => d.zoomLevels[z] && d.zoomLevels[z].size,
-        onHover,
-        onClick,
-        updateTriggers: {
-          getIcon: {version, z},
-          getSize: {version, z}
-        }
+        getPosition: d => d.geometry.coordinates,
+        getIcon: d => getIconName(d.properties.cluster ? d.properties.point_count : 1),
+        getSize: d => getIconSize(d.properties.cluster ? d.properties.point_count : 1)
       })
     );
-  }
-
-  // Compute icon clusters
-  // We use the projected positions instead of longitude and latitude to build
-  // the spatial index, because this particular dataset is distributed all over
-  // the world, we can't use some fixed deltaLon and deltaLat
-  _updateCluster({data, sizeScale}, viewport) {
-    const {tree, version} = this.state;
-
-    if (!data) {
-      return version;
-    }
-
-    const transform = new WebMercatorViewport({
-      ...viewport,
-      zoom: 0
-    });
-
-    data.forEach(p => {
-      const screenCoords = transform.project(p.coordinates);
-      p.x = screenCoords[0];
-      p.y = screenCoords[1];
-      p.zoomLevels = [];
-    });
-
-    tree.clear();
-    tree.load(data);
-
-    for (let z = 0; z <= 20; z++) {
-      const radius = sizeScale / Math.sqrt(2) / Math.pow(2, z);
-
-      data.forEach(p => {
-        if (p.zoomLevels[z] === undefined) {
-          // this point does not belong to a cluster
-          const {x, y} = p;
-
-          // find all points within radius that do not belong to a cluster
-          const neighbors = tree
-            .search({
-              minX: x - radius,
-              minY: y - radius,
-              maxX: x + radius,
-              maxY: y + radius
-            })
-            .filter(neighbor => neighbor.zoomLevels[z] === undefined);
-
-          // only show the center point at this zoom level
-          neighbors.forEach(neighbor => {
-            if (neighbor === p) {
-              p.zoomLevels[z] = {
-                icon: getIconName(neighbors.length),
-                size: getIconSize(neighbors.length),
-                points: neighbors
-              };
-            } else {
-              neighbor.zoomLevels[z] = null;
-            }
-          });
-        }
-      });
-    }
-
-    return version + 1;
   }
 }
