4c0ebe0a5b21bcdbf40520aa294bf3b0730aae01
Xiaoji Chen <xiaoji@uber.com>
2019-06-04 15:42:00 -0700

    fix polyfill

--- website/src/main.js
+++ website/src/main.js
@@ -1,20 +1,25 @@
-/* global document */
-import '@babel/polyfill';
+/* global window, document */
+// import '@babel/polyfill';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {HashRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom';
 import {Route} from 'react-router';
 
 import AppState from './reducers';
 import App from './components/app';
 
+// polyfill breaks `instanceof Promise` check
+const _fetch = window.fetch;
+
+window.fetch = (resource, init) => Promise.resolve(_fetch(resource, init));
+
 const el = document.createElement('div');
 document.body.appendChild(el);
 
 ReactDOM.render(
-  <Provider store={AppState} >
+  <Provider store={AppState}>
     <HashRouter>
       <Route path="/" component={App} />
     </HashRouter>
