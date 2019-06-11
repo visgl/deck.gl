6d093fc9a317ab2339c8f07695fb79e36cc0c216
Xiaoji Chen <xiaoji@uber.com>
2019-06-11 14:50:13 -0700

    enable unit tests for canvas-based layers

---
+++ test/modules/layers/text-layer/text-layer.spec.js
@@ -0,0 +1,27 @@
+import test from 'tape-catch';
+
+import {TextLayer} from '@deck.gl/layers';
+import * as FIXTURES from 'deck.gl-test/data';
+import {testLayer, generateLayerTests} from '@deck.gl/test-utils';
+
+test('TextLayer', t => {
+  const testCases = generateLayerTests({
+    Layer: TextLayer,
+    sampleProps: {
+      data: FIXTURES.points,
+      getText: d => d.ADDRESS,
+      getPosition: d => d.COORDINATES
+    },
+    assert: t.ok,
+    onBeforeUpdate: ({testCase}) => t.comment(testCase.title),
+    onAfterUpdate: ({layer, subLayer}) => {
+      if (layer.props.data.length) {
+        t.ok(layer.state.data.length, 'Creates sublayer data');
+        t.ok(subLayer, 'Renders sublayer');
+      }
+    }
+  });
+  testLayer({Layer: TextLayer, testCases, onError: t.notOk});
+
+  t.end();
+});
