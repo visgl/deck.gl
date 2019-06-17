6ab48a3f6f336cc909f23aea6cc0b1e6975688df
Andrew Duberstein <ajduberstein@users.noreply.github.com>
2019-06-16 22:30:47 -0700

    Add viewport helper functions, add documentation, support saving state across kernel restarts (#3205)

--- bindings/python/pydeck/pydeck/bindings/deck.py
+++ bindings/python/pydeck/pydeck/bindings/deck.py
@@ -7,22 +7,7 @@ from ..widget import DeckGLWidget
 
 
 class Deck(JSONMixin):
-    """
-    Data visualization configuration
-
-    Wrapper around deck.gl JSON API bindings
-
-    Parameters
-    ---------
-    layers : array, default []
-        list of pydeck.Layer objects to render
-    views : array, default [View()]
-        list of pydeck.View objects to render
-    map_style : str, default "mapbox://styles/mapbox/dark-v9"
-        Style of basemap
-    initial_view_state : pydeck.ViewState, default [pydeck.ViewState()]
-        Initial camera angle relative to the map
-    """
+    """The renderer and configuration for a visualization"""
     def __init__(
         self,
         layers=[],
@@ -30,33 +15,74 @@ class Deck(JSONMixin):
         map_style='mapbox://styles/mapbox/dark-v9',
         initial_view_state=ViewState(),
     ):
-        self.layers = layers
+        """Constructor for a Deck object, similar to the `Deck`_ class from deck.gl
+
+        Parameters
+        ----------
+        layers : :obj:`pydeck.Layer` or :obj:`list` of :obj:`pydeck.Layer`, default []
+            List of pydeck.Layer objects to render
+        views : :obj:`list` of :obj:`pydeck.View`, default [pydeck.View()]
+            List of `pydeck.View` objects to render
+        map_style : str, default "mapbox://styles/mapbox/dark-v9"
+            URI for Mapbox basemap style
+        initial_view_state : pydeck.ViewState, default pydeck.ViewState()
+            Initial camera angle relative to the map, defaults to a fully zoomed out 0, 0-centered map
+            To compute a viewport from data, see `pydeck.data_utils.autocompute_viewport`
+
+
+        .. _Deck:
+            https://deck.gl/#/documentation/deckgl-api-reference/deck
+        """
+        self.layers = []
+        if isinstance(layers, Layer):
+            self.layers.append(layers)
+        else:
+            self.layers = layers
         self.views = views
         self.map_style = map_style
+        # Use passed view state
         self.initial_view_state = initial_view_state
         self.deck_widget = DeckGLWidget()
-
 
     def __add__(self, obj):
         """
         Override of the addition operator to add attributes to the Deck object
+
+        Parameters
+        ----------
+        obj : object
+            pydeck.Layer, pydeck.View, or pydeck.ViewState
+
+        Examples
+        --------
+        >>> pydeck.Deck() + pydeck.View(controller=False)
+        >>> pydeck.Deck()
+        {"initialViewState": {"bearing": 0, ... , "views": [{"controller": false, "type": "MapView"}}... }
         """
-        if isinstance(Layer, obj):
+        if isinstance(obj, Layer):
             self.layers.append(obj)
-        elif isinstance(View, obj):
-            self.views.append(obj)
-        elif isinstance(ViewState, obj):
+        elif isinstance(obj, View):
+            self.views = [obj]
+        elif isinstance(obj, ViewState):
             self.initial_view_state = obj
         else:
             obj_type = type(obj).__name__
             raise TypeError("Cannot join object of type", obj_type)
 
     def show(self):
-        """
-        Displays current Deck object for a Jupyter notebook
-        """
+        """Displays current Deck object for a Jupyter notebook"""
         self.update()
         return self.deck_widget
 
     def update(self):
+        """Updates a deck.gl map to reflect the current state of the configuration
+
+        For example, if you've modified data passed to Layer and rendered the map using `.show()`,
+        you can call `update` to pass the new configuration to the map
+
+        Intended for use in a Jupyter notebook
+        """
         self.deck_widget.json_input = self.to_json()
+
+    def to_html(self, filename=None):
+        pass
---
+++ bindings/python/pydeck/pydeck/data_utils/__init__.py
@@ -0,0 +1,3 @@
+from .viewport_helpers import autocompute_viewport  # noqa
+from .type_checking import is_pandas_df  # noqa
+from .color_scales import assign_random_colors  # noqa
