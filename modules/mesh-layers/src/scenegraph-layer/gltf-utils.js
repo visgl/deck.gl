db09e5d3af55176212337f4ccb8e0c4b34737e15
Ib Green <ibgreen@users.noreply.github.com>
2019-06-27 18:59:29 -0700

    Update ScenegraphLayer to use GLTFLoader (#3288)

---
+++ modules/mesh-layers/src/scenegraph-layer/gltf-utils.js
@@ -0,0 +1,23 @@
+/* global requestAnimationFrame */
+
+export async function waitForGLTFAssets(gltfObjects) {
+  const remaining = [];
+
+  gltfObjects.scenes.forEach(scene => {
+    scene.traverse(model => {
+      Object.values(model.model.program.uniforms).forEach(uniform => {
+        if (uniform.loaded === false) {
+          remaining.push(uniform);
+        }
+      });
+    });
+  });
+
+  return await waitWhileCondition(() => remaining.some(uniform => !uniform.loaded));
+}
+
+async function waitWhileCondition(condition) {
+  while (condition()) {
+    await new Promise(resolve => requestAnimationFrame(resolve));
+  }
+}
