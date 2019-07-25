0754bfce3921de77dcb085aa4a88c189403849b3
Jian Huang <jianh@uber.com>
2019-07-25 14:43:36 -0700

    improve projection API and unit test

--- modules/core/src/shaderlib/project/project.glsl.js
+++ modules/core/src/shaderlib/project/project.glsl.js
@@ -157,19 +157,6 @@ vec2 project_position(vec2 position) {
   return projected_position.xy;
 }
 
-//
-// Projects from "world" coordinates to clip space.
-// Uses project_uViewProjectionMatrix
-//
-vec4 project_common_position_to_clipspace(vec4 position) {
-  if (project_uCoordinateSystem == COORDINATE_SYSTEM_METER_OFFSETS ||
-    project_uCoordinateSystem == COORDINATE_SYSTEM_LNGLAT_OFFSETS) {
-    // Needs to be divided with project_uCommonUnitsPerMeter
-    position.w *= project_uCommonUnitsPerMeter.z;
-  }
-  return project_uViewProjectionMatrix * position + project_uCenter;
-}
-
 vec4 project_common_position_to_clipspace(vec4 position, mat4 viewProject, vec4 center) {
   if (project_uCoordinateSystem == COORDINATE_SYSTEM_METER_OFFSETS ||
     project_uCoordinateSystem == COORDINATE_SYSTEM_LNGLAT_OFFSETS) {
@@ -177,6 +164,14 @@ vec4 project_common_position_to_clipspace(vec4 position, mat4 viewProject, vec4 
     position.w *= project_uCommonUnitsPerMeter.z;
   }
   return viewProject * position + center;
+}
+
+//
+// Projects from "world" coordinates to clip space.
+// Uses project_uViewProjectionMatrix
+//
+vec4 project_common_position_to_clipspace(vec4 position) {
+  return project_common_position_to_clipspace(position, project_uViewProjectionMatrix, project_uCenter);
 }
 
 // Returns a clip space offset that corresponds to a given number of screen pixels
