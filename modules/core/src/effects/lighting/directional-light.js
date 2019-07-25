6458f189d43e6c0e49a3806ff5d6e2805652b42c
Jian Huang <jianh@uber.com>
2019-07-25 14:44:27 -0700

    remove redundant code

--- modules/core/src/effects/lighting/directional-light.js
+++ modules/core/src/effects/lighting/directional-light.js
@@ -1,12 +1,6 @@
 import {DirectionalLight as BaseDirectionalLight} from '@luma.gl/core';
 
 export default class DirectionalLight extends BaseDirectionalLight {
-  constructor(props) {
-    super(props);
-    const {castShadow = false} = props;
-    this.castShadow = castShadow;
-  }
-
   getProjectedLight() {
     return this;
   }
