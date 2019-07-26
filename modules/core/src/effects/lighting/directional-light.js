d01914bbba0189b8ba476662d4ed49a01c2aeaf8
Jian Huang <jianh@uber.com>
2019-07-26 16:33:47 -0700

    add castShadow flag back

--- modules/core/src/effects/lighting/directional-light.js
+++ modules/core/src/effects/lighting/directional-light.js
@@ -1,6 +1,12 @@
 import {DirectionalLight as BaseDirectionalLight} from '@luma.gl/core';
 
 export default class DirectionalLight extends BaseDirectionalLight {
+  constructor(props) {
+    super(props);
+    const {castShadow = false} = props;
+    this.castShadow = castShadow;
+  }
+
   getProjectedLight() {
     return this;
   }
