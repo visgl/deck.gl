0fbed9c4aa6d9ea3b59f6393b25f6ef340f0252a
Andrew Duberstein <duberstein@uber.com>
2019-08-27 12:50:38 -0700

    Update hook

--- scripts/bindings-precommit-hook.sh
+++ scripts/bindings-precommit-hook.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 IS_PYTHON=$(git diff --cached --name-only | grep "bindings/python/")
+HAS_PYTEST=$(command -v pytest)
 
 if [[ -n "$IS_PYTHON" ]]; then
   echo "Running Python pre-commit hook"
+
+  if [[ -z "$HAS_PYTEST" ]]; then
+    echo "pytest is not installed. See the pydeck README."
+    exit 1
+  fi
+
   pytest bindings/python/pydeck/tests/
 fi
 RESULT=$?
