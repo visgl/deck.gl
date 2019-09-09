d8355534fdda05ee1dc39b978cd75c482412cb0d
Taylor Baldwin <tbaldwin@uber.com>
2019-09-09 12:00:27 -0400

    add comment to padBuffer definition

--- modules/core/src/lib/attribute-transition-utils.js
+++ modules/core/src/lib/attribute-transition-utils.js
@@ -25,6 +25,13 @@ export function normalizeTransitionSettings(userSettings, layerSettings = {}) {
   return Object.assign({}, DEFAULT_TRANSITION_SETTINGS, layerSettings, userSettings);
 }
 
+// This helper is used when transitioning attributes from a set of values in one buffer layout
+// to a set of values in a different buffer layout. (Buffer layouts are used when attribute values
+// within a buffer should be grouped for drawElements, like the Polygon layer.) For example, a
+// buffer layout of [3, 4] might have data [A1, A2, A3, B1, B2, B3, B4]. If it needs to transition
+// to a buffer layout of [4, 2], it should produce a buffer, using the transition setting's `enter`
+// function, that looks like this: [A1, A2, A3, A4 (user `enter` fn), B1, B2, 0]. Note: the final
+// 0 in this buffer is because we never shrink buffers, only grow them, for performance reasons.
 export function padBuffer({
   fromState,
   toState,
