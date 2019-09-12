2f0e6252238a4dc5c95221c3d492b037e2520b5a
Jian Huang <jianh@uber.com>
2019-09-12 13:15:57 -0700

    code review and integrate 3d tiles example with website

--- examples/website/3d-tiles/app.js
+++ examples/website/3d-tiles/app.js
@@ -1,24 +1,17 @@
-import '@babel/polyfill';
-
 import React, {PureComponent} from 'react';
 import {render} from 'react-dom';
 import {StaticMap} from 'react-map-gl';
 import DeckGL from '@deck.gl/react';
-import {MapController, FlyToInterpolator} from '@deck.gl/core';
-import {lumaStats} from '@luma.gl/core';
-import {StatsWidget} from '@probe.gl/stats-widget';
+import {Tile3DLayer} from '@deck.gl/geo-layers';
 
 // To manage dependencies and bundle size, the app must decide which supporting loaders to bring in
 import {DracoLoader, DracoWorkerLoader} from '@loaders.gl/draco';
-
-import {Tile3DLayer} from '@deck.gl/geo-layers';
 
 // Set your mapbox token here
 const MAPBOX_TOKEN = process.env.MapboxAccessToken; // eslint-disable-line
 const ION_TOKEN =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIxN2NhMzkwYi0zNWM4LTRjNTYtYWE3Mi1jMDAxYzhlOGVmNTAiLCJpZCI6OTYxOSwic2NvcGVzIjpbImFzbCIsImFzciIsImFzdyIsImdjIl0sImlhdCI6MTU2MjE4MTMxM30.OkgVr6NaKYxabUMIGqPOYFe0V5JifXLVLfpae63x-tA';
 
-const TRANSITION_DURAITON = 4000;
 const EXAMPLES_VIEWSTATE = {
   latitude: 40.04248558075302,
   longitude: -75.61213987669433
@@ -34,12 +27,11 @@ export const INITIAL_VIEW_STATE = {
   zoom: 17
 };
 
-export default class App extends PureComponent {
+export class App extends PureComponent {
   constructor(props) {
     super(props);
 
     this.state = {
-      // CURRENT VIEW POINT / CAMERA POSITIO
       viewState: INITIAL_VIEW_STATE,
       attributions: []
     };
@@ -49,34 +41,9 @@ export default class App extends PureComponent {
     this._onTilesetChange = this._onTilesetChange.bind(this);
   }
 
-  async componentDidMount() {
-    const container = this._statsWidgetContainer;
-    // TODO - This is noisy. Default formatters should already be pre-registered on the stats object
-    // TODO - Revisit after upgrade luma to use most recent StatsWidget API
-    this._memWidget = new StatsWidget(lumaStats.get('Memory Usage'), {
-      framesPerUpdate: 1,
-      formatters: {
-        'GPU Memory': 'memory',
-        'Buffer Memory': 'memory',
-        'Renderbuffer Memory': 'memory',
-        'Texture Memory': 'memory'
-      },
-      container
-    });
-
-    this._tilesetStatsWidget = new StatsWidget(null, {container});
-  }
-
-  // Updates stats, called every frame
-  _updateStatWidgets() {
-    this._memWidget.update();
-    this._tilesetStatsWidget.update();
-  }
-
   // Called by Tile3DLayer when a new tileset is loaded
   _onTilesetLoad(tileset) {
     this.setState({attributions: tileset.credits.attributions});
-    this._tilesetStatsWidget.setStats(tileset.stats);
     this._centerViewOnTileset(tileset);
   }
 
@@ -92,41 +59,19 @@ export default class App extends PureComponent {
         latitude: cartographicCenter[1],
         zoom: zoom + 1.5, // TODO - remove adjustment when Tileset3D calculates correct zoom
         bearing: INITIAL_VIEW_STATE.bearing,
-        pitch: INITIAL_VIEW_STATE.pitch,
-
-        // Tells deck.gl to animate the camera move to the new tileset
-        transitionDuration: TRANSITION_DURAITON,
-        transitionInterpolator: new FlyToInterpolator()
+        pitch: INITIAL_VIEW_STATE.pitch
       }
     });
   }
 
   // Called by Tile3DLayer whenever an individual tile in the current tileset is load or unload
   _onTilesetChange(tileHeader) {
-    this._updateStatWidgets();
     this.forceUpdate();
   }
 
   // Called by DeckGL when user interacts with the map
   _onViewStateChange({viewState}) {
     this.setState({viewState});
-  }
-
-  _renderStats() {
-    // TODO - too verbose, get more default styling from stats widget?
-    return (
-      <div
-        style={{
-          position: 'absolute',
-          padding: 12,
-          zIndex: '10000',
-          maxWidth: 300,
-          background: '#000',
-          color: '#fff'
-        }}
-        ref={_ => (this._statsWidgetContainer = _)}
-      />
-    );
   }
 
   _renderTile3DLayer() {
@@ -146,19 +91,17 @@ export default class App extends PureComponent {
   render() {
     const {viewState} = this.state;
     const tile3DLayer = this._renderTile3DLayer();
-    const {mapStyle = 'mapbox://styles/mapbox/light-v9'} = this.props;
+    const {mapStyle = 'mapbox://styles/mapbox/dark-v9'} = this.props;
 
     return (
       <div>
-        {this._renderStats()}
         <DeckGL
           ref={_ => (this._deckRef = _)}
           layers={[tile3DLayer]}
           initialViewState={INITIAL_VIEW_STATE}
           viewState={viewState}
           onViewStateChange={this._onViewStateChange.bind(this)}
-          controller={{type: MapController, maxPitch: 85}}
-          onAfterRender={() => this._updateStatWidgets()}
+          controller={true}
         >
           <StaticMap mapStyle={mapStyle} mapboxApiAccessToken={MAPBOX_TOKEN} preventStyleDiffing />
         </DeckGL>
---
+++ website/src/components/demos/tiles-3d-demo.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import {readableInteger} from '../../utils/format-utils';
+import {MAPBOX_STYLES} from '../../constants/defaults';
+import {App} from 'website-examples/3d-tiles/app';
+
+export default class GeoJsonDemo extends Component {
+  static get data() {
+    return [];
+  }
+
+  static get parameters() {
+    return {};
+  }
+
+  static get mapStyle() {
+    return MAPBOX_STYLES.LIGHT;
+  }
+
+  static renderInfo(meta) {
+    return (
+      <div>
+        <h3>Vancouver Property Value</h3>
+        <p>The property value of Vancouver, BC</p>
+        <p>
+          Height of polygons - average property value per square meter of lot
+          <br />
+          Color - value growth from last assessment
+        </p>
+        <p>
+          Data source:&nbsp;
+          <a href="http://data.vancouver.ca/">City of Vancouver</a>
+        </p>
+        <div className="layout">
+          <div className="stat col-1-2">
+            No. of Polygons
+            <b>{readableInteger(meta.count) || 0}</b>
+          </div>
+          <div className="stat col-1-2">
+            No. of Vertices
+            <b>{readableInteger(meta.vertexCount || 0)}</b>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    return <App {...this.props} />;
+  }
+}
