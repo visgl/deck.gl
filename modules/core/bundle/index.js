087f4cb616917198a4ddc1c66f529c53d50e47f5
Xiaoji Chen <xiaoji@uber.com>
2019-09-13 15:15:39 -0700

    expose loaders.gl endpoints from the core bundle

--- modules/core/bundle/index.js
+++ modules/core/bundle/index.js
@@ -3,13 +3,16 @@ const LumaGL = require('./lumagl');
 const deckGLCore = require('../src');
 
 const DeckGL = require('./deckgl').default;
+const {registerLoaders, load, parse} = require('@loaders.gl/core');
 
 /* global window, global */
 const _global = typeof window === 'undefined' ? global : window;
 _global.deck = _global.deck || {};
 _global.luma = _global.luma || {};
+_global.loaders = _global.loaders || {};
 
 Object.assign(_global.deck, deckGLCore, {DeckGL});
 Object.assign(_global.luma, LumaGL);
+Object.assign(_global.loaders, {registerLoaders, load, parse});
 
 module.exports = _global.deck;
