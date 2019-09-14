d87af62ed065b181e037be5a34bb8b41b5711391
Andrew Duberstein <duberstein@uber.com>
2019-09-13 17:40:41 -0700

    update dependencies

--- modules/jupyter-widget/webpack.config.js
+++ modules/jupyter-widget/webpack.config.js
@@ -43,15 +43,7 @@ const config = {
     ]
   },
   // Packages that shouldn't be bundled but loaded at runtime
-  externals: [
-    '@jupyter-widgets/base',
-    'deck.gl',
-    'mapbox-gl',
-    'h3',
-    'S2',
-    'loaders.gl/core',
-    'loaders.gl/csv'
-  ],
+  externals: ['@jupyter-widgets/base', 'deck.gl', 'mapbox-gl', 'h3', 'S2', 'loaders.gl/csv'],
   plugins: [
     // Uncomment for bundle size debug
     // new (require('webpack-bundle-analyzer')).BundleAnalyzerPlugin()
