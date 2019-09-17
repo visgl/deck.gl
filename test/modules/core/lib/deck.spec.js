58daa2664454eeb1c13ce3dfbcc5e215056e4844
Xiaoji Chen <xiaoji@uber.com>
2019-09-17 16:59:58 -0700

    beta.2

--- test/modules/core/lib/deck.spec.js
+++ test/modules/core/lib/deck.spec.js
@@ -55,7 +55,7 @@ test('Deck#constructor', t => {
   t.pass('Deck constructor did not throw');
 });
 
-test.only('Deck#rendering, picking, logging', t => {
+test('Deck#rendering, picking, logging', t => {
   // Test logging functionalities
   log.priority = 4;
 
