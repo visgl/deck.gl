083539ab69d591a5f207188241df024080dd8d58
Andrew Duberstein <duberstein@uber.com>
2019-09-23 17:29:50 -0700

    Update geojson example

--- bindings/python/pydeck/examples/code/geojson_layer_paths_demo.py
+++ bindings/python/pydeck/examples/code/geojson_layer_paths_demo.py
@@ -17,11 +17,12 @@ import pandas as pd
 
 # pydeck can load data from a Pandas data frame or from a URL
 WHALE_LOCATIONS = 'https://raw.githubusercontent.com/ajduberstein/migration_data/master/whales.csv'
-df = pd.read_csv(WHALE_LOCATIONS)[['timestamp', 'ind_ident', 'lat1', 'lng1']]
+df = pd.read_csv(WHALE_LOCATIONS)[['timestamp', 'ind_ident', 'long', 'lat']]
+viewport = pydeck.data_utils.compute_view(df[['long', 'lat']])
+color_lookup = pydeck.data_utils.assign_random_colors(df['ind_ident'])
+df['color'] = df['ind_ident'].apply(lambda x: color_lookup[x])
 
-viewport = pydeck.data_utils.compute_view(df['lat1', 'lng1'])
-
-GEOJSON_PATH_URL = 'https://raw.githubusercontent.com/ajduberstein/migration_data/master/whales.csv'
+GEOJSON_PATH_URL = 'https://raw.githubusercontent.com/ajduberstein/migration_data/master/whale_paths.geojson'
 
 geojson_layer = pydeck.Layer(
     'GeoJsonLayer',
@@ -29,24 +30,28 @@ geojson_layer = pydeck.Layer(
     stroked=False,
     filled=True,
     extruded=True,
-    line_width_scale=20,
     line_width_min_pixels=2,
-    get_fill_colors=[160, 160, 180, 200],
+    line_width_max_pixels=3,
+    get_line_color=[160, 160, 180, 10],
 )
 
 scatterplot = pydeck.Layer(
     'ScatterplotLayer',
-    GEOJSON_PATH_URL,
+    df,
     stroked=False,
+    get_position='[long, lat]',
     filled=True,
     extruded=True,
     pickable=True,
     auto_highlight=True,
-    radius=10000,
-    get_fill_colors=[0, 160, 180, 200])
+    radius_min_pixels=3,
+    picking_radius=5,
+    radius=1000,
+    get_fill_color='color')
 
 r = pydeck.Deck(
-    layers=[geojson_layer],
-    initial_view_state=viewport)
+    layers=[scatterplot, geojson_layer],
+    initial_view_state=viewport,
+    height=1000)
 
-r.to_html()
+r.to_html('whales.html')
