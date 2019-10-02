ac31b76ecd02f4ef1781eff66366997e4d90d037
Andrew Duberstein <duberstein@uber.com>
2019-10-02 14:15:47 -0700

    Cut comments in plugin.js

--- modules/jupyter-widget/src/plugin.js
+++ modules/jupyter-widget/src/plugin.js
@@ -4,10 +4,6 @@
  * https://github.com/jupyter-widgets/widget-ts-cookiecutter/blob/51e9fed8687e3b9cf1ed2fd307b7675e864f89ae/%7B%7Bcookiecutter.github_project_name%7D%7D/src/plugin.ts
  */
 
-// ES2017 stopgap to get requirejs working with a jupyterlab, so that we don't have to maintain two
-// separate dependency management strategies for the Lab and Notebook ecosystems
-// eslint-disable-next-line
-
 import {IJupyterWidgetRegistry} from '@jupyter-widgets/base';
 
 import * as widgetExports from './widget';
