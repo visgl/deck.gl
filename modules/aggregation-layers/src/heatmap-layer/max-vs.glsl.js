0755d5ccaf5ea18cddde822835e6ddbed5d65fb1
1chandu <chandu@uber.com>
2019-10-03 00:23:01 -0700

    HeatmapLyaer: set point size to address ANGLE bug (#3732)

--- modules/aggregation-layers/src/heatmap-layer/max-vs.glsl.js
+++ modules/aggregation-layers/src/heatmap-layer/max-vs.glsl.js
@@ -6,5 +6,7 @@ void main()
 {
 outTexture = inTexture;
 gl_Position = vec4(0, 0, 0, 1.);
+// Enforce default value for ANGLE issue (https://bugs.chromium.org/p/angleproject/issues/detail?id=3941)
+gl_PointSize = 1.0;
 }
 `;
