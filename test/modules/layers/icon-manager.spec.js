39755a9584b5b0947c5797014c27ae7d42cdb762
Xintong Xia <xintong@uber.com>
2019-11-05 17:44:05 -0800

    Fix IcanLaye autopacking bug

--- test/modules/layers/icon-manager.spec.js
+++ test/modules/layers/icon-manager.spec.js
@@ -1,50 +1,58 @@
 import test from 'tape';
 import {buildMapping, getDiffIcons} from '@deck.gl/layers/icon-layer/icon-manager';
 
-test('IconManager#buildMapping', t => {
-  const data = [
-    {
-      icon: {
-        width: 12,
-        height: 12,
-        anchorY: 12,
-        url: '/icon/0'
-      }
-    },
-    {
-      icon: {
-        width: 24,
-        height: 24,
-        anchorY: 24,
-        url: '/icon/1'
-      }
-    },
-    {
-      icon: {
-        width: 36,
-        height: 36,
-        anchorY: 36,
-        url: '/icon/2'
-      }
-    },
-    {
-      icon: {
-        width: 16,
-        height: 16,
-        anchorY: 16,
-        url: '/icon/3'
-      }
-    },
-    {
-      icon: {
-        width: 28,
-        height: 28,
-        anchorY: 28,
-        url: '/icon/4'
-      }
+const DATA = [
+  {
+    icon: {
+      width: 12,
+      height: 12,
+      anchorY: 12,
+      url: '/icon/0'
     }
-  ];
+  },
+  {
+    icon: {
+      width: 24,
+      height: 24,
+      anchorY: 24,
+      url: '/icon/1'
+    }
+  },
+  {
+    icon: {
+      width: 36,
+      height: 36,
+      anchorY: 36,
+      url: '/icon/2'
+    }
+  },
+  {
+    icon: {
+      width: 16,
+      height: 16,
+      anchorY: 16,
+      url: '/icon/3'
+    }
+  },
+  {
+    icon: {
+      width: 28,
+      height: 28,
+      anchorY: 28,
+      url: '/icon/4'
+    }
+  }
+];
 
+const EXPECTED_MAPPING = {
+  '/icon/0': Object.assign({}, DATA[0].icon, {x: 0, y: 0}),
+  '/icon/1': Object.assign({}, DATA[1].icon, {x: 14, y: 0}),
+  '/icon/2': Object.assign({}, DATA[2].icon, {x: 0, y: 26}),
+  '/icon/3': Object.assign({}, DATA[3].icon, {x: 38, y: 26}),
+  '/icon/4': Object.assign({}, DATA[4].icon, {x: 0, y: 64})
+};
+
+test('IconManager#buildMapping', t => {
   /*
    *   +-----------+----------------+----------------+
    *   | /icon/0   | /icon/1        |                |
@@ -74,24 +82,53 @@ test('IconManager#buildMapping', t => {
    *   +---------------------+-----------------------+
   */
 
-  const expected = {
-    '/icon/0': Object.assign({}, data[0].icon, {x: 0, y: 0}),
-    '/icon/1': Object.assign({}, data[1].icon, {x: 14, y: 0}),
-    '/icon/2': Object.assign({}, data[2].icon, {x: 0, y: 26}),
-    '/icon/3': Object.assign({}, data[3].icon, {x: 38, y: 26}),
-    '/icon/4': Object.assign({}, data[4].icon, {x: 0, y: 64})
-  };
-
-  const {mapping, xOffset, yOffset, canvasHeight} = buildMapping({
-    icons: data.map(d => d.icon),
+  const results = buildMapping({
+    icons: DATA.map(d => d.icon),
     buffer: 2,
     canvasWidth: 64
   });
 
-  t.deepEqual(mapping, expected, 'Should generate mapping as expectation.');
-  t.equal(canvasHeight, 128, 'Canvas height should match expectation.');
-  t.equal(xOffset, 30, 'xOffset should match expectation.');
-  t.equal(yOffset, 64, 'yOffset height should match expectation.');
+  t.deepEqual(results.mapping, EXPECTED_MAPPING, 'Should generate mapping as expectation.');
+  t.equal(results.canvasHeight, 128, 'Canvas height should match expectation.');
+  t.equal(results.xOffset, 30, 'xOffset should match expectation.');
+  t.equal(results.yOffset, 64, 'yOffset height should match expectation.');
+  t.equal(results.rowHeight, 28, 'rowHeight should match expectation.');
+
+  t.end();
+});
+
+test('IconManager#buildMapping with additional icons', t => {
+  const additionalData = [
+    {
+      icon: {
+        width: 36,
+        height: 36,
+        anchorY: 36,
+        url: '/icon/5'
+      }
+    }
+  ];
+
+  const results = buildMapping({
+    mapping: EXPECTED_MAPPING,
+    icons: additionalData.map(d => d.icon),
+    rowHeight: 28,
+    xOffset: 30,
+    yOffset: 64,
+    buffer: 2,
+    canvasWidth: 64
+  });
+
+  const expectedMapping = {
+    ...EXPECTED_MAPPING,
+    '/icon/5': Object.assign({}, additionalData[0].icon, {x: 0, y: 94})
+  };
+
+  t.deepEqual(results.mapping, expectedMapping, 'Should generate mapping as expectation.');
+  t.equal(results.canvasHeight, 256, 'Canvas height should match expectation.');
+  t.equal(results.xOffset, 38, 'xOffset should match expectation.');
+  t.equal(results.yOffset, 94, 'yOffset height should match expectation.');
+  t.equal(results.rowHeight, 36, 'rowHeight height should match expectation.');
 
   t.end();
 });
