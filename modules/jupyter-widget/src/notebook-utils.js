9a1ebaed283108ddd327d0d55297f05f654713dd
Andrew Duberstein <duberstein@uber.com>
2019-11-05 16:57:39 -0800

    Change require to requirejs to see if we can skip some build issues

--- modules/jupyter-widget/src/notebook-utils.js
+++ modules/jupyter-widget/src/notebook-utils.js
@@ -1,20 +1,12 @@
-/* global document, window */
+/* global requirejs */
 /* Supports pydeck in a RequireJS-driven environment, like
  * a Jupyter notebook or pydeck's standalone HTML page.
  */
 import createDeckFromDependencies from './initializer';
 
-const REQUIREJS_URL = 'https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.6/require.min.js';
-
-if (!window.require) {
-  const requirejs = document.createElement('script');
-  requirejs.setAttribute('src', REQUIREJS_URL);
-  document.head.appendChild(requirejs);
-}
-
 export function initDeck({mapboxApiKey, container, jsonInput, tooltip, onComplete, handleClick}) {
-  require(['mapbox-gl', 'h3', 's2Geometry'], mapboxgl => {
-    require(['deck.gl', 'loaders.gl/csv'], (deck, loaders) => {
+  requirejs(['mapbox-gl', 'h3', 's2Geometry'], mapboxgl => {
+    requirejs(['deck.gl', 'loaders.gl/csv'], (deck, loaders) => {
       createDeckFromDependencies({
         dependencies: {deck, loaders, mapboxgl},
         mapboxApiKey,
