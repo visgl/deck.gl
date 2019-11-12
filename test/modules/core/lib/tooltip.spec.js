932d0a0c213d41fd46d1e804a81e8249fa98c2f9
Andrew Duberstein <duberstein@uber.com>
2019-11-11 23:29:47 -0800

    Update tooltip spec

--- test/modules/core/lib/tooltip.spec.js
+++ test/modules/core/lib/tooltip.spec.js
@@ -5,6 +5,10 @@ import DocumentTest from 'deck.gl-test/utils/document';
 
 const pickedInfo = {object: {elevationValue: 10}, x: 0, y: 0};
 
+function createTooltip(canvas) {
+  return new Tooltip(canvas);
+}
+
 function getTooltipFunc(pickedValue) {
   return {
     className: 'coolTooltip',
@@ -22,63 +26,57 @@ function getTooltipFuncDefault(pickedValue) {
 }
 
 test('Tooltip#constructor', t => {
-  const documentTest = new DocumentTest(Tooltip);
-  documentTest.setup();
+  const documentTest = new DocumentTest(createTooltip);
   t.ok(documentTest.document.getElementsByClassName('deck-tooltip'), 'Tooltip exists in document');
   t.equals(documentTest.document.getElementsByClassName('deck-tooltip')[0].style.top, '0px');
-  documentTest.teardown();
+  documentTest.finalize();
   t.end();
 });
 
 test('Tooltip#setTooltip', t => {
-  const documentTest = new DocumentTest(Tooltip);
-  documentTest.setup();
+  const documentTest = new DocumentTest(createTooltip);
   const tooltip = documentTest.testObject;
   tooltip.setTooltip(getTooltipFunc(pickedInfo), pickedInfo.x, pickedInfo.y);
   t.equals(tooltip.el.style.backgroundColor, 'lemonchiffon');
   t.equals(tooltip.el.innerHTML, '<strong>Number of points:</strong> 10');
   t.equals(tooltip.el.className, 'coolTooltip');
-  documentTest.teardown();
+  documentTest.finalize();
   t.end();
 });
 
 test('Tooltip#setTooltipWithString', t => {
-  const documentTest = new DocumentTest(Tooltip);
-  documentTest.setup();
+  const documentTest = new DocumentTest(createTooltip);
   const tooltip = documentTest.testObject;
   const pickedInfoFunc = info => `Number of points: ${info.object.elevationValue}`;
   tooltip.setTooltip(pickedInfoFunc(pickedInfo), pickedInfo.x, pickedInfo.y);
   t.equals(tooltip.el.innerText, 'Number of points: 10');
   t.equals(tooltip.el.className, 'deck-tooltip');
-  documentTest.teardown();
+  documentTest.finalize();
   t.end();
 });
 
 test('Tooltip#setTooltipDefaults', t => {
-  const documentTest = new DocumentTest(Tooltip);
-  documentTest.setup();
+  const documentTest = new DocumentTest(createTooltip);
   const tooltip = documentTest.testObject;
   const tooltipResult = getTooltipFuncDefault(pickedInfo);
   tooltip.setTooltip(tooltipResult, pickedInfo.x, pickedInfo.y);
   t.equals(tooltip.el.innerText, 'Number of points: 10');
   t.equals(tooltip.el.className, 'deck-tooltip');
-  documentTest.teardown();
+  documentTest.finalize();
   t.end();
 });
 
 test('Tooltip#setTooltipNullCase', t => {
-  const documentTest = new DocumentTest(Tooltip);
-  documentTest.setup();
+  const documentTest = new DocumentTest(createTooltip);
   const tooltip = documentTest.testObject;
   tooltip.setTooltip(null, pickedInfo.x, pickedInfo.y);
   t.equals(tooltip.el.style.display, 'none');
-  documentTest.teardown();
+  documentTest.finalize();
   t.end();
 });
 
 test('Tooltip#remove', t => {
-  const documentTest = new DocumentTest(Tooltip);
-  documentTest.setup();
+  const documentTest = new DocumentTest(createTooltip);
   const tooltip = documentTest.testObject;
   t.equals(
     documentTest.document.getElementsByClassName('deck-tooltip').length,
@@ -91,6 +89,6 @@ test('Tooltip#remove', t => {
     0,
     'Tooltip element successfully removed'
   );
-  documentTest.teardown();
+  documentTest.finalize();
   t.end();
 });
