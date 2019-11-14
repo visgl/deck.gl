8e438ffe43500b31135c93c8fc8c9542301b01ce
Andrew Duberstein <duberstein@uber.com>
2019-11-13 16:55:58 -0800

    Update canvas test

--- test/utils/canvas-test.js
+++ test/utils/canvas-test.js
@@ -3,17 +3,24 @@
 /** Create a clean document object for testing */
 function createDocument() {
   // Creates a global document object via JSDOM or a shallow clone of the browser's document
+  let documentClone;
   if (typeof window === undefined || !window.document) {
     const {JSDOM} = require('jsdom');
     const dom = new JSDOM(`<!DOCTYPE html>`);
-    return dom.window.document;
+    documentClone = dom.window.document.cloneNode(false);
+  } else {
+    documentClone = window.document.cloneNode(false);
   }
-  const documentClone = window.document.cloneNode(false);
+  const html = window.document.createElement('html');
+  const body = window.document.createElement('body');
+  html.appendChild(body);
+  documentClone.appendChild(html);
+  documentClone.body = body;
   return documentClone;
 }
 
 /* Create a clean canvas, its parent, and a document node for testing */
-export function setup() {
+export default function setup() {
   // Creates a canvas which can be passed to canvasCallback
   // to add a node to
   const localDocument = createDocument();
