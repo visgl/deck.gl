aeb6e713ec111b5117cbdf591a91531543748d20
Andrew Duberstein <duberstein@uber.com>
2019-11-14 16:18:53 -0800

    Update canvas test to exclude document

--- test/utils/canvas-test.js
+++ test/utils/canvas-test.js
@@ -1,38 +1,13 @@
-/* global window */
+/* global document */
 
-/** Create a clean document object for testing */
-function createDocument() {
-  // Creates a global document object via JSDOM or a shallow clone of the browser's document
-  let documentClone;
-  if (typeof window === undefined || !window.document) {
-    const {JSDOM} = require('jsdom');
-    const dom = new JSDOM(`<!DOCTYPE html>`);
-    documentClone = dom.window.document.cloneNode(false);
-  } else {
-    documentClone = window.document.cloneNode(false);
-  }
-  const html = window.document.createElement('html');
-  const body = window.document.createElement('body');
-  html.appendChild(body);
-  documentClone.appendChild(html);
-  documentClone.body = body;
-  return documentClone;
-}
-
-/* Create a clean canvas, its parent, and a document node for testing */
-export default function setup() {
-  // Creates a canvas which can be passed to canvasCallback
-  // to add a node to
-  const localDocument = createDocument();
-  const canvas = localDocument.createElement('canvas');
-
-  const canvasParent = localDocument.createElement('div');
-  canvasParent.className = 'canvas-parent';
-  canvasParent.appendChild(canvas);
-  localDocument.body.appendChild(canvasParent);
+/* Create a clean canvas and its container testing */
+export default function setupCanvasTest() {
+  const canvas = document.createElement('canvas');
+  const canvasContainer = document.createElement('div');
+  canvasContainer.className = 'canvas-parent';
+  canvasContainer.appendChild(canvas);
   return {
-    testDocument: localDocument,
-    canvasContainer: canvasParent,
+    canvasContainer,
     canvas
   };
 }
