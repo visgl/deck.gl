18bd232c687544e0e2433de592722769d2e83ed5
Travis Gorkin <tgorkin@uber.com>
2019-11-18 14:24:04 -0800

    Port line layer example to ocular gatsby website.  Includes porting over functionality for loading data on worker threads.

---
+++ website-gatsby/src/utils/data-utils.js
@@ -0,0 +1,32 @@
+import {request, json, text} from 'd3-request';
+
+import {StreamParser} from './worker-utils';
+
+export function loadData(url, worker, onSuccess) {
+  if (worker) {
+    const req = request(url);
+    // use a web worker to parse data
+    const dataParser = new StreamParser(worker, (data, meta) => {
+      onSuccess(data, meta);
+    });
+
+    req
+      .on('progress', dataParser.onProgress)
+      .on('load', dataParser.onLoad)
+      .get();
+  } else if (/\.(json|geojson)$/.test(url)) {
+    // load as json
+    json(url, (error, response) => {
+      if (!error) {
+        onSuccess(response, {});
+      }
+    });
+  } else {
+    // load as plain text
+    text(url, (error, response) => {
+      if (!error) {
+        onSuccess(response, {});
+      }
+    });
+  }
+}
