407bfb009ac6ff8f467734e7940a56a3863f0ca4
Andrew Duberstein <duberstein@uber.com>
2019-11-19 02:29:50 -0800

    wip support jupyterlab, no basemap yet

--- modules/jupyter-widget/src/initializer.js
+++ modules/jupyter-widget/src/initializer.js
@@ -1,7 +1,9 @@
+/* global document */
 import makeTooltip from './widget-tooltip';
 
 export default function createDeckFromDependencies({
   dependencies,
+  jsonConverterConfiguration,
   mapboxApiKey,
   container,
   jsonInput,
@@ -12,32 +14,26 @@ export default function createDeckFromDependencies({
 }) {
   try {
     // Filter down to the deck.gl classes of interest
-    const {deck, loaders, mapboxgl} = dependencies;
-    const classesDict = {};
-    const classes = Object.keys(deck).filter(
-      x => (x.indexOf('Layer') > 0 || x.indexOf('View') > 0) && x.indexOf('_') !== 0
-    );
-    classes.map(k => (classesDict[k] = deck[k]));
-
-    loaders.registerLoaders([loaders.CSVLoader]);
+    const {deck, mapboxgl} = dependencies;
 
     const jsonConverter = new deck.JSONConverter({
-      configuration: {
-        classes: classesDict
-      }
+      configuration: jsonConverterConfiguration
     });
 
     const props = jsonConverter.convert(jsonInput);
 
     const getTooltip = makeTooltip(tooltip);
 
-    const deckgl = new deck.DeckGL({
+    container.appendChild(document.createElement('canvas'));
+    const canvas = container.firstElementChild;
+
+    const deckgl = new deck.Deck({
       ...props,
       map: mapboxgl,
       mapboxApiAccessToken: mapboxApiKey,
       onClick: handleClick,
       getTooltip,
-      container
+      canvas
     });
 
     const warn = deck.log.warn;
--- modules/jupyter-widget/src/lab-widget.js
+++ modules/jupyter-widget/src/lab-widget.js
@@ -2,12 +2,71 @@ import {DeckGLModel, DeckGLView} from './widget';
 import createDeckFromDependencies from './initializer';
 
 import * as mapboxgl from 'mapbox-gl';
-import * as loaders from '@loaders.gl/csv';
-import * as deck from 'deck.gl';
 
-function createDeckWithImport(...args) {
-  const dependencies = {deck, loaders, mapboxgl};
-  createDeckFromDependencies(dependencies, ...args);
+import {CSVLoader} from '@loaders.gl/csv';
+import {Tile3DLoader} from '@loaders.gl/3d-tiles';
+import {LASWorkerLoader} from '@loaders.gl/las';
+import * as loaders from '@loaders.gl/core';
+
+import {
+  COORDINATE_SYSTEM,
+  Deck,
+  MapView,
+  FirstPersonView,
+  OrbitView,
+  OrthographicView,
+  log
+} from '@deck.gl/core';
+
+// import {DeckGL} from '@deck.gl/core/bundle';
+import * as Layers from '@deck.gl/layers';
+import * as AggregationLayers from '@deck.gl/aggregation-layers';
+import * as GeoLayers from '@deck.gl/geo-layers';
+import * as MeshLayers from '@deck.gl/mesh-layers';
+import GL from '@luma.gl/constants';
+
+import {JSONConverter} from '@deck.gl/json';
+
+function createDeckWithImport(args) {
+  const jsonConverterConfiguration = {
+    classes: Object.assign(
+      {MapView, FirstPersonView, OrbitView, OrthographicView},
+      Layers,
+      AggregationLayers,
+      GeoLayers,
+      MeshLayers
+    ),
+
+    // Will be resolved as `<enum-name>.<enum-value>`
+    enumerations: {
+      COORDINATE_SYSTEM,
+      GL
+    },
+
+    // Constants that should be resolved with the provided values by JSON converter
+    constants: {
+      Tile3DLoader,
+      LASWorkerLoader
+    }
+  };
+
+  const deck = {
+    log,
+    AggregationLayers,
+    Deck,
+    FirstPersonView,
+    COORDINATE_SYSTEM,
+    GeoLayers,
+    JSONConverter,
+    Layers,
+    MapView,
+    MeshLayers,
+    OrbitView,
+    OrthographicView
+  };
+  const dependencies = {deck, mapboxgl};
+  loaders.registerLoaders([CSVLoader, Tile3DLoader, LASWorkerLoader]);
+  createDeckFromDependencies({dependencies, jsonConverterConfiguration, ...args});
 }
 
 DeckGLView.deckInitFunction = createDeckWithImport;
