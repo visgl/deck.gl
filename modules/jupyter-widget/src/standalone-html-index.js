2562fa7faae4279003ffb58a9106df7291e40b0e
Andrew Duberstein <duberstein@uber.com>
2019-11-19 18:21:03 -0800

    Add standalone route

---
+++ modules/jupyter-widget/src/standalone-html-index.js
@@ -0,0 +1,4 @@
+// Entry point for standalone html bundle
+// Does not expect or need Jupyter to be present
+export {createDeckWithImports} from './create-deck';
+export {MODULE_VERSION, MODULE_NAME} from './version';
