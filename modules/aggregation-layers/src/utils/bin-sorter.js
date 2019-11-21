a458fbf83d6b6d648e528d4e98fafaf1b8c84705
Shan He <heshan0131@gmail.com>
2019-11-21 15:49:53 -0800

    add bin-sorter spec
    Signed-off-by: Shan He <heshan0131@gmail.com>

--- modules/aggregation-layers/src/utils/bin-sorter.js
+++ modules/aggregation-layers/src/utils/bin-sorter.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-undef */
-/* eslint-disable no-console */
 // Copyright (c) 2015 - 2017 Uber Technologies, Inc.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -130,7 +128,7 @@ export default class BinSorter {
     return [this.sortedBins[lowerIdx].value, this.sortedBins[upperIdx].value];
   }
 
-  getValueDomainByScale(scale, [lower, upper]) {
+  getValueDomainByScale(scale, [lower = 0, upper = 100] = []) {
     if (!this.sortedBins.length) {
       return [];
     }
