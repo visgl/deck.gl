c719c06664793cd2e8174259a3641d857df91d11
Xiaoji Chen <xiaoji@uber.com>
2019-11-21 17:08:15 -0800

    revert H3ClusterLayer change

--- modules/geo-layers/src/h3-layers/h3-cluster-layer.js
+++ modules/geo-layers/src/h3-layers/h3-cluster-layer.js
@@ -3,32 +3,6 @@ import {h3SetToMultiPolygon} from 'h3-js';
 import {CompositeLayer, createIterable} from '@deck.gl/core';
 import {PolygonLayer} from '@deck.gl/layers';
 
-// Given a nested GeoJSON-style polygon, returns {positions, holeIndices}
-// This function does not validate or normalize
-function flattenPolygon(polygon) {
-  let vertices = 0;
-  for (const ring of polygon) {
-    vertices += ring.length;
-  }
-
-  const positions = new Float64Array(vertices * 2);
-  const numRings = polygon.length;
-  const holeIndices = numRings > 1 && new Array(numRings - 1);
-
-  let i = 0;
-  for (let ringIndex = 0; ringIndex < numRings; ringIndex++) {
-    if (ringIndex > 1) {
-      holeIndices[ringIndex - 1] = i;
-    }
-    for (const point of polygon[ringIndex]) {
-      positions[i++] = point[0];
-      positions[i++] = point[1];
-    }
-  }
-
-  return holeIndices ? {positions, holeIndices} : positions;
-}
-
 const defaultProps = Object.assign(
   {
     getHexagons: {type: 'accessor', value: d => d.hexagons}
@@ -52,9 +26,7 @@ export default class H3ClusterLayer extends CompositeLayer {
         const multiPolygon = h3SetToMultiPolygon(hexagons, true);
 
         for (const polygon of multiPolygon) {
-          polygons.push(
-            this.getSubLayerRow({polygon: flattenPolygon(polygon)}, object, objectInfo.index)
-          );
+          polygons.push(this.getSubLayerRow({polygon}, object, objectInfo.index));
         }
       }
 
@@ -117,8 +89,6 @@ export default class H3ClusterLayer extends CompositeLayer {
       }),
       {
         data: this.state.polygons,
-        _normalize: false,
-        positionFormat: 'XY',
         getPolygon: d => d.polygon
       }
     );
