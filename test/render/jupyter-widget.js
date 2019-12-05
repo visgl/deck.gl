aa7b59c5a729ad6c62a89a692e464696d37113a3
Andrew Duberstein <duberstein@uber.com>
2019-12-04 16:56:06 -0800

    Add screen diff tests

--- test/render/jupyter-widget.js
+++ test/render/jupyter-widget.js
@@ -24,6 +24,91 @@ const TEST_CASES = [
       ]
     },
     goldenImage: './test/render/golden-images/jupyter-widget-scatterplot.png'
+  },
+  {
+    name: 'ScatterplotLayer and TextLayer',
+    json: {
+      description: 'Test of plotting multiple layers at once',
+      initialViewState: {
+        maxZoom: 20,
+        zoom: 15
+      },
+      layers: [
+        {
+          data: [
+            {
+              position: [-0.002, 0.002],
+              rgb: [136, 45, 97]
+            },
+            {
+              position: [-0.002, -0.002],
+              rgb: [170, 57, 57]
+            },
+            {
+              position: [0.002, -0.002],
+              rgb: [45, 136, 45]
+            },
+            {
+              position: [0.002, 0.002],
+              rgb: [123, 159, 53]
+            }
+          ],
+          getColor: 'rgb',
+          getPosition: 'position',
+          getRadius: 100,
+          type: 'ScatterplotLayer'
+        },
+        {
+          data: [
+            {
+              position: [0, 0],
+              text: 'Test'
+            }
+          ],
+          fontSize: 72,
+          getColor: [255, 255, 255],
+          getPosition: 'position',
+          getTextAnchor: 'end',
+          type: 'TextLayer'
+        }
+      ]
+    },
+    goldenImage: './test/render/golden-images/jupyter-widget-scatterplot-and-text.png'
+  },
+  {
+    json: {
+      description: 'Test of GeoJsonLayer',
+      initialViewState: {
+        longitude: -122.45,
+        latitude: 37.8,
+        zoom: 12
+      },
+      layers: [
+        {
+          type: 'GeoJsonLayer',
+          data: {
+            type: 'FeatureCollection',
+            features: [
+              {
+                type: 'Feature',
+                properties: {},
+                geometry: {
+                  type: 'Point',
+                  coordinates: [-122.42923736572264, 37.80544394934271]
+                }
+              }
+            ]
+          },
+          stroked: true,
+          filled: true,
+          lineWidthMinPixels: 2,
+          opacity: 0.4,
+          getLineColor: [255, 100, 100],
+          getFillColor: [200, 160, 0, 180]
+        }
+      ]
+    },
+    goldenImage: './test/render/golden-images/jupyter-widget-geojsonlayer.png'
   }
 ];
 
