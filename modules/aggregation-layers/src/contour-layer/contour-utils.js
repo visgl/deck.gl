5166894426950f9f9b3c779ac569c0afb237ba50
1chandu <chandu@uber.com>
2019-12-09 15:29:20 -0800

    Remove push/inline functions

--- modules/aggregation-layers/src/contour-layer/contour-utils.js
+++ modules/aggregation-layers/src/contour-layer/contour-utils.js
@@ -14,8 +14,10 @@ export function generateContours({
   const contourPolygons = [];
   const width = gridSize[0];
   const height = gridSize[1];
+  let segmentIndex = 0;
+  let polygonIndex = 0;
 
-  thresholdData.forEach((data) => {
+  for (const data of thresholdData) {
     const {threshold, index} = data;
     for (let x = -1; x < width; x++) {
       for (let y = -1; y < height; y++) {
@@ -42,29 +44,29 @@ export function generateContours({
         if (Array.isArray(threshold)) {
           opts.type = CONTOUR_TYPE.ISO_BANDS;
           const polygons = getVertices(opts);
-          polygons.forEach(polygon => {
-            contourPolygons.push({
+          for (const polygon of polygons) {
+            contourPolygons[polygonIndex++] = {
               vertices: polygon,
               threshold,
               index
-            });
-          });
+            };
+          }
         } else {
           // Get the intersection vertices based on MarchingSquares code.
           opts.type = CONTOUR_TYPE.ISO_LINES;
           const vertices = getVertices(opts);
           for (let i = 0; i < vertices.length; i += 2) {
-            contourSegments.push({
+            contourSegments[segmentIndex++] = {
               start: vertices[i],
               end: vertices[i + 1],
               threshold,
               index
-            });
+            };
           }
         }
       }
     }
-  });
+  }
   return {contourSegments, contourPolygons};
 }
 /* eslint-enable max-depth */
