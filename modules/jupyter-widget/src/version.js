6876f579991ddd5e734e74f1450392f1b766fc78
Andrew Duberstein <ajduberstein@users.noreply.github.com>
2020-01-03 12:17:32 -0800

    @deck.gl/jupyter-widget: Read version and module name from bundle for Jupyter Lab (#4089)

--- modules/jupyter-widget/src/version.js
+++ modules/jupyter-widget/src/version.js
@@ -5,16 +5,6 @@
  * The html widget manager assumes that this is the same as the npm package
  * version number.
  */
-let version;
 
-// JupyterLab path
-if (typeof __VERSION__ === 'undefined') {
-  const deck = require('../dist/index');
-  version = deck.version;
-} else {
-  // Standard build
-  version = __VERSION__;
-}
-
-export const MODULE_VERSION = version;
+export const MODULE_VERSION = __VERSION__;
 export const MODULE_NAME = '@deck.gl/jupyter-widget';
