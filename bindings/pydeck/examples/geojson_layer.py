c0c6fbcab320342679544fd1d83d509656fed1cf
Andrew Duberstein <duberstein@uber.com>
2020-01-27 19:10:07 -0800

    Add geojsonlayer example

---
+++ bindings/pydeck/examples/geojson_layer.py
@@ -0,0 +1,42 @@
+import pydeck
+
+DATA_URL = "https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/geojson/vancouver-blocks.json"
+LAND_COVER = [[[-123.0, 49.196], [-123.0, 49.324], [-123.306, 49.324], [-123.306, 49.196]]]
+
+INITIAL_VIEW_STATE = pydeck.ViewState(
+    latitude=49.254,
+    longitude=-123.13,
+    zoom=11,
+    max_zoom=16,
+    pitch=45,
+    bearing=0
+)
+
+polygon = pydeck.Layer(
+    'PolygonLayer',
+    LAND_COVER,
+    stroked=False,
+    # processes the data as a flat longitude-latitude pair
+    get_polygon='-',
+    get_fill_color=[0, 0, 0, 20]
+)
+
+geojson = pydeck.Layer(
+    'GeoJsonLayer',
+    DATA_URL,
+    opacity=0.8,
+    stroked=False,
+    filled=True,
+    extruded=True,
+    wireframe=True,
+    get_elevation='properties.valuePerSqm / 20',
+    get_fill_color='[255, 255, properties.growth * 255]',
+    get_line_color=[255, 255, 255]
+)
+
+r = pydeck.Deck(
+    layers=[polygon, geojson],
+    initial_view_state=INITIAL_VIEW_STATE
+)
+
+r.to_html('geojson_layer.html', notebook_display=False)
