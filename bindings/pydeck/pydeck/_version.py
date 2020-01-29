e2346694eda73014100368b413f53485b1a24925
Andrew Duberstein <duberstein@uber.com>
2020-01-28 16:20:25 -0800

    pydeck: Update to 0.2.1

--- bindings/pydeck/pydeck/_version.py
+++ bindings/pydeck/pydeck/_version.py
@@ -2,5 +2,5 @@
 
 # Copyright (c) Uber Technologies, Inc.
 # Distributed under the terms of the Modified BSD License.
-version_info = (0, 2, 0)
+version_info = (0, 2, 1)
 __version__ = '.'.join(map(str, version_info))
