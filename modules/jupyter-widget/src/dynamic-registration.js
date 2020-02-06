09f036715112cbdfa2226cb3ed7577514ef76c1b
Andrew Duberstein <duberstein@uber.com>
2020-02-06 10:23:27 -0800

    Use promise syntax

--- modules/jupyter-widget/src/dynamic-registration.js
+++ modules/jupyter-widget/src/dynamic-registration.js
@@ -1,26 +1,28 @@
-/* global console, document, XMLHttpRequest, window */
+/* global document, XMLHttpRequest, window */
 import * as deck from './deck-bundle';
 
-function loadScript({resourceUri, onComplete, onError}) {
+function loadScript(resourceUri) {
   const xhr = new XMLHttpRequest();
   const tag = document.createElement('script');
-  xhr.open('GET', resourceUri, true);
-  xhr.onload = e => {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200) {
-        tag.text = xhr.response;
-        document.head.appendChild(tag);
-        onComplete();
-      } else {
-        onError(xhr.response);
+  return new Promise((onComplete, onError) => {
+    xhr.open('GET', resourceUri, true);
+    xhr.onload = e => {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          tag.text = xhr.response;
+          document.head.appendChild(tag);
+          onComplete();
+        } else {
+          onError(xhr.response);
+        }
       }
-    }
-  };
-  xhr.send();
+    };
+    xhr.send();
+  });
 }
 
 export default function addClassToConverter({jsonConverter, className, resourceUri}) {
-  const onComplete = () => {
+  loadScript(resourceUri).then(res => {
     // Opinionated choice, requires that the user load only one layer at a time
     // and that layer must be the sole default export of the library
     // TODO better choice here?
@@ -28,6 +30,5 @@ export default function addClassToConverter({jsonConverter, className, resourceU
     jsonConverter = new deck.JSONConverter({
       configuration: jsonConverter.configuration
     });
-  };
-  loadScript({resourceUri, onComplete, onError: console.error}); // eslint-disable-line no-console
+  });
 }
