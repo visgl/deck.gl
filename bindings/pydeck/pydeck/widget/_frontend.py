be96de815f17ce176ae00088aab5566094cd2519
Andrew Duberstein <ajduberstein@users.noreply.github.com>
2020-02-11 17:31:46 -0800

    pydeck: Enforce pydeck notebook deck.gl semver on sdist (#4227)

--- bindings/pydeck/pydeck/widget/_frontend.py
+++ bindings/pydeck/pydeck/widget/_frontend.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+from ..frontend_semver import DECKGL_SEMVER
+
 """
 Information about the frontend package of the widget.
 """
@@ -9,4 +11,4 @@ Information about the frontend package of the widget.
 module_name = "@deck.gl/jupyter-widget"
 # module_version is the current version of the module of the JS portion of the widget
 # It appears to be important only for JupyterLab and ignored for Jupyter Notebooks
-module_version = "^8.0.0"
+module_version = DECKGL_SEMVER
