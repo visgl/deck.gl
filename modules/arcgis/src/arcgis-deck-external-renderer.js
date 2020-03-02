4f08098e9e673cd0f4326b2f839eb4f1feb7224f
Dario D'Amico <dario.damico@gmail.com>
2020-03-02 13:02:31 -0800

    [arcgis] Fix .gl and .state access (#4338)

--- modules/arcgis/src/arcgis-deck-external-renderer.js
+++ modules/arcgis/src/arcgis-deck-external-renderer.js
@@ -20,8 +20,8 @@ export default function loadArcGISDeckExternalRenderer(externalRenderers, Collec
     // eslint-disable-next-line
     const dpr = window.devicePixelRatio;
     this.deckFbo = new Framebuffer(gl, {
-      width: Math.round(this.view.state.size[0] * dpr),
-      height: Math.round(this.view.state.size[1] * dpr)
+      width: Math.round(this.view.size[0] * dpr),
+      height: Math.round(this.view.size[1] * dpr)
     });
     this.initializeDeckGL(gl);
     this.deckLayers.on('change', () => {
@@ -31,15 +31,15 @@ export default function loadArcGISDeckExternalRenderer(externalRenderers, Collec
 
   ArcGISDeckExternalRenderer.prototype.setup = setup;
 
-  function render(renderParameters) {
-    const gl = renderParameters.context;
+  function render(context) {
+    const gl = context.gl;
     const screenFbo = gl.getParameter(gl.FRAMEBUFFER_BINDING);
     // eslint-disable-next-line
     const dpr = window.devicePixelRatio;
     this.createOrResizeFramebuffer(
       gl,
-      Math.round(this.view.state.size[0] * dpr),
-      Math.round(this.view.state.size[1] * dpr)
+      Math.round(this.view.size[0] * dpr),
+      Math.round(this.view.size[1] * dpr)
     );
 
     this.deckgl.setProps({
@@ -63,12 +63,7 @@ export default function loadArcGISDeckExternalRenderer(externalRenderers, Collec
         blend: true,
         blendFunc: [gl.ONE, gl.ONE_MINUS_SRC_ALPHA],
         framebuffer: screenFbo,
-        viewport: [
-          0,
-          0,
-          Math.round(this.view.state.size[0] * dpr),
-          Math.round(this.view.state.size[1] * dpr)
-        ]
+        viewport: [0, 0, Math.round(this.view.size[0] * dpr), Math.round(this.view.size[1] * dpr)]
       },
       () => {
         this.model.setUniforms({u_texture: this.deckFbo}).draw();
