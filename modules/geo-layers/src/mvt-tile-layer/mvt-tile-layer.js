a2ace35335d77de72b112ac598f8d8d62ebaa29a
jesusbotella <jesus.botella@gmail.com>
2020-03-09 11:37:40 +0100

    Handle getHighlightedObject in MVTTileLayer

--- modules/geo-layers/src/mvt-tile-layer/mvt-tile-layer.js
+++ modules/geo-layers/src/mvt-tile-layer/mvt-tile-layer.js
@@ -9,7 +9,8 @@ import ClipExtension from './clip-extension';
 
 const defaultProps = Object.assign({}, TileLayer.defaultProps, {
   renderSubLayers: {type: 'function', value: renderSubLayers, compare: false},
-  urlTemplates: {type: 'array', value: [], compare: true}
+  urlTemplates: {type: 'array', value: [], compare: true},
+  uniquePropertyId: {type: 'string', value: '', compare: false}
 });
 
 export default class MVTTileLayer extends TileLayer {
@@ -25,6 +26,44 @@ export default class MVTTileLayer extends TileLayer {
     const tileURL = urlTemplates[tileURLIndex].replace(/\{ *([\w_-]+) *\}/g, templateReplacer);
 
     return await load(tileURL, MVTLoader);
+  }
+
+  onHover(info, pickingEvent) {
+    const {highlightedFeatureId} = this.state;
+    const {uniquePropertyId} = this.props;
+    const hoveredFeature = info.object;
+
+    if (hoveredFeature) {
+      const hoveredFeatureId = hoveredFeature.properties[uniquePropertyId] || hoveredFeature.id;
+
+      if (hoveredFeatureId !== highlightedFeatureId) {
+        this.setState({hoveredFeatureId});
+      }
+    }
+
+    if (this.props.onHover) {
+      return this.props.onHover(info, pickingEvent);
+    }
+
+    return false;
+  }
+
+  getHighlightedObjectIndex(tile) {
+    // TODO: This implementation is faulty because highlightedObjectId
+    // does not correspond to the index within provided data array
+    // but to the picking index
+    const {highlightedFeatureId: featureId} = this.state;
+    const {uniquePropertyId} = this.props;
+    const {tileData} = tile;
+
+    if (!Array.isArray(tileData)) {
+      return -1;
+    }
+
+    return tileData.findIndex(
+      feature =>
+        feature.id ? feature.id === featureId : feature.properties[uniquePropertyId] === featureId
+    );
   }
 }
 
