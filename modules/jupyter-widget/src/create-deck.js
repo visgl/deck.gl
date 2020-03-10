e1ba57de0b3fc8a42900c5c229727bb11f08d060
Ib Green <ib@unfolded.ai>
2020-03-10 15:08:00 -0700

    pydeck: fix repeated script loading (#4376)

--- modules/jupyter-widget/src/create-deck.js
+++ modules/jupyter-widget/src/create-deck.js
@@ -9,7 +9,7 @@ import {registerLoaders} from '@loaders.gl/core';
 
 import * as deck from './deck-bundle';
 
-import {loadScript, alreadyLoaded} from './script-utils';
+import {loadScript} from './script-utils';
 
 import GL from '@luma.gl/constants';
 
@@ -60,11 +60,8 @@ function addCustomLibraries(customLibraries) {
   if (!customLibraries) {
     return;
   }
-  for (const obj of customLibraries) {
-    // obj is an object of the form `{libraryName, resourceUri}`
-    if (!alreadyLoaded(obj.resourceUri)) {
-      loadExternalLibrary(obj);
-    }
+  for (const {libraryName, resourceUri} of customLibraries) {
+    loadExternalLibrary({libraryName, resourceUri});
   }
 }
 
