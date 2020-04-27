2b63cdc7bad934ff6a4f68d12f47b1c066afa107
Xiaoji Chen <cxiaoji@gmail.com>
2020-04-27 12:56:49 -0700

    add harp.gl example

---
+++ examples/get-started/pure-js/harp.gl/decoder.js
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2017-2019 HERE Europe B.V.
+ * Licensed under Apache 2.0, see full license in LICENSE
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {OmvTileDecoderService, OmvTilerService} from '@here/harp-omv-datasource/index-worker';
+
+OmvTileDecoderService.start();
+OmvTilerService.start();
