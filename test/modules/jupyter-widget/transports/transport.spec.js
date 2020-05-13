9d3e4566b36059ec7716336b4f48939231f38c5a
Andrew Duberstein <duberstein@uber.com>
2020-05-13 13:44:57 -0700

    Update test

--- test/modules/jupyter-widget/transports/transport.spec.js
+++ test/modules/jupyter-widget/transports/transport.spec.js
@@ -1,15 +1,23 @@
 import test from 'tape-catch';
 
-import {Transport} from '@deck.gl/jupyter-widget';
+test('@deck.gl/jupyter-widget Transport', t => {
+  let transportModule;
+  try {
+    transportModule = require('@deck.gl/jupyter-widget/transport');
+  } catch (error) {
+    t.comment('dist mode, skipping binary-transport tests');
+    t.end();
+    return;
+  }
 
-// TODO re-activate test
-test('Transport - delayed onInitialize()', t => {
-  const transport = new Transport();
+  t.test('delayed onInitialized()', t0 => {
+    const transport = new transportModule.Transport();
 
-  transport._initialize();
+    transport._initialize();
 
-  transport.setCallbacks({
-    onIntialize: () => t.end()
+    transport.setCallbacks({
+      onIntialize: () => t0.end()
+    });
   });
 
   t.end();
