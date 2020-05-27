b2c81867b00fabadfd9187b12001fd5d3b61c0a8
Andrew Duberstein <ajduberstein@gmail.com>
2020-05-27 12:16:19 -0700

    Remove deepequal

--- examples/playground/src/deck-with-google-maps.js
+++ examples/playground/src/deck-with-google-maps.js
@@ -20,15 +20,6 @@ function injectScript(src) {
   });
 }
 
-function deepEqual(x, y) {
-  const ok = Object.keys;
-  const tx = typeof x;
-  const ty = typeof y;
-  return x && y && tx === 'object' && tx === ty
-    ? ok(x).length === ok(y).length && ok(x).every(key => deepEqual(x[key], y[key]))
-    : x === y;
-}
-
 function loadGoogleMapApi(apiKey, onComplete) {
   const url = `${HOST}?key=${apiKey}&libraries=places`;
   injectScript(url)
