a8a6c401a5274767e7909a8de3d7ef1403c8ffad
Andrew Duberstein <ajduberstein@gmail.com>
2020-06-02 16:59:04 -0700

    update

--- test/modules/jupyter-widget/index.js
+++ test/modules/jupyter-widget/index.js
@@ -1,4 +1,9 @@
-import './binary-transport.spec';
-import './create-deck.spec';
-import './index.spec';
-import './widget-tooltip.spec';
+try {
+  require('./binary-transport.spec');
+  require('./create-deck.spec');
+  require('./index.spec');
+  require('./widget-tooltip.spec');
+} catch (err) {
+  // eslint-disable-next-line no-console,no-undef
+  console.log('Skipping browser tests');
+}
