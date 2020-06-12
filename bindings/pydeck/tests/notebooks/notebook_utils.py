67e7f67a1136ec64d586b7a2b91e4b8085b0e357
Andrew Duberstein <ajduberstein@gmail.com>
2020-06-12 16:53:21 -0700

    Add notebook tests to discourage regressions

--- bindings/pydeck/tests/notebooks/notebook_utils.py
+++ bindings/pydeck/tests/notebooks/notebook_utils.py
@@ -5,8 +5,12 @@ import os
 def nbconvert(notebook_path):
     if not os.path.exists(os.path.exists(notebook_path)):
         raise Exception('Invalid path %s' % notebook_path)
-    CMD = 'jupyter nbconvert --to=html --execute {}'.format(notebook_path)
-    status = subprocess.Popen(CMD, shell=True, check=True)
-    if status != 0:
-        raise Exception('Non-zero exit status during nbconvert for %s' % notebook_path)
-    return True
+    CMD = (
+        "jupyter nbconvert --to=html --ExecutePreprocessor.timeout=600 "
+        "--ExecutePreprocessor.kernel_name='python3' "
+        "--ExecutePreprocessor.store_widget_state=True "
+        "--execute '{}'"
+    )
+    cmd = CMD.format(notebook_path)
+    status = subprocess.check_call(cmd, shell=True)
+    return status == 0
