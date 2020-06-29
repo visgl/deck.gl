410d608b5f36566ee4ba852687ff9d5e6b1885e3
Andrew Duberstein <ajduberstein@gmail.com>
2020-06-29 10:31:02 -0700

    update case

--- bindings/pydeck/pydeck/io/html.py
+++ bindings/pydeck/pydeck/io/html.py
@@ -15,8 +15,7 @@ def in_jupyter():
     try:
         ip = get_ipython()  # noqa
 
-        if ip.has_trait("kernel"):
-            return True
+        return ip.has_trait("kernel")
     except NameError:
         return False
 
