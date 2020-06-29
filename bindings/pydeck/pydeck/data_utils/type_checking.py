352f25655e831f008d9bc707826ccac86734b8af
Andrew Duberstein <ajduberstein@gmail.com>
2020-06-29 00:58:00 -0700

    Add support for geopandas interface

--- bindings/pydeck/pydeck/data_utils/type_checking.py
+++ bindings/pydeck/pydeck/data_utils/type_checking.py
@@ -1,10 +1,16 @@
+def is_geopandas_df(obj):
+    """Check if an object is a GeoPandas DataFrame
+
+    Returns
+    -------
+    bool
+        Returns True if object is a GeoPandas DataFrame and False otherwise
+    """
+    return obj.__class__.__module__ == "geopandas.geodataframe"
+
+
 def is_pandas_df(obj):
     """Check if an object is a Pandas DataFrame
-
-    The benefit here is that Pandas doesn't have to be included
-    in the dependencies for pydeck
-
-    The drawback of course is that the Pandas API might change and break this function
 
     Returns
     -------
