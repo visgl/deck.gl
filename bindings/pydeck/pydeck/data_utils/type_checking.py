a191a057847c7dfba3d3fb83a076606ef5d9845a
Andrew Duberstein <ajduberstein@gmail.com>
2020-06-29 11:02:02 -0700

    Update formatting

--- bindings/pydeck/pydeck/data_utils/type_checking.py
+++ bindings/pydeck/pydeck/data_utils/type_checking.py
@@ -10,18 +10,18 @@ def is_pandas_df(obj):
 
 
 def has_geo_interface(obj):
-    return hasattr(obj, '__geo_interface__')
+    return hasattr(obj, "__geo_interface__")
 
 
 def records_from_geo_interface(data):
     """Un-nest data from object implementing __geo_interface__ standard"""
     flattened_records = []
-    for d in data.__geo_interface__.get('features'):
+    for d in data.__geo_interface__.get("features"):
         record = {}
-        properties = d.get('properties')
-        geom = d.get('geometry')
+        properties = d.get("properties")
+        geom = d.get("geometry")
         for k in properties.keys():
             record[k] = properties[k]
-            record['geometry'] = geom
+            record["geometry"] = geom
         flattened_records.append(record)
     return flattened_records
