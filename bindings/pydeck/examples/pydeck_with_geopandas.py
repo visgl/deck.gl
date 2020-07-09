16623042fadf71c33e162e0068b05f88b846452b
Andrew Duberstein <ajduberstein@gmail.com>
2020-07-08 22:42:08 -0700

    Update format

--- bindings/pydeck/examples/pydeck_with_geopandas.py
+++ bindings/pydeck/examples/pydeck_with_geopandas.py
@@ -1,27 +1,17 @@
 import pydeck
 import geopandas
 
-world = geopandas.read_file(
-    geopandas.datasets.get_path('naturalearth_lowres')
-)
+world = geopandas.read_file(geopandas.datasets.get_path("naturalearth_lowres"))
 
 centroids = geopandas.GeoDataFrame()
-centroids['geometry'] = world.geometry.centroid
-centroids['name'] = world.name
+centroids["geometry"] = world.geometry.centroid
+centroids["name"] = world.name
 
 layers = [
+    pydeck.Layer("GeoJsonLayer", data=world, get_fill_color=[0, 0, 0],),
     pydeck.Layer(
-        'GeoJsonLayer',
-        data=world,
-        get_fill_color=[0, 0, 0],
+        "TextLayer", data=centroids, get_position="geometry.coordinates", get_color=[255, 255, 255], get_text="name"
     ),
-    pydeck.Layer(
-        'TextLayer',
-        data=centroids,
-        get_position='geometry.coordinates',
-        get_color=[255, 255, 255],
-        get_text='name'
-    )
 ]
 
-pydeck.Deck(layers, map_style='').to_html('pydeck-with-geopandas.html')
+pydeck.Deck(layers, map_style="").to_html("pydeck-with-geopandas.html")
