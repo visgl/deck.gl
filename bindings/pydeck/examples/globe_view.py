0b553656d542f7f514cf8825af4775c6836ddd59
Andrew Duberstein <ajduberstein@gmail.com>
2020-07-26 22:35:06 -0700

    Add comment to GlobeView example

--- bindings/pydeck/examples/globe_view.py
+++ bindings/pydeck/examples/globe_view.py
@@ -1,8 +1,16 @@
+"""
+GlobeView
+=========
+
+Over 33,000 power plants of the world plotted by their production capacity (given by height)
+and fuel type (green if renewable) on an experimental deck.gl GlobeView.
+"""
 import pydeck
 import pandas as pd
 
 COUNTRIES = "https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_admin_0_scale_rank.geojson"
 POWER_PLANTS = "https://raw.githubusercontent.com/ajduberstein/geo_datasets/master/global_power_plant_database.csv"
+OCEANS = "https://unpkg.com/@geo-maps/earth-waterbodies-10km@0.6.0/map.geo.json"
 
 df = pd.read_csv(POWER_PLANTS)
 
@@ -15,7 +23,7 @@ def is_green(fuel_type):
 
 df["color"] = df["primary_fuel"].apply(is_green)
 
-view_state = pydeck.ViewState(latitude=51.47, longitude=0.45, zoom=2)
+view_state = pydeck.ViewState(latitude=51.47, longitude=0.45, zoom=2, min_zoom=2)
 
 layers = []
 # Set height and width variables
@@ -29,13 +37,12 @@ layers = [
         data=COUNTRIES,
         stroked=False,
         filled=True,
-        get_line_color=[60, 60, 60],
         get_fill_color=[200, 200, 200],
     ),
     pydeck.Layer(
         "ColumnLayer",
         id="power-plant",
-        data=df,
+        data=POWER_PLANTS,
         get_elevation="capacity_mw",
         get_position=["longitude", "latitude"],
         elevation_scale=100,
@@ -44,15 +51,21 @@ layers = [
         radius=20000,
         get_fill_color="color",
     ),
+    pydeck.Layer(
+        "GeoJsonLayer",
+        id="oceans",
+        data=OCEANS,
+        stroked=False,
+        filled=True,
+        get_fill_color=[0, 120, 200],
+    ),
 ]
 
 deck = pydeck.Deck(
     views=[view],
     initial_view_state=view_state,
     tooltip={"text": "{name}, {primary_fuel} plant, {country}"},
-    layers=layers,
-    # Note that this must be set for the globe to be opaque
-    parameters={"cull": True},
+    layers=layers
 )
 
 deck.to_html("globe_view.html", css_background_color="black")
