1fa670c726af2a9a067abb2b644c3f5ec219b4f4
Clay Anderson <supersonicclay@gmail.com>
2020-08-27 15:24:38 -0600

    Get current layer instance for TileLayer

--- modules/geo-layers/src/tile-layer/tile-layer.js
+++ modules/geo-layers/src/tile-layer/tile-layer.js
@@ -143,7 +143,7 @@ export default class TileLayer extends CompositeLayer {
   // Methods for subclass to override
 
   getTileData(tile) {
-    const {getTileData, fetch, data} = this.props;
+    const {getTileData, fetch, data} = this.getCurrentLayer().props;
     const {signal} = tile;
 
     tile.url = getURLFromTemplate(data, tile);
