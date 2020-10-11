275105e2c58fb308c654a1165215ef5f54c4fd2a
Xiaoji Chen <cxiaoji@gmail.com>
2020-10-10 18:25:51 -0700

    clean up app.js

--- examples/website/election/app.js
+++ examples/website/election/app.js
@@ -46,65 +46,6 @@ export async function renderToDOM(container, options = {}) {
     streetViewControl: false
   });
 
-  const getTooltip = ({object, layer}) => {
-    if (!object) {
-      return null;
-    }
-    const {year} = layer.props;
-    const votes = object[year];
-    return {
-      html: `\
-      <p><b>${object.name}</b></p>
-      <div>
-        Total: ${votes.total}
-        <br/>Democrat: ${((votes.dem / votes.total) * 100).toFixed(1)}%
-        <br/>Republican: ${((votes.rep / votes.total) * 100).toFixed(1)}%
-      </div>`
-    };
-  };
-
-  const renderLayers = ({data = DATA_URL, year = 2016}) => {
-    return [
-      new ScatterplotLayer({
-        data,
-        opacity: 0.7,
-        getPosition: d => [d.longitude, d.latitude],
-        getRadius: d => {
-          const votes = d[year];
-          return votes ? Math.sqrt(votes.total) : 0;
-        },
-        getFillColor: d => {
-          const votes = d[year];
-          const demPercent = (votes.dem / votes.total) * 100;
-          const repPercent = (votes.rep / votes.total) * 100;
-          return demPercent >= repPercent
-            ? demColorScale(demPercent - repPercent + 1)
-            : repColorScale(repPercent - demPercent + 1);
-        },
-        radiusUnits: 'pixels',
-        radiusScale: 0.02,
-        radiusMinPixels: 2,
-
-        pickable: true,
-        autoHighlight: true,
-        highlightColor: [255, 200, 0, 200],
-
-        updateTriggers: {
-          getRadius: year,
-          getFillColor: year
-        },
-        transitions: {
-          getRadius: 1000,
-          getFillColor: 1000
-        },
-
-        // This is not a standard ScatterplotLayer prop
-        // We attach it to the layer to access it in getTooltip
-        year
-      })
-    ];
-  };
-
   const overlay = new DeckOverlay({
     parameters: {
       // Additive blending
@@ -126,4 +67,63 @@ export async function renderToDOM(container, options = {}) {
       overlay.finalize();
     }
   };
+}
+
+function getTooltip({object, layer}) {
+  if (!object) {
+    return null;
+  }
+  const {year} = layer.props;
+  const votes = object[year];
+  return {
+    html: `\
+    <p><b>${object.name}</b></p>
+    <div>
+      Total: ${votes.total}
+      <br/>Democrat: ${((votes.dem / votes.total) * 100).toFixed(1)}%
+      <br/>Republican: ${((votes.rep / votes.total) * 100).toFixed(1)}%
+    </div>`
+  };
+}
+
+function renderLayers({data = DATA_URL, year = 2016}) {
+  return [
+    new ScatterplotLayer({
+      data,
+      opacity: 0.7,
+      getPosition: d => [d.longitude, d.latitude],
+      getRadius: d => {
+        const votes = d[year];
+        return votes ? Math.sqrt(votes.total) : 0;
+      },
+      getFillColor: d => {
+        const votes = d[year];
+        const demPercent = (votes.dem / votes.total) * 100;
+        const repPercent = (votes.rep / votes.total) * 100;
+        return demPercent >= repPercent
+          ? demColorScale(demPercent - repPercent + 1)
+          : repColorScale(repPercent - demPercent + 1);
+      },
+      radiusUnits: 'pixels',
+      radiusScale: 0.02,
+      radiusMinPixels: 2,
+
+      pickable: true,
+      autoHighlight: true,
+      highlightColor: [255, 200, 0, 200],
+
+      updateTriggers: {
+        getRadius: year,
+        getFillColor: year
+      },
+      transitions: {
+        getRadius: 1000,
+        getFillColor: 1000
+      },
+
+      // This is not a standard ScatterplotLayer prop
+      // We attach it to the layer to access it in getTooltip
+      year
+    })
+  ];
 }
