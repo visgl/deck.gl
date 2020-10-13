2d64f4485b94cf4e26d00cbf59e0eae869236a36
Xiaoji Chen <cxiaoji@gmail.com>
2020-10-12 17:46:46 -0700

    Updates

---
+++ workers/election-data-decoder.js
@@ -0,0 +1,46 @@
+"use strict";
+
+importScripts('./util.js');
+var result = [];
+
+onmessage = function onmessage(e) {
+  var lines = e.data.text.split('\n');
+  lines.forEach(function (line) {
+    if (!line) {
+      return;
+    }
+
+    var parts = line.split('\x01');
+    var d = {
+      name: parts[0],
+      longitude: decodeNumber(parts[1], 90, 32) / 1e5 - 180,
+      latitude: decodeNumber(parts[2], 90, 32) / 1e5
+    };
+
+    for (var i = parts.length - 1, year = 2016; i >= 3; i -= 3, year -= 4) {
+      var dem = decodeNumber(parts[i - 2], 90, 32);
+      var rep = decodeNumber(parts[i - 1], 90, 32);
+      var others = decodeNumber(parts[i], 90, 32);
+      d[year] = {
+        dem: dem,
+        rep: rep,
+        total: dem + rep + others
+      };
+    }
+
+    result.push(d);
+  });
+
+  if (e.data.event === 'load') {
+    postMessage({
+      action: 'add',
+      data: result,
+      meta: {
+        count: result.length
+      }
+    });
+    postMessage({
+      action: 'end'
+    });
+  }
+};
---
+++ workers/poi-data-decoder.js
@@ -0,0 +1,41 @@
+"use strict";
+
+importScripts('./util.js');
+var result = [];
+
+onmessage = function onmessage(e) {
+  var lines = e.data.text.split('\n');
+  lines.forEach(function (line) {
+    if (!line) {
+      return;
+    }
+
+    var parts = line.split('\x01');
+    var hex = parts[0];
+
+    for (var i = 1; i < parts.length; i += 3) {
+      var lng = decodeNumber(parts[i], 90, 32) / 1e5 - 180;
+      var lat = decodeNumber(parts[i + 1], 90, 32) / 1e5;
+      var count = decodeNumber(parts[i + 2], 90, 32);
+      result.push({
+        hex: hex,
+        home_lng: lng,
+        home_lat: lat,
+        count: count
+      });
+    }
+  });
+
+  if (e.data.event === 'load') {
+    postMessage({
+      action: 'add',
+      data: result,
+      meta: {
+        count: result.length
+      }
+    });
+    postMessage({
+      action: 'end'
+    });
+  }
+};
