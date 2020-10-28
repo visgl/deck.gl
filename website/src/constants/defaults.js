ec803f84c9192d8af72237936f0f589c9354b849
Iván Moreno <ivan@geographica.gs>
2020-10-28 18:00:42 +0100

    change basemap url to staging

--- website/src/constants/defaults.js
+++ website/src/constants/defaults.js
@@ -1,6 +1,6 @@
 export const MAPBOX_STYLES = {
   LIGHT: 'https://gist.githubusercontent.com/Josmorsot/9001e0dbb01a61a2ddd5dbc1a6c18392/raw/7c273769b2dcd69f1a25f8799373a3d1826df33e/positron-no-labels.json',
-  DARK: 'https://gist.githubusercontent.com/Josmorsot/36354c7fe0e847a559affde83ba36889/raw/6846e70b77c655f516965623d4620505491ad43e/dark-matter-no-labels.json',
+  DARK: 'http://bmp-01.stag.cartodb.net/gl/dark-matter-nolabels-gl-style/style.json',
   BLANK: {
     version: 8, 
     sources: {},
