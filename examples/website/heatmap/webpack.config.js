ee04a30c0024f3340015b9bc46b8285ea525d25a
Alberto Asuero <albertoasuero@gmail.com>
2020-10-28 15:43:30 +0100

    Remove token info from doc

--- examples/website/heatmap/webpack.config.js
+++ examples/website/heatmap/webpack.config.js
@@ -1,8 +1,6 @@
 // NOTE: To use this example standalone (e.g. outside of deck.gl repo)
 // delete the local development overrides at the bottom of this file
 
-const webpack = require('webpack');
-
 const CONFIG = {
   mode: 'development',
 
@@ -27,10 +25,7 @@ const CONFIG = {
         }
       }
     ]
-  },
-
-  // Optional: Enables reading mapbox token from environment variable
-  plugins: [new webpack.EnvironmentPlugin(['MapboxAccessToken'])]
+  }
 };
 
 // This line enables bundling against src in this repo rather than installed module
--- examples/website/icon/README.md
+++ examples/website/icon/README.md
@@ -5,16 +5,6 @@ on [deck.gl](http://deck.gl) website.
 
 Copy the content of this folder to your project. 
 
-To see the base map, you need a [Mapbox access token](https://docs.mapbox.com/help/how-mapbox-works/access-tokens/). You can either set an environment variable:
-
-```bash
-export MapboxAccessToken=<mapbox_access_token>
-```
-
-Or set `MAPBOX_TOKEN` directly in `app.js`.
-
-Other options can be found at [using with Mapbox GL](../../../docs/get-started/using-with-mapbox-gl.md).
-
 ```bash
 # install dependencies
 npm install
