a54371674118c7ded1a39e908cce6a29d51a69a6
Xiaoji Chen <cxiaoji@gmail.com>
2020-11-05 08:54:24 -0800

    Updates

---
+++ workers/contour-data-decoder.js
@@ -0,0 +1,41 @@
+"use strict";
+
+importScripts('./util.js');
+var result = [];
+
+onmessage = function onmessage(e) {
+  var lines = e.data.text.split('\n');
+  lines.forEach(function (line) {
+    if (!line) {
+      return;
+    }
+
+    var parts = line.split('\x01');
+    var d = {
+      longitude: decodeNumber(parts[0], 90, 32) / 1e5 - 180,
+      latitude: decodeNumber(parts[1], 90, 32) / 1e5,
+      population: decodeNumber(parts[2], 90, 32),
+      casesByWeek: {}
+    };
+    var firstWeek = decodeNumber(parts[3], 90, 32);
+
+    for (var i = 4, week = firstWeek; i < parts.length; i++, week++) {
+      d.casesByWeek[week] = decodeNumber(parts[i], 90, 32);
+    }
+
+    result.push(d);
+  });
+
+  if (e.data.event === 'load') {
+    postMessage({
+      action: 'add',
+      data: result,
+      meta: {
+        count: result.length
+      }
+    });
+    postMessage({
+      action: 'end'
+    });
+  }
+};
