b8ab3d9eb582b4ab9e368448d627349c28a792c0
Andrew Duberstein <ajduberstein@gmail.com>
2020-11-14 21:50:02 -0800

    Update format

--- bindings/pydeck/tests/bindings/test_view.py
+++ bindings/pydeck/tests/bindings/test_view.py
@@ -5,4 +5,4 @@ from pydeck import View
 
 def test_view_constructor():
     EXPECTED = '{"@@type": "MapView", "controller": false, "repeat": true}'
-    assert View(type='MapView', controller=False, repeat=True).to_json() == EXPECTED
+    assert View(type="MapView", controller=False, repeat=True).to_json() == EXPECTED
