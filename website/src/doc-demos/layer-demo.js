247d47079df9261ed5ac9c3b8cd2ec367dd620ca
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2020-12-01 11:37:59 -0800

    [website] update embedded demos in documentation (#5204) (#5205)
    * [website] update gatsby theme (#5204)
    * [website] update embedded demos in documentation (#5205)

--- website/src/doc-demos/layer-demo.js
+++ website/src/doc-demos/layer-demo.js
@@ -1,11 +1,9 @@
-import React, {Component} from 'react';
+import React from 'react';
 import DeckGL from '@deck.gl/react';
 import {StaticMap} from 'react-map-gl';
 import styled from 'styled-components';
 
 import {MAPBOX_STYLES} from '../constants/defaults';
-import {getLayerParams, ASYNC_ORIGINAL} from '../utils/layer-params';
-import makeExample from '../components/example';
 
 const INITIAL_VIEW_STATE = {
   longitude: -122.4,
@@ -15,6 +13,10 @@ const INITIAL_VIEW_STATE = {
   pitch: 30,
   bearing: 0
 };
+
+const initialViewStateSerialized = JSON.stringify(INITIAL_VIEW_STATE, null, 2)
+  .replace(/"/g, '')  // remove quotes
+  .replace(/\n/g, '\n  ');  // add indent
 
 const TOOLTIP_STYLE = {
   padding: '4px',
@@ -26,128 +28,161 @@ const TOOLTIP_STYLE = {
 };
 
 const DemoPlaceholder = styled.div`
-height: 30vh;
+height: 50vh;
 min-height: 200px;
-transition: height 300ms;
-pointer-events: none;
 
-&:hover {
-  height: 80vh;
-}
 @media screen and (max-width: 768px) {
-  &:hover {
-    height: 60vh;
-  }
+  height: 60vh;
 }
 `;
 
-const DemoOuterContainer = styled.div`
-height: 30vh;
+const DemoContainer = styled.div`
+height: 50vh;
 min-height: 200px;
-transition: height 300ms;
 position: absolute;
 width: 100%;
 left: 0;
 top: 0;
 overflow: hidden;
 
-&:hover {
-  height: 80vh;
-}
-@media screen and (max-width: 768px) {
-  &:hover {
-    height: 60vh;
-  }
-}
-`;
-
-const DemoInnerContainer = styled.div`
-position: absolute;
-width: 100%;
-height: 80vh;
-pointer-events: all;
-
-#deckgl-wrapper {
-  transform: translateY(-25%);
-  transition: transform 300ms;
-}
-&:hover #deckgl-wrapper {
-  transform: translateY(0);
-}
 @media screen and (max-width: 768px) {
   height: 60vh;
 }
 `;
 
-export default function makeLayerDemo({layer, getTooltip, parameters, mapStyle = MAPBOX_STYLES.LIGHT}) {
+const DemoSourceLink = styled.div`
+position: absolute;
+top: 0;
+right: 0;
+padding: 8px;
+background: #fff;
+margin: 12px;
+box-shadow: 0 2px 4px rgba(0,0,0,0.3);
+cursor: pointer;
+font-weight: bold;
+font-size: 12px;
 
-  const renderLayer = (params, props = {}) => {
-    if (params) {
-      Object.keys(params).forEach(key => {
-        props[key] = params[key].value;
-        if (key.startsWith('get')) {
-          props.updateTriggers = props.updateTriggers || {};
-          props.updateTriggers[key] = props[key];
-        }
-      });
-    }
+&:hover {
+  color: ${props => props.theme.colors.primary};
+}
 
-    return layer.clone(props);
-  };
+svg {
+  width: 20px;
+  vertical-align: middle;
+  margin-right: 4px;
+}
+`;
 
-  class DemoClass extends Component {
-    static parameters = getLayerParams(layer, parameters);
+function gotoSource(config) {
+  const {Layer, getTooltip, props, mapStyle = MAPBOX_STYLES.LIGHT, dependencies = [], loaders, imports} = config;
 
-    static title = layer.constructor.layerName;
-
-    static renderInfo() {
-      const name = layer.constructor.layerName;
-      const dataUrl = layer.props[ASYNC_ORIGINAL].data;
-
-      return (
-        <div>
-          <p>
-            Explore {name}
-            's API <br />
-            {dataUrl && (
-              <a href={dataUrl} target="_new">
-                Sample data
-              </a>
-            )}
-          </p>
-        </div>
-      );
-    }
-
-    _getTooltip(pickedInfo) {
-      if (!pickedInfo.picked) {
-        return null;
-      }
-      return {
-        text: getTooltip ? getTooltip(pickedInfo) : pickedInfo.index,
-        style: TOOLTIP_STYLE
-      };
-    }
-
-    render() {
-      const {params} = this.props;
-      const layers = [renderLayer(params)];
-
-      return (
-        <DeckGL
-          pickingRadius={5}
-          initialViewState={INITIAL_VIEW_STATE}
-          getTooltip={this._getTooltip}
-          controller={true}
-          layers={layers}
-        >
-          {mapStyle && <StaticMap reuseMaps mapStyle={mapStyle} preventStyleDiffing={true} />}
-        </DeckGL>
-      );
-    }
+  const symbols = ['DeckGL', Layer.layerName];
+  if (imports) {
+    symbols.push(...Object.keys(imports));
   }
 
-  const Demo = makeExample(DemoClass);
+  // https://blog.codepen.io/documentation/prefill/
+  const source = `\
+/*
+ * ${window.location.href}
+ */
+const {${symbols.join(', ')}} = deck;
+${loaders ? `\
+const {${['registerLoaders', ...loaders].join(', ')}} = loaders;
 
-  return () => <DemoPlaceholder><DemoOuterContainer><DemoInnerContainer><Demo /></DemoInnerContainer></DemoOuterContainer></DemoPlaceholder>;
+registerLoaders([${loaders.join(', ')}]);` : ''}
+
+const layer = new ${Layer.layerName}(${props});
+
+new DeckGL({
+  ${mapStyle ? `mapStyle: 'https://basemaps.cartocdn.com/gl/positron-gl-style/style.json',` : ''}
+  initialViewState: ${initialViewStateSerialized},
+  controller: true,
+  ${getTooltip ? `getTooltip: ${getTooltip},` : ''}
+  layers: [layer]
+});
+  `;
+  
+  const formData = {
+    js_external: dependencies.concat([
+      'https://unpkg.com/deck.gl@latest/dist.min.js',
+      'https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.js'
+    ]).join(';'),
+    title: `deck.gl ${Layer.layerName}`,
+    parent: 48721472,
+    tags: ['webgl', 'data visualization'],
+    editors: '001',
+    css: `
+  body {
+    margin: 0;
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+  }
+  
+  .deck-tooltip {
+    font-size: 0.8em;
+    font-family: Helvetica, Arial, sans-serif;
+  }
+  `,
+    js: source
+  };
+
+  const form = document.createElement('form');
+  form.action = 'https://codepen.io/pen/define/';
+  form.method = 'POST';
+  form.style.display = 'none';
+  document.body.appendChild(form);
+  const input = document.createElement('input');
+  input.type = 'text';
+  input.name = 'data';
+  input.value = JSON.stringify(formData);
+  form.appendChild(input);
+  window.open('', 'deck-example-codepen');
+  form.target = 'deck-example-codepen';
+  form.submit();
+  form.remove();
+}
+
+export default function makeLayerDemo(config) {
+  const {Layer, getTooltip, props, mapStyle = MAPBOX_STYLES.LIGHT, imports} = config;
+
+  function Demo() {
+    const _getTooltip = getTooltip && eval(getTooltip);
+    const styledGetTooltip = pickingInfo => {
+      const text = _getTooltip && _getTooltip(pickingInfo);
+      return text && {
+        text,
+        style: TOOLTIP_STYLE
+      };
+    };
+
+    const layerProps = eval(`(function getProps(globals){
+      const _global = typeof global === 'undefined' ? self : global;
+      Object.assign(_global, globals);
+      return ${props};
+    })`)(imports);
+    const layer = new Layer(layerProps);
+
+    return (
+      <DemoPlaceholder>
+        <DemoContainer>
+          <DeckGL
+            pickingRadius={5}
+            initialViewState={INITIAL_VIEW_STATE}
+            getTooltip={styledGetTooltip}
+            controller={true}
+            layers={[layer]}
+          >
+            {mapStyle && <StaticMap reuseMaps mapStyle={mapStyle} preventStyleDiffing={true} />}
+          </DeckGL>
+        </DemoContainer>
+        <DemoSourceLink onClick={() => gotoSource(config)}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" ><path d="M0 0h24v24H0V0z" fill="none"/><path d="M9.4 16.6L4.8 12l4.6-4.6L8 6l-6 6 6 6 1.4-1.4zm5.2 0l4.6-4.6-4.6-4.6L16 6l6 6-6 6-1.4-1.4z"/></svg>
+          Edit on Codepen
+        </DemoSourceLink>
+      </DemoPlaceholder>
+    );
+  }
+  return React.memo(Demo);
 }
