fe5878d1620adb32417708935af6cf758ec54d0c
Andrew Duberstein <ajduberstein@gmail.com>
2020-12-13 17:00:02 -0800

    Add Carto layer example and update layer basemaps

--- bindings/pydeck/examples/geojson_layer.py
+++ bindings/pydeck/examples/geojson_layer.py
@@ -5,14 +5,14 @@ GeoJsonLayer
 Property values in Vancouver, Canada, adapted from the deck.gl example pages. Input data is in a GeoJSON format.
 """
 
-import pydeck
+import pydeck as pdk
 
 DATA_URL = "https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/geojson/vancouver-blocks.json"
 LAND_COVER = [[[-123.0, 49.196], [-123.0, 49.324], [-123.306, 49.324], [-123.306, 49.196]]]
 
-INITIAL_VIEW_STATE = pydeck.ViewState(latitude=49.254, longitude=-123.13, zoom=11, max_zoom=16, pitch=45, bearing=0)
+INITIAL_VIEW_STATE = pdk.ViewState(latitude=49.254, longitude=-123.13, zoom=11, max_zoom=16, pitch=45, bearing=0)
 
-polygon = pydeck.Layer(
+polygon = pdk.Layer(
     "PolygonLayer",
     LAND_COVER,
     stroked=False,
@@ -21,7 +21,7 @@ polygon = pydeck.Layer(
     get_fill_color=[0, 0, 0, 20],
 )
 
-geojson = pydeck.Layer(
+geojson = pdk.Layer(
     "GeoJsonLayer",
     DATA_URL,
     opacity=0.8,
@@ -34,6 +34,6 @@ geojson = pydeck.Layer(
     get_line_color=[255, 255, 255],
 )
 
-r = pydeck.Deck(layers=[polygon, geojson], initial_view_state=INITIAL_VIEW_STATE)
+r = pdk.Deck(layers=[polygon, geojson], initial_view_state=INITIAL_VIEW_STATE)
 
 r.to_html("geojson_layer.html")
