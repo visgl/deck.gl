eabe4e7defdb075159ab1c253160f7c9812ff613
Alberto Asuero <albertoasuero@gmail.com>
2021-01-15 20:07:44 +0100

    Wrong message with rebase

--- test/modules/carto/mock-fetch.js
+++ test/modules/carto/mock-fetch.js
@@ -37,15 +37,3 @@ export function mockFetchMapsV1() {
 export function restoreFetch(fetch) {
   _global.fetch = fetch;
 }
-© 2021 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
