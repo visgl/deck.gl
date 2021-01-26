2955ade217535456150b56ac3dec8e986474fb85
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2021-01-25 18:08:18 -0800

    Fix S2Layer handling of coordinates across the antimeridian (#5378)

--- test/modules/geo-layers/s2-layer.spec.js
+++ test/modules/geo-layers/s2-layer.spec.js
@@ -84,7 +84,10 @@ test('S2Layer#getS2Polygon', t => {
     '6b', // face 3
     'ab', // face 5
     '4/001003',
-    new Long(0, -2138636288, false)
+    '54', // antimeridian
+    '5c', // antimeridian
+    new Long(0, -2138636288, false),
+    new Long(0, 1832910848, false)
   ];
 
   for (const token of TEST_TOKENS) {
@@ -92,6 +95,14 @@ test('S2Layer#getS2Polygon', t => {
     t.ok(polygon instanceof Float64Array, 'polygon is flat array');
     t.is((polygon.length / 2 - 1) % 4, 0, 'polygon has 4 sides');
     t.deepEqual(polygon.slice(0, 2), polygon.slice(-2), 'polygon is closed');
+
+    let minLng = 180;
+    let maxLng = -180;
+    for (let i = 0; i < polygon.length; i += 2) {
+      minLng = Math.min(minLng, polygon[i]);
+      maxLng = Math.max(maxLng, polygon[i]);
+    }
+    t.ok(maxLng - minLng < 180, 'longitude is adjusted cross the antimeridian');
   }
 
   t.end();
