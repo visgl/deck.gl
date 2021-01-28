d3de9c90564bb5c79f33dba7ed62019f3b24d68b
Xiaoji Chen <cxiaoji@gmail.com>
2021-01-28 02:52:58 -0800

    Fix SunLight calculation bugs

--- modules/core/src/effects/lighting/sun-light.js
+++ modules/core/src/effects/lighting/sun-light.js
@@ -9,8 +9,17 @@ export default class SunLight extends DirectionalLight {
   }
 
   getProjectedLight({layer}) {
-    const {latitude, longitude} = layer.context.viewport;
-    this.direction = getSunlightDirection(this.timestamp, latitude, longitude);
+    const {viewport} = layer.context;
+    const isGlobe = viewport.resolution > 0;
+
+    if (isGlobe) {
+      // Rotate vector to align with the direction of the globe projection (up at lon:0,lat:0 is [0, -1, 0])
+      const [x, y, z] = getSunlightDirection(this.timestamp, 0, 0);
+      this.direction = [x, -z, y];
+    } else {
+      const {latitude, longitude} = viewport;
+      this.direction = getSunlightDirection(this.timestamp, latitude, longitude);
+    }
 
     return this;
   }
--- test/modules/core/effects/lighting/sunlight.spec.js
+++ test/modules/core/effects/lighting/sunlight.spec.js
@@ -2,9 +2,10 @@
 import test from 'tape-catch';
 import {getSolarPosition} from '@deck.gl/core/effects/lighting/suncalc';
 import {equals, config} from 'math.gl';
-import {MapView, PolygonLayer} from 'deck.gl';
+import {WebMercatorViewport, _GlobeViewport as GlobeViewport, PolygonLayer} from 'deck.gl';
 import {_SunLight as SunLight} from '@deck.gl/core';
-import * as FIXTURES from 'deck.gl-test/data';
+
+import {angle} from 'gl-matrix/vec3';
 
 const MS_A_HOUR = 3.6e6;
 
@@ -75,31 +76,102 @@ test('Sunlight#Constructor', t => {
 });
 
 test('Sunlight#getProjectedLight', t => {
+  const testCases = [
+    {
+      title: 'Tropic of Cancer on Summer Solstice at noon',
+      timestamp: new Date('2021-06-20 12:00:00Z').getTime(),
+      viewport: new WebMercatorViewport({
+        longitude: 0,
+        latitude: 23.43655,
+        zoom: 10
+      }),
+      // Sun should be pointing straight down
+      expected: [0, 0, -1]
+    },
+    {
+      title: 'Tropic of Capricorn on Summer Solstice at midnight',
+      timestamp: new Date('2021-06-20 00:00:00Z').getTime(),
+      viewport: new WebMercatorViewport({
+        longitude: 0,
+        latitude: -23.43655,
+        zoom: 10
+      }),
+      // Sun should be pointing straight up
+      expected: [0, 0, 1]
+    },
+    {
+      title: 'Sunrise, Seattle',
+      timestamp: new Date('2021-01-28 07:47:00Z-8').getTime(),
+      viewport: new WebMercatorViewport({
+        longitude: -122.3258,
+        latitude: 47.6347,
+        zoom: 10
+      }),
+      // Sun should be at horizon from the southeast
+      expected: [-0.8871, 0.4616, 0]
+    },
+    {
+      title: 'Sunset, Seattle',
+      timestamp: new Date('2021-01-28 17:00:00Z-8').getTime(),
+      viewport: new WebMercatorViewport({
+        longitude: -122.3258,
+        latitude: 47.6347,
+        zoom: 10
+      }),
+      // Sun should be at horizon from the southwest
+      expected: [0.8894, 0.457, 0]
+    },
+    {
+      title: 'Equator on Spring Equinox at noon - Globe',
+      timestamp: new Date('2021-03-20 12:00:00Z').getTime(),
+      viewport: new GlobeViewport({
+        longitude: -122.45,
+        latitude: 37.78,
+        zoom: 10
+      }),
+      // Sun should be pointing straight down at lon: 0, lat: 0
+      expected: [0, 1, 0]
+    },
+    {
+      title: 'Sunrise, London - Globe',
+      timestamp: new Date('2021-01-28 07:43:00Z').getTime(),
+      viewport: new GlobeViewport({
+        longitude: -122.45,
+        latitude: 37.78,
+        zoom: 10
+      }),
+      // Sun should be at horizon from the southeast
+      expected: [-0.8784, 0.3617, 0.3123]
+    },
+    {
+      title: 'Sunset, London - Globe',
+      timestamp: new Date('2021-01-28 16:43:00Z').getTime(),
+      viewport: new GlobeViewport({
+        longitude: -122.45,
+        latitude: 37.78,
+        zoom: 10
+      }),
+      // Sun should be at horizon from the southwest
+      expected: [0.8773, 0.3659, 0.3107]
+    }
+  ];
+
   const sunLight = new SunLight({
-    timestamp: new Date('2019-08-01 15:00:00 Z-7').getTime(),
+    timestamp: 0,
     color: [255, 255, 255],
     intensity: 1.0
   });
 
-  const viewport = new MapView().makeViewport({
-    width: 100,
-    height: 100,
-    viewState: {longitude: -122, latitude: 37, zoom: 13}
-  });
+  const layer = new PolygonLayer({});
+  layer.context = {};
 
-  const layer = new PolygonLayer({
-    data: FIXTURES.polygons.slice(0, 3),
-    getPolygon: f => f,
-    getFillColor: (f, {index}) => [index, 0, 0]
-  });
+  for (const testCase of testCases) {
+    sunLight.timestamp = testCase.timestamp;
+    layer.context.viewport = testCase.viewport;
+    const projectedLight = sunLight.getProjectedLight({layer});
+    t.comment(projectedLight.direction.join(','));
+    t.ok(angle(projectedLight.direction, testCase.expected) < 0.05, testCase.title);
+  }
 
-  layer.context = {viewport};
-
-  const projectedLight = sunLight.getProjectedLight({layer});
-  t.deepEqual(
-    projectedLight.direction,
-    [0.8448592259153318, 0.5349886806145961, -0.8670767876117542],
-    'Sun light is ok'
-  );
   t.end();
 });
