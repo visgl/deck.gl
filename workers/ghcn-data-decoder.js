0725f1f73b3be320f2c90b6c6919053aa3722dd2
Xiaoji Chen <cxiaoji@gmail.com>
2021-02-01 13:00:39 -0800

    Updates

---
+++ workers/ghcn-data-decoder.js
@@ -0,0 +1,66 @@
+"use strict";
+
+importScripts('./util.js');
+var result = [];
+var countries = {};
+var vertices = 0;
+
+onmessage = function onmessage(e) {
+  var lines = e.data.text.split('\n');
+  lines.forEach(function (line) {
+    if (!line) {
+      return;
+    }
+
+    if (line[2] === ' ') {
+      countries[line.slice(0, 2)] = line.slice(3);
+      return;
+    }
+
+    var parts = line.split('\t');
+    var startYear = decodeNumber(parts[3], 90, 32);
+    var meanTemp = [];
+
+    for (var i = 4; i < parts.length; i++) {
+      if (parts[i]) {
+        var year = startYear + i - 4;
+        var value = usePrecision(40 - decodeNumber(parts[i], 90, 32) / 100, 2);
+        meanTemp.push([year, value]);
+        vertices++;
+      }
+    }
+
+    result.push({
+      id: parts[0].slice(0, 11),
+      country: countries[parts[0].slice(0, 2)],
+      name: parts[0].slice(11),
+      latitude: usePrecision(decodeNumber(parts[1], 90, 32) / 1e4 - 90, 4),
+      altitude: usePrecision(decodeNumber(parts[2], 90, 32) / 10 - 500, 1),
+      meanTemp: meanTemp
+    });
+  });
+
+  if (e.data.event === 'load') {
+    flush();
+    postMessage({
+      action: 'end'
+    });
+  }
+};
+
+function flush() {
+  postMessage({
+    action: 'add',
+    data: result,
+    meta: {
+      stations: result.length,
+      vertices: vertices
+    }
+  });
+  result = [];
+}
+
+function usePrecision(x, precision) {
+  var m = Math.pow(10, precision);
+  return Math.round(x * m) / m;
+}
---
+++ workers/radio-data-decoder.js
@@ -0,0 +1,51 @@
+"use strict";
+
+importScripts('./util.js');
+var result = [];
+
+onmessage = function onmessage(e) {
+  var lines = e.data.text.split('\n');
+  lines.forEach(function (line) {
+    if (!line) {
+      return;
+    }
+
+    var parts = line.split('\t');
+
+    if (parts.length < 5) {
+      return;
+    }
+
+    var type = parts[0][0] + 'M';
+    var frequency = decodeNumber(parts[0].slice(1, 3), 90, 32);
+    if (type === 'FM') frequency /= 10;
+    result.push({
+      type: type,
+      frequency: frequency,
+      callSign: parts[0].slice(3),
+      name: parts[4],
+      state: parts[3].slice(0, 2),
+      city: parts[3].slice(2),
+      longitude: decodeNumber(parts[1], 90, 32) / 1e5 - 180,
+      latitude: decodeNumber(parts[2], 90, 32) / 1e5 - 90
+    });
+  });
+
+  if (e.data.event === 'load') {
+    flush();
+    postMessage({
+      action: 'end'
+    });
+  }
+};
+
+function flush() {
+  postMessage({
+    action: 'add',
+    data: result,
+    meta: {
+      count: result.length
+    }
+  });
+  result = [];
+}
