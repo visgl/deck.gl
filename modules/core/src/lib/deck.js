e5a6b7d3800ae6d3b3bafcbd725c246cd58651de
Igor Dykhta <igorDykhta@users.noreply.github.com>
2021-03-22 23:31:49 +0200

    [feat] WebGL context lost handling > pass to listeners of onError (#5399)
    Co-authored-by: Ib Green <ib@unfolded.ai>

--- modules/core/src/lib/deck.js
+++ modules/core/src/lib/deck.js
@@ -133,7 +133,7 @@ const defaultProps = {
   onBeforeRender: noop,
   onAfterRender: noop,
   onLoad: noop,
-  onError: null,
+  onError: (error, ...args) => log.error(...args, error)(),
   _onMetrics: null,
 
   getCursor,
@@ -175,7 +175,6 @@ export default class Deck {
     this._onPointerDown = this._onPointerDown.bind(this);
     this._onPointerMove = this._onPointerMove.bind(this);
     this._pickAndCallback = this._pickAndCallback.bind(this);
-    this._onRendererInitialized = this._onRendererInitialized.bind(this);
     this._onRenderFrame = this._onRenderFrame.bind(this);
     this._onViewStateChange = this._onViewStateChange.bind(this);
     this._onInteractionStateChange = this._onInteractionStateChange.bind(this);
@@ -496,8 +495,16 @@ export default class Deck {
       autoResizeDrawingBuffer,
       autoResizeViewport: false,
       gl,
-      onCreateContext: opts => createGLContext({...glOptions, ...opts, canvas: this.canvas, debug}),
-      onInitialize: this._onRendererInitialized,
+      onCreateContext: options =>
+        createGLContext({
+          ...glOptions,
+          ...options,
+          canvas: this.canvas,
+          debug,
+          onContextLost: event => this.props.onError(event)
+        }),
+      // eslint-disable-next-line no-shadow
+      onInitialize: ({gl}) => this._setGLContext(gl),
       onRender: this._onRenderFrame,
       onBeforeRender: props.onBeforeRender,
       onAfterRender: props.onAfterRender,
@@ -702,10 +709,6 @@ export default class Deck {
   }
 
   // Callbacks
-
-  _onRendererInitialized({gl}) {
-    this._setGLContext(gl);
-  }
 
   _onRenderFrame(animationProps) {
     this._getFrameStats();
