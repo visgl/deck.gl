7b1a98015fc7f86262c22bf3b57e4b969de2c43c
AdriSolid <biotekgis@gmail.com>
2021-04-30 10:40:34 +0200

    test: add cartosqllayer

--- test/modules/carto/carto-sql-layer.spec.js
+++ test/modules/carto/carto-sql-layer.spec.js
@@ -1,9 +1,10 @@
 import test from 'tape-catch';
 import {testLayer, testLayerAsync, generateLayerTests} from '@deck.gl/test-utils';
-import {CartoSQLLayer} from '@deck.gl/carto';
-import {mockFetchMapsV1, mockFetchMapsV2, restoreFetch} from './mock-fetch';
 import {makeSpy} from '@probe.gl/test-utils';
+import {setConfig, CartoSQLLayer} from '@deck.gl/carto';
 import {MVTLayer} from '@deck.gl/geo-layers';
+import {log} from '@deck.gl/core';
+import {mockFetchMapsV2, restoreFetch} from './mock-fetch';
 
 test('CartoSQLLayer', t => {
   const testCases = generateLayerTests({
@@ -16,131 +17,61 @@ test('CartoSQLLayer', t => {
   t.end();
 });
 
-test('CartoSQLLayer#updateTileJSON', t => {
-  const fetch = mockFetchMapsV2();
-
-  const testCases = [
-    {
-      spies: ['updateTileJSON'],
-      onAfterUpdate({spies}) {
-        t.notOk(spies.updateTileJSON.called, 'no data, no map instantiation');
-        t.ok(spies.updateTileJSON.callCount === 0);
-      }
-    },
-    {
-      updateProps: {data: 'table_name'},
-      spies: ['updateTileJSON'],
-      onAfterUpdate({spies}) {
-        t.ok(spies.updateTileJSON.called, 'initial data triggers map instantiation');
-        t.ok(spies.updateTileJSON.callCount === 1);
-      }
-    },
-    {
-      updateProps: {data: 'table_name'},
-      spies: ['updateTileJSON'],
-      onAfterUpdate({spies}) {
-        t.ok(
-          spies.updateTileJSON.callCount === 0,
-          'same data does not trigger a new map instantiation'
-        );
-      }
-    },
-    {
-      updateProps: {data: 'ANOTHER_TABLE'},
-      spies: ['updateTileJSON'],
-      onAfterUpdate({spies}) {
-        t.ok(
-          spies.updateTileJSON.callCount === 1,
-          'different data triggers a new map instantiation'
-        );
-      }
-    }
-  ];
-
-  testLayer({Layer: CartoSQLLayer, testCases, onError: t.notOk});
-  t.end();
-
-  restoreFetch(fetch);
-});
-
-test('CartoSQLLayer#Maps API', async t => {
-  const fetchMock = mockFetchMapsV1();
-
-  const spy = makeSpy(MVTLayer.prototype, 'getTileData');
-  spy.returns([]);
-
-  const onAfterUpdate = ({layer, subLayers, subLayer}) => {
-    const {tilejson} = layer.state;
-    if (!tilejson) {
-      t.is(subLayers.length, 0, 'should no render subLayers');
-    } else {
-      t.is(subLayers.length, 1, 'should render a subLayer');
-      t.ok(Array.isArray(tilejson.tiles), 'tiles should be an array');
-      t.ok(subLayer instanceof MVTLayer, 'subLayer should be a MVT layer ');
-      t.is(subLayer.props.data, tilejson, 'data should be a tileJSON');
-    }
-  };
+test('CartoSQLLayer#should throws an error due to invalid mode param', t => {
+  makeSpy(log, 'assert');
 
   const testCases = [
     {
       props: {
-        data: 'table',
-        credentials: {
-          mapsUrl: 'https://server/api/v1/map'
-        }
+        mode: 'not-allowed-mode',
+        data: 'SELECT * FROM table'
       },
-      onBeforeUpdate: () => {
-        t.comment('Maps API V1');
-      },
-      onAfterUpdate
-    },
-    {
-      updateProps: {
-        data: 'table2',
-        credentials: null
-      },
-      onBeforeUpdate: () => {
-        t.comment('Maps API V2');
-      },
-      onAfterUpdate
+      onAfterUpdate: () => {
+        t.ok(
+          log.assert.called,
+          'should produce an assert message if CartoSQLLayer mode is not allowed'
+        );
+      }
     }
   ];
 
-  await testLayerAsync({Layer: CartoSQLLayer, testCases, onError: t.notOk});
+  testLayer({Layer: CartoSQLLayer, testCases, onError: t.ok});
 
-  spy.restore();
+  log.assert.restore();
+
   t.end();
-
-  restoreFetch(fetchMock);
 });
 
-test('CartoSQLLayer#onDataLoad', async t => {
+test('CartoSQLLayer#should render a MVTLayer as sublayer', async t => {
+  makeSpy(log, 'warn');
   const fetchMock = mockFetchMapsV2();
 
-  const spy = makeSpy(MVTLayer.prototype, 'getTileData');
-  spy.returns([]);
-
-  let counter = 0;
-  const onDataLoad = () => {
-    counter++;
-  };
-
   const testCases = [
     {
       props: {
-        data: 'table',
-        onDataLoad
+        data: 'SELECT * FROM table'
       },
-      onAfterUpdate: ({layer}) => {
-        if (layer.isLoaded) t.is(counter, 1, 'should call once to onDataLoad');
+      onBeforeUpdate: () => {
+        setConfig({
+          mode: 'carto',
+          mapsUrl: 'whatever-map-url',
+          accessToken: 'whatever-access-token'
+        });
+      },
+      onAfterUpdate: ({layer, subLayer, subLayers}) => {
+        t.ok(log.warn.called, 'should produce an assert message regarding the layer deprecation.');
+
+        if (subLayer) {
+          t.ok(subLayer instanceof MVTLayer, 'should be a MVTLayer layer');
+        }
       }
     }
   ];
 
   await testLayerAsync({Layer: CartoSQLLayer, testCases, onError: t.notOk});
 
-  spy.restore();
-  t.end();
-
+  log.warn.restore();
   restoreFetch(fetchMock);
+
+  t.end();
 });
