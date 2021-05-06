5764c2d73f9e563f5dbeb81fe4d95714e2f10b12
Xiaoji Chen <cxiaoji@gmail.com>
2021-05-06 15:57:52 -0700

    fix test

--- test/modules/geo-layers/tile-layer/utils.spec.js
+++ test/modules/geo-layers/tile-layer/utils.spec.js
@@ -412,11 +412,17 @@ test('tileToBoundingBox#Infovis', t => {
 });
 
 test('urlType', t => {
-  t.ok(urlType.validate(urlType.value), 'default value is validated');
-  t.ok(urlType.validate('https://server.com/{z}/{x}/{y}.png'), 'string is validated');
-  t.ok(urlType.validate(['https://server.com/{z}/{x}/{y}.png']), 'array of string is validated');
-  t.notOk(urlType.validate(null), 'is not valid');
-  t.notOk(urlType.validate(['https://server.com/{z}/{x}/{y}.png', null]), 'is not valid');
+  t.ok(urlType.validate('https://server.com/{z}/{x}/{y}.png', urlType), 'string is validated');
+  t.ok(
+    urlType.validate(['https://server.com/{z}/{x}/{y}.png'], urlType),
+    'array of string is validated'
+  );
+  t.notOk(urlType.validate(urlType.value, urlType), 'unspecified value is not valid');
+  t.ok(
+    urlType.validate(urlType.value, {...urlType, optional: true}),
+    'unspecified value is valid if optional:true'
+  );
+  t.notOk(urlType.validate(['https://server.com/{z}/{x}/{y}.png', null], urlType), 'is not valid');
 
   t.ok(urlType.equals('', ''), 'should be equal');
   t.ok(
