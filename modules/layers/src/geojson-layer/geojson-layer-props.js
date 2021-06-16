a123c8f4785ea5c9e31d330edf99ae5268f64e85
Felix Palmer <felixpalmer@gmail.com>
2021-06-16 13:49:39 +0200

    Length is always based on polygonIndices

--- modules/layers/src/geojson-layer/geojson-layer-props.js
+++ modules/layers/src/geojson-layer/geojson-layer-props.js
@@ -79,7 +79,8 @@ export function createLayerPropsFromBinary(geojsonBinary, uniqueIdProperty, enco
   layerProps.lines._pathType = 'open';
 
   layerProps.polygons.data = {
-    startIndices: polygons.primitivePolygonIndices.value,
+    length: polygons.polygonIndices.value.length - 1,
+    startIndices: polygons.polygonIndices.value,
     attributes: {
       getPolygon: polygons.positions,
       pickingColors: {
@@ -94,9 +95,6 @@ export function createLayerPropsFromBinary(geojsonBinary, uniqueIdProperty, enco
   layerProps.polygons._normalize = false;
   if (polygons.triangles) {
     layerProps.polygons.data.attributes.indices = polygons.triangles.value;
-    layerProps.polygons.data.length = polygons.polygonIndices.value.length - 1;
-  } else {
-    layerProps.polygons.data.length = polygons.primitivePolygonIndices.value.length - 1;
   }
 
   layerProps.polygonsOutline.data = {
