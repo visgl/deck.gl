0fb6424169af682ffc1c8372abb17fd95bfa365d
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2021-07-01 15:12:32 -0700

    MVTLayer use all loaders (#5934)

--- modules/geo-layers/src/mvt-layer/mvt-layer.js
+++ modules/geo-layers/src/mvt-layer/mvt-layer.js
@@ -99,9 +99,9 @@ export default class MVTLayer extends TileLayer {
     let loadOptions = this.getLoadOptions();
     const {binary, fetch} = this.props;
     const {signal, x, y, z} = tile;
-    const loaders = this.props.loaders[0];
     loadOptions = {
       ...loadOptions,
+      mimeType: 'application/x-protobuf',
       mvt: {
         ...loadOptions?.mvt,
         coordinates: this.context.viewport.resolution ? 'wgs84' : 'local',
@@ -113,7 +113,7 @@ export default class MVTLayer extends TileLayer {
       },
       gis: binary ? {format: 'binary'} : {}
     };
-    return fetch(url, {propName: 'data', layer: this, loaders, loadOptions, signal});
+    return fetch(url, {propName: 'data', layer: this, loadOptions, signal});
   }
 
   renderSubLayers(props) {
