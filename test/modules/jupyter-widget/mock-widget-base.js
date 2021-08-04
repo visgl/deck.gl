4cb89ec3c86ded0c2b11b6827467d325e9c02d81
Andrew Duberstein <andrew.duberstein@instacart.com>
2021-08-04 13:21:48 -0700

    Expand DummyWidget class for @deck.gl/jupyter-widget tests

--- test/modules/jupyter-widget/mock-widget-base.js
+++ test/modules/jupyter-widget/mock-widget-base.js
@@ -1,23 +1,53 @@
-class BlankClass {
-  constructor(attributes = {}) {
-    Object.assign(attributes, this.defaults());
-    this.model = new Map(Object.entries(attributes));
-    this.model.widget_manager = {};
-    this.el = {appendChild: x => x};
+/* global window*/
+const widgets = require('@jupyter-widgets/base');
+
+// Adapted from https://github.com/jupyter-widgets/widget-ts-cookiecutter/blob/f8c4f1d2c61374811423eb724b9eb11c6e2f10b1/%7B%7Bcookiecutter.github_project_name%7D%7D/src/__tests__/utils.ts#L106
+export class DummyManager extends widgets.ManagerBase {
+  constructor() {
+    super();
+    this.el = window.document.createElement('div');
+    this.testClasses = {};
   }
 
-  defaults() {
-    return {};
+  display_view(msg, view, options) {
+    return Promise.resolve(view).then(v => {
+      this.el.appendChild(v.el);
+      v.on('remove', () => {});
+      return v.el;
+    });
+  }
+
+  loadClass(className, moduleName, moduleVersion) {
+    if (moduleName === '@jupyter-widgets/base') {
+      if (widgets[className]) {
+        return Promise.resolve(widgets[className]);
+      }
+      return Promise.reject(`Cannot find class ${className}`);
+    } else if (moduleName === 'jupyter-datawidgets') {
+      if (this.testClasses[className]) {
+        return Promise.resolve(this.testClasses[className]);
+      }
+      return Promise.reject(`Cannot find class ${className}`);
+    }
+    return Promise.reject(`Cannot find module ${moduleName}`);
+  }
+
+  _get_comm_info() {
+    return Promise.resolve({});
+  }
+
+  _create_comm() {
+    return Promise.resolve({});
   }
 }
 
-class DOMWidgetModel extends BlankClass {}
-class DOMWidgetView extends BlankClass {}
-class ManagerBase extends BlankClass {}
-
-export {DOMWidgetModel, DOMWidgetView, ManagerBase};
-
 export function createTestModel(constructor, attributes) {
-  const obj = new constructor(attributes);
-  return obj.model;
+  const id = widgets.uuid();
+  const widget_manager = new DummyManager();
+  const modelOptions = {
+    widget_manager,
+    model_id: id
+  };
+
+  return new constructor(attributes, modelOptions);
 }
