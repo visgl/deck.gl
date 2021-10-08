ee3e970d090a7e4af07394854e6e427e0259dde1
felixpalmer <felixpalmer@gmail.com>
2021-10-08 10:35:14 +0100

    Fix text in Google example (#6277)

--- website/src/examples/google-maps.js
+++ website/src/examples/google-maps.js
@@ -1,26 +1,26 @@
 import React, {Component, createRef} from 'react';
 import {GITHUB_TREE} from '../constants/defaults';
-import {renderToDOM} from 'website-examples/election/app';
+import {renderToDOM} from 'website-examples/google-3d/app';
 
 import makeExample from '../components/example';
 
 class GoogleMapsDemo extends Component {
-  static title = 'US Presidential Election 2000-2016';
-  
-  static code = `${GITHUB_TREE}/examples/website/google-3d`;
+  static title = 'Animated Truck Routes';
 
-  static parameters = {
-    year: {displayName: 'Year', type: 'range', value: 2016, step: 4, min: 2000, max: 2016}
-  };
+  static code = `${GITHUB_TREE}/examples/website/google-3d`;
 
   static renderInfo(meta) {
     return (
       <div>
         <p>Interleaving 3D layers with Google Maps</p>
 
-        <p><a href="https://sketchfab.com/3d-models/low-poly-truck-98826ebd44e2492298ac925461509216">Low Poly Truck</a>
-        model by <a href="https://sketchfab.com/Arifido._">Arifido._</a>
-        licensed under <a href="http://creativecommons.org/licenses/by/4.0/">CC-BY-4.0</a></p>
+        <p>
+          <a href="https://sketchfab.com/3d-models/low-poly-truck-98826ebd44e2492298ac925461509216">
+            Low Poly Truck
+          </a>{' '}
+          model by <a href="https://sketchfab.com/Arifido._">Arifido._</a> licensed under{' '}
+          <a href="http://creativecommons.org/licenses/by/4.0/">CC-BY-4.0</a>
+        </p>
       </div>
     );
   }
