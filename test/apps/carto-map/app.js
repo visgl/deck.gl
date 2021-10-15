968ee6e7f5e597a13f93ba87eb6ded610c5c1399
Felix Palmer <felixpalmer@gmail.com>
2021-10-15 11:07:24 +0100

    Load in points

--- test/apps/carto-map/app.js
+++ test/apps/carto-map/app.js
@@ -1,4 +1,3 @@
-/* global document, google */
 import {CartoLayer, setDefaultCredentials, API_VERSIONS, MAP_TYPES} from '@deck.gl/carto';
 import {GoogleMapsOverlay} from '@deck.gl/google-maps';
 
@@ -15,22 +14,25 @@ setDefaultCredentials({
   apiBaseUrl: 'https://gcp-us-east1.api.carto.com'
 });
 
-const powerLayer = new CartoLayer({
-  id: 'power-lines',
+const layer = new CartoLayer({
+  id: 'retail',
   connection: 'bigquery',
-  type: MAP_TYPES.TILESET,
-  data: 'cartobq.nexus_demo.transmission_lines_tileset_simplified',
-  getLineColor: [83, 135, 185, 180],
-  filled: false,
-  stroked: true,
-  lineWidthMinPixels: 1,
+  type: MAP_TYPES.TABLE,
+  data: 'cartobq.public_account.retail_stores',
   credentials: {
     accessToken:
-      'eyJhbGciOiJIUzI1NiJ9.eyJhIjoiYWNfN3hoZnd5bWwiLCJqdGkiOiJjOTVkODFlZCJ9.6T9_NBCsS5t-MJXDHLLkSSRmh6Pyqrutr9NbdrL5YN8'
-  }
+      'eyJhbGciOiJIUzI1NiJ9.eyJhIjoiYWNfN3hoZnd5bWwiLCJqdGkiOiJiMTlmNTkyNyJ9.A0mFn-Lb5J50aw65BoaKLxjZDWKN_5zH7qYqQ29zsxg'
+  },
+
+  filled: true,
+  stroked: false,
+  pointType: 'circle',
+  pointRadiusUnits: 'pixels',
+  getPointRadius: 3,
+  getFillColor: [255, 0, 0, 255]
 });
 
 const overlay = new GoogleMapsOverlay({
-  layers: [powerLayer]
+  layers: [layer]
 });
 overlay.setMap(map);
