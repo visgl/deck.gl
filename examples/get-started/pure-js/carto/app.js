401ff3b60d9a483e72e72890f6f4b989d143931b
Felix Palmer <felixpalmer@gmail.com>
2021-11-25 11:36:45 +0100

    Update scripting example

--- examples/get-started/pure-js/carto/app.js
+++ examples/get-started/pure-js/carto/app.js
@@ -2,11 +2,12 @@ import mapboxgl from 'mapbox-gl';
 import {Deck} from '@deck.gl/core';
 import {fetchMap} from '@deck.gl/carto';
 
-const cartoMapId = 'ff6ac53f-741a-49fb-b615-d040bc5a96b8';
-const deck = new Deck({canvas: 'deck-canvas'});
+const cartoMapId = '52dcbaa4-f84c-4fc8-93a9-628ed859f937';
 
 // Get map info from CARTO and update deck
 fetchMap({cartoMapId}).then(({initialViewState, mapStyle, layers}) => {
+  const deck = new Deck({canvas: 'deck-canvas', controller: true, initialViewState, layers});
+
   // Add Mapbox GL for the basemap. It's not a requirement if you don't need a basemap.
   const MAP_STYLE = `https://basemaps.cartocdn.com/gl/${mapStyle.styleType}-gl-style/style.json`;
   const map = new mapboxgl.Map({container: 'map', style: MAP_STYLE, interactive: false});
@@ -16,6 +17,4 @@ fetchMap({cartoMapId}).then(({initialViewState, mapStyle, layers}) => {
       map.jumpTo({center: [longitude, latitude], ...rest});
     }
   });
-
-  deck.setProps({controller: true, initialViewState, layers});
 });
