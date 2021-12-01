a8b510b5ab7da03736f29b148820021029690035
Ib Green <ib@unfolded.ai>
2021-11-30 17:30:14 -0800

    chore: TNYC config for tpescript coverage

---
+++ nyc.config.js
@@ -0,0 +1,12 @@
+// luma.gl, MIT license
+module.exports = {
+  extends: '@istanbuljs/nyc-config-typescript',
+  all: 'true',
+  sourceMap: false,
+  instrument: true,
+  extensions: ['.ts'],
+  include: ['modules/**/src'],
+  exclude: [
+    'modules/test-utils/'
+  ]
+};
