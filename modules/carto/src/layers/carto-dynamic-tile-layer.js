bd657c1342be8b250c0aed1c3aebe2dbb13ca14e
felixpalmer <felixpalmer@gmail.com>
2021-12-17 10:37:24 +0100

    CartoLayer support for dynamic tiled maps (geojson) (#6509)

---
+++ modules/carto/src/layers/carto-dynamic-tile-layer.js
@@ -0,0 +1,87 @@
+/* global TextDecoder */
+import {log} from '@deck.gl/core';
+import {ClipExtension} from '@deck.gl/extensions';
+import {MVTLayer} from '@deck.gl/geo-layers';
+import {GeoJsonLayer} from '@deck.gl/layers';
+import {geojsonToBinary} from '@loaders.gl/gis';
+import {encodeParameter} from '../api/maps-api-common';
+import {getURLFromTemplate} from '@deck.gl/geo-layers/tile-layer/utils';
+
+function parseJSON(arrayBuffer) {
+  return JSON.parse(new TextDecoder().decode(arrayBuffer));
+}
+
+function parseCartoDynamicTile(arrayBuffer, options) {
+  if (!arrayBuffer) return null;
+  const formatTiles = options && options.cartoDynamicTile && options.cartoDynamicTile.formatTiles;
+  if (formatTiles === 'geojson') return geojsonToBinary(parseJSON(arrayBuffer).features);
+  return null;
+}
+
+const CartoDynamicTileLoader = {
+  name: 'CARTO Dynamic Tile',
+  id: 'cartoDynamicTile',
+  module: 'carto',
+  extensions: ['pbf'],
+  mimeTypes: ['application/x-protobuf'],
+  category: 'geometry',
+  worker: false,
+  parse: async (arrayBuffer, options) => parseCartoDynamicTile(arrayBuffer, options),
+  parseSync: parseCartoDynamicTile,
+  options: {
+    cartoDynamicTile: {
+      formatTiles: 'geojson'
+    }
+  }
+};
+
+export default class CartoDynamicTileLayer extends MVTLayer {
+  getTileData(tile) {
+    let url = getURLFromTemplate(this.state.data, tile);
+    if (!url) {
+      return Promise.reject('Invalid URL');
+    }
+
+    let loadOptions = this.getLoadOptions();
+    const {fetch, formatTiles} = this.props;
+    const {signal} = tile;
+
+    loadOptions = {
+      ...loadOptions,
+      mimeType: 'application/x-protobuf'
+    };
+
+    if (formatTiles) {
+      log.assert(
+        ['geojson'].includes(formatTiles),
+        `Invalid value for formatTiles: ${formatTiles}`
+      );
+      url += `&${encodeParameter('formatTiles', formatTiles)}`;
+      loadOptions.cartoBinaryTile = {formatTiles};
+    }
+
+    return fetch(url, {propName: 'data', layer: this, loadOptions, signal});
+  }
+
+  renderSubLayers(props) {
+    if (props.data === null) {
+      return null;
+    }
+
+    props.autoHighlight = false;
+
+    const {
+      bbox: {west, south, east, north}
+    } = props.tile;
+    props.extensions = [new ClipExtension()];
+    props.clipBounds = [west, south, east, north];
+
+    const subLayer = new GeoJsonLayer({
+      ...props
+    });
+    return subLayer;
+  }
+}
+
+CartoDynamicTileLayer.layerName = 'CartoDynamicTileLayer';
+CartoDynamicTileLayer.defaultProps = {...MVTLayer.defaultProps, loaders: [CartoDynamicTileLoader]};
---
+++ test/apps/carto-dynamic-tile/app.js
@@ -0,0 +1,76 @@
+/* global document */
+/* eslint-disable no-console */
+import React, {useState} from 'react';
+import {render} from 'react-dom';
+import DeckGL from '@deck.gl/react';
+import {ClipExtension} from '@deck.gl/extensions';
+import {MVTLayer} from '@deck.gl/geo-layers';
+import {CartoLayer, FORMATS, MAP_TYPES} from '@deck.gl/carto';
+import {GeoJsonLayer} from '@deck.gl/layers';
+
+const INITIAL_VIEW_STATE = {longitude: -73.95643, latitude: 40.8039, zoom: 9};
+const COUNTRIES =
+  'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_admin_0_scale_rank.geojson';
+
+const apiBaseUrl = 'https://direct-gcp-us-east1.api.carto.com';
+const connection = 'alasarr';
+const table = 'cartodb-gcp-backend-data-team.dynamic_tiling.polygons_3k_usacounty_viz';
+
+const accessToken = 'XXX';
+
+const showBasemap = true;
+const showCarto = true;
+
+function Root() {
+  return (
+    <>
+      <DeckGL
+        initialViewState={INITIAL_VIEW_STATE}
+        controller={true}
+        layers={[showBasemap && createBasemap(), showCarto && createCarto()]}
+      />
+    </>
+  );
+}
+
+function createBasemap() {
+  return new GeoJsonLayer({
+    id: 'base-map',
+    data: COUNTRIES,
+    // Styles
+    stroked: true,
+    filled: true,
+    lineWidthMinPixels: 2,
+    opacity: 0.4,
+    getLineColor: [60, 60, 60],
+    getFillColor: [200, 200, 200]
+  });
+}
+
+function createCarto() {
+  return new CartoLayer({
+    id: 'carto',
+    connection,
+    data: table,
+    credentials: {accessToken},
+
+    // Dynamic tiling. Request TILEJSON format with TABLE
+    type: MAP_TYPES.TABLE,
+    format: FORMATS.TILEJSON,
+    formatTiles: 'geojson', // 'geojson' only until binary is implemented
+
+    // Styling
+    getFillColor: [233, 71, 251],
+    getElevation: 1000,
+    // extruded: true,
+    stroked: true,
+    filled: true,
+    pointType: 'circle',
+    pointRadiusUnits: 'pixels',
+    lineWidthMinPixels: 0.5,
+    getPointRadius: 1.5,
+    getLineColor: [0, 0, 200]
+  });
+}
+
+render(<Root />, document.body.appendChild(document.createElement('div')));
