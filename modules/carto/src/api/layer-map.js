4a39dd8444127f47f0c5b4f7857c5de83c046e11
padawannn <jaragon@cartodb.com>
2022-01-19 15:56:19 +0100

    in carto module, method to get domain for scales takes into account the scale type

--- modules/carto/src/api/layer-map.js
+++ modules/carto/src/api/layer-map.js
@@ -104,6 +104,11 @@ function domainFromAttribute(attribute, scaleType) {
 function domainFromValues(values, scaleType) {
   if (scaleType === 'ordinal') {
     return [...new Set(values)].sort();
+  } else if (scaleType === 'quantile') {
+    return values.sort((a, b) => a - b);
+  } else if (scaleType === 'log') {
+    const [d0, d1] = extent(values);
+    return [d0 === 0 ? 1e-5 : d0, d1];
   }
   return extent(values);
 }
