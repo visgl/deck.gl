83ced6c0d0aa48f4f4d5679f59f5d975683decff
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2022-04-27 23:49:27 +0200

    Export CartoLayerLibrary in carto module for pydeck integration

--- modules/carto/bundle.ts
+++ modules/carto/bundle.ts
@@ -7,4 +7,7 @@ if (!deck.LineLayer) {
   throw new Error('@deck.gl/layers is not found');
 }
 
+// Export carto layer library for pydeck integration
+globalThis.CartoLayerLibrary = {CartoLayer: CartoUtils.CartoLayer};
+
 module.exports = Object.assign(deck, {carto: CartoUtils});
