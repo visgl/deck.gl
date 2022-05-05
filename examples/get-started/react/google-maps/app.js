e35d21e0d8011b001a59bfb7aa946b706f742a6d
dereklowlind <38629827+dereklowlind@users.noreply.github.com>
2022-05-05 09:09:57 -0700

    update code to follow react patterns

--- examples/get-started/react/google-maps/app.js
+++ examples/get-started/react/google-maps/app.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect, useRef, useState, useMemo} from 'react';
 import {render} from 'react-dom';
 import {GeoJsonLayer, ArcLayer} from 'deck.gl';
 import {Wrapper, Status} from '@googlemaps/react-wrapper';
@@ -12,75 +12,66 @@ const AIR_PORTS =
 const GOOGLE_MAPS_API_KEY = process.env.GoogleMapsAPIKey; // eslint-disable-line
 const GOOGLE_MAP_ID = process.env.GoogleMapsMapId; // eslint-disable-line
 
-const overlay = new DeckOverlay({
-  layers: [
-    new GeoJsonLayer({
-      id: 'airports',
-      data: AIR_PORTS,
-      // Styles
-      filled: true,
-      pointRadiusMinPixels: 2,
-      pointRadiusScale: 2000,
-      getPointRadius: f => 11 - f.properties.scalerank,
-      getFillColor: [200, 0, 80, 180],
-      // Interactive props
-      pickable: true,
-      autoHighlight: true,
-      onClick: info =>
-        // eslint-disable-next-line
-        info.object && alert(`${info.object.properties.name} (${info.object.properties.abbrev})`)
-    }),
-    new ArcLayer({
-      id: 'arcs',
-      data: AIR_PORTS,
-      dataTransform: d => d.features.filter(f => f.properties.scalerank < 4),
-      // Styles
-      getSourcePosition: f => [-0.4531566, 51.4709959], // London
-      getTargetPosition: f => f.geometry.coordinates,
-      getSourceColor: [0, 128, 200],
-      getTargetColor: [200, 0, 80],
-      getWidth: 1
-    })
-  ]
-});
-
 const renderMap = status => {
   if (status === Status.LOADING) return <h3>{status} ..</h3>;
   if (status === Status.FAILURE) return <h3>{status} ...</h3>;
   return null;
 };
 
-// if using the create-react-app template try this
-// function MyMapComponent({center, zoom}) {
-//   const ref = useRef();
-//   const [map, setMap] = useState(null);
-  
-//   useEffect(() => {
-//     const map = new window.google.maps.Map(ref.current, {
-//       center: center,
-//       zoom: zoom,
-//       mapId: GOOGLE_MAP_ID
-//     });
-//     setMap(map);
-    
-//   }, [center, zoom]);
-
-//   useEffect(() => {
-//     overlay.setMap(map);
-//   }, [map]);
-
 function MyMapComponent({center, zoom}) {
   const ref = useRef();
+  const [map, setMap] = useState(null);
+  const overlay = useMemo(
+    () =>
+      new DeckOverlay({
+        layers: [
+          new GeoJsonLayer({
+            id: 'airports',
+            data: AIR_PORTS,
+            // Styles
+            filled: true,
+            pointRadiusMinPixels: 2,
+            pointRadiusScale: 2000,
+            getPointRadius: f => 11 - f.properties.scalerank,
+            getFillColor: [200, 0, 80, 180],
+            // Interactive props
+            pickable: true,
+            autoHighlight: true,
+            onClick: info =>
+              // eslint-disable-next-line
+              info.object &&
+              alert(`${info.object.properties.name} (${info.object.properties.abbrev})`)
+          }),
+          new ArcLayer({
+            id: 'arcs',
+            data: AIR_PORTS,
+            dataTransform: d => d.features.filter(f => f.properties.scalerank < 4),
+            // Styles
+            getSourcePosition: f => [-0.4531566, 51.4709959], // London
+            getTargetPosition: f => f.geometry.coordinates,
+            getSourceColor: [0, 128, 200],
+            getTargetColor: [200, 0, 80],
+            getWidth: 1
+          })
+        ]
+      }),
+    []
+  );
+
+  useEffect(() => {
+    if (map) {
+      map.setCenter(center);
+      map.setZoom(zoom);
+      overlay.setMap(map);
+    }
+  }, [map, center, zoom, overlay]);
 
   useEffect(() => {
     const map = new window.google.maps.Map(ref.current, {
-      center: center,
-      zoom: zoom,
       mapId: GOOGLE_MAP_ID
     });
-    overlay.setMap(map);
-  }, [center, zoom]);
-
+    setMap(map);
+  }, []);
   return (
     <>
       <div ref={ref} id="map" style={{height: '100vh', width: '100wh'}} />
