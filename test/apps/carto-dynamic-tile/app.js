a36092e2906b7c871c9595afde948d1c718e2323
Felix Palmer <felixpalmer@gmail.com>
2022-05-17 14:33:31 +0200

    Revert changes to test app

--- test/apps/carto-dynamic-tile/app.js
+++ test/apps/carto-dynamic-tile/app.js
@@ -6,7 +6,7 @@ import DeckGL from '@deck.gl/react';
 import {CartoLayer, FORMATS, TILE_FORMATS, MAP_TYPES} from '@deck.gl/carto';
 import {GeoJsonLayer} from '@deck.gl/layers';
 
-const INITIAL_VIEW_STATE = {longitude: 8, latitude: 47, zoom: 6};
+const INITIAL_VIEW_STATE = {longitude: -73.95643, latitude: 40.8039, zoom: 12};
 const COUNTRIES =
   'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_admin_0_scale_rank.geojson';
 
@@ -19,40 +19,53 @@ const apiBaseUrl = 'https://direct-gcp-us-east1.api.carto.com';
 
 const config = {
   bigquery: {
-    che15: 'carto-dev-data.public.derived_spatialfeatures_che_quadgrid15_v1_yearly_v2',
-    che18: 'carto-dev-data.public.derived_spatialfeatures_che_quadgrid18_v1_yearly_v2',
-    esp15: 'carto-dev-data.public.derived_spatialfeatures_esp_quadgrid15_v1_yearly_v2',
-    esp18: 'carto-dev-data.public.derived_spatialfeatures_esp_quadgrid18_v1_yearly_v2',
-    ukr15: 'carto-dev-data.public.derived_spatialfeatures_ukr_quadgrid15_v1_yearly_v2',
-    ukr18: 'carto-dev-data.public.derived_spatialfeatures_ukr_quadgrid18_v1_yearly_v2',
-    usa15: 'carto-dev-data.public.derived_spatialfeatures_usa_quadgrid15_v1_yearly_v2',
-    che15_quadkey:
-      'carto-dev-data.public.derived_spatialfeatures_che_quadgrid15_v1_yearly_v2_quadkey',
-    che18_quadkey:
-      'carto-dev-data.public.derived_spatialfeatures_che_quadgrid18_v1_yearly_v2_quadkey',
-    esp15_quadkey:
-      'carto-dev-data.public.derived_spatialfeatures_esp_quadgrid15_v1_yearly_v2_quadkey',
-    esp18_quadkey:
-      'carto-dev-data.public.derived_spatialfeatures_esp_quadgrid18_v1_yearly_v2_quadkey',
-    ukr15_quadkey:
-      'carto-dev-data.public.derived_spatialfeatures_ukr_quadgrid15_v1_yearly_v2_quadkey',
-    ukr18_quadkey:
-      'carto-dev-data.public.derived_spatialfeatures_ukr_quadgrid18_v1_yearly_v2_quadkey',
-    usa15_quadkey:
-      'carto-dev-data.public.derived_spatialfeatures_usa_quadgrid15_v1_yearly_v2_quadkey'
+    points_1M: 'cartodb-gcp-backend-data-team.dynamic_tiling.points_1M',
+    points_5M: 'cartodb-gcp-backend-data-team.dynamic_tiling.points_5M',
+    censustract: 'carto-do-public-data.carto.geography_usa_censustract_2019',
+    blockgroup: 'carto-do-public-data.carto.geography_usa_blockgroup_2019',
+    zipcodes: 'carto-do-public-data.carto.geography_usa_zcta5_2019',
+    h3: 'carto-do-public-data.carto.geography_usa_h3res8_v1',
+    block: 'carto-do-public-data.carto.geography_usa_block_2019'
+  },
+  snowflake: {
+    points_1M: 'carto_backend_data_team.dynamic_tiling.points_1M',
+    points_5M: 'carto_backend_data_team.dynamic_tiling.points_5M',
+    censustract: 'carto_backend_data_team.dynamic_tiling.usa_censustract_2019',
+    blockgroup: 'carto_backend_data_team.dynamic_tiling.usa_blockgroup_2019',
+    zipcodes: 'carto_backend_data_team.dynamic_tiling.usa_zcta5_2019',
+    h3: 'carto_backend_data_team.dynamic_tiling.usa_h3res8_v1',
+    block: 'carto_backend_data_team.dynamic_tiling.usa_block_2019'
+  },
+  redshift: {
+    points_1M: 'carto_backend_data_team.dynamic_tiling.points_1m',
+    points_5M: 'carto_backend_data_team.dynamic_tiling.points_5m',
+    censustract: 'carto_backend_data_team.dynamic_tiling.usa_censustract_2019',
+    blockgroup: 'carto_backend_data_team.dynamic_tiling.usa_blockgroup_2019',
+    zipcodes: 'carto_backend_data_team.dynamic_tiling.usa_zcta5_2019',
+    h3: 'carto_backend_data_team.dynamic_tiling.usa_h3res8_v1',
+    block: 'carto_backend_data_team.dynamic_tiling.usa_block_2019'
+  },
+  postgres: {
+    points_1M: 'demo.demo_tables.points_1m',
+    points_5M: 'demo.demo_tables.points_5m',
+    points_10M: 'demo.demo_tables.points_10m',
+    censustract: 'demo.demo_tables.usa_censustract_2019',
+    blockgroup: 'demo.demo_tables.usa_blockgroup_2019',
+    zipcodes: 'demo.demo_tables.usa_zcta5_2019',
+    county: 'demo.demo_tables.usa_county_2019',
+    block: 'demo.demo_tables.usa_block_2019'
   }
 };
 
-const accessToken =
-  'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImRVNGNZTHAwaThjYnVMNkd0LTE0diJ9.eyJodHRwOi8vYXBwLmNhcnRvLmNvbS9lbWFpbCI6ImFsYmVydG9AY2FydG9kYi5jb20iLCJodHRwOi8vYXBwLmNhcnRvLmNvbS9hY2NvdW50X2lkIjoiYWNfN3hoZnd5bWwiLCJpc3MiOiJodHRwczovL2F1dGguY2FydG8uY29tLyIsInN1YiI6Imdvb2dsZS1vYXV0aDJ8MTA4NDA5NTYzMzQxMzU5MDQxNjg0IiwiYXVkIjoiY2FydG8tY2xvdWQtbmF0aXZlLWFwaSIsImlhdCI6MTY1Mjc3NjQwNSwiZXhwIjoxNjUyODYyODA1LCJhenAiOiJqQ1duSEs2RTJLMmFPeTlqTHkzTzdaTXBocUdPOUJQTCIsInNjb3BlIjoicmVhZDpjdXJyZW50X3VzZXIiLCJwZXJtaXNzaW9ucyI6WyJhZG1pbjphY2NvdW50IiwicmVhZDphY2NvdW50IiwicmVhZDphcHBzIiwicmVhZDpjb25uZWN0aW9ucyIsInJlYWQ6Y3VycmVudF91c2VyIiwicmVhZDppbXBvcnRzIiwicmVhZDpsaXN0ZWRfYXBwcyIsInJlYWQ6bWFwcyIsInJlYWQ6dGlsZXNldHMiLCJyZWFkOnRva2VucyIsInVwZGF0ZTpjdXJyZW50X3VzZXIiLCJ3cml0ZTphcHBzIiwid3JpdGU6Y29ubmVjdGlvbnMiLCJ3cml0ZTppbXBvcnRzIiwid3JpdGU6bGlzdGVkX2FwcHMiLCJ3cml0ZTptYXBzIiwid3JpdGU6dG9rZW5zIl19.rfxRrGkVd1A_JAAAJu-F30I04rgXzoFiKui9DCFfP92xuaIeD09BCryRAURM53oOylib7HXuJLsUCwsZrhITzM_OR5BQiVp1-lYgUKQu63Z2Ju2RRZf0vdBkIjgSJW-NYXxqTOiw0HMZ4mMly04fBW7kSDcP6LvMuj7uLEFfgqRg0I0_yQgHzB7BJbA9dp5N3zTaxyEC08KJdAZ1EnsC6R-o4Y5Jg993U4yJ6-pUF_H6b7HziFZTqB7nTm4uW25jmmPMQ4oBYSVgfUp-2zdgKgzaqdctmTO1fdtgrl7qxGxAkUiyWy5uQhleiW7ckKL-jIJniQg0ZSaHkJPpyek4gQ';
+const accessToken = 'XXXX';
 
 const showBasemap = true;
 const showCarto = true;
 
 function Root() {
   const [connection, setConnection] = useState('bigquery');
-  const [dataset, setDataset] = useState('che15');
-  const [formatTiles, setFormatTiles] = useState(TILE_FORMATS.JSON);
+  const [dataset, setDataset] = useState('points_1M');
+  const [formatTiles, setFormatTiles] = useState(TILE_FORMATS.BINARY);
   const table = config[connection][dataset];
   return (
     <>
@@ -100,9 +113,7 @@ function createBasemap() {
   });
 }
 
-// Add aggregation expressions
 function createCarto(connection, formatTiles, table) {
-  const geoColumn = table.endsWith('_quadkey') ? 'quadkey' : 'quadint';
   return new CartoLayer({
     id: 'carto',
     connection,
@@ -114,25 +125,17 @@ function createCarto(connection, formatTiles, table) {
     format: FORMATS.TILEJSON,
     formatTiles,
 
-    // Aggregation
-    aggregationExp: 'avg(population) as value, 0.1*avg(population) as elevation',
-    aggregationResLevel: 6,
-    geoColumn,
-    getQuadkey: d => d.id,
-
     // Styling
-    getFillColor: d => [
-      Math.pow((d.properties.value || d.properties.VALUE) / 200, 0.1) * 255,
-      255 - (d.properties.value || d.properties.VALUE),
-      79
-    ],
-    getElevation: d =>
-      'elevation' in d.properties
-        ? d.properties.elevation
-        : d.properties.value || d.properties.VALUE,
-    extruded: true,
-    opacity: 0.3,
-    elevationScale: 100
+    getFillColor: [233, 71, 251],
+    getElevation: 1000,
+    // extruded: true,
+    stroked: true,
+    filled: true,
+    pointType: 'circle',
+    pointRadiusUnits: 'pixels',
+    lineWidthMinPixels: 0.5,
+    getPointRadius: 1.5,
+    getLineColor: [0, 0, 200]
   });
 }
 
