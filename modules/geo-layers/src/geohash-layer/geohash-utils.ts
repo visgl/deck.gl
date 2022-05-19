8a72a0362723ddc981e0563bc99733afe6365e01
Kevin Nacios <kevin.nacios@gmail.com>
2022-05-19 09:48:32 -0400

    convert to typescript

--- modules/geo-layers/src/geohash-layer/geohash-utils.ts
+++ modules/geo-layers/src/geohash-layer/geohash-utils.ts
@@ -1,6 +1,7 @@
+// eslint-disable-next-line camelcase
 import {decode_bbox} from 'ngeohash';
 
-export function getGeohashPolygon(geohash) {
+export function getGeohashPolygon(geohash: string): number[] {
   const [s, w, n, e] = decode_bbox(geohash);
 
   return [e, n, e, s, w, s, w, n, e, n];
