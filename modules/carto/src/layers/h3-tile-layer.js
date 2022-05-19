328519bc9249d4fc3ad072520329b608eaf676c1
Felix Palmer <felixpalmer@gmail.com>
2022-05-19 15:51:35 +0200

    Calculate the maximum resolution of h3 tiles

--- modules/carto/src/layers/h3-tile-layer.js
+++ modules/carto/src/layers/h3-tile-layer.js
@@ -28,14 +28,17 @@ export default class H3TileLayer extends CompositeLayer {
   }
 
   renderLayers() {
-    const {data} = this.state;
+    const {data, tileJSON} = this.state;
+    const {maxresolution} = tileJSON;
+    const maxZoom = maxresolution - this.props.aggregationResLevel;
     return [
       new TileLayer(this.props, {
         id: 'h3-tile-layer',
         data,
         getHexagon: d => d.id,
         TilesetClass: H3Tileset2D,
-        renderSubLayers
+        renderSubLayers,
+        maxZoom
       })
     ];
   }
