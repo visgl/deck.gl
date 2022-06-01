67a782950a7718d462a13f1d30995296705c9d2d
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2022-06-01 11:33:13 -0700

    [ts] ScatterplotLayerProps documentation (#6963)

--- modules/layers/src/scatterplot-layer/scatterplot-layer.ts
+++ modules/layers/src/scatterplot-layer/scatterplot-layer.ts
@@ -34,25 +34,92 @@ export type ScatterplotLayerProps<DataT = any> = _ScatterplotLayerProps<DataT> &
 
 /** Props added by the ScatterplotLayer */
 type _ScatterplotLayerProps<DataT> = {
+  /**
+   * The units of the radius, one of `'meters'`, `'common'`, and `'pixels'`.
+   * @default 'meters'
+   */
   radiusUnits?: Unit;
+  /**
+   * Radius multiplier.
+   * @default 1
+   */
   radiusScale?: number;
+  /**
+   * The minimum radius in pixels. This prop can be used to prevent the circle from getting too small when zoomed out.
+   * @default 0
+   */
   radiusMinPixels?: number;
+  /**
+   * The maximum radius in pixels. This prop can be used to prevent the circle from getting too big when zoomed in.
+   * @default Number.MAX_SAFE_INTEGER
+   */
   radiusMaxPixels?: number;
 
+  /**
+   * The units of the stroke width, one of `'meters'`, `'common'`, and `'pixels'`.
+   * @default 'meters'
+   */
   lineWidthUnits?: Unit;
+  /**
+   * Stroke width multiplier.
+   * @default 1
+   */
   lineWidthScale?: number;
+  /**
+   * The minimum stroke width in pixels. This prop can be used to prevent the line from getting too thin when zoomed out.
+   * @default 0
+   */
   lineWidthMinPixels?: number;
+  /**
+   * The maximum stroke width in pixels. This prop can be used to prevent the circle from getting too thick when zoomed in.
+   * @default Number.MAX_SAFE_INTEGER
+   */
   lineWidthMaxPixels?: number;
 
+  /**
+   * Draw the outline of points.
+   * @default false
+   */
   stroked?: boolean;
+  /**
+   * Draw the filled area of points.
+   * @default true
+   */
   filled?: boolean;
+  /**
+   * If `true`, rendered circles always face the camera. If `false` circles face up (i.e. are parallel with the ground plane).
+   * @default false
+   */
   billboard?: boolean;
+  /**
+   * If `true`, circles are rendered with smoothed edges. If `false`, circles are rendered with rough edges. Antialiasing can cause artifacts on edges of overlapping circles.
+   * @default true
+   */
   antialiasing?: boolean;
 
+  /**
+   * Center position accessor.
+   */
   getPosition?: Accessor<DataT, Position>;
+  /**
+   * Radius accessor.
+   * @default 1
+   */
   getRadius?: Accessor<DataT, number>;
+  /**
+   * Fill color accessor.
+   * @default [0, 0, 0, 255]
+   */
   getFillColor?: Accessor<DataT, Color>;
+  /**
+   * Stroke color accessor.
+   * @default [0, 0, 0, 255]
+   */
   getLineColor?: Accessor<DataT, Color>;
+  /**
+   * Stroke width accessor.
+   * @default 1
+   */
   getLineWidth?: Accessor<DataT, number>;
 };
 
@@ -110,7 +177,7 @@ export default class ScatterplotLayer<DataT = any, ExtraPropsT = {}> extends Lay
         defaultValue: 1
       },
       instanceFillColors: {
-        size: this.props.colorFormat.length as 3 | 4,
+        size: this.props.colorFormat.length,
         transition: true,
         normalized: true,
         type: GL.UNSIGNED_BYTE,
@@ -118,7 +185,7 @@ export default class ScatterplotLayer<DataT = any, ExtraPropsT = {}> extends Lay
         defaultValue: [0, 0, 0, 255]
       },
       instanceLineColors: {
-        size: this.props.colorFormat.length as 3 | 4,
+        size: this.props.colorFormat.length,
         transition: true,
         normalized: true,
         type: GL.UNSIGNED_BYTE,
@@ -180,7 +247,7 @@ export default class ScatterplotLayer<DataT = any, ExtraPropsT = {}> extends Lay
       .draw();
   }
 
-  _getModel(gl) {
+  protected _getModel(gl) {
     // a square that minimally cover the unit circle
     const positions = [-1, -1, 0, 1, -1, 0, 1, 1, 0, -1, 1, 0];
 
