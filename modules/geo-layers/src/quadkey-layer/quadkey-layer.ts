55c86a878feb4cc14b8b06e07c52312589454162
Zbigniew Zagórski <1507542+zbigg@users.noreply.github.com>
2022-06-01 12:22:31 +0200

    [ts] Fix prop types of Polygon, GeoCell and Queadkey layers. (#6965)

--- modules/geo-layers/src/quadkey-layer/quadkey-layer.ts
+++ modules/geo-layers/src/quadkey-layer/quadkey-layer.ts
@@ -1,15 +1,16 @@
 import {AccessorFunction} from '@deck.gl/core';
-import GeoCellLayer from '../geo-cell-layer/GeoCellLayer';
+import GeoCellLayer, {GeoCellLayerProps} from '../geo-cell-layer/GeoCellLayer';
 import {getQuadkeyPolygon} from './quadkey-utils';
 
 const defaultProps = {
   getQuadkey: {type: 'accessor', value: d => d.quadkey}
 };
 
-/**
- * Properties of `QuadkeyLayer`.
- */
-type QuadkeyLayerProps<DataT = any> = {
+/** All properties supported by QuadkeyLayer. */
+export type QuadkeyLayerProps<DataT = any> = _QuadkeyLayerProps<DataT> & GeoCellLayerProps<DataT>;
+
+/** Properties added by QuadkeyLayer. */
+type _QuadkeyLayerProps<DataT = any> = {
   /**
    * Called for each data object to retrieve the quadkey string identifier.
    *
@@ -20,7 +21,7 @@ type QuadkeyLayerProps<DataT = any> = {
 
 export default class QuadkeyLayer<DataT = any, ExtraProps = {}> extends GeoCellLayer<
   DataT,
-  Required<QuadkeyLayerProps> & ExtraProps
+  Required<_QuadkeyLayerProps> & ExtraProps
 > {
   static layerName = 'QuadkeyLayer';
   static defaultProps: any = defaultProps;
