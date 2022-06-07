6c6a833f7c203c0f33546d448e2cedd067cb1e45
Felix Palmer <felixpalmer@gmail.com>
2022-06-07 15:55:47 +0200

    Tests fro custom tilesets

---
+++ test/modules/carto/layers/h3-tileset-2d.spec.js
@@ -0,0 +1,47 @@
+import test from 'tape-promise/tape';
+import H3Tileset2D from '@deck.gl/carto/layers/h3-tileset-2d';
+import {WebMercatorViewport} from '@deck.gl/core';
+
+test('H3Tileset2D', async t => {
+  const tileset = new H3Tileset2D({});
+  const viewport = new WebMercatorViewport({
+    latitude: 0,
+    longitude: 0,
+    zoom: 6,
+    width: 300,
+    height: 200
+  });
+
+  const indices = tileset.getTileIndices({viewport});
+  t.deepEqual(
+    indices,
+    [
+      {i: '82754ffffffffff'},
+      {i: '82755ffffffffff'},
+      {i: '827547fffffffff'},
+      {i: '8274effffffffff'},
+      {i: '82756ffffffffff'}
+    ],
+    'indices in viewport'
+  );
+  t.equal(tileset.getTileId({i: '82754ffffffffff'}), '82754ffffffffff', 'tile id');
+  t.deepEqual(
+    tileset.getTileMetadata({i: '82754ffffffffff'}),
+    {
+      bbox: {
+        west: -0.8199508788179312,
+        south: -1.855492618547643,
+        east: 1.9211969045935835,
+        north: 0.9361637645983679
+      }
+    },
+    'tile metadata'
+  );
+  t.equal(tileset.getTileZoom({i: '82754ffffffffff'}), 2, 'tile zoom');
+  t.deepEqual(
+    tileset.getParentIndex({i: '82754ffffffffff'}),
+    {i: '81757ffffffffff'},
+    'tile parent'
+  );
+  t.end();
+});
