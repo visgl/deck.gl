07c4221a4df54bc9bf56b3dba2029c0a9714a896
Felix Palmer <felixpalmer@gmail.com>
2022-06-07 11:24:36 +0200

    Add picking test for quadkeytilelayer

--- test/modules/carto/layers/quadkey-tile-layer.spec.js
+++ test/modules/carto/layers/quadkey-tile-layer.spec.js
@@ -1,6 +1,8 @@
 import test from 'tape-promise/tape';
 import {generateLayerTests, testLayerAsync} from '@deck.gl/test-utils';
 import {_QuadkeyTileLayer as QuadkeyTileLayer} from '@deck.gl/carto';
+import {WebMercatorViewport} from '@deck.gl/core';
+import {testPickingLayer} from '../../layers/test-picking-layer';
 
 const TILES = ['https://tile-url.com/{i}'];
 const TILEJSON = {
@@ -37,5 +39,45 @@ test('QuadkeyTileLayer tilejson', async t => {
     }
   ];
   await testLayerAsync({Layer: QuadkeyTileLayer, testCases, onError: t.notOk});
+  t.end();
+});
+
+test('QuadkeyTileLayer autoHighlight', async t => {
+  await testPickingLayer({
+    layer: new QuadkeyTileLayer({
+      id: 'quadkey-tile',
+      data: TILEJSON,
+      getTileData(e) {
+        // Generate 4 quadkeys to fill the tile
+        return [0, 1, 2, 3].map(i => ({id: `${e.index.i}${i}`, value: i}));
+      },
+      autoHighlight: true,
+      pickable: true
+    }),
+    viewport: new WebMercatorViewport({latitude: 0, longitude: 0, zoom: 1}),
+    testCases: [
+      {
+        pickedColor: new Uint8Array([4, 0, 0, 0]),
+        pickedLayerId: 'quadkey-tile-layer-quadkey-tile-2-cell-fill',
+        mode: 'hover',
+        onAfterUpdate: ({layer, subLayers, info}) => {
+          t.comment('hover over quadkey');
+          t.ok(info.object, 'info.object is populated');
+          t.equal(info.object.id, '23', 'quadkey is correct');
+          t.equal(info.object.value, 3, 'object value is correct');
+        }
+      },
+      {
+        pickedColor: new Uint8Array([0, 0, 0, 0]),
+        pickedLayerId: '',
+        mode: 'hover',
+        onAfterUpdate: ({layer, subLayers, info}) => {
+          t.comment('pointer leave');
+          t.notOk(info.object, 'info.object is not populated');
+        }
+      }
+    ]
+  });
+
   t.end();
 });
