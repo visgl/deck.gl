93d81c6438451fa0176afb87471db5688176d495
Felix Palmer <felixpalmer@gmail.com>
2022-06-12 19:27:03 +0200

    Export prop types

--- modules/geo-layers/src/index.ts
+++ modules/geo-layers/src/index.ts
@@ -35,6 +35,8 @@ export {default as _Tileset2D} from './tile-layer/tileset-2d';
 export {default as _Tile2DHeader} from './tile-layer/tile-2d-header';
 
 // Types
+export type {H3ClusterLayerProps} from './h3-layers/h3-cluster-layer';
+export type {H3HexagonLayerProps} from './h3-layers/h3-hexagon-layer';
 export type {TileLayerProps} from './tile-layer/tile-layer';
 export type {GeoBoundingBox} from './tile-layer/types';
 export type {QuadkeyLayerProps} from './quadkey-layer/quadkey-layer';
