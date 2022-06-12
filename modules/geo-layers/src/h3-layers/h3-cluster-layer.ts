d7776b7b86252c4bfbfba7a2f754fb6c7937cfc7
Felix Palmer <felixpalmer@gmail.com>
2022-06-12 19:20:28 +0200

    Types for H3ClusterLayer

--- modules/geo-layers/src/h3-layers/h3-cluster-layer.ts
+++ modules/geo-layers/src/h3-layers/h3-cluster-layer.ts
@@ -1,20 +1,40 @@
-import {h3SetToMultiPolygon} from 'h3-js';
+import {h3SetToMultiPolygon, H3IndexInput} from 'h3-js';
 
-import {createIterable} from '@deck.gl/core';
-import GeoCellLayer from '../geo-cell-layer/GeoCellLayer';
+import {AccessorFunction, createIterable, UpdateParameters} from '@deck.gl/core';
+import GeoCellLayer, {GeoCellLayerProps} from '../geo-cell-layer/GeoCellLayer';
 
 const defaultProps = {
   getHexagons: {type: 'accessor', value: d => d.hexagons}
 };
 
-export default class H3ClusterLayer extends GeoCellLayer {
-  updateState({props, oldProps, changeFlags}) {
+/** All properties supported by H3ClusterLayer. */
+export type H3ClusterLayerProps<DataT = any> = _H3ClusterLayerProps<DataT> &
+  GeoCellLayerProps<DataT>;
+
+/** Properties added by H3ClusterLayer. */
+type _H3ClusterLayerProps<DataT = any> = {
+  /**
+   * Called for each data object to retrieve the hexagon identifiers.
+   *
+   * By default, it reads `hexagons` property of data object.
+   */
+  getHexagons?: AccessorFunction<DataT, H3IndexInput[]>;
+};
+
+export default class H3ClusterLayer<DataT = any, ExtraProps = {}> extends GeoCellLayer<
+  DataT,
+  Required<_H3ClusterLayerProps> & ExtraProps
+> {
+  static layerName = 'H3ClusterLayer';
+  static defaultProps: any = defaultProps;
+
+  updateState({props, changeFlags}: UpdateParameters<this>): void {
     if (
       changeFlags.dataChanged ||
-      (changeFlags.updateTriggers && changeFlags.updateTriggers.getHexagons)
+      (changeFlags.updateTriggersChanged && changeFlags.updateTriggersChanged.getHexagons)
     ) {
       const {data, getHexagons} = props;
-      const polygons = [];
+      const polygons: {polygon: number[][][]}[] = [];
 
       const {iterable, objectInfo} = createIterable(data);
       for (const object of iterable) {
@@ -31,13 +51,10 @@ export default class H3ClusterLayer extends GeoCellLayer {
     }
   }
 
-  indexToBounds() {
+  indexToBounds(): Partial<GeoCellLayer['props']> | null {
     return {
       data: this.state.polygons,
       getPolygon: d => d.polygon
     };
   }
 }
-
-H3ClusterLayer.defaultProps = defaultProps;
-H3ClusterLayer.layerName = 'H3ClusterLayer';
