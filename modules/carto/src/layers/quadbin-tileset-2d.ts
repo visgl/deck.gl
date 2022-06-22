e491ac1116361d3b92526e31975a71f236501aa7
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2022-06-22 07:53:52 +0200

    Add quadbin tileset 2d functions

---
+++ modules/carto/src/layers/quadbin-tileset-2d.ts
@@ -0,0 +1,113 @@
+import {_Tileset2D as Tileset2D} from '@deck.gl/geo-layers';
+
+type QuadbinTileIndex = {i: string};
+
+function tileToQuadbin(tile): QuadbinTileIndex {
+  if (tile.z < 0 || tile.z > 26) {
+    throw new Error('Wrong zoom');
+  }
+  const B = [
+    0x5555555555555555n, 0x3333333333333333n, 0x0F0F0F0F0F0F0F0Fn,
+    0x00FF00FF00FF00FFn, 0x0000FFFF0000FFFFn];
+  const S = [1n, 2n, 4n, 8n, 16n];
+  let z = BigInt(tile.z);
+  let x = BigInt(tile.x) << (32n - z);
+  let y = BigInt(tile.y) << (32n - z);
+
+  x = (x | (x << S[4])) & B[4];
+  y = (y | (y << S[4])) & B[4];
+
+  x = (x | (x << S[3])) & B[3];
+  y = (y | (y << S[3])) & B[3];
+
+  x = (x | (x << S[2])) & B[2];
+  y = (y | (y << S[2])) & B[2];
+
+  x = (x | (x << S[1])) & B[1];
+  y = (y | (y << S[1])) & B[1];
+
+  x = (x | (x << S[0])) & B[0];
+  y = (y | (y << S[0])) & B[0];
+
+  let quadbin = 0x4000000000000000n
+    | (1n << 59n) // | (mode << 59) | (mode_dep << 57)
+    | (z << 52n)
+    | ((x | (y << 1n)) >> 12n)
+    | (0xFFFFFFFFFFFFFn >> (z * 2n));
+  return {i: quadbin.toString(16)};
+}
+
+function quadbinToTile(index: QuadbinTileIndex) {
+  const B = [
+    0x5555555555555555n, 0x3333333333333333n, 0x0F0F0F0F0F0F0F0Fn,
+    0x00FF00FF00FF00FFn, 0x0000FFFF0000FFFFn, 0x00000000FFFFFFFFn];
+  const S = [0n, 1n, 2n, 4n, 8n, 16n];
+  const quadbin = BigInt('0x' + index.i);
+  const mode = (quadbin >> 59n) & 7n;
+  const modeDep = (quadbin >> 57n) & 3n;
+  const z = (quadbin >> 52n) & 0x1Fn;
+  const q = (quadbin & 0xFFFFFFFFFFFFFn) << 12n;
+
+  if (mode != 1n && modeDep != 0n) {
+    throw new Error('Wrong mode');
+  }
+
+  let x = q;
+  let y = q >> 1n;
+
+  x = (x | (x >> S[0])) & B[0];
+  y = (y | (y >> S[0])) & B[0];
+
+  x = (x | (x >> S[1])) & B[1];
+  y = (y | (y >> S[1])) & B[1];
+
+  x = (x | (x >> S[2])) & B[2];
+  y = (y | (y >> S[2])) & B[2];
+
+  x = (x | (x >> S[3])) & B[3];
+  y = (y | (y >> S[3])) & B[3];
+
+  x = (x | (x >> S[4])) & B[4];
+  y = (y | (y >> S[4])) & B[4];
+
+  x = (x | (x >> S[5])) & B[5];
+  y = (y | (y >> S[5])) & B[5];
+
+  x = x >> (32n - z);
+  y = y >> (32n - z);
+
+  return { z: Number(z), x: Number(x), y: Number(y) };
+}
+
+export default class QuadbinTileset2D extends Tileset2D {
+  // @ts-expect-error for spatial indices, TileSet2d should be parametrized by TileIndexT
+  getTileIndices(opts): QuadbinTileIndex[] {
+    return super.getTileIndices(opts).map(tileToQuadbin);
+  }
+
+  // @ts-expect-error TileIndex must be generic
+  getTileId({i}: QuadbinTileIndex) {
+    return i;
+  }
+
+  // @ts-expect-error TileIndex must be generic
+  getTileMetadata(index: QuadbinTileIndex) {
+    return super.getTileMetadata(quadbinToTile(index));
+  }
+
+  // @ts-expect-error TileIndex must be generic
+  getTileZoom(index: QuadbinTileIndex) {
+    const quadbin = BigInt('0x' + index.i);
+    return (quadbin >> 52n) & 0x1Fn;
+  }
+
+  // @ts-expect-error TileIndex must be generic
+  getParentIndex(index: QuadbinTileIndex) {
+    const quadbin = BigInt('0x' + index.i);
+    const zparent = (quadbin >> 52n) & 0x1Fn - 1n;
+    const parent = (quadbin & ~(0x1Fn << 52n))
+      | (zparent << 52n)
+      | (0xFFFFFFFFFFFFFn >> (zparent * 2n));
+    return {i: parent.toString(16)};
+  }
+}
