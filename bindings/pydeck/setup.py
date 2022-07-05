83b67ed0eae833397faf2814d60405199ff68eb2
Andrew Duberstein <ajduberstein@gmail.com>
2022-07-05 16:23:27 -0700

    Remove traitlets and ipywidgets

--- bindings/pydeck/setup.py
+++ bindings/pydeck/setup.py
@@ -209,9 +209,7 @@ if __name__ == "__main__":
         extras_require={
             "carto": ["pydeck_carto"],
             "jupyter": [
-                "ipywidgets>=7.0.0",
                 'ipython>=5.8.0;python_version<"3.4"',
-                "traitlets>=4.3.2",
                 'ipykernel>=5.1.2;python_version>="3.4"',
             ],
         },
