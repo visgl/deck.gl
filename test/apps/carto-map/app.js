779f56a10d69cf8e2e1340de48752a117f299b13
felixpalmer <felixpalmer@gmail.com>
2022-07-18 15:21:23 +0200

    CARTO: fetchMap support spatial index layers (#7065)

--- test/apps/carto-map/app.js
+++ test/apps/carto-map/app.js
@@ -1,10 +1,14 @@
-import {fetchMap} from '@deck.gl/carto';
+import {fetchMap, setDefaultCredentials} from '@deck.gl/carto';
 import {Deck} from '@deck.gl/core';
 import mapboxgl from 'mapbox-gl';
 
 // // Simplest instantiation
 // const cartoMapId = 'ff6ac53f-741a-49fb-b615-d040bc5a96b8';
 // fetchMap({cartoMapId}).then(map => new Deck(map));
+
+const apiBaseUrl = 'https://gcp-us-east1.api.carto.com';
+// const apiBaseUrl = 'https://gcp-us-east1-05.dev.api.carto.com';
+setDefaultCredentials({apiBaseUrl});
 
 async function createMap(cartoMapId) {
   const deck = new Deck({canvas: 'deck-canvas'});
@@ -38,6 +42,17 @@ async function createMap(cartoMapId) {
 
 // Helper UI for dev
 const examples = [
+  // Spatial index layers
+  '202252d8-5647-424a-9317-9e392be59d65', // dynamic spatial index
+  '907ee05f-b05c-4784-8226-c59e34773be5', // dynamic tiling
+  'a29d4941-a683-4516-9479-9db05a5b6bd9', // dynamic spatial index h3 ana (table)
+  '38255824-e1d5-4a8f-b803-324aa75ef08a', // dynamic spatial index h3 ana (tileset)
+  'bee8c9c4-f12a-4cec-87f6-021bd52df942', // dynamic spatial index h3 ana (query)
+  '465f9b51-8256-4582-a9f0-a23757aefc39', // dynamic spatial index quadbin ana (table)
+  '433b5f7e-af50-4da3-8803-26dfe58972d8', // dynamic tiling ana
+  'ab8cc16b-e6b2-409a-8e0a-39ad6f6dfc12', // dynamic tiling snowflake
+  'a42992e9-df58-451e-ad71-d7e91fe4a0df', // No aggregationExp provided
+
   // Aggregation layers
   'd91a6a6f-0290-4b22-b9cc-fa174cb31a23', // Grid
   '4b34fd92-7890-4bab-8601-008aefebc359', // hex bin
@@ -65,7 +80,7 @@ const id = params.has('id') ? params.get('id') : examples[0];
 const iframe = document.createElement('iframe');
 iframe.style.width = '100%';
 iframe.style.height = 'calc(50% + 20px)';
-iframe.src = `https://gcp-us-east1.app.carto.com/map/${id}`;
+iframe.src = `${apiBaseUrl.replace('api', 'app')}/map/${id}`;
 document.body.appendChild(iframe);
 
 for (const e of examples) {
