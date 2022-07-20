a4e65f7ea2750ebb9e939d7126e26363a77c1fa5
Felix Palmer <felixpalmer@gmail.com>
2022-07-20 10:07:12 +0200

    Simple data structure in spatialjsonToBinary

--- modules/carto/src/layers/schema/spatialjson-utils.ts
+++ modules/carto/src/layers/schema/spatialjson-utils.ts
@@ -1,3 +1,21 @@
+import {indexToBigInt} from '../quadbin-utils';
+
 export function spatialjsonToBinary(spatial): any {
-  return spatial;
+  const count = spatial.length;
+  const cells = {
+    indices: {value: new BigUint64Array(count), size: 1},
+    numericProps: {},
+    properties: []
+  };
+
+  let i = 0;
+  for (const {id, properties} of spatial) {
+    cells.indices.value[i] = indexToBigInt(id);
+    i++;
+  }
+
+  return {
+    scheme: 'h3', // TODO infer
+    cells
+  };
 }
--- test/modules/carto/layers/spatialjson.spec.js
+++ test/modules/carto/layers/spatialjson.spec.js
@@ -1,22 +1,57 @@
 import test from 'tape-promise/tape';
-import spatialjsonToBinary from '@deck.gl/carto/layers/schema/spatialjson-utils';
+import {spatialjsonToBinary} from '@deck.gl/carto/layers/schema/spatialjson-utils';
+
+const TEST_CASES = [
+  {
+    name: 'H3 index only',
+    spatial: [{id: '85283473fffffff', properties: {}}],
+    expected: {
+      scheme: 'h3',
+      cells: {
+        indices: {value: new BigUint64Array([1234n]), size: 1},
+        numericProps: {},
+        properties: []
+      }
+    }
+  },
+  {
+    name: 'Quadbin index only',
+    spatial: [{id: '48e62966eaffffff', properties: {}}],
+    expected: {
+      scheme: 'quadbin',
+      cells: {
+        indices: {value: new BigUint64Array([1234n]), size: 1},
+        numericProps: {},
+        properties: []
+      }
+    }
+  },
+  {
+    name: 'Mixed props',
+    spatial: [
+      {id: '48e62966eaffffff', properties: {stringProp: 'abc', floatProp: 123.45, intProp: 12345}}
+    ],
+    expected: {
+      scheme: 'quadbin',
+      cells: {
+        indices: {value: new BigUint64Array([1234n]), size: 1},
+        numericProps: {
+          floatProp: {value: new Float32Array([123.45]), size: 1},
+          intProp: {value: new Int32Array([12345]), size: 1}
+        },
+        properties: [{data: [{key: 'stringProp', value: 'abc'}]}]
+      }
+    }
+  }
+];
 
 test.only('Spatialjson to binary', async t => {
-  const spatial = [
-    {id: '48e62966eaffffff', properties: {stringProp: 'abc', floatProp: 123.45, intProp: 12345}}
-  ];
-  const expected = {
-    scheme: 'h3',
-    cells: {
-      indices: {value: new BigUint64Array(1234n), size: 1},
-      numericProps: {
-        floatProp: {value: new Float32Array(123, 45), size: 1},
-        intProp: {value: new Int32Array(12345), size: 1}
-      },
-      properties: [{data: [{key: 'stringProp', value: 'abc'}]}]
-    }
-  };
-
-  t.deepEqual(spatialjsonToBinary(spatial), expected, 'Spatialjson is converted to binary');
+  for (const {name, spatial, expected} of TEST_CASES) {
+    t.deepEqual(
+      spatialjsonToBinary(spatial),
+      expected,
+      `Spatialjson is converted to binary: ${name}`
+    );
+  }
   t.end();
 });
