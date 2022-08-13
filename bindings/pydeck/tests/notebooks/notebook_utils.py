8fd5eb421e23ced333cfd80160740ede21355b02
Andrew Duberstein <ajduberstein@gmail.com>
2022-08-13 13:17:28 -0700

    flake8

--- bindings/pydeck/tests/notebooks/notebook_utils.py
+++ bindings/pydeck/tests/notebooks/notebook_utils.py
@@ -6,7 +6,7 @@ def nbconvert(notebook_path):
     if not os.path.exists(os.path.exists(notebook_path)):
         raise Exception("Invalid path %s" % notebook_path)
     CMD = (
-        "jupyter nbconvert --to=html --ExecutePreprocessor.timeout=600 "
+        "python -m jupyter nbconvert --to=html --ExecutePreprocessor.timeout=600 "
         "--ExecutePreprocessor.kernel_name='python3' "
         "--ExecutePreprocessor.store_widget_state=True "
         "--execute '{}'"
