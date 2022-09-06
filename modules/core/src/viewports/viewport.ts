8782b5e3eb188077750a10e8b20ea522776462d2
Felix Palmer <felixpalmer@gmail.com>
2022-09-06 14:24:57 +0200

    More succinct clamp

--- modules/core/src/viewports/viewport.ts
+++ modules/core/src/viewports/viewport.ts
@@ -316,10 +316,7 @@ export default class Viewport {
   projectFlat(xyz: number[]): [number, number] {
     if (this.isGeospatial) {
       // Shader clamps latitude to +-89.9, see /shaderlib/project/project.glsl.js
-      // lngLatToWorld([0, -89.9])[1] = -317.9934163758329
-      // lngLatToWorld([0, 89.9])[1] = 829.9934163758271
-      const clamped = [...xyz];
-      clamped[1] = clamp(clamped[1], -89.9, 89.9);
+      const clamped = [xyz[0], clamp(xyz[1], -89.9, 89.9)];
       const result = lngLatToWorld(clamped);
       return result;
     }
