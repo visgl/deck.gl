275110ecf3d088d39d87047ae95e442c2d35cd42
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2022-09-08 21:31:46 +0200

    Update deck.gl version in pydeck/pydeck-carto

--- bindings/pydeck-carto/setup.py
+++ bindings/pydeck-carto/setup.py
@@ -33,6 +33,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     zip_safe=False,
 )
