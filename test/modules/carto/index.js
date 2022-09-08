aff66837917082beedeff39a8eda7332c93ae900
felixpalmer <felixpalmer@gmail.com>
2022-09-08 17:45:03 +0200

    CARTO: Support fields when parsing CartoVectorTiles (#7248)

--- test/modules/carto/index.js
+++ test/modules/carto/index.js
@@ -4,6 +4,7 @@ import './api/maps-api-client.spec';
 import './carto-layer.spec';
 import './config.spec';
 import './layers/carto-tile-layer.spec';
+import './layers/carto-vector-tile.spec';
 import './layers/h3-tile-layer.spec';
 import './layers/h3-tileset-2d.spec';
 import './layers/spatialjson.spec';
