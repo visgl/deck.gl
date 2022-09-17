6241b3320c3878c2761e7b805af97c3671446606
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2022-09-17 16:47:18 +0200

    Add styling examples

---
+++ bindings/carto-pydeck/examples/scripts/carto_styles_color_bins.py
@@ -0,0 +1,33 @@
+"""
+CartoLayer
+==========
+
+Render cloud data with color bins style.
+"""
+import pydeck as pdk
+
+from carto_auth import CartoAuth
+from carto_pydeck import register_carto_layer, get_layer_credentials, notify_error
+from carto_pydeck.layer import MapType, CartoConnection
+from carto_pydeck.styles import color_bins
+
+register_carto_layer()
+
+carto_auth = CartoAuth.from_oauth()
+
+layer = pdk.Layer(
+    "CartoLayer",
+    data="SELECT geom, pct_higher_ed FROM `cartobq.public_account.higher_edu_by_county`",
+    type_=MapType.QUERY,
+    connection=CartoConnection.CARTO_DW,
+    credentials=get_layer_credentials(carto_auth),
+    get_fill_color=color_bins("pct_higher_ed", [0, 20, 30, 40, 50, 60, 70], "PinkYl"),
+    get_line_color=[0, 0, 0, 100],
+    line_width_min_pixels=0.5,
+    pickable=True,
+    on_data_error=notify_error(),
+)
+view_state = pdk.ViewState(latitude=38, longitude=-98, zoom=3)
+
+r = pdk.Deck(layer, map_style=pdk.map_styles.ROAD, initial_view_state=view_state)
+r.to_html("carto_styles_color_bins.html", open_browser=True)
---
+++ bindings/carto-pydeck/examples/scripts/carto_styles_color_categories.py
@@ -0,0 +1,49 @@
+"""
+CartoLayer
+==========
+
+Render cloud data with color categories style.
+"""
+import pydeck as pdk
+
+from carto_auth import CartoAuth
+from carto_pydeck import register_carto_layer, get_layer_credentials, notify_error
+from carto_pydeck.layer import MapType, CartoConnection
+from carto_pydeck.styles import color_categories
+
+register_carto_layer()
+
+carto_auth = CartoAuth.from_oauth()
+
+layer = pdk.Layer(
+    "CartoLayer",
+    data="SELECT geom, landuse_type FROM `cartobq.public_account.wburg_parcels`",
+    type_=MapType.QUERY,
+    connection=CartoConnection.CARTO_DW,
+    credentials=get_layer_credentials(carto_auth),
+    get_fill_color=color_categories(
+        "landuse_type",
+        [
+            "Multi-Family Walk-Up Buildings",
+            "Multi-Family Elevator Buildings",
+            "Mixed Residential And Commercial Buildings",
+            "Parking Facilities",
+            "1 and 2 Family Buildings",
+            "Commercial and Office Buildings",
+            "Vacant Land",
+            "Public Facilities and Institutions",
+            "Transportation and Utility",
+            "Open Space and Outdoor Recreation",
+            "Industrial and Manufacturing",
+        ],
+        "Bold",
+    ),
+    get_line_color=[0, 0, 0, 100],
+    line_width_min_pixels=0.5,
+    pickable=True,
+    on_data_error=notify_error(),
+)
+view_state = pdk.ViewState(latitude=40.715, longitude=-73.959, zoom=14)
+
+r = pdk.Deck(layer, map_style=pdk.map_styles.LIGHT, initial_view_state=view_state)
+r.to_html("carto_styles_color_categories.html", open_browser=True)
