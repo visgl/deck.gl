3dec16f362c3316db622acdf3064014ca8515734
Felix Palmer <felixpalmer@gmail.com>
2022-09-19 15:41:38 +0200

    Display helpful error if h3 v4 is accidentally included

--- modules/main/bundle.ts
+++ modules/main/bundle.ts
@@ -13,11 +13,14 @@ Object.assign(
 
 // Check for H3 library. In the standalone bundle, h3-js is not included (see `scripts/bundle.config.js`)
 deck.H3HexagonLayer._checkH3Lib = () => {
+  const installHelp =
+    'include the <script src="https://unpkg.com/h3-js@^3.0.0"></script> tag before the deck.gl script tag. https://deck.gl/docs/api-reference/geo-layers/h3-hexagon-layer';
   // @ts-ignore
   if (!window.h3) {
-    throw new Error(
-      'To use H3 functionality, include the <script src="https://unpkg.com/h3-js"></script> tag before the deck.gl script tag. https://deck.gl/docs/api-reference/geo-layers/h3-hexagon-layer'
-    );
+    throw new Error(`To use H3 functionality, ${installHelp}`);
+    // @ts-ignore
+  } else if (!window.h3.polyfill && window.h3.polygonToCells) {
+    throw new Error(`Only H3-js v3 is supported, you are using v4. To use v3, ${installHelp}`);
   }
 };
 
