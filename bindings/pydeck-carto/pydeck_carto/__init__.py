7c5f64c8ad85b288406393b8e6a753cab8041444
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2022-10-12 14:44:15 +0200

    Remove get_error_notifier functions

--- bindings/pydeck-carto/pydeck_carto/__init__.py
+++ bindings/pydeck-carto/pydeck_carto/__init__.py
@@ -1,10 +1,8 @@
 from ._version import __version__
 from .layer import register_carto_layer, get_layer_credentials
-from .error import get_error_notifier
 
 __all__ = [
     "__version__",
     "register_carto_layer",
     "get_layer_credentials",
-    "get_error_notifier",
 ]
