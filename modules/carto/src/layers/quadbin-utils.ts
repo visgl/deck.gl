a20c92f466cc56d4d5156a3cd2732355a414d468
Felix Palmer <felixpalmer@gmail.com>
2022-12-05 12:28:17 +0100

    To avoid z-fighting reduce polygon footprint when extruding

--- modules/carto/src/layers/quadbin-utils.ts
+++ modules/carto/src/layers/quadbin-utils.ts
@@ -3,18 +3,18 @@ import {cellToTile} from 'quadbin';
 
 const TILE_SIZE = 512;
 
-export function quadbinToWorldBounds(quadbin: bigint): [number[], number[]] {
+export function quadbinToWorldBounds(quadbin: bigint, coverage: number): [number[], number[]] {
   const {x, y, z} = cellToTile(quadbin);
   const mask = 1 << z;
   const scale = mask / TILE_SIZE;
   return [
     [x / scale, TILE_SIZE - y / scale],
-    [(x + 0.99) / scale, TILE_SIZE - (y + 0.99) / scale]
+    [(x + coverage) / scale, TILE_SIZE - (y + coverage) / scale]
   ];
 }
 
-export function getQuadbinPolygon(quadbin: bigint): number[] {
-  const [topLeft, bottomRight] = quadbinToWorldBounds(quadbin);
+export function getQuadbinPolygon(quadbin: bigint, coverage = 1): number[] {
+  const [topLeft, bottomRight] = quadbinToWorldBounds(quadbin, coverage);
   const [w, n] = worldToLngLat(topLeft);
   const [e, s] = worldToLngLat(bottomRight);
   return [e, n, e, s, w, s, w, n, e, n];
