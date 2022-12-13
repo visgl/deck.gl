e967dd373d837ac510088736a29ea924a8ae9426
Felix Palmer <felixpalmer@gmail.com>
2022-12-13 14:40:37 +0100

    Improve performance of new deepEqual

--- modules/core/src/utils/deep-equal.ts
+++ modules/core/src/utils/deep-equal.ts
@@ -1,17 +1,24 @@
-// Partial deep equal (only recursive on arrays)
-export function deepEqual(a: any, b: any, recursive: boolean = false): boolean {
+/**
+ * Fast partial deep equal for Arrays/Objects.
+ *
+ * @param a Array/Object
+ * @param b Array/Object to compare against `a`
+ * @param recursive If true, perform recursive comparison on Objects. Otherwise, only recursive on Arrays.
+ */
+export function deepEqual(a: any, b: any, recursive?: boolean): boolean {
   if (a === b) {
     return true;
   }
   if (!a || !b) {
     return false;
   }
-  const keys = [...new Set([...Object.keys(a), ...Object.keys(b)])];
-  for (const key of keys) {
+  for (const key in a) {
     const aValue = a[key];
     const bValue = b[key];
-    const recurse = recursive || (Array.isArray(aValue) && Array.isArray(bValue));
-    const equals = aValue === bValue || (recurse && deepEqual(aValue, bValue));
+    const equals =
+      aValue === bValue ||
+      ((recursive || (Array.isArray(aValue) && Array.isArray(bValue))) &&
+        deepEqual(aValue, bValue, recursive));
     if (!equals) {
       return false;
     }
