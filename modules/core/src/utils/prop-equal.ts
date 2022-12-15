3229144858fe4146bc15dc6de496ae765ee5832b
Felix Palmer <felixpalmer@gmail.com>
2022-12-15 11:32:09 +0100

    Check for array length first

--- modules/core/src/utils/prop-equal.ts
+++ modules/core/src/utils/prop-equal.ts
@@ -20,6 +20,15 @@ export function propEqual(a: any, b: any, depth: number): boolean {
     return false;
   }
 
+  // Handle case where key in b is missing from a
+  if (Array.isArray(b)) {
+    if (b.length !== a.length) return false;
+  } else {
+    for (const key in b) {
+      if (!(key in a)) return false;
+    }
+  }
+
   for (const key in a) {
     if (depth) {
       // Often will have shallow equality, so skip function invocation
@@ -28,15 +37,6 @@ export function propEqual(a: any, b: any, depth: number): boolean {
       }
     } else if (a[key] !== b[key]) {
       return false;
-    }
-  }
-
-  // Handle case where key in b is missing from a
-  if (Array.isArray(b)) {
-    if (b.length !== a.length) return false;
-  } else {
-    for (const key in b) {
-      if (!(key in a)) return false;
     }
   }
 
