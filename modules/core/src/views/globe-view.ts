bc8c7fd76065e76364bab40cdd52b661bf5f09de
Jeff Peck <jeffrey.peck@10xgenomics.com>
2022-12-19 15:14:42 -0500

    fix GlobeViewProps types

--- modules/core/src/views/globe-view.ts
+++ modules/core/src/views/globe-view.ts
@@ -19,9 +19,9 @@ type GlobeViewProps = {
   /** The resolution at which to turn flat features into 3D meshes, in degrees. Smaller numbers will generate more detailed mesh. Default `10`. */
   resolution?: number;
   /** Scaler for the near plane, 1 unit equals to the height of the viewport. Default to `0.1`. Overwrites the `near` parameter. */
-  nearZMultiplier?: boolean;
+  nearZMultiplier?: number;
   /** Scaler for the far plane, 1 unit equals to the distance from the camera to the top edge of the screen. Default to `1.01`. Overwrites the `far` parameter. */
-  farZMultiplier?: boolean;
+  farZMultiplier?: number;
   /** Distance of the camera relative to viewport height. Default `1.5`. */
   altitude?: number;
 };
