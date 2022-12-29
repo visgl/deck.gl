e31fa6b1b33c8300f96ef50cfaa9e832d16cce0d
Xiaoji Chen <cxiaoji@gmail.com>
2022-12-29 11:54:35 -0800

    upgrade glsl-transpiler

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -9,7 +9,3 @@ cp node_modules/d3-interpolate/dist/d3-interpolate.js node_modules/d3-interpolat
 cp node_modules/d3-scale/dist/d3-scale.js node_modules/d3-scale/dist/d3-scale.cjs
 cp node_modules/d3-time/dist/d3-time.js node_modules/d3-time/dist/d3-time.cjs
 cp node_modules/d3-time-format/dist/d3-time-format.js node_modules/d3-time-format/dist/d3-time-format.cjs
-
-# Patch a bug in glsl-transpiler when running in strict mode.
-# TODO: wait for https://github.com/stackgl/glsl-transpiler/pull/52
-curl https://raw.githubusercontent.com/Pessimistress/glsl-transpiler/3cc1f3ac3dda48a8b7baf15f46561eaead80fd7c/lib/descriptor.js > node_modules/glsl-transpiler/lib/descriptor.js
