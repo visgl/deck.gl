21c561e3cd2ec0f58e900a0b289c87440cdb9cb1
Xiaoji Chen <cxiaoji@gmail.com>
2023-01-31 09:36:29 -0800

    address comments

--- test/modules/extensions/terrain/terrain-cover.spec.js
+++ test/modules/extensions/terrain/terrain-cover.spec.js
@@ -1,5 +1,5 @@
 import test from 'tape-promise/tape';
-import TerrainCover from '@deck.gl/extensions/terrain/terrain-cover';
+import {TerrainCover} from '@deck.gl/extensions/terrain/terrain-cover';
 import {TerrainExtension} from '@deck.gl/extensions';
 
 import {WebMercatorViewport, OrthographicViewport} from '@deck.gl/core';
@@ -11,26 +11,26 @@ import {LifecycleTester} from '../utils';
 test('TerrainCover#viewport diffing#geo', async t => {
   const lifecycle = new LifecycleTester();
   let viewport = new WebMercatorViewport({width: 400, height: 300, zoom: 0});
-  let owner = new ScatterplotLayer();
+  let targetLayer = new ScatterplotLayer();
 
-  await lifecycle.update({viewport, layers: [owner]});
+  await lifecycle.update({viewport, layers: [targetLayer]});
 
-  const tc = new TerrainCover(owner);
+  const tc = new TerrainCover(targetLayer);
   t.notOk(tc.shouldUpdate({viewport}), 'Should not need update');
-  t.notOk(tc.commonBounds, 'Empty owner does not have bounds');
+  t.notOk(tc.commonBounds, 'Empty targetLayer does not have bounds');
   t.notOk(tc.renderTexture, 'Render texture should be empty');
   t.notOk(tc.pickingTexture, 'Picking texture should be empty');
 
-  owner = new ScatterplotLayer({
+  targetLayer = new ScatterplotLayer({
     data: [
       [-90, -40.97989806962013],
       [0, 0]
     ],
     getPosition: d => d
   });
-  await lifecycle.update({viewport, layers: [owner]});
+  await lifecycle.update({viewport, layers: [targetLayer]});
 
-  t.ok(tc.shouldUpdate({owner, viewport}), 'Should require update');
+  t.ok(tc.shouldUpdate({targetLayer, viewport}), 'Should require update');
   t.deepEqual(
     tc.commonBounds,
     [
@@ -43,10 +43,10 @@ test('TerrainCover#viewport diffing#geo', async t => {
   t.is(tc.renderViewport.zoom, 0, 'Render viewport zoom');
 
   viewport = new WebMercatorViewport({width: 400, height: 300, zoom: 0.5});
-  t.notOk(tc.shouldUpdate({owner, viewport}), 'Should not need update');
+  t.notOk(tc.shouldUpdate({targetLayer, viewport}), 'Should not need update');
 
   viewport = new WebMercatorViewport({width: 400, height: 300, zoom: 2});
-  t.ok(tc.shouldUpdate({owner, viewport}), 'Should require update');
+  t.ok(tc.shouldUpdate({targetLayer, viewport}), 'Should require update');
   t.is(tc.renderViewport.zoom, 2, 'Render viewport zoom');
 
   tc.delete();
@@ -57,7 +57,7 @@ test('TerrainCover#viewport diffing#geo', async t => {
 test('TerrainCover#viewport diffing#non-geo', async t => {
   const lifecycle = new LifecycleTester();
   const viewport = new OrthographicViewport({width: 400, height: 300, zoom: 0});
-  const owner = new ScatterplotLayer({
+  const targetLayer = new ScatterplotLayer({
     data: [
       [-90, -40],
       [0, 20]
@@ -65,10 +65,10 @@ test('TerrainCover#viewport diffing#non-geo', async t => {
     getPosition: d => d
   });
 
-  await lifecycle.update({viewport, layers: [owner]});
+  await lifecycle.update({viewport, layers: [targetLayer]});
 
-  const tc = new TerrainCover(owner);
-  t.ok(tc.shouldUpdate({owner, viewport}), 'Should require update');
+  const tc = new TerrainCover(targetLayer);
+  t.ok(tc.shouldUpdate({targetLayer, viewport}), 'Should require update');
   t.deepEqual(
     tc.commonBounds,
     [
@@ -117,13 +117,13 @@ test('TerrainCover#layers diffing#non-geo', async t => {
   let viewport = new OrthographicViewport({width: 800, height: 600, zoom: 0, target: [400, 300]});
   await lifecycle.update({viewport, layers: [terrainSource, overlay, scatterplotLayer]});
 
-  const owner = lifecycle.layers.find(l => l.id === 'terrain-0-0-0');
-  const tc = new TerrainCover(owner);
+  const targetLayer = lifecycle.layers.find(l => l.id === 'terrain-0-0-0');
+  const tc = new TerrainCover(targetLayer);
   let drapeLayers = lifecycle.layers.filter(
     l => !l.isComposite && l.state.terrainFittingMode === 'drape'
   );
   t.ok(drapeLayers.length >= 5, 'Found drape layers');
-  t.ok(tc.shouldUpdate({owner, layers: drapeLayers}), 'Should require update');
+  t.ok(tc.shouldUpdate({targetLayer, layers: drapeLayers}), 'Should require update');
   t.deepEqual(tc.layers, ['overlay-0-0-0-points-circle', 'scatterplot'], 'Correctly culled layers');
 
   viewport = new OrthographicViewport({width: 800, height: 600, zoom: 0, target: [-400, -300]});
@@ -133,13 +133,13 @@ test('TerrainCover#layers diffing#non-geo', async t => {
     l => !l.isComposite && l.state.terrainFittingMode === 'drape'
   );
   t.ok(drapeLayers.length >= 9, 'Found drape layers');
-  t.notOk(tc.shouldUpdate({owner, layers: drapeLayers}), 'Should not need update');
+  t.notOk(tc.shouldUpdate({targetLayer, layers: drapeLayers}), 'Should not need update');
 
   await lifecycle.update({layers: [terrainSource, overlay]});
   drapeLayers = lifecycle.layers.filter(
     l => !l.isComposite && l.state.terrainFittingMode === 'drape'
   );
-  t.ok(tc.shouldUpdate({owner, layers: drapeLayers}), 'Should require update');
+  t.ok(tc.shouldUpdate({targetLayer, layers: drapeLayers}), 'Should require update');
   t.deepEqual(tc.layers, ['overlay-0-0-0-points-circle'], 'Correctly culled layers');
 
   tc.delete();
@@ -186,13 +186,13 @@ test('TerrainCover#layers diffing#geo', async t => {
   });
   await lifecycle.update({viewport, layers: [terrainSource, overlay, scatterplotLayer]});
 
-  const owner = lifecycle.layers.find(l => l.id === 'terrain-0-1-2');
-  const tc = new TerrainCover(owner);
+  const targetLayer = lifecycle.layers.find(l => l.id === 'terrain-0-1-2');
+  const tc = new TerrainCover(targetLayer);
   let drapeLayers = lifecycle.layers.filter(
     l => !l.isComposite && l.state.terrainFittingMode === 'drape'
   );
   t.ok(drapeLayers.length >= 5, 'Found drape layers');
-  t.ok(tc.shouldUpdate({owner, layers: drapeLayers}), 'Should require update');
+  t.ok(tc.shouldUpdate({targetLayer, layers: drapeLayers}), 'Should require update');
   t.deepEqual(tc.layers, ['overlay-0-1-2-points-circle', 'scatterplot'], 'Correctly culled layers');
 
   tc.delete();
