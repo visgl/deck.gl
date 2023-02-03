9cae34d1964c9c7dce7ad831fcadf60ce38e6aef
Felix Palmer <felixpalmer@gmail.com>
2023-02-03 16:23:51 +0100

    Extract debugFBO

--- modules/extensions/src/collide/collide-effect.ts
+++ modules/extensions/src/collide/collide-effect.ts
@@ -1,9 +1,9 @@
 import {Framebuffer, Renderbuffer, Texture2D, cssToDeviceRatio} from '@luma.gl/core';
-import {readPixelsToArray} from '@luma.gl/core';
 import {equals} from '@math.gl/core';
 import type {Effect, Layer, PreRenderOptions, Viewport} from '@deck.gl/core';
 import CollidePass from './collide-pass';
 import MaskEffect from '../mask/mask-effect';
+// import {debugFBO} from '../utils/debug';
 
 type CollideExtensionProps = {
   collideTestProps?: {};
@@ -80,11 +80,7 @@ export default class CollideEffect implements Effect {
       });
     }
 
-    // // @ts-ignore
-    // if (window.debug && Object.keys(channels)[0]) {
-    //   const collideFBO = this.collideFBOs[Object.keys(channels)[0]];
-    //   this._debug(collideFBO);
-    // }
+    // debugFBO(this.collideFBOs[Object.keys(channels)[0]], {minimap: true});
   }
 
   private _render(
@@ -243,55 +239,4 @@ export default class CollideEffect implements Effect {
   }
 
   // Debug show FBO contents on screen
-  // _debug(collideMap) {
-  //   const minimap = true;
-  //   const color = readPixelsToArray(collideMap);
-  //   let canvas = document.getElementById('fbo-canvas') as HTMLCanvasElement;
-  //   const canvasHeight = (minimap ? 2 : 1) * collideMap.height;
-  //   if (!canvas) {
-  //     canvas = document.createElement('canvas');
-  //     canvas.id = 'fbo-canvas';
-  //     canvas.style.zIndex = '100';
-  //     canvas.style.position = 'absolute';
-  //     canvas.style.top = '0';
-  //     canvas.style.right = '0';
-  //     canvas.style.border = 'blue 1px solid';
-  //     canvas.style.transform = 'scaleY(-1)';
-  //     document.body.appendChild(canvas);
-  //   }
-  //   if (canvas.width !== collideMap.width || canvas.height !== canvasHeight) {
-  //     canvas.width = collideMap.width;
-  //     canvas.height = canvasHeight;
-  //     canvas.style.width = `${0.125 * DOWNSCALE * canvas.width}px`; // Fit with 80% width #app
-  //   }
-  //   const ctx = canvas.getContext('2d')!;
-  //   const imageData = ctx.createImageData(canvas.width, canvas.height);
-
-  //   // Minimap
-  //   if (minimap) {
-  //     const zoom = 8; // Zoom factor for minimap
-  //     const {width, height} = canvas;
-  //     for (let y = 0; y < height; y++) {
-  //       for (let x = 0; x < width; x++) {
-  //         const d = 4 * (x + y * width); // destination pixel
-  //         const s = 4 * (Math.floor(x / zoom) + Math.floor(y / zoom) * width); // source
-  //         imageData.data[d + 0] = color[s + 0];
-  //         imageData.data[d + 1] = color[s + 1];
-  //         imageData.data[d + 2] = color[s + 2];
-  //         imageData.data[d + 3] = color[s + 3];
-  //       }
-  //     }
-  //   }
-
-  //   // Full map
-  //   const offset = minimap ? color.length : 0;
-  //   for (let i = 0; i < color.length; i += 4) {
-  //     imageData.data[offset + i + 0] = color[i + 0];
-  //     imageData.data[offset + i + 1] = color[i + 1];
-  //     imageData.data[offset + i + 2] = color[i + 2];
-  //     imageData.data[offset + i + 3] = color[i + 3];
-  //   }
-
-  //   ctx.putImageData(imageData, 0, 0);
-  // }
 }
