e86b5290312bc20d4a34e6d0cfa8c1c0b30cd311
Jannik <72194488+JannikGM@users.noreply.github.com>
2023-02-20 15:44:44 +0100

    Consistent picker comments in Mapbox- and GoogleMapsOverlay

--- modules/mapbox/src/mapbox-overlay.ts
+++ modules/mapbox/src/mapbox-overlay.ts
@@ -145,13 +145,13 @@ export default class MapboxOverlay implements IControl {
     return 'top-left';
   }
 
-  /** Forwards the Deck.pickObject method */
+  /** Equivalent of `pickObject`. */
   pickObject(params: Parameters<Deck['pickObject']>[0]): ReturnType<Deck['pickObject']> {
     assert(this._deck);
     return this._deck.pickObject(params);
   }
 
-  /** Forwards the Deck.pickMultipleObjects method */
+  /** Equivalent of `pickMultipleObjects`. */
   pickMultipleObjects(
     params: Parameters<Deck['pickMultipleObjects']>[0]
   ): ReturnType<Deck['pickMultipleObjects']> {
@@ -159,7 +159,7 @@ export default class MapboxOverlay implements IControl {
     return this._deck.pickMultipleObjects(params);
   }
 
-  /** Forwards the Deck.pickObjects method */
+  /** Equivalent of `pickObjects`. */
   pickObjects(params: Parameters<Deck['pickObjects']>[0]): ReturnType<Deck['pickObjects']> {
     assert(this._deck);
     return this._deck.pickObjects(params);
