11db58e0ce720d2944e21bff5cdd346b8f42da28
menusal <daniarlandis@gmail.com>
2023-03-02 13:27:44 +0100

    add app example and data

--- examples/website/collision-filter/app.jsx
+++ examples/website/collision-filter/app.jsx
@@ -1,97 +1,103 @@
-/* eslint-disable max-statements */
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import {createRoot} from 'react-dom/client';
+import {Map} from 'react-map-gl';
+import maplibregl from 'maplibre-gl';
 import DeckGL from '@deck.gl/react';
-import {OrthographicView} from '@deck.gl/core';
+import {GeoJsonLayer, TextLayer} from '@deck.gl/layers';
+import data from './data/data.json';
+import dataLabels from './data/labels.json';
+import {CollisionFilterExtension} from '@deck.gl/extensions';
 
-import { LineLayer, IconLayer, TextLayer } from '@deck.gl/layers';
-
-import { CollisionFilterExtension } from '@deck.gl/extensions';
-
-const ICON_MISSING = 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTAwIiBoZWlnaHQ9IjEwMCIgdmlld0JveD0iLTQtNCA4IDgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGNpcmNsZSByPSI0IiBmaWxsPSIjY2NjIi8+PHRleHQgeT0iLjUiIGZvbnQtZmFtaWx5PSJtb25vc3BhY2UiIGZvbnQtc2l6ZT0iNiIgdGV4dC1hbmNob3I9Im1pZGRsZSIgYWxpZ25tZW50LWJhc2VsaW5lPSJtaWRkbGUiIGZpbGw9IiNmZmYiPj88L3RleHQ+PC9zdmc+';
+const COLOR_SEVERITY = [
+  [240, 59, 32], // Severity 1 - Red
+  [254, 224, 139], // Severity 2 - Orange
+  [26, 152, 80] // Severity 3 - Yellow
+];
 
 const INITIAL_VIEW_STATE = {
-  target: [1000, 1000, 0],
-  zoom: 0
+  longitude: -3.989668,
+  latitude: 54.726504,
+  zoom: 5,
+  maxZoom: 16,
+  pitch: 0,
+  bearing: 0
 };
 
+export const COLORS_STEPS = [
+  {threshold: 'High', color: COLOR_SEVERITY[0], strokeWidth: 2},
+  {threshold: 'Medium', color: COLOR_SEVERITY[1], strokeWidth: 2},
+  {threshold: 'Low', color: COLOR_SEVERITY[2], strokeWidth: 2}
+];
 
 export default function App({
-  initialViewState = INITIAL_VIEW_STATE
+  mapStyle = 'https://basemaps.cartocdn.com/gl/dark-matter-nolabels-gl-style/style.json',
+  radiusScale = 1,
+  priorityDesc = false,
+  sizeScale = 5,
+  priorityLabelDesc = 'Max'
 }) {
-  const [data, setData] = useState(() => ({}))
-
-  useEffect(() => {
-    const fn = async () => {
-      const resp = await fetch('https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/got-graph.json');
-      const {nodes, links} = await resp.json();
-      for (const link of links) {
-        const source = nodes[link[0]];
-        source.links = (source.links || 0) + 1;
-        const target = nodes[link[1]];
-        target.links = (target.links || 0) + 1;
-      }
-
-      setData({nodes, links});
-    }
-    fn();
-  }, []);
-
-  const {nodes = {}, links = []} = data;
-
-  const layer = [
-    new LineLayer({
-      id: 'links',
-      data: links,
-      getSourcePosition: link => {
-        const source = nodes[link[0]];
-        return [source.x, source.y];
-      },
-      getTargetPosition: link => {
-        const target = nodes[link[1]];
-        return [target.x, target.y];
-      },
-      getColor: [100, 100, 100],
-      getWidth: 1,
-      widthUnits: 'common',
-      widthMaxPixels: 1
-    }),
-    new IconLayer({
-      id: 'nodes',
-      data: Object.values(nodes),
-      getPosition: node => [node.x, node.y],
-      getIcon: node => ({
-        url: node.thumbnail || ICON_MISSING,
-        width: 100,
-        height: 100
-      }),
-      getSize: getIconSize,
-      getCollidePriority: node => node.links,
-      collideTestProps: {
-        sizeScale: 2
+  const labels = dataLabels.features;
+  const layers = [
+    new GeoJsonLayer({
+      id: 'geojson',
+      data,
+      opacity: 0.8,
+      stroked: false,
+      filled: true,
+      pointRadiusUnits: 'pixels',
+      pointType: 'circle',
+      getPointRadius: 5,
+      getFillColor: d =>
+        d.properties.accident_severity === 1
+          ? COLOR_SEVERITY[0]
+          : d.properties.accident_severity === 2
+          ? COLOR_SEVERITY[1]
+          : COLOR_SEVERITY[2],
+      pickable: true,
+      // CollisionFilterExtension props
+      collisionTestProps: {radiusScale},
+      getCollisionPriority: d =>
+        priorityDesc === 'Low' ? d.properties.accident_severity : -d.properties.accident_severity,
+      collisionGroup: 'visualization',
+      updateTriggers: {
+        getCollisionPriority: [priorityDesc]
       },
       extensions: [new CollisionFilterExtension()]
     }),
     new TextLayer({
-      id: 'labels',
-      data: Object.values(nodes),
-      getPosition: node => [node.x, node.y],
-      getText: node => node.name,
-      getSize: 12,
-      getAlignmentBaseline: 'top',
-      getPixelOffset: node => [0, getIconSize(node) / 2],
-      background: true,
+      id: 'text-layer',
+      data: labels,
+      pickable: true,
+      getPosition: d => d.geometry.coordinates,
+      getText: d => d.properties.NAME,
+      getColor: [255, 255, 255, 255],
+      getSize: d => d.properties.RANK_MAX * 3,
+      getAngle: 0,
+      getTextAnchor: 'middle',
+      getAlignmentBaseline: 'bottom',
+      outlineWidth: 2,
+      fontSettings: {
+        sdf: true
+      },
+      // CollisionFilterExtension props
+      getCollisionPriority: d =>
+        priorityLabelDesc === 'Max' ? d.properties.RANK_MAX : -d.properties.RANK_MAX,
+      collisionTestProps: {sizeScale},
+      collisionGroup: 'legend',
+      updateTriggers: {
+        getCollisionPriority: [priorityLabelDesc]
+      },
       extensions: [new CollisionFilterExtension()]
     })
   ];
 
-  return <DeckGL initialViewState={initialViewState} controller={true} layers={[layer]} views={new OrthographicView()} />;
+  return (
+    <DeckGL layers={layers} initialViewState={INITIAL_VIEW_STATE} controller={true}>
+      <Map reuseMaps mapLib={maplibregl} mapStyle={mapStyle} preventStyleDiffing={true} />
+    </DeckGL>
+  );
 }
 
 export function renderToDOM(container) {
   createRoot(container).render(<App />);
-}
-
-function getIconSize(node) {
-  return Math.sqrt(node.links) * 10 + 20;
 }
