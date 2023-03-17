6abe821f306a170bdf606b171b5166fcd13eca4c
Felix Palmer <felixpalmer@gmail.com>
2023-03-17 11:19:48 +0100

    Fast PBF parsing & remove unpackProperties

--- modules/carto/src/layers/schema/tile-loader-utils.ts
+++ modules/carto/src/layers/schema/tile-loader-utils.ts
@@ -7,16 +7,3 @@ export function parsePbf(buffer: ArrayBuffer, TileReader) {
   const tile = TileReader.read(pbf);
   return tile;
 }
-
-export function unpackProperties(properties: KeyValueProperties[]): Properties[] {
-  if (!properties || !properties.length) {
-    return [];
-  }
-  return properties.map(item => {
-    const currentRecord: Properties = {};
-    item.data.forEach(({key, value}) => {
-      currentRecord[key] = value;
-    });
-    return currentRecord;
-  });
-}
