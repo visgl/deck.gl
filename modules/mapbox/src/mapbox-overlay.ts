c698ac865aa96eb5768c0310ceb211f7e3b6d9fa
Xiaoji Chen <cxiaoji@gmail.com>
2023-03-23 10:46:18 -0700

    MapboxOverlay handles drag events when interleaved:false

--- modules/mapbox/src/mapbox-overlay.ts
+++ modules/mapbox/src/mapbox-overlay.ts
@@ -32,6 +32,7 @@ export default class MapboxOverlay implements IControl {
   private _map?: Map;
   private _container?: HTMLDivElement;
   private _interleaved: boolean;
+  private _lastMouseDownPoint?: {x: number; y: number};
 
   constructor(props: MapboxOverlayProps) {
     const {interleaved = false, ...otherProps} = props;
@@ -77,6 +78,10 @@ export default class MapboxOverlay implements IControl {
 
     map.on('resize', this._updateContainerSize);
     map.on('render', this._updateViewState);
+    map.on('mousedown', this._handleMouseEvent);
+    map.on('dragstart', this._handleMouseEvent);
+    map.on('drag', this._handleMouseEvent);
+    map.on('dragend', this._handleMouseEvent);
     map.on('mousemove', this._handleMouseEvent);
     map.on('mouseout', this._handleMouseEvent);
     map.on('click', this._handleMouseEvent);
@@ -125,6 +130,10 @@ export default class MapboxOverlay implements IControl {
   private _onRemoveOverlaid(map: Map): void {
     map.off('resize', this._updateContainerSize);
     map.off('render', this._updateViewState);
+    map.off('mousedown', this._handleMouseEvent);
+    map.off('dragstart', this._handleMouseEvent);
+    map.off('drag', this._handleMouseEvent);
+    map.off('dragend', this._handleMouseEvent);
     map.off('mousemove', this._handleMouseEvent);
     map.off('mouseout', this._handleMouseEvent);
     map.off('click', this._handleMouseEvent);
@@ -204,15 +213,34 @@ export default class MapboxOverlay implements IControl {
       tapCount?: number;
     } = {
       type: event.type,
-      offsetCenter: event.point,
+      // drag* events do not contain a `point` field
+      offsetCenter: event.point || this._lastMouseDownPoint,
       srcEvent: event
     };
 
-    switch (event.type) {
+    switch (mockEvent.type) {
+      case 'mousedown':
+        deck._onPointerDown(mockEvent as MjolnirGestureEvent);
+        this._lastMouseDownPoint = event.point;
+        break;
+
+      case 'dragstart':
+        mockEvent.type = 'panstart';
+        deck._onEvent(mockEvent as MjolnirGestureEvent);
+        break;
+
+      case 'drag':
+        mockEvent.type = 'panmove';
+        deck._onEvent(mockEvent as MjolnirGestureEvent);
+        break;
+
+      case 'dragend':
+        mockEvent.type = 'panend';
+        deck._onEvent(mockEvent as MjolnirGestureEvent);
+        break;
+
       case 'click':
         mockEvent.tapCount = 1;
-        // Hack: because we do not listen to pointer down, perform picking now
-        deck._onPointerDown(mockEvent as MjolnirGestureEvent);
         deck._onEvent(mockEvent as MjolnirGestureEvent);
         break;
 
