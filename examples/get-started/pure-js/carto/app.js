c47048042b388681070b2f50975ddda5d117a35e
Felix Palmer <felixpalmer@gmail.com>
2023-04-20 14:01:21 +0200

    Revert changes to test app

--- examples/get-started/pure-js/carto/app.js
+++ examples/get-started/pure-js/carto/app.js
@@ -1,46 +1,20 @@
 import maplibregl from 'maplibre-gl';
 import {Deck} from '@deck.gl/core';
-import {CartoLayer, setDefaultCredentials, BASEMAP, MAP_TYPES} from '@deck.gl/carto';
+import {fetchMap} from '@deck.gl/carto';
 
-setDefaultCredentials({
-  accessToken: 'XXX'
-});
+const cartoMapId = 'ff6ac53f-741a-49fb-b615-d040bc5a96b8';
 
-// Add basemap for context.
-const map = new maplibregl.Map({container: 'map', style: BASEMAP.VOYAGER, interactive: false});
+// Get map info from CARTO and update deck
+fetchMap({cartoMapId}).then(({initialViewState, mapStyle, layers}) => {
+  const deck = new Deck({canvas: 'deck-canvas', controller: true, initialViewState, layers});
 
-new Deck({
-  canvas: 'deck-canvas',
-
-  initialViewState: {latitude: 50, longitude: 14.5, zoom: 10},
-  controller: true,
-
-  ...(map && {
-    mapStyle: BASEMAP.VOYAGER,
+  // Add Mapbox GL for the basemap. It's not a requirement if you don't need a basemap.
+  const MAP_STYLE = `https://basemaps.cartocdn.com/gl/${mapStyle.styleType}-gl-style/style.json`;
+  const map = new maplibregl.Map({container: 'map', style: MAP_STYLE, interactive: false});
+  deck.setProps({
     onViewStateChange: ({viewState}) => {
       const {longitude, latitude, ...rest} = viewState;
       map.jumpTo({center: [longitude, latitude], ...rest});
     }
-  }),
-
-  layers: [
-    new CartoLayer({
-      connection: 'bigquery',
-      type: MAP_TYPES.RASTER,
-      data: 'cartobq.public_account.temperature_raster',
-      formatTiles: 'binary',
-      tileSize: 64,
-      getFillColor: d => {
-        const {band_1} = d.properties;
-        return [10 * (band_1 - 20), 0, 300 - 5 * band_1];
-      },
-      opacity: map ? 0.5 : 1
-      // coverage: 0.8
-      // stroked: true
-      // extruded: true,
-      // getElevation: ({properties}) => {
-      //   return 100 * properties.band_1;
-      // }
-    })
-  ]
+  });
 });
