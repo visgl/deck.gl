a5a7d48636d5a2e2cb4730fa048e4875956a727b
Xiaoji Chen <cxiaoji@gmail.com>
2023-07-06 09:16:51 -0700

    Add todo comment

--- modules/core/src/lib/layer.ts
+++ modules/core/src/lib/layer.ts
@@ -547,6 +547,8 @@ export default abstract class Layer<PropsT extends {} = {}> extends Component<
   /** (Internal) Propagate an error event through the system */
   raiseError(error: Error, message: string): void {
     if (message) {
+      // Duplicating error message for backward compatibility, see #7986
+      // TODO - revisit in v9
       error = new Error(`${message}: ${error.message}`, {cause: error});
     }
     if (!this.props.onError?.(error)) {
