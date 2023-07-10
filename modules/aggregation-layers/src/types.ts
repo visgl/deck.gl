991610ee8699e48d72933280c4e9eb6a87afde9f
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2023-07-10 10:41:51 -0700

    Update modules/aggregation-layers/src/types.ts
    Co-authored-by: Ib Green <ib@foursquare.com>

--- modules/aggregation-layers/src/types.ts
+++ modules/aggregation-layers/src/types.ts
@@ -1,3 +1,4 @@
+/** Type assigned to e.g. getColorValue/getElevationValue props in aggregation layers (e.g. CPUGridLayer, HexagonLayer)*/ 
 export type AggregateAccessor<DataT = any> = (args: {
   /** a list of objects whose positions fall inside this cell. */
   objects: DataT[];
