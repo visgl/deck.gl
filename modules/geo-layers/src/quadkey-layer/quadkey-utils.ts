ed1c04cc368a001156ae808cb9954bf0eb5e0496
felixpalmer <felixpalmer@gmail.com>
2023-07-14 09:43:00 +0200

    Remove hairline present on non-stroked QuadkeyLayer (#7995)

--- modules/geo-layers/src/quadkey-layer/quadkey-utils.ts
+++ modules/geo-layers/src/quadkey-layer/quadkey-utils.ts
@@ -2,7 +2,7 @@ import {worldToLngLat} from '@math.gl/web-mercator';
 
 const TILE_SIZE = 512;
 
-export function quadkeyToWorldBounds(quadkey: string): [number[], number[]] {
+export function quadkeyToWorldBounds(quadkey: string, coverage: number): [number[], number[]] {
   let x = 0;
   let y = 0;
   let mask = 1 << quadkey.length;
@@ -16,12 +16,12 @@ export function quadkeyToWorldBounds(quadkey: string): [number[], number[]] {
   }
   return [
     [x / scale, TILE_SIZE - y / scale],
-    [(x + 0.99) / scale, TILE_SIZE - (y + 0.99) / scale]
+    [(x + coverage) / scale, TILE_SIZE - (y + coverage) / scale]
   ];
 }
 
-export function getQuadkeyPolygon(quadkey: string): number[] {
-  const [topLeft, bottomRight] = quadkeyToWorldBounds(quadkey);
+export function getQuadkeyPolygon(quadkey: string, coverage = 1): number[] {
+  const [topLeft, bottomRight] = quadkeyToWorldBounds(quadkey, coverage);
   const [w, n] = worldToLngLat(topLeft);
   const [e, s] = worldToLngLat(bottomRight);
   return [e, n, e, s, w, s, w, n, e, n];
