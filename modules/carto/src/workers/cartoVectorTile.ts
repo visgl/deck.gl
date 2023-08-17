de8d50b102f2c0a54ff70d66be48f4b003bfcf60
felixpalmer <felixpalmer@gmail.com>
2023-08-17 11:27:47 +0200

    CARTO worker loaders (#8056)

---
+++ modules/carto/src/workers/cartoVectorTile.ts
@@ -0,0 +1,4 @@
+import {createLoaderWorker} from '@loaders.gl/loader-utils';
+import CartoVectorTileLoader from '../layers/schema/carto-vector-tile-loader';
+
+createLoaderWorker(CartoVectorTileLoader);
