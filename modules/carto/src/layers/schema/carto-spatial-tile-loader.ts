7643eea0c8c0ad0132e37110fb16ea2f29f3c6ee
felixpalmer <felixpalmer@gmail.com>
2023-08-17 11:27:47 +0200

    CARTO worker loaders (#8056)

--- modules/carto/src/layers/schema/carto-spatial-tile-loader.ts
+++ modules/carto/src/layers/schema/carto-spatial-tile-loader.ts
@@ -2,35 +2,49 @@ import {LoaderOptions, LoaderWithParser} from '@loaders.gl/loader-utils';
 
 import {Tile, TileReader} from './carto-spatial-tile';
 import {parsePbf} from './tile-loader-utils';
+import {getWorkerUrl} from '../../utils';
 import {IndexScheme, binaryToSpatialjson, SpatialJson} from './spatialjson-utils';
+
+const VERSION = typeof __VERSION__ !== 'undefined' ? __VERSION__ : 'latest';
+const id = 'cartoSpatialTile';
+
+type CartoSpatialTileLoaderOptions = LoaderOptions & {
+  cartoSpatialTile?: {
+    scheme: IndexScheme;
+    workerUrl: string;
+  };
+};
+
+const DEFAULT_OPTIONS: CartoSpatialTileLoaderOptions = {
+  cartoSpatialTile: {
+    scheme: 'quadbin',
+    workerUrl: getWorkerUrl(id, VERSION)
+  }
+};
 
 const CartoSpatialTileLoader: LoaderWithParser = {
   name: 'CARTO Spatial Tile',
-  version: '1',
-  id: 'cartoSpatialTile',
+  version: VERSION,
+  id,
   module: 'carto',
   extensions: ['pbf'],
   mimeTypes: ['application/vnd.carto-spatial-tile'],
   category: 'geometry',
-  worker: false,
-  parse: async (arrayBuffer, options) => parseCartoSpatialTile(arrayBuffer, options),
+  parse: async (arrayBuffer, options?: CartoSpatialTileLoaderOptions) =>
+    parseCartoSpatialTile(arrayBuffer, options),
   parseSync: parseCartoSpatialTile,
-  options: {
-    cartoSpatialTile: {
-      scheme: 'quadbin'
-    } as {scheme: IndexScheme}
-  }
+  worker: false, // TODO set to true once workers deployed to unpkg
+  options: DEFAULT_OPTIONS
 };
 
 function parseCartoSpatialTile(
   arrayBuffer: ArrayBuffer,
-  options?: LoaderOptions
+  options?: CartoSpatialTileLoaderOptions
 ): SpatialJson | null {
   if (!arrayBuffer) return null;
   const tile: Tile = parsePbf(arrayBuffer, TileReader);
 
   const {cells} = tile;
-  // @ts-expect-error
   const scheme = options?.cartoSpatialTile?.scheme;
   const data = {cells, scheme};
 
