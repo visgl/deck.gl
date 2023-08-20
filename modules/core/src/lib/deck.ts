7c7729bd73021b36e3dae7813e6e9d6d808f5b95
Xiaoji Chen <cxiaoji@gmail.com>
2023-08-20 10:39:52 -0700

    Fix Deck class canvas creation

--- modules/core/src/lib/deck.ts
+++ modules/core/src/lib/deck.ts
@@ -383,7 +383,10 @@ export default class Deck {
       // TODO v9 should we install WebGL backend as default for now?
       luma.registerDevices([WebGLDevice]);
 
-      deviceOrPromise = luma.createDevice(this.props.deviceProps);
+      deviceOrPromise = luma.createDevice({
+        ...props.deviceProps,
+        canvas: this._createCanvas(props)
+      });
       deviceOrPromise.then(device => {
         this.device = device;
       });
