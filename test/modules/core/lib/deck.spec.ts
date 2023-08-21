f86f617bad84ad883f27f32eee0dc5a94e6838be
Felix Palmer <felixpalmer@gmail.com>
2023-08-21 17:37:24 +0200

    v9 API

--- test/modules/core/lib/deck.spec.ts
+++ test/modules/core/lib/deck.spec.ts
@@ -58,10 +58,10 @@ test('Deck#constructor', t => {
 
 test('Deck#no views', t => {
   const deck = new Deck({
-    gl,
+    device,
     width: 1,
     height: 1,
-    autoResizeDrawingBuffer: gl.canvas.clientWidth > 0,
+    autoResizeDrawingBuffer: device.canvas.clientWidth > 0,
 
     viewState: {longitude: 0, latitude: 0, zoom: 0},
     views: [],
