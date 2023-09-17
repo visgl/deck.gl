ed1906d68b4af6befba0224fc2fdaeea2ed0d544
Chris Gervang <chris.gervang@joby.aero>
2023-09-17 14:14:53 -0700

    Implement icons in CSS as replaceable masks

--- modules/widgets/src/components.tsx
+++ modules/widgets/src/components.tsx
@@ -1,20 +1,11 @@
 import {h, render} from 'preact';
 
 export const IconButton = props => {
-  const {size, label, onClick} = props;
+  const {className, label, onClick} = props;
   return (
     <div className="deck-widget-button-border">
-      <button className="deck-widget-button" type="button" onClick={onClick} title={label}>
-        <svg
-          className="deck-widget-button-icon"
-          fill="none"
-          height="100%"
-          viewBox={`0 0 ${size} ${size}`}
-          width="100%"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          {props.children}
-        </svg>
+      <button className={`deck-widget-button ${className}`} type="button" onClick={onClick} title={label}>
+        <div className="deck-widget-icon" />
       </button>
     </div>
   );
