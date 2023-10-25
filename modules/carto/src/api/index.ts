7d037402300026d5a13bd734bebed4ed907fe884
Felix Palmer <felixpalmer@gmail.com>
2023-10-25 12:32:40 +0200

    Rename maps-v3-client to fetch-map

--- modules/carto/src/api/index.ts
+++ modules/carto/src/api/index.ts
@@ -1,4 +1,4 @@
 export {CartoAPIError} from './carto-api-error';
-export {fetchMap} from './maps-v3-client';
-export type {FetchMapOptions} from './maps-v3-client';
+export {fetchMap} from './fetch-map';
+export type {FetchMapOptions} from './fetch-map';
 export type {APIErrorContext, Format, MapType, RequestType, QueryParameters} from './types';
