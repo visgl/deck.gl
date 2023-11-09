23b85ed618abbd8b89263ef4d24fd6fd9a730d99
Felix Palmer <felixpalmer@gmail.com>
2023-11-09 12:32:54 +0100

    Support queryParameters for boundaryQuerySource

--- modules/carto/src/sources/boundary-query-source.ts
+++ modules/carto/src/sources/boundary-query-source.ts
@@ -1,3 +1,4 @@
+import {QueryParameters} from '../api';
 import {baseSource} from './base-source';
 import type {SourceOptions, TilejsonResult} from './types';
 
@@ -5,21 +6,26 @@ export type BoundaryQuerySourceOptions = SourceOptions & {
   boundaryId: string;
   matchingColumn?: string;
   sqlQuery: string;
+  queryParameters: QueryParameters;
 };
 type UrlParameters = {
   boundaryId: string;
   matchingColumn: string;
   propertiesQuerySource: string;
+  queryParameters?: string;
 };
 
 export const boundaryQuerySource = async function (
   options: BoundaryQuerySourceOptions
 ): Promise<TilejsonResult> {
-  const {boundaryId, matchingColumn = 'id', sqlQuery} = options;
+  const {boundaryId, matchingColumn = 'id', sqlQuery, queryParameters} = options;
   const urlParameters: UrlParameters = {
     boundaryId,
     matchingColumn,
     propertiesQuerySource: sqlQuery
   };
+  if (queryParameters) {
+    urlParameters.queryParameters = JSON.stringify(queryParameters);
+  }
   return baseSource<UrlParameters>('boundary', options, urlParameters) as Promise<TilejsonResult>;
 };
