a4dd0d94bd7085c880081c711b81867067b6673d
Felix Palmer <felixpalmer@gmail.com>
2023-11-09 14:40:06 +0100

    Remove columns

--- modules/carto/src/sources/boundary-tileset-source.ts
+++ modules/carto/src/sources/boundary-tileset-source.ts
@@ -1,23 +1,14 @@
 import {baseSource} from './base-source';
 import type {SourceOptions, TilejsonResult} from './types';
 
-export type BoundaryTilesetSourceOptions = SourceOptions & {
-  boundaryId: string;
-  columns?: string[];
-};
-type UrlParameters = {
-  boundaryId: string;
-  columns?: string;
-};
+export type BoundaryTilesetSourceOptions = SourceOptions & {boundaryId: string};
+type UrlParameters = {boundaryId: string};
 
 export const boundaryTilesetSource = async function (
   options: BoundaryTilesetSourceOptions
 ): Promise<TilejsonResult> {
-  const {boundaryId, columns} = options;
+  const {boundaryId} = options;
   const urlParameters: UrlParameters = {boundaryId};
 
-  if (columns) {
-    urlParameters.columns = columns.join(',');
-  }
   return baseSource<UrlParameters>('boundary', options, urlParameters) as Promise<TilejsonResult>;
 };
