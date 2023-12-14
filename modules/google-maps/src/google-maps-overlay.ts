c2ba79b08b0ea807c6779d8fe1aaa307ebc22f91
Zbigniew Zagórski <1507542+zbigg@users.noreply.github.com>
2023-12-14 17:55:41 +0100

    [v9] Fix GoogleMapsOverlay by not clearing canvas (#8351)

--- modules/google-maps/src/google-maps-overlay.ts
+++ modules/google-maps/src/google-maps-overlay.ts
@@ -242,6 +242,11 @@ export default class GoogleMapsOverlay {
       // @ts-expect-error
       deck.setProps({_framebuffer});
 
+      // With external gl context, animation loop doesn't resize webgl-canvas and thus fails to
+      // calculate corrext pixel ratio. Force this manually.
+      // @ts-expect-error
+      deck.device.canvasContext.resize();
+
       // Camera changed, will trigger a map repaint right after this
       // Clear any change flag triggered by setting viewState so that deck does not request
       // a second repaint
