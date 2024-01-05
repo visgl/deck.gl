aa5a51abed016d266c29ff11de78c4a9d1fbe470
Chris Gervang <chris@gervang.com>
2024-01-05 09:33:29 -0800

    apply MVT change
    Signed-off-by: Chris Gervang <chris@gervang.com>

--- modules/geo-layers/src/mvt-layer/mvt-layer.ts
+++ modules/geo-layers/src/mvt-layer/mvt-layer.ts
@@ -131,7 +131,7 @@ export default class MVTLayer<ExtraProps extends {} = {}> extends TileLayer<
   }
 
   get isLoaded(): boolean {
-    return this.state && this.state.data && this.state.tileset && super.isLoaded;
+    return this.state?.data && super.isLoaded;
   }
 
   updateState({props, oldProps, context, changeFlags}: UpdateParameters<this>) {
