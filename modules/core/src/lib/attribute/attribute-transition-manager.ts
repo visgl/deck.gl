5aeb93963a1bb5daf7982d5bdee1ea7baeb8db96
Don McCurdy <donmccurdy@cartodb.com>
2024-01-09 10:56:06 -0500

    lint fix

--- modules/core/src/lib/attribute/attribute-transition-manager.ts
+++ modules/core/src/lib/attribute/attribute-transition-manager.ts
@@ -149,7 +149,8 @@ export default class AttributeTransitionManager {
     // previous buffers, currentLength, startIndices, etc, to be used as the starting point
     // for the next transition
     let needsUpdate = !transition || transition.type !== settings.type;
-    needsUpdate ||= attribute.buffer.byteLength > transition?.attributeInTransition.buffer.byteLength;
+    needsUpdate ||=
+      attribute.buffer.byteLength > transition?.attributeInTransition.buffer.byteLength;
 
     if (needsUpdate) {
       if (!this.isSupported) {
