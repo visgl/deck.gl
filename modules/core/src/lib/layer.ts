dcc22de9164556f332bc1f8c30cac6eb4970a980
Felix Palmer <felixpalmer@gmail.com>
2024-01-15 14:13:00 +0100

    Use PickingProps

--- modules/core/src/lib/layer.ts
+++ modules/core/src/lib/layer.ts
@@ -55,7 +55,7 @@ import type {LayerData, LayerProps} from '../types/layer-props';
 import type {LayerContext} from './layer-manager';
 import type {BinaryAttribute} from './attribute/attribute';
 import {RenderPass} from '@luma.gl/core';
-import {PickingSettings} from '@luma.gl/shadertools';
+import {PickingProps} from '@luma.gl/shadertools';
 
 const TRACE_CHANGE_FLAG = 'layer.changeFlag';
 const TRACE_INITIALIZE = 'layer.initialize';
@@ -1213,7 +1213,7 @@ export default abstract class Layer<PropsT extends {} = {}> extends Component<
   // TODO - simplify subclassing interface
   /** Update picking module parameters to highlight the hovered object */
   protected _updateAutoHighlight(info: PickingInfo): void {
-    const picking: PickingSettings = {
+    const picking: PickingProps = {
       highlightedObjectColor: info.picked ? info.color : null
     };
     const {highlightColor} = this.props;
@@ -1257,7 +1257,7 @@ export default abstract class Layer<PropsT extends {} = {}> extends Component<
       oldProps.highlightedObjectIndex !== highlightedObjectIndex ||
       oldProps.highlightColor !== highlightColor
     ) {
-      const picking: PickingSettings = {};
+      const picking: PickingProps = {};
       if (!autoHighlight) {
         picking.highlightedObjectColor = null;
       }
