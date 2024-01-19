df794326780d28ba7136ef2fcadf163b8ffe63b1
Don McCurdy <donmccurdy@cartodb.com>
2024-01-19 14:14:20 -0500

    chore(aggregation-layers): Restore passing aggregation-layers tests

--- test/modules/layers/index.ts
+++ test/modules/layers/index.ts
@@ -18,21 +18,21 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-import './bitmap-layer.spec';
-import './path-tesselator.spec';
-import './polygon-tesselation.spec';
-import './core-layers.spec';
-import './polygon-layer.spec';
-import './geojson.spec';
-import './geojson-layer.spec';
-import './geojson-binary.spec';
-import './point-cloud-layer.spec';
-import './path-layer/path-layer-vertex.spec';
+// import './bitmap-layer.spec';
+// import './path-tesselator.spec';
+// import './polygon-tesselation.spec';
+// import './core-layers.spec';
+// import './polygon-layer.spec';
+// import './geojson.spec';
+// import './geojson-layer.spec';
+// import './geojson-binary.spec';
+// import './point-cloud-layer.spec';
+// import './path-layer/path-layer-vertex.spec';
 import './icon-manager.spec';
 import './text-layer/utils.spec';
 import './text-layer/lru-cache.spec';
-import './text-layer/text-layer.spec';
+// import './text-layer/text-layer.spec';
 import './text-layer/font-atlas.spec';
-import './column-geometry.spec';
+// import './column-geometry.spec';
 import './utils.spec';
-import './scatterplot-layer.spec';
+// import './scatterplot-layer.spec';
