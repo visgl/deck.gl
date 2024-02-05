940da34c207c9a980ad7162dd7b700b1dfd127d7
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2024-02-05 15:01:48 -0800

    Move to ESM modules (#8460)

--- modules/carto/bundle.ts
+++ modules/carto/bundle.ts
@@ -1,4 +1,8 @@
-import * as CartoUtils from './src';
+// Import from package name instead of relative path
+// This will be resolved to src or dist by esbuild depending on bundle settings
+// dist has TS transformers applied
+/* eslint-disable import/no-extraneous-dependencies */
+import * as CartoUtils from '@deck.gl/carto';
 
 export * from '../layers/bundle/peer-dependency';
 
