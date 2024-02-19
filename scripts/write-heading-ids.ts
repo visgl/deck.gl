6d3b6d2b022e28026e98a0b53175fa078ad9b85e
felixpalmer <felixpalmer@gmail.com>
2024-02-19 15:16:49 +0100

    Fix website to work with ESM (#8509)

--- scripts/write-heading-ids.ts
+++ scripts/write-heading-ids.ts
@@ -23,8 +23,12 @@
     + #needsredraw
 
  */
-const fs = require('fs/promises');
-const path = require('path');
+import { promises as fs } from 'fs'
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const docsDir: string = path.resolve(__dirname, '../docs');
 /** Should match if the line is a header */
