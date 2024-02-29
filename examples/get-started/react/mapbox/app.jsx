06907e53b90b48305e5b48254fefddf60704631d
Chris Gervang <chris@gervang.com>
2024-02-29 14:45:33 -0800

    token
    Signed-off-by: Chris Gervang <chris@gervang.com>

--- examples/get-started/react/mapbox/app.jsx
+++ examples/get-started/react/mapbox/app.jsx
@@ -9,8 +9,8 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 const AIR_PORTS =
   'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_10m_airports.geojson';
 
-  // Set your Mapbox API key here or via environment variable
-const MAPBOX_API_KEY = process.env.MapboxAccessToken; // eslint-disable-line
+// Set your Mapbox token here or via environment variable
+const MAPBOX_TOKEN = process.env.MapboxAccessToken; // eslint-disable-line
 
 const INITIAL_VIEW_STATE = {
   latitude: 51.47,
@@ -67,7 +67,7 @@ function Root() {
     <Map
       initialViewState={INITIAL_VIEW_STATE}
       mapStyle={MAP_STYLE}
-      mapboxAccessToken={MAPBOX_API_KEY}
+      mapboxAccessToken={MAPBOX_TOKEN}
     >
       <DeckGLOverlay layers={layers} />
       <NavigationControl position='top-left' />
