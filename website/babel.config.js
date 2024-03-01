fa17c7ffe1d374bb272cda06b617debdbc653f13
Xiaoji Chen <cxiaoji@gmail.com>
2024-03-01 09:24:43 -0800

    website use ts transformed source

--- website/babel.config.js
+++ website/babel.config.js
@@ -1,14 +1,6 @@
 module.exports = {
   presets: [require.resolve('@docusaurus/core/lib/babel/preset')],
   plugins: [
-    'version-inline',
-    "inline-webgl-constants",
-    [
-      'remove-glsl-comments',
-      {
-        patterns: ['**/*.glsl.js', '**/*.glsl.ts']
-      }
-    ],
     // Ensure consistently hashed component classNames between environments (a must for SSR)
     'styled-components'
   ]
