51da9fe70ac53759994f3732934fb7eb6e846337
jaredscarr <jaredscarr@pm.me>
2024-03-07 18:52:50 -0800

    Render looks good, but need to verify code.

--- modules/arcgis/src/commons.ts
+++ modules/arcgis/src/commons.ts
@@ -1,7 +1,6 @@
 /* eslint-disable no-invalid-this */
 
 import type {Device} from '@luma.gl/core';
-import {loadImageBitmap} from '@luma.gl/core';
 import {Model} from '@luma.gl/engine';
 import {GL} from '@luma.gl/constants';
 import {Deck} from '@deck.gl/core';
@@ -10,13 +9,21 @@ import {WebGLDevice} from '@luma.gl/webgl';
 export function initializeResources(device: Device) {
   // 'this' refers to the BaseLayerViewGL2D class from
   // import BaseLayerViewGL2D from "@arcgis/core/views/2d/layers/BaseLayerViewGL2D.js";
-  // const deckglTexture = device.createTexture({ width: 1, height: 1 });
-  // TODO: Remove all references to image texture shader and other logic
-  const imageTexture = device.createTexture({data: loadImageBitmap('https://as2.ftcdn.net/jpg/02/25/74/91/500_F_225749110_EF3DzP1hwjPyalkJkVRHAS8vggkM4Z19.jpg')});
-  const imageTexture2 = device.createTexture({data: loadImageBitmap('https://homework.study.com/cimages/multimages/16/matrix.0008916091751524111603.jpg')});
 
-  this.imageTexture = imageTexture;
-  this.imageTexture2 = imageTexture2;
+  const deckglTexture = device.createTexture({
+    format: 'rgba8unorm',
+    width: 500,
+    height: 500,
+    sampler: {
+      minFilter: 'linear',
+      magFilter: 'linear',
+      addressModeU: 'clamp-to-edge',
+      addressModeV: 'clamp-to-edge'
+    }
+  });
+
+  this.deckglTexture = deckglTexture;
+
   this.buffer = device.createBuffer(new Int8Array([
     // Triangle 1
     -1, -1, // bottom left
@@ -42,46 +49,28 @@ void main(void) {
     fs: `\
 #version 300 es
 precision mediump float;
-uniform sampler2D imageTexture;
+uniform sampler2D deckglTexture;
 in vec2 v_texcoord;
 out vec4 fragColor;
 
 void main(void) {
-    vec4 imageColor = texture(imageTexture, v_texcoord);
-    imageColor *= 0.5;
-    
-    if (v_texcoord.x < 0.5) {
-      fragColor = vec4(imageColor.r, imageColor.g, imageColor.b,  1.0);
-    }
+    vec4 imageColor = texture(deckglTexture, v_texcoord);
+    fragColor = vec4(imageColor.r, imageColor.g, imageColor.b,  1.0);
 }
     `,
     bufferLayout: [{name: 'a_pos', format: 'sint8x2'}],
     bindings: {
-      // deckglTexture,
-      imageTexture
+      deckglTexture,
     },
     parameters: {
       depthWriteEnabled: true,
       depthCompare: 'less-equal'
     },
     attributes: {
-      // eslint-disable-next-line camelcase
       a_pos: this.buffer
     },
     vertexCount: 6,
     drawMode: GL.TRIANGLE_STRIP
-  });
-
-  const deckglTexture = device.createTexture({
-    format: 'rgba8unorm',
-    width: 500,
-    height: 500,
-    sampler: {
-      minFilter: 'linear',
-      magFilter: 'linear',
-      addressModeU: 'clamp-to-edge',
-      addressModeV: 'clamp-to-edge'
-    }
   });
 
   this.deckFbo = device.createFramebuffer({
@@ -115,7 +104,7 @@ void main(void) {
 
     _customRender: redrawReason => {
       if (redrawReason === 'arcgis') {
-        // this.deckInstance._drawLayers(redrawReason);
+        this.deckInstance._drawLayers(redrawReason);
       } else {
         this.redraw();
       }
@@ -124,7 +113,7 @@ void main(void) {
 }
 
 export function render({gl, width, height, viewState}) {
-  const screenFbo = gl.getParameter(gl.FRAMEBUFFER_BINDING);
+  // const screenFbo = gl.getParameter(gl.FRAMEBUFFER_BINDING);
 
   /* global window */
   const dpr = window.devicePixelRatio;
@@ -140,10 +129,10 @@ export function render({gl, width, height, viewState}) {
   // We overlay the texture on top of the map using the full-screen quad.
   const device: WebGLDevice = this.deckInstance.device;
 
-  const imageToTexturePass = device.beginRenderPass({
+  const textureToScreenPass = device.beginRenderPass({
     framebuffer: this.deckFbo,
     parameters: {viewport: [0, 0, width, height]},
-    clearColor: [0, 1, 0, 1],
+    clearColor: [0, 0, 0, 0],
     clearDepth: 1
   });
 
@@ -151,39 +140,11 @@ export function render({gl, width, height, viewState}) {
     {
       blend: true,
       blendFunc: [gl.ONE, gl.ONE_MINUS_SRC_ALPHA],
-      // framebuffer: this.deckFbo, // TODO: not required
       viewport: [0, 0, width, height]
     },
     () => {
-      // eslint-disable-next-line camelcase
       this.model.setBindings({
-        // 'imageTexture': this.imageTexture,
-        'imageTexture': this.imageTexture2,
-      });
-      this.model.draw(imageToTexturePass);
-    },
-  );
-
-  const textureToScreenPass = device.beginRenderPass({
-    framebuffer: screenFbo,
-    parameters: {viewport: [0, 0, width, height]},
-    clearColor: [1, 0, 0, 1],
-    clearDepth: 1
-  });
-
-  device.withParametersWebGL(
-    {
-      blend: true,
-      blendFunc: [gl.ONE, gl.ONE_MINUS_SRC_ALPHA],
-      // framebuffer: screenFbo,
-      viewport: [0, 0, width, height]
-    },
-    () => {
-      // eslint-disable-next-line camelcase
-      this.model.setBindings({
-        'imageTexture': this.deckFbo.colorAttachments[0],
-        // 'imageTexture': this.imageTexture,
-        // 'imageTexture': this.imageTexture2,
+        'deckglTexture': this.deckFbo.colorAttachments[0],
       });
       this.model.draw(textureToScreenPass);
     },
