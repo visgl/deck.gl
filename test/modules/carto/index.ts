eee10e5908e2016ad0b7d5cdf1ce4f7c2a95f304
Don McCurdy <donmccurdy@cartodb.com>
2024-03-12 16:10:57 -0400

    chore(carto): Add test coverage for scaleIdentity(), parseMap() (#8629)

--- test/modules/carto/index.ts
+++ test/modules/carto/index.ts
@@ -1,7 +1,7 @@
 import './api/carto-api-error.spec';
+import './api/fetch-map.spec';
 import './api/layer-map.spec';
-// import './api/parseMap.spec';
-// import './config.spec';
+import './api/parse-map.spec';
 import './utils.spec';
 import './layers/carto-vector-tile.spec';
 import './layers/h3-tile-layer.spec';
