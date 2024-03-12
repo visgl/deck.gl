3ecb3efd3fc406554b5ab5373c5e98930b5e87a0
Don McCurdy <donmccurdy@cartodb.com>
2024-03-11 20:15:28 -0400

    Lint fix

--- test/modules/carto/api/fetch-map.spec.ts
+++ test/modules/carto/api/fetch-map.spec.ts
@@ -1,7 +1,7 @@
-import { fetchMap } from '@deck.gl/carto';
+import {fetchMap} from '@deck.gl/carto';
 import test from 'tape-catch';
 
 test('fetchMap#export', async t => {
-    t.ok(fetchMap, 'fetchMap exists');
-    t.end();
+  t.ok(fetchMap, 'fetchMap exists');
+  t.end();
 });
