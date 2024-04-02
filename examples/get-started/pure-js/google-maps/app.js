f6c0bd9f1679425002539cf24de1aa26b7d0586d
Xiaoji Chen <cxiaoji@gmail.com>
2024-04-02 09:01:31 -0700

    Update google-maps example code

--- examples/get-started/pure-js/google-maps/app.js
+++ examples/get-started/pure-js/google-maps/app.js
@@ -1,7 +1,7 @@
 /* global document, google */
 import {GoogleMapsOverlay as DeckOverlay} from '@deck.gl/google-maps';
 import {GeoJsonLayer, ArcLayer} from '@deck.gl/layers';
-import {Loader} from "@googlemaps/js-api-loader";
+import {Loader} from '@googlemaps/js-api-loader';
 
 // source: Natural Earth http://www.naturalearthdata.com/ via geojson.xyz
 const AIR_PORTS =
@@ -11,49 +11,45 @@ const AIR_PORTS =
 const GOOGLE_MAPS_API_KEY = process.env.GoogleMapsAPIKey; // eslint-disable-line
 const GOOGLE_MAP_ID = process.env.GoogleMapsMapId; // eslint-disable-line
 
-const loader = new Loader({
-  apiKey: GOOGLE_MAPS_API_KEY,
-  version: "quarterly"
+const loader = new Loader({apiKey: GOOGLE_MAPS_API_KEY});
+const googlemaps = await loader.importLibrary('maps');
+
+const map = new googlemaps.Map(document.getElementById('map'), {
+  center: {lat: 51.47, lng: 0.45},
+  zoom: 5,
+  mapId: GOOGLE_MAP_ID
 });
 
-loader.importLibrary('maps').then((googlemaps) => {
-  const map = new googlemaps.Map(document.getElementById('map'), {
-    center: {lat: 51.47, lng: 0.45},
-    zoom: 5,
-    mapId: GOOGLE_MAP_ID
-  });
-
-  const overlay = new DeckOverlay({
-    layers: [
-      new GeoJsonLayer({
-        id: 'airports',
-        data: AIR_PORTS,
-        // Styles
-        filled: true,
-        pointRadiusMinPixels: 2,
-        pointRadiusScale: 2000,
-        getPointRadius: f => 11 - f.properties.scalerank,
-        getFillColor: [200, 0, 80, 180],
-        // Interactive props
-        pickable: true,
-        autoHighlight: true,
-        onClick: info =>
-          // eslint-disable-next-line
-          info.object && alert(`${info.object.properties.name} (${info.object.properties.abbrev})`)
-      }),
-      new ArcLayer({
-        id: 'arcs',
-        data: AIR_PORTS,
-        dataTransform: d => d.features.filter(f => f.properties.scalerank < 4),
-        // Styles
-        getSourcePosition: f => [-0.4531566, 51.4709959], // London
-        getTargetPosition: f => f.geometry.coordinates,
-        getSourceColor: [0, 128, 200],
-        getTargetColor: [200, 0, 80],
-        getWidth: 1
-      })
-    ]
-  });
-
-  overlay.setMap(map);
+const overlay = new DeckOverlay({
+  layers: [
+    new GeoJsonLayer({
+      id: 'airports',
+      data: AIR_PORTS,
+      // Styles
+      filled: true,
+      pointRadiusMinPixels: 2,
+      pointRadiusScale: 2000,
+      getPointRadius: f => 11 - f.properties.scalerank,
+      getFillColor: [200, 0, 80, 180],
+      // Interactive props
+      pickable: true,
+      autoHighlight: true,
+      onClick: info =>
+        // eslint-disable-next-line
+        info.object && alert(`${info.object.properties.name} (${info.object.properties.abbrev})`)
+    }),
+    new ArcLayer({
+      id: 'arcs',
+      data: AIR_PORTS,
+      dataTransform: d => d.features.filter(f => f.properties.scalerank < 4),
+      // Styles
+      getSourcePosition: f => [-0.4531566, 51.4709959], // London
+      getTargetPosition: f => f.geometry.coordinates,
+      getSourceColor: [0, 128, 200],
+      getTargetColor: [200, 0, 80],
+      getWidth: 1
+    })
+  ]
 });
+
+overlay.setMap(map);
