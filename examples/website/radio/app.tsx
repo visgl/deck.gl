cdfb21ee21f002e976f841a2e943d74ef3226d7d
Xiaoji Chen <cxiaoji@gmail.com>
2024-04-04 16:33:15 -0700

    minimap

--- examples/website/radio/app.tsx
+++ examples/website/radio/app.tsx
@@ -6,14 +6,12 @@ import {MapView, WebMercatorViewport, FlyToInterpolator} from '@deck.gl/core';
 import {ScatterplotLayer, PathLayer} from '@deck.gl/layers';
 import {MVTLayer, H3HexagonLayer} from '@deck.gl/geo-layers';
 import DeckGL from '@deck.gl/react';
-
-import {load} from '@loaders.gl/core';
 import {CSVLoader} from '@loaders.gl/csv';
-
 import {scaleSqrt, scaleLinear} from 'd3-scale';
-import {GeoJsonLayer} from '@deck.gl/layers';
-
 import SearchBar from './search-bar';
+
+import type {Color, DeckProps, MapViewState, PickingInfo} from '@deck.gl/core';
+import type {Feature, Geometry} from 'geojson';
 
 // Data source
 const DATA_URL_BASE = 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/radio';
@@ -33,7 +31,7 @@ const minimapView = new MapView({
   clear: true
 });
 
-const minimapBackgroundStyle = {
+const minimapBackgroundStyle: React.CSSProperties = {
   position: 'absolute',
   zIndex: -1,
   width: '100%',
@@ -42,7 +40,7 @@ const minimapBackgroundStyle = {
   boxShadow: '0 0 8px 2px rgba(0,0,0,0.15)'
 };
 
-const INITIAL_VIEW_STATE = {
+const INITIAL_VIEW_STATE: {main: MapViewState; minimap: MapViewState} = {
   main: {
     longitude: -100,
     latitude: 40,
@@ -56,38 +54,61 @@ const INITIAL_VIEW_STATE = {
   }
 };
 
-const coverageColorScale = scaleSqrt()
+const coverageColorScale = scaleSqrt<Color>()
   .domain([1, 10, 100])
   .range([
     [237, 248, 177],
     [127, 205, 187],
     [44, 127, 184]
   ]);
-const fmColorScale = scaleLinear()
+const fmColorScale = scaleLinear<Color>()
   .domain([87, 108])
   .range([
     [0, 60, 255],
     [0, 255, 40]
   ]);
-const amColorScale = scaleLinear()
+const amColorScale = scaleLinear<Color>()
   .domain([530, 1700])
   .range([
     [200, 0, 0],
     [255, 240, 0]
   ]);
 
-function layerFilter({layer, viewport}) {
+export type Station = {
+  callSign: string;
+  frequency: number;
+  type: 'AM' | 'FM';
+  city: string;
+  state: string;
+  name: string;
+  longitude: number;
+  latitude: number;
+};
+
+type ServiceContour = {
+  callSign: string;
+};
+
+const layerFilter: DeckProps["layerFilter"] = ({layer, viewport}) => {
   const shouldDrawInMinimap =
     layer.id.startsWith('coverage') || layer.id.startsWith('viewport-bounds');
   if (viewport.id === 'minimap') return shouldDrawInMinimap;
   return !shouldDrawInMinimap;
-}
+};
 
-function getTooltipText(stationMap, {object}) {
+function getTooltipText(stationMap: {[callSign: string]: Station}, object: Feature<Geometry, ServiceContour> | Station) {
   if (!object) {
     return null;
   }
-  const {callSign} = object.properties || object;
+  let callSign: string;
+  if ('properties' in object) {
+    callSign = object.properties.callSign;
+  } else {
+    callSign = object.callSign;
+  }
+  if (!(callSign in stationMap)) {
+    return null;
+  }
   const {name, frequency, type, city, state} = stationMap[callSign];
   return `\
     ${callSign}
@@ -98,25 +119,27 @@ function getTooltipText(stationMap, {object}) {
 
 /* eslint-disable react/no-deprecated */
 export default function App({
-  data,
+  data = DATA_URL.STATIONS,
   mapStyle = 'https://basemaps.cartocdn.com/gl/positron-gl-style/style.json',
   showMinimap = true
+}: {
+  data?: string | Station[];
+  mapStyle?: string;
+  showMinimap?: boolean;
 }) {
+  const [stations, setStations] = useState<Station[]>([]);
   const [viewState, setViewState] = useState(INITIAL_VIEW_STATE);
   const [highlight, setHighlight] = useState(null);
 
-  const stationMap = useMemo(() => {
-    if (data) {
-      const result = {};
-      for (const station of data) {
-        result[station.callSign] = station;
-      }
-      return result;
+  const stationMap: {[callSign: string]: Station} = useMemo(() => {
+    const result = {};
+    for (const station of stations) {
+      result[station.callSign] = station;
     }
-    return null;
-  }, [data]);
+    return result;
+  }, [stations]);
 
-  const onViewStateChange = useCallback(({viewState: newViewState}) => {
+  const onViewStateChange: DeckProps<MapView[]>["onViewStateChange"] = useCallback(({viewState: newViewState}) => {
     setViewState(() => ({
       main: newViewState,
       minimap: {
@@ -127,7 +150,7 @@ export default function App({
     }));
   }, []);
 
-  const onSelectStation = useCallback(station => {
+  const onSelectStation = useCallback((station: Station) => {
     if (station) {
       setViewState(currentViewState => ({
         minimap: currentViewState.minimap,
@@ -157,81 +180,80 @@ export default function App({
     return [[topLeft, topRight, bottomRight, bottomLeft, topLeft]];
   }, [viewState]);
 
-  const getTooltip = useCallback(info => getTooltipText(stationMap, info), [stationMap]);
+  const getTooltip = useCallback(({object}: PickingInfo) => getTooltipText(stationMap, object), [stationMap]);
 
-  const layers = data
-    ? [
-        new MVTLayer({
-          id: 'service-contours',
-          data: DATA_URL.CONTOURS,
-          maxZoom: 8,
-          onTileError: () => {},
-          pickable: true,
-          autoHighlight: true,
-          highlightColor: [255, 200, 0, 100],
-          uniqueIdProperty: 'callSign',
-          highlightedFeatureId: highlight,
-          opacity: 0.2,
+  const layers = [
+    new MVTLayer<ServiceContour>({
+      id: 'service-contours',
+      data: DATA_URL.CONTOURS,
+      maxZoom: 8,
+      onTileError: () => {},
+      pickable: true,
+      autoHighlight: true,
+      highlightColor: [255, 200, 0, 100],
+      uniqueIdProperty: 'callSign',
+      highlightedFeatureId: highlight,
+      opacity: 0.2,
+      lineWidthMinPixels: 2,
+      getLineColor: f => {
+        const {type, frequency} = stationMap[f.properties.callSign];
+        return type === 'AM' ? amColorScale(frequency) : fmColorScale(frequency);
+      },
+      getFillColor: [255, 255, 255, 0]
+    }),
 
-          renderSubLayers: props => {
-            return new GeoJsonLayer(props, {
-              lineWidthMinPixels: 2,
-              getLineColor: f => {
-                const {type, frequency} = stationMap[f.properties.callSign];
-                return type === 'AM' ? amColorScale(frequency) : fmColorScale(frequency);
-              },
-              getFillColor: [255, 255, 255, 0]
-            });
-          }
-        }),
+    new ScatterplotLayer<Station>({
+      id: 'stations',
+      data,
+      onDataLoad: (arr) => setStations(arr as Station[]),
+      getPosition: d => [d.longitude, d.latitude],
+      getFillColor: [40, 40, 40, 128],
+      getRadius: 20,
+      radiusMinPixels: 2,
 
-        new ScatterplotLayer({
-          id: 'stations',
-          data,
-          getPosition: d => [d.longitude, d.latitude],
-          getColor: [40, 40, 40, 128],
-          getRadius: 20,
-          radiusMinPixels: 2
-        }),
+      loaders: [CSVLoader],
+      loadOptions: {
+        csv: {delimitersToGuess: '\t', skipEmptyLines: true}
+      }
+    }),
 
-        new H3HexagonLayer({
-          id: 'coverage',
-          data: DATA_URL.COVERAGE,
-          getHexagon: d => d.hex,
-          stroked: false,
-          extruded: false,
-          getFillColor: d => coverageColorScale(d.count),
+    new H3HexagonLayer<{hex: string; count: number;}>({
+      id: 'coverage',
+      data: DATA_URL.COVERAGE,
+      getHexagon: d => d.hex,
+      stroked: false,
+      extruded: false,
+      getFillColor: d => coverageColorScale(d.count),
 
-          loaders: [CSVLoader]
-        }),
+      loaders: [CSVLoader]
+    }),
 
-        new PathLayer({
-          id: 'viewport-bounds',
-          data: viewportBounds,
-          getPath: d => d,
-          getColor: [255, 0, 0],
-          getWidth: 2,
-          widthUnits: 'pixels'
-        })
-      ]
-    : [];
+    new PathLayer({
+      id: 'viewport-bounds',
+      data: viewportBounds,
+      getPath: d => d,
+      getColor: [255, 0, 0],
+      getWidth: 2,
+      widthUnits: 'pixels'
+    })
+  ];
 
   return (
     <DeckGL
       layers={layers}
       views={showMinimap ? [mainView, minimapView] : mainView}
       viewState={viewState}
-      parameters={{depthTest: false}}
+      parameters={{depthCompare: 'always'}}
       onViewStateChange={onViewStateChange}
       layerFilter={layerFilter}
       getTooltip={getTooltip}
     >
       <MapView id="main">
-        <Map reuseMaps mapStyle={mapStyle} />
-        <SearchBar data={data} onChange={onSelectStation} />
+        <Map reuseMaps mapStyle={mapStyle} key="map" />
+        <SearchBar data={stations} onChange={onSelectStation} />
       </MapView>
       {showMinimap && (
-        <MapView id="minimap">
+        <MapView id="minimap" >
           <div style={minimapBackgroundStyle} />
         </MapView>
       )}
@@ -239,11 +261,7 @@ export default function App({
   );
 }
 
-export function renderToDOM(container) {
+export async function renderToDOM(container: HTMLDivElement) {
   const root = createRoot(container);
   root.render(<App />);
-
-  load(DATA_URL.STATIONS, CSVLoader, {csv: {delimiter: '\t', skipEmptyLines: true}}).then(data => {
-    root.render(<App data={data.data} />);
-  });
 }
