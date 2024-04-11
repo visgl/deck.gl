59bc3cca80d6be3771806ea924e09022baa98965
padawannn <jaragon@cartodb.com>
2024-04-11 13:57:07 +0200

    linter

--- modules/carto/src/api/common.ts
+++ modules/carto/src/api/common.ts
@@ -1,4 +1,4 @@
-import { VERSION } from "@deck.gl/core";
+import {VERSION} from '@deck.gl/core';
 
 export const DEFAULT_API_BASE_URL = 'https://gcp-us-east1.api.carto.com';
 export const DEFAULT_CLIENT = 'deck-gl-carto';
