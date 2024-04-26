c3cd79a06c8ba04a25d8ed8da4ad688e3da61d54
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2024-04-26 19:42:41 +0200

    feat(pydeck): Support Deck v9 in bindings (#8577)
    * feat(pydeck): Update to Deck v9
    * feat(pydeck-carto): Add new CARTO layers, sources, and auth flow (#8791, #8806)
    
    ---------
    
    Co-authored-by: Don McCurdy <donmccurdy@cartodb.com>

--- bindings/pydeck-carto/examples/scripts/carto_layer_geo_query.py
+++ bindings/pydeck-carto/examples/scripts/carto_layer_geo_query.py
@@ -7,17 +7,22 @@ Render cloud data from a query.
 import pydeck as pdk
 import pydeck_carto as pdkc
 from carto_auth import CartoAuth
+from os.path import join, dirname
 
 carto_auth = CartoAuth.from_oauth()
 
-pdkc.register_carto_layer()
+pdkc.register_layers()
+
+data = pdkc.sources.vector_query_source(
+    access_token=carto_auth.get_access_token(),
+    api_base_url=carto_auth.get_api_base_url(),
+    connection_name="carto_dw",
+    sql_query="SELECT geom, name FROM carto-demo-data.demo_tables.world_airports",
+)
 
 layer = pdk.Layer(
-    "CartoLayer",
-    data="SELECT geom, name FROM carto-demo-data.demo_tables.airports",
-    type_=pdkc.MapType.QUERY,
-    connection=pdkc.CartoConnection.CARTO_DW,
-    credentials=pdkc.get_layer_credentials(carto_auth),
+    "VectorTileLayer",
+    data=data,
     get_fill_color=[238, 77, 90],
     point_radius_min_pixels=2.5,
     pickable=True,
@@ -26,4 +31,4 @@ layer = pdk.Layer(
 view_state = pdk.ViewState(latitude=0, longitude=0, zoom=1)
 
 r = pdk.Deck(layer, map_style=pdk.map_styles.ROAD, initial_view_state=view_state)
-r.to_html("carto_layer_geo_query.html", open_browser=True)
+r.to_html(join(dirname(__file__), "carto_layer_geo_query.html"))
--- bindings/pydeck-carto/examples/scripts/carto_styles_color_continuous.py
+++ bindings/pydeck-carto/examples/scripts/carto_styles_color_continuous.py
@@ -7,17 +7,23 @@ Render cloud data with color continuous style.
 import pydeck as pdk
 import pydeck_carto as pdkc
 from carto_auth import CartoAuth
+from os.path import join, dirname
 
 carto_auth = CartoAuth.from_oauth()
 
-pdkc.register_carto_layer()
+pdkc.register_layers()
+
+data = pdkc.sources.vector_query_source(
+    access_token=carto_auth.get_access_token(),
+    api_base_url=carto_auth.get_api_base_url(),
+    connection_name="carto_dw",
+    sql_query="SELECT geom, value FROM cartobq.public_account.temps",
+)
+
 
 layer = pdk.Layer(
-    "CartoLayer",
-    data="SELECT geom, value FROM cartobq.public_account.temps",
-    type_=pdkc.MapType.QUERY,
-    connection=pdkc.CartoConnection.CARTO_DW,
-    credentials=pdkc.get_layer_credentials(carto_auth),
+    "VectorTileLayer",
+    data=data,
     get_fill_color=pdkc.styles.color_continuous(
         "value", [70, 75, 80, 85, 90, 95, 100], "Peach"
     ),
@@ -27,4 +33,4 @@ layer = pdk.Layer(
 view_state = pdk.ViewState(latitude=34, longitude=-98, zoom=3)
 
 r = pdk.Deck(layer, map_style=pdk.map_styles.ROAD, initial_view_state=view_state)
-r.to_html("carto_styles_color_continuous.html", open_browser=True)
+r.to_html(join(dirname(__file__), "carto_styles_color_continuous.html"))
