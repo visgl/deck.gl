2d6dde3c8e78536673f15b24d5a39c243ab3867a
Jorge Tarrero <thedae@gmail.com>
2024-05-29 20:09:36 +0200

    feat(carto): Support filters parameter in rasterSource (#8928)
    ---------
    
    Co-authored-by: Don McCurdy <donmccurdy@cartodb.com>

--- modules/carto/src/sources/raster-source.ts
+++ modules/carto/src/sources/raster-source.ts
@@ -1,12 +1,17 @@
 import {baseSource} from './base-source';
-import type {SourceOptions, TilejsonResult, TilesetSourceOptions} from './types';
+import type {FilterOptions, SourceOptions, TilejsonResult, TilesetSourceOptions} from './types';
 
-export type RasterSourceOptions = SourceOptions & TilesetSourceOptions;
-type UrlParameters = {name: string};
+export type RasterSourceOptions = SourceOptions & TilesetSourceOptions & FilterOptions;
+type UrlParameters = {
+  name: string;
+  filters?: Record<string, unknown>;
+};
 
 export const rasterSource = async function (options: RasterSourceOptions): Promise<TilejsonResult> {
-  const {tableName} = options;
+  const {tableName, filters} = options;
   const urlParameters: UrlParameters = {name: tableName};
-
+  if (filters) {
+    urlParameters.filters = filters;
+  }
   return baseSource<UrlParameters>('raster', options, urlParameters) as Promise<TilejsonResult>;
 };
