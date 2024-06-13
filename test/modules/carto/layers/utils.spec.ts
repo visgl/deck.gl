395a6113f70516892b5f0f2ba97b192b7cab510c
Felix Palmer <felixpalmer@gmail.com>
2024-06-13 14:29:26 +0200

    mergeBoundaryData test

--- test/modules/carto/layers/utils.spec.ts
+++ test/modules/carto/layers/utils.spec.ts
@@ -1,5 +1,7 @@
 import test from 'tape-promise/tape';
-import {injectAccessToken} from '@deck.gl/carto/layers/utils';
+import {injectAccessToken, mergeBoundaryData} from '@deck.gl/carto/layers/utils';
+import {Tile as PropertiesTile} from '/carto/layers/schema/carto-properties-tile';
+import {Tile as VectorTile} from '@deck.gl/carto/layers/schema/carto-tile';
 
 test('injectAccessToken', async t => {
   let loadOptions = {} as any;
@@ -9,5 +11,40 @@ test('injectAccessToken', async t => {
   loadOptions = {fetch: {headers: {Custom: 123}}};
   injectAccessToken(loadOptions, 'ACCESS-XXX');
   t.deepEqual(loadOptions.fetch.headers, {Authorization: 'Bearer ACCESS-XXX', Custom: 123});
+  t.end();
+});
+
+test('mergeBoundaryData', async t => {
+  const geometry: VectorTile = {
+    points: {positions: {value: new Float32Array(), size: 2}} as any,
+    lines: {positions: {value: new Float32Array(), size: 2}} as any,
+    polygons: {
+      positions: {
+        value: new Float32Array([
+          100, 0, 110, 0, 110, 10, 100, 10, 100, 0, 108, 8, 108, 2, 102, 2, 102, 8, 108, 8
+        ]),
+        size: 2
+      },
+      polygonIndices: {value: new Uint32Array([0, 10]), size: 1},
+      primitivePolygonIndices: {value: new Uint32Array([0, 5, 10]), size: 1},
+      properties: [{custom_id: 'a'}, {custom_id: 'b'}],
+      numericProps: {},
+      featureIds: {value: new Uint32Array([0, 1]), size: 1},
+      globalFeatureIds: {value: new Uint32Array([31, 32]), size: 1},
+      fields: [],
+      triangles: {value: new Uint32Array([0, 1]), size: 1}
+    }
+  };
+
+  const properties: PropertiesTile = {
+    properties: [
+      {custom_id: 'b', name: 'B'},
+      {custom_id: 'a', name: 'A'}
+    ],
+    numericProps: {}
+  };
+
+  const merged = mergeBoundaryData(geometry, properties, 'custom_id');
+  t.deepEqual(merged.polygons.properties, [{name: 'A'}, {name: 'B'}]);
   t.end();
 });
