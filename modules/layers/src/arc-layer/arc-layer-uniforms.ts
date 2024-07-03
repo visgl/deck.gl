ae10c477c1c878a847c2cf141758f55ba0b8e3de
Felix Palmer <felixpalmer@gmail.com>
2024-07-03 10:31:07 +0200

    Create arc-layer-uniforms

---
+++ modules/layers/src/arc-layer/arc-layer-uniforms.ts
@@ -0,0 +1,39 @@
+import {ShaderModule} from '@luma.gl/shadertools';
+import {UniformTypes} from '@deck.gl/core';
+
+const uniformBlock = `\
+uniform arcUniforms {
+  bool greatCircle;
+  bool useShortestPath;
+  float numSegments;
+  float widthScale;
+  float widthMinPixels;
+  float widthMaxPixels;
+  highp int widthUnits;
+} arc;
+`;
+
+export type ArcProps = {
+  greatCircle?: boolean;
+  useShortestPath?: boolean;
+  numSegments?: number;
+  widthScale?: number;
+  widthMinPixels?: number;
+  widthMaxPixels?: number;
+  widthUnits?: number;
+};
+
+export const arcUniforms = {
+  name: 'arc',
+  vs: uniformBlock,
+  fs: uniformBlock,
+  uniformTypes: {
+    greatCircle: 'f32',
+    useShortestPath: 'f32',
+    numSegments: 'f32',
+    widthScale: 'f32',
+    widthMinPixels: 'f32',
+    widthMaxPixels: 'f32',
+    widthUnits: 'i32'
+  } as const satisfies UniformTypes<ArcProps>
+} as const satisfies ShaderModule<ArcProps>;
