3b0840b48376d1288a38ad06dd07aa5a2751afd4
felixpalmer <felixpalmer@gmail.com>
2024-07-04 16:40:17 +0200

    Object.assign
    Co-authored-by: Don McCurdy <donmccurdy@cartodb.com>

--- modules/test-utils/src/utils/layer.ts
+++ modules/test-utils/src/utils/layer.ts
@@ -5,10 +5,10 @@ import {Layer} from '@deck.gl/core';
  * Extract uniform values set for a Layer in the underlying UniformBlock store
  */
 export function getLayerUniforms(layer: Layer): Record<string, UniformValue> {
-  let uniforms = {};
+  const uniforms = {};
   const uniformBlocks = layer.getModels()[0]._uniformStore.uniformBlocks.values();
   for (const block of uniformBlocks) {
-    uniforms = {...uniforms, ...block.uniforms};
+    Object.assign(uniforms, block.uniforms);
   }
 
   return uniforms;
