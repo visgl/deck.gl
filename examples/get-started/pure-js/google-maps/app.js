771678d246917eb4c3c2bb071bac4eb7fdd23a04
Felix Palmer <felixpalmer@gmail.com>
2024-08-01 13:02:06 +0200

    Sync view state

--- examples/get-started/pure-js/google-maps/app.js
+++ examples/get-started/pure-js/google-maps/app.js
@@ -1,28 +1,34 @@
 /* global document */
 import {GeoJsonLayer, ArcLayer} from '@deck.gl/layers';
-import {Deck} from '@deck.gl/core';
+import {Deck, WebMercatorViewport} from '@deck.gl/core';
 import {fetchMap} from '@deck.gl/carto';
 
 const cartoMapId = 'ff6ac53f-741a-49fb-b615-d040bc5a96b8';
 
-// source: Natural Earth http://www.naturalearthdata.com/ via geojson.xyz
-const AIR_PORTS =
-  'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_10m_airports.geojson';
-
+let map;
 function initMapKit() {
-  const map = new mapkit.Map('map', {center: new mapkit.Coordinate(37.334883, -122.008977)});
-  console.log('load');
+  map = new mapkit.Map('map', {center: new mapkit.Coordinate(37.334883, -122.008977)});
 }
 
 setTimeout(initMapKit, 1000);
 
 fetchMap({cartoMapId}).then(({initialViewState, mapStyle, layers}) => {
+  initialViewState.maxPitch = 0; // lock tilt
   const deck = new Deck({canvas: 'deck-canvas', controller: true, initialViewState, layers});
 
-  // deck.setProps({
-  //   onViewStateChange: ({viewState}) => {
-  //     const {longitude, latitude, ...rest} = viewState;
-  //     map.jumpTo({center: [longitude, latitude], ...rest});
-  //   }
-  // });
+  deck.setProps({
+    onViewStateChange: ({viewState}) => {
+      if (!map) return;
+      const {longitude, latitude, bearing, ...rest} = viewState;
+      const viewport = new WebMercatorViewport({...viewState, bearing: 0});
+      const bounds = viewport.getBounds();
+
+      const center = new mapkit.Coordinate(latitude, longitude);
+      const span = new mapkit.CoordinateSpan(bounds[3] - bounds[1], bounds[2] - bounds[0]);
+      const region = new mapkit.CoordinateRegion(center, span);
+
+      map.setRegionAnimated(region, false);
+      map.setRotationAnimated(-bearing, false);
+    }
+  });
 });
