2aa06c1160cef855f4718bbf341b94f2008058bc
Felix Palmer <felixpalmer@gmail.com>
2024-08-01 12:10:55 +0200

    Display basic map

--- examples/get-started/pure-js/google-maps/app.js
+++ examples/get-started/pure-js/google-maps/app.js
@@ -1,56 +1,13 @@
 /* global document */
-import {GoogleMapsOverlay as DeckOverlay} from '@deck.gl/google-maps';
 import {GeoJsonLayer, ArcLayer} from '@deck.gl/layers';
-import {Loader} from '@googlemaps/js-api-loader';
 
 // source: Natural Earth http://www.naturalearthdata.com/ via geojson.xyz
 const AIR_PORTS =
   'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_10m_airports.geojson';
 
-// Set your Google Maps API key here or via environment variable
-const GOOGLE_MAPS_API_KEY = process.env.GoogleMapsAPIKey; // eslint-disable-line
-const GOOGLE_MAP_ID = process.env.GoogleMapsMapId; // eslint-disable-line
+function initMapKit() {
+  const map = new mapkit.Map('map', {center: new mapkit.Coordinate(37.334883, -122.008977)});
+  console.log('load');
+}
 
-const loader = new Loader({apiKey: GOOGLE_MAPS_API_KEY});
-
-loader.importLibrary('maps').then(googlemaps => {
-  const map = new googlemaps.Map(document.getElementById('map'), {
-    center: {lat: 51.47, lng: 0.45},
-    zoom: 5,
-    mapId: GOOGLE_MAP_ID
-  });
-
-  const overlay = new DeckOverlay({
-    layers: [
-      new GeoJsonLayer({
-        id: 'airports',
-        data: AIR_PORTS,
-        // Styles
-        filled: true,
-        pointRadiusMinPixels: 2,
-        pointRadiusScale: 2000,
-        getPointRadius: f => 11 - f.properties.scalerank,
-        getFillColor: [200, 0, 80, 180],
-        // Interactive props
-        pickable: true,
-        autoHighlight: true,
-        onClick: info =>
-          // eslint-disable-next-line
-          info.object && alert(`${info.object.properties.name} (${info.object.properties.abbrev})`)
-      }),
-      new ArcLayer({
-        id: 'arcs',
-        data: AIR_PORTS,
-        dataTransform: d => d.features.filter(f => f.properties.scalerank < 4),
-        // Styles
-        getSourcePosition: f => [-0.4531566, 51.4709959], // London
-        getTargetPosition: f => f.geometry.coordinates,
-        getSourceColor: [0, 128, 200],
-        getTargetColor: [200, 0, 80],
-        getWidth: 1
-      })
-    ]
-  });
-
-  overlay.setMap(map);
-});
+setTimeout(initMapKit, 1000);
