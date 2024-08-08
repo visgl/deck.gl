5a90f5ff1a6cebb483ba3cec70e63cb9010885d8
felixpalmer <felixpalmer@gmail.com>
2024-08-08 17:25:57 +0200

    CARTO: Move cluster stats into data.points.attributes (#9081)

--- modules/carto/src/layers/cluster-tile-layer.ts
+++ modules/carto/src/layers/cluster-tile-layer.ts
@@ -146,10 +146,9 @@ class ClusterGeoJsonLayer<
 
     if (needsUpdate) {
       const stats = computeAggregationStats(data, properties);
-      for (const d of data) {
-        d.stats = stats;
-      }
-      this.setState({data: clustersToBinary(data)});
+      const binaryData = clustersToBinary(data);
+      binaryData.points.attributes = {stats};
+      this.setState({data: binaryData});
     }
 
     const props = {
