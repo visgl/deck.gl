ab1d0334103107794309e8c94e4a20e39749aaf1
Don McCurdy <donmccurdy@cartodb.com>
2024-09-20 11:23:06 -0400

    feat(carto): Allow configuring max URL length for GET requests (#9159)

--- modules/carto/src/api/common.ts
+++ modules/carto/src/api/common.ts
@@ -1,4 +1,6 @@
 export const DEFAULT_API_BASE_URL = 'https://gcp-us-east1.api.carto.com';
 export const DEFAULT_CLIENT = 'deck-gl-carto';
 export const V3_MINOR_VERSION = '3.4';
-export const MAX_GET_LENGTH = 8192;
+
+// Fastly default limit is 8192; leave some padding.
+export const DEFAULT_MAX_LENGTH_URL = 7000;
