aa96a5770182cca5ceaa8ceb6b83ffa84ceb79db
Ib Green <ib@foursquare.com>
2024-09-26 08:54:39 -0400

    binary-search

--- test/modules/index.ts
+++ test/modules/index.ts
@@ -23,17 +23,17 @@ import './imports-spec';
 import './core';
 
 // Layers and extensions
-import './layers';
-import './mesh-layers';
-import './geo-layers';
-import './aggregation-layers';
-import './extensions';
+// import './layers';
+// import './mesh-layers';
+// import './geo-layers';
+// import './aggregation-layers';
+// import './extensions';
 
-// Integration
-import './carto';
-import './mapbox';
-import './google-maps';
-import './json';
-import './jupyter-widget';
-import './react';
-import './main/bundle';
+// // Integration
+// import './carto';
+// import './mapbox';
+// import './google-maps';
+// import './json';
+// import './jupyter-widget';
+// import './react';
+// import './main/bundle';
