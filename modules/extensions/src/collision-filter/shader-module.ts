0432b7ae953dcda06e70c4c48efc18162be146ec
Felix Palmer <felixpalmer@gmail.com>
2024-09-30 10:12:37 +0200

    Fix collision vertex discard

--- modules/extensions/src/collision-filter/shader-module.ts
+++ modules/extensions/src/collision-filter/shader-module.ts
@@ -72,7 +72,7 @@ const inject = {
     collision_fade = collision_isVisible(collision_texCoords, geometry.pickingColor / 255.0);
     if (collision_fade < 0.0001) {
       // Position outside clip space bounds to discard
-      // position = vec4(0.0, 0.0, 2.0, 1.0);
+      position = vec4(0.0, 0.0, 2.0, 1.0);
     }
   }
   `,
@@ -106,7 +106,7 @@ const getCollisionUniforms = (
   }
   const {enabled, collisionFBO, drawToCollisionMap, dummyCollisionMap} = opts;
   return {
-    enabled,
+    enabled: enabled && !Boolean(drawToCollisionMap),
     sort: Boolean(drawToCollisionMap),
     collision_texture:
       !drawToCollisionMap && collisionFBO ? collisionFBO.colorAttachments[0] : dummyCollisionMap
