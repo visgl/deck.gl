6832a4bc1f4bc74b923c67234e558cd69ea47116
marsy <98871822+marsy-invests@users.noreply.github.com>
2024-10-11 18:47:50 +1100

    Revised props caching fix due to failing tests.

--- modules/core/src/lifecycle/create-props.ts
+++ modules/core/src/lifecycle/create-props.ts
@@ -14,7 +14,7 @@ import {
   ASYNC_DEFAULTS_SYMBOL
 } from './constants';
 import {StatefulComponentProps} from './component';
-import type Component from './component';
+import Component from './component';
 
 // Create a property object
 export function createProps<PropsT extends {}>(
@@ -66,6 +66,13 @@ const MergedDefaultPropsCacheKey = '_mergedDefaultProps';
 // Return precalculated defaultProps and propType objects if available
 // build them if needed
 function getPropsPrototype(componentClass, extensions?: any[]) {
+  // Bail out if we're not looking at a component - for two reasons:
+  // 1. There's no reason for an ancestor of component to have props
+  // 2. If we don't bail out, we'll follow the prototype chain all the way back to the global
+  // function prototype and add _mergedDefaultProps to it, which may break other frameworks 
+  // (e.g. the react-three-fiber reconciler)
+  if (!(componentClass instanceof Component.constructor)) return {}
+
   // A string that uniquely identifies the extensions involved
   let cacheKey = MergedDefaultPropsCacheKey;
   if (extensions) {
@@ -98,10 +105,6 @@ function createPropsPrototypeAndTypes(
   }
 
   const parentClass = Object.getPrototypeOf(componentClass);
-  if (parentClass === Function.prototype) {
-    // don't add props to the global function prototype
-    return null;
-  }
   const parentDefaultProps = getPropsPrototype(parentClass);
 
   // Parse propTypes from Component.defaultProps
