5549372bbe991dfe251de429eb6e6dccd19b1ded
Chris Gervang <chris@gervang.com>
2024-10-25 11:26:12 -0700

    Fix bootstrap
    Signed-off-by: Chris Gervang <chris@gervang.com>

--- modules/react/src/widgets/fullscreen-widget.tsx
+++ modules/react/src/widgets/fullscreen-widget.tsx
@@ -1,6 +1,6 @@
 import {FullscreenWidget as VanillaFullscreenWidget} from '@deck.gl/widgets';
 import type {FullscreenWidgetProps} from '@deck.gl/widgets';
-import useWidget from './utils/use-widget';
+import useWidget from '../utils/use-widget';
 
 export const FullscreenWidget = (props: FullscreenWidgetProps = {}) => {
   const widget = useWidget(VanillaFullscreenWidget, props);
--- modules/react/src/widgets/zoom-widget.tsx
+++ modules/react/src/widgets/zoom-widget.tsx
@@ -1,6 +1,6 @@
 import {ZoomWidget as VanillaZoomWidget} from '@deck.gl/widgets';
 import type {ZoomWidgetProps} from '@deck.gl/widgets';
-import useWidget from './utils/use-widget';
+import useWidget from '../utils/use-widget';
 
 export const ZoomWidget = (props: ZoomWidgetProps = {}) => {
   const widget = useWidget(VanillaZoomWidget, props);
