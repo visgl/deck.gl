01be3e387651d80ce0be402bfc7f44ec450afd0a
Chris Gervang <chris@gervang.com>
2024-12-12 14:14:36 -0800

    removeStyles test

--- test/modules/core/utils/apply-styles.spec.ts
+++ test/modules/core/utils/apply-styles.spec.ts
@@ -1,8 +1,8 @@
 /* global document */
 import test from 'tape-promise/tape';
-import {applyStyles} from '@deck.gl/core/utils/apply-styles';
+import {applyStyles, removeStyles} from '@deck.gl/core/utils/apply-styles';
 
-const TEST_CASES = [
+const APPLY_TEST_CASES = [
   {
     title: 'CSS variable',
     argument: {property: '--my-var', value: 'red'},
@@ -17,13 +17,42 @@ const TEST_CASES = [
 
 test('applyStyles', t => {
   const container = document.body.appendChild(document.createElement('div'));
-  for (const tc of TEST_CASES) {
+  for (const tc of APPLY_TEST_CASES) {
     const {argument, result} = tc;
     applyStyles(container, {[argument.property]: argument.value});
     t.deepEqual(
       container.style.getPropertyValue(result.property),
       result.value,
       `applyStyles ${tc.title} returned expected result`
+    );
+  }
+  t.end();
+});
+
+const REMOVE_TEST_CASES = [
+  {
+    title: 'CSS variable',
+    argument: {property: '--my-var', value: 'red'},
+    result: {property: '--my-var', value: ''}
+  },
+  {
+    title: 'camelCase property',
+    argument: {property: 'backgroundColor', value: 'red'},
+    result: {property: 'background-color', value: ''}
+  }
+];
+
+test('removeStyles', t => {
+  const container = document.body.appendChild(document.createElement('div'));
+  for (const tc of REMOVE_TEST_CASES) {
+    const {argument, result} = tc;
+    // setProperty expects kabab-case
+    container.style.setProperty(result.property, argument.value);
+    removeStyles(container, {[argument.property]: argument.value});
+    t.deepEqual(
+      container.style.getPropertyValue(result.property),
+      result.value,
+      `removeStyles ${tc.title} returned expected result`
     );
   }
   t.end();
