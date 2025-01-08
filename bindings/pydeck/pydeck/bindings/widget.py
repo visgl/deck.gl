575fb79ce9f7022e44dd2b310eb3cf611479160a
Chris Gervang <chris@gervang.com>
2025-01-08 15:25:45 -0800

    docs

--- bindings/pydeck/pydeck/bindings/widget.py
+++ bindings/pydeck/pydeck/bindings/widget.py
@@ -5,19 +5,20 @@ TYPE_IDENTIFIER = "@@type"
 
 class Widget(JSONMixin):
     """
-    Represents a widget
+    Represents a deck.gl widget, which are UI components around the WebGL2/WebGPU canvas 
+    to offer controls and information for a better user experience.
 
     Parameters
     ---------
     type : str, default None
-        deck.gl widget to display, e.g., CompassWidget
+        deck.gl widget to display, e.g., 'CompassWidget'
     id : str, default None
-            Unique name for layer
+        Unique name for widget
     placement : string, default 'top-left'
         Placement of the widget on the map. Options are 'top-left', 'top-right', 'bottom-left', 'bottom-right', and 'fill'.
         Note that not all widgets support custom placement.
-    view_id : string, default null
-        ID of the view to which the widget should be added.
+    view_id : string, default None
+        ID of the view to which the widget should be added. The widget will be added to the default view if not specified.
         Note that not all widgets support custom view_id.
     **kwargs
         Any of the parameters passable to a deck.gl Widget
