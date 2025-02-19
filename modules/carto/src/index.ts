5cab353a51efec77780808f229e5d32f3c7bfc08
Felix Palmer <felixpalmer@gmail.com>
2025-02-19 15:08:03 +0100

    Remove export

--- modules/carto/src/index.ts
+++ modules/carto/src/index.ts
@@ -71,8 +71,6 @@ export type {
   GoogleBasemap as _GoogleBasemap
 } from './api/index';
 
-export {createPointsFromPolygons, createPointsFromLines} from './layers/label-utils';
-
 // TODO(v10): Consider removing re-exports from '@carto/api-client' below.
 
 import {
