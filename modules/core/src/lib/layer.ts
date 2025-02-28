5c26d330ff6cf1a3c85e88de061fc78303a35ecb
Ib Green <ib@unfolded.ai>
2025-02-28 10:54:01 -0500

    wip

--- modules/core/src/lib/layer.ts
+++ modules/core/src/lib/layer.ts
@@ -1068,12 +1068,7 @@ export default abstract class Layer<PropsT extends {} = {}> extends Component<
       }
 
       for (const model of this.getModels()) {
-        if (model.device.type === 'webgpu') {
-          // TODO(ibgreen): model.setParameters currently wipes parameters. Semantics TBD.
-          model.setParameters({...model.parameters, ...parameters});
-        } else {
-          model.setParameters(parameters);
-        }
+        model.setParameters(parameters);
       }
 
       // Call subclass lifecycle method
