3ba23a14a960b7871ca8f357ecf5117571ccfa71
Ib Green <ib@unfolded.ai>
2025-03-02 06:22:22 -0500

    feat(widgets): New LoadingWidget

--- modules/widgets/src/index.ts
+++ modules/widgets/src/index.ts
@@ -7,12 +7,14 @@ export {CompassWidget} from './compass-widget';
 export {ZoomWidget} from './zoom-widget';
 export {ScreenshotWidget} from './screenshot-widget';
 export {ResetViewWidget} from './reset-view-widget';
+export {LoadingWidget} from './loading-widget';
 
 export type {FullscreenWidgetProps} from './fullscreen-widget';
 export type {CompassWidgetProps} from './compass-widget';
 export type {ZoomWidgetProps} from './zoom-widget';
 export type {ScreenshotWidgetProps} from './screenshot-widget';
 export type {ResetViewWidgetProps} from './reset-view-widget';
+export type {LoadingWidgetProps} from './loading-widget';
 
 export type {WidgetImplProps} from './widget-impl';
 export {WidgetImpl as _WidgetImpl} from './widget-impl';
