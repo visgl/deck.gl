b96fa4ca7d588c3c90cc12adfc1aac78d7144819
Ib Green <ib@unfolded.ai>
2025-03-15 11:33:19 -0400

    chore: webgpu deck enabler

--- modules/core/src/lib/deck.ts
+++ modules/core/src/lib/deck.ts
@@ -15,7 +15,7 @@ import {deepEqual} from '../utils/deep-equal';
 import typedArrayManager from '../utils/typed-array-manager';
 import {VERSION} from './init';
 
-import {luma} from '@luma.gl/core';
+import {luma, Adapter} from '@luma.gl/core';
 import {webgl2Adapter} from '@luma.gl/webgl';
 import {Timeline} from '@luma.gl/engine';
 import {AnimationLoop} from '@luma.gl/engine';
@@ -116,6 +116,9 @@ export type DeckProps<ViewsT extends ViewOrViews = null> = {
 
   /** Use an existing luma.gl GPU device. @note If not supplied, a new device will be created using props.deviceProps */
   device?: Device | null;
+
+  /** Supply adapters to use when a new device is created */
+  adapters?: Adapter[];
 
   /** A new device will be created using these props, assuming that an existing device is not supplied using props.device) */
   deviceProps?: CreateDeviceProps;
@@ -373,16 +376,21 @@ export default class Deck<ViewsT extends ViewOrViews = null> {
 
     // Create a new device
     if (!deviceOrPromise) {
+      const canvasContextUserProps = this.props.deviceProps?.createCanvasContext;
+      const canvasContextProps =
+        typeof canvasContextUserProps === 'object' ? canvasContextUserProps : undefined;
       // Create the "best" device supported from the registered adapters
+      const adapters = Array.from(new Set([...(props.adapters || []), webgl2Adapter]));
       deviceOrPromise = luma.createDevice({
         type: 'best-available',
         // luma by default throws if a device is already attached
         // asynchronous device creation could happen after finalize() is called
         // TODO - createDevice should support AbortController?
         _reuseDevices: true,
-        adapters: [webgl2Adapter],
+        adapters,
         ...props.deviceProps,
         createCanvasContext: {
+          ...canvasContextProps,
           canvas: this._createCanvas(props),
           useDevicePixels: this.props.useDevicePixels,
           // TODO v9.2 - replace AnimationLoop's `autoResizeDrawingBuffer` with CanvasContext's `autoResize`
