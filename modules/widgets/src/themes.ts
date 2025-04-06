895995984216db6e684c2c4d7105efe7ab31b4c4
Chris Gervang <chris@gervang.com>
2025-04-06 15:55:11 -0700

    Define all theme properties as optional and fill in missing provided theme values

--- modules/widgets/src/themes.ts
+++ modules/widgets/src/themes.ts
@@ -9,21 +9,24 @@ export type DeckWidgetTheme = {
   // layout
   '--widget-margin'?: string;
   // button and button group
-  '--button-background': string;
-  '--button-stroke': string;
-  '--button-inner-stroke': string;
-  '--button-shadow': string;
-  '--button-backdrop-filter': string;
-  '--button-icon-idle': string;
-  '--button-icon-hover': string;
   '--button-size'?: string;
   '--button-corner-radius'?: string;
+  '--button-background'?: string;
+  '--button-stroke'?: string;
+  '--button-inner-stroke'?: string;
+  '--button-shadow'?: string;
+  '--button-backdrop-filter'?: string;
+  '--button-icon-idle'?: string;
+  '--button-icon-hover'?: string;
   // inter-icon color
-  '--icon-compass-north-color': string;
-  '--icon-compass-south-color': string;
+  '--icon-compass-north-color'?: string;
+  '--icon-compass-south-color'?: string;
 };
 
 export const LightTheme = {
+  '--widget-margin': '12px',
+  '--button-size': '28px',
+  '--button-corner-radius': '8px',
   '--button-background': '#fff',
   '--button-stroke': 'rgba(255, 255, 255, 0.3)',
   '--button-inner-stroke': 'unset',
@@ -33,9 +36,12 @@ export const LightTheme = {
   '--button-icon-hover': 'rgba(24, 24, 26, 1)',
   '--icon-compass-north-color': 'rgb(240, 92, 68)',
   '--icon-compass-south-color': 'rgb(204, 204, 204)'
-} as const satisfies DeckWidgetTheme;
+} as const satisfies Required<DeckWidgetTheme>;
 
 export const DarkTheme = {
+  '--widget-margin': '12px',
+  '--button-size': '28px',
+  '--button-corner-radius': '8px',
   '--button-background': 'rgba(18, 18, 20, 1)',
   '--button-stroke': 'rgba(18, 18, 20, 0.30)',
   '--button-inner-stroke': 'unset',
@@ -45,9 +51,12 @@ export const DarkTheme = {
   '--button-icon-hover': 'rgba(215, 214, 229, 1)',
   '--icon-compass-north-color': 'rgb(240, 92, 68)',
   '--icon-compass-south-color': 'rgb(200, 199, 209)'
-} as const satisfies DeckWidgetTheme;
+} as const satisfies Required<DeckWidgetTheme>;
 
 export const LightGlassTheme = {
+  '--widget-margin': '12px',
+  '--button-size': '28px',
+  '--button-corner-radius': '8px',
   '--button-background': 'rgba(255, 255, 255, 0.6)',
   '--button-stroke': 'rgba(255, 255, 255, 0.3)',
   '--button-inner-stroke': '1px solid rgba(255, 255, 255, 0.6)',
@@ -58,9 +67,12 @@ export const LightGlassTheme = {
   '--button-icon-hover': 'rgba(24, 24, 26, 1)',
   '--icon-compass-north-color': 'rgb(240, 92, 68)',
   '--icon-compass-south-color': 'rgb(204, 204, 204)'
-} as const satisfies DeckWidgetTheme;
+} as const satisfies Required<DeckWidgetTheme>;
 
 export const DarkGlassTheme = {
+  '--widget-margin': '12px',
+  '--button-size': '28px',
+  '--button-corner-radius': '8px',
   '--button-background': 'rgba(18, 18, 20, 0.75)',
   '--button-stroke': 'rgba(18, 18, 20, 0.30)',
   '--button-inner-stroke': '1px solid rgba(18, 18, 20, 0.75)',
@@ -71,4 +83,4 @@ export const DarkGlassTheme = {
   '--button-icon-hover': 'rgba(215, 214, 229, 1)',
   '--icon-compass-north-color': 'rgb(240, 92, 68)',
   '--icon-compass-south-color': 'rgb(200, 199, 209)'
-} as const satisfies DeckWidgetTheme;
+} as const satisfies Required<DeckWidgetTheme>;
