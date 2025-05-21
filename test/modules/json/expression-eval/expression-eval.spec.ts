8403417a6051f010a5bf11d8a6dca1208e3f7300
Ib Green <ib.green.home@gmail.com>
2025-05-21 07:44:42 -0400

    prettier

--- test/modules/json/expression-eval/expression-eval.spec.ts
+++ test/modules/json/expression-eval/expression-eval.spec.ts
@@ -3,7 +3,12 @@
 // Copyright (c) vis.gl contributors
 
 import test from 'tape-promise/tape';
-import {compile, compileAsync, addUnaryOp, addBinaryOp} from '@deck.gl/json/expression-eval/expression-eval';
+import {
+  compile,
+  compileAsync,
+  addUnaryOp,
+  addBinaryOp
+} from '@deck.gl/json/expression-eval/expression-eval';
 
 const fixtures = [
   // array expression
