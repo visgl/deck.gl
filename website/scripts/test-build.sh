32d94a01186f981795b5d44075b5a2185f7da5a2
Felix Palmer <felixpalmer@gmail.com>
2025-06-02 13:54:37 +0200

    Dummy env vars

--- website/scripts/test-build.sh
+++ website/scripts/test-build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+# Set dummy environment variables for build
+export GoogleMapsAPIKey="dummy-google-maps-api-key"
+export GoogleMapsMapId="dummy-google-maps-map-id"
+export MapboxAccessToken="dummy-mapbox-access-token"
+
 # rebuild modules from source
 (
   cd ..
