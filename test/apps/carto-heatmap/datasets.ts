ba59eee79f3a9c429edb79cf41580def65c942c3
Felix Palmer <felixpalmer@gmail.com>
2025-08-28 13:40:24 +0200

    remove datasetss

--- test/apps/carto-heatmap/datasets.ts
+++ test/apps/carto-heatmap/datasets.ts
@@ -3,9 +3,7 @@
 // Copyright (c) vis.gl contributors
 
 import {
-  h3TilesetSource,
   h3TableSource,
-  quadbinTilesetSource,
   quadbinTableSource
 } from '@carto/api-client';
 
@@ -13,9 +11,7 @@ export interface DatasetConfig {
   type: 'h3' | 'quadbin';
   source:
     | typeof h3TableSource
-    | typeof h3TilesetSource
-    | typeof quadbinTableSource
-    | typeof quadbinTilesetSource;
+    | typeof quadbinTableSource;
   tableName: string;
   aggregationExp?: string;
   getWeight: (d: any) => number;
@@ -31,14 +27,6 @@ export const datasets: Record<string, DatasetConfig> = {
     getWeight: d => d.properties.population_sum || 1,
     description: 'H3 cells with population data from table source'
   },
-  'H3 Tileset (Retail)': {
-    type: 'h3',
-    source: h3TilesetSource,
-    tableName:
-      'carto-demo-data.demo_tilesets.derived_spatialfeatures_usa_h3res8_v1_yearly_v2_tileset',
-    getWeight: d => d.properties.retail || 1,
-    description: 'H3 cells with retail data from tileset source'
-  },
   'Quadbin Table (Population)': {
     type: 'quadbin',
     source: quadbinTableSource,
@@ -47,12 +35,4 @@ export const datasets: Record<string, DatasetConfig> = {
     getWeight: d => d.properties.population_sum || 1,
     description: 'Quadbin cells with population data from table source'
   },
-  'Quadbin Tileset (Retail)': {
-    type: 'quadbin',
-    source: quadbinTilesetSource,
-    tableName:
-      'carto-demo-data.demo_tilesets.derived_spatialfeatures_usa_quadbin15_v1_yearly_v2_tileset',
-    getWeight: d => d.properties.avg_retail || 1,
-    description: 'Quadbin cells with retail data from tileset source'
-  }
 };
