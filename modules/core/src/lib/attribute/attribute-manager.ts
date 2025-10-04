ad9e5e43c82fc0967f15662e07f0610cef0e92d4
Jack Blazes <cjhblazes@gmail.com>
2025-10-03 17:20:13 -0700

    attribute manager fix

--- modules/core/src/lib/attribute/attribute-manager.ts
+++ modules/core/src/lib/attribute/attribute-manager.ts
@@ -380,7 +380,7 @@ export default class AttributeManager {
       // The attribute is flagged as constant outside of an update cycle
       // Skip allocation and updater call
       // @ts-ignore value can be set to an array by user but always cast to typed array during attribute update
-      attribute.setConstantValue(context, attribute.value);
+      attribute.setConstantValue(opts.context, attribute.value);
       return;
     }
 
