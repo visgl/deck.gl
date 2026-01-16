cf731c189fc3fe5ab190ae3e1f99ca35c7e1d6b6
Zbigniew Zagórski <zbigniew@cartodb.com>
2026-01-16 12:06:57 +0100

    tests for mapbox-overlay.spec.ts

--- test/modules/mapbox/index.ts
+++ test/modules/mapbox/index.ts
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) vis.gl contributors
 
-// import './mapbox-layer.spec';
-// import './mapbox-overlay.spec';
-// import './resolve-layers.spec';
+import './mapbox-layer.spec';
+import './mapbox-overlay.spec';
+import './resolve-layers.spec';
 import './resolve-layer-groups.spec';
