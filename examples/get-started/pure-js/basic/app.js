7273eb05d1b18b50a2cda20ae78d7af58bdd7116
Felix Palmer <felixpalmer@gmail.com>
2026-01-27 14:22:04 +0100

    Revert demo

--- examples/get-started/pure-js/basic/app.js
+++ examples/get-started/pure-js/basic/app.js
@@ -3,74 +3,63 @@
 // Copyright (c) vis.gl contributors
 
 import {Deck} from '@deck.gl/core';
-import {Tile3DLayer} from '@deck.gl/geo-layers';
+import {GeoJsonLayer, ArcLayer} from '@deck.gl/layers';
 
-const GOOGLE_MAPS_API_KEY = process.env.GoogleMapsAPIKey; // eslint-disable-line
-const TILESET_URL = 'https://tile.googleapis.com/v1/3dtiles/root.json';
+// source: Natural Earth http://www.naturalearthdata.com/ via geojson.xyz
+const COUNTRIES =
+  'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_admin_0_scale_rank.geojson'; //eslint-disable-line
+const AIR_PORTS =
+  'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_10m_airports.geojson';
 
-// Matterhorn coordinates: 45.9763° N, 7.6586° E
 const INITIAL_VIEW_STATE = {
-  latitude: 51,
-  longitude: 0,
-  zoom: 12,
+  latitude: 51.47,
+  longitude: 0.45,
+  zoom: 4,
   bearing: 0,
-  pitch: 60,
-  position: [0, 0, 0]
+  pitch: 30
 };
 
-const deck = new Deck({
+new Deck({
   initialViewState: INITIAL_VIEW_STATE,
   controller: true,
-  onClick: info => {
-    console.log('=== CLICK INFO ===');
-    console.log('picked:', info.picked);
-    console.log('coordinate:', info.coordinate);
-    console.log('object:', info.object);
-    console.log('layer:', info.layer?.id);
-
-    if (info.picked && info.coordinate && info.coordinate.length === 3) {
-      console.log('3D coordinate - altitude:', info.coordinate[2], 'meters');
-    }
-  },
-  getTooltip: info => {
-    if (info.picked && info.coordinate && info.coordinate.length === 3) {
-      const altitude = info.coordinate[2];
-      return {
-        html: `<div style="background: rgba(0, 0, 0, 0.8); color: white; padding: 8px 12px; border-radius: 4px; font-family: monospace;">
-          Altitude: ${altitude.toFixed(1)} m
-        </div>`,
-        style: {
-          padding: '0'
-        }
-      };
-    }
-    return null;
-  },
   layers: [
-    new Tile3DLayer({
-      id: 'google-3d-tiles',
-      data: TILESET_URL,
-      pickable: '3d',
-      onTilesetLoad: tileset3d => {
-        tileset3d.options.onTraversalComplete = selectedTiles => {
-          return selectedTiles;
-          const uniqueCredits = new Set();
-          selectedTiles.forEach(tile => {
-            const {copyright} = tile.content.gltf.asset;
-            copyright.split(';').forEach(uniqueCredits.add, uniqueCredits);
-          });
-          const creditsText = [...uniqueCredits].join('; ');
-          // Display credits in console
-          return selectedTiles;
-        };
-      },
-      loadOptions: {
-        fetch: {headers: {'X-GOOG-API-KEY': GOOGLE_MAPS_API_KEY}}
-      },
-      operation: 'terrain+draw'
+    new GeoJsonLayer({
+      id: 'base-map',
+      data: COUNTRIES,
+      // Styles
+      stroked: true,
+      filled: true,
+      lineWidthMinPixels: 2,
+      opacity: 0.4,
+      getLineColor: [60, 60, 60],
+      getFillColor: [200, 200, 200]
+    }),
+    new GeoJsonLayer({
+      id: 'airports',
+      data: AIR_PORTS,
+      // Styles
+      filled: true,
+      pointRadiusMinPixels: 2,
+      pointRadiusScale: 2000,
+      getPointRadius: f => 11 - f.properties.scalerank,
+      getFillColor: [200, 0, 80, 180],
+      // Interactive props
+      pickable: true,
+      autoHighlight: true,
+      onClick: info =>
+        // eslint-disable-next-line
+        info.object && alert(`${info.object.properties.name} (${info.object.properties.abbrev})`)
+    }),
+    new ArcLayer({
+      id: 'arcs',
+      data: AIR_PORTS,
+      dataTransform: d => d.features.filter(f => f.properties.scalerank < 4),
+      // Styles
+      getSourcePosition: f => [-0.4531566, 51.4709959], // London
+      getTargetPosition: f => f.geometry.coordinates,
+      getSourceColor: [0, 128, 200],
+      getTargetColor: [200, 0, 80],
+      getWidth: 1
     })
   ]
 });
-
-// Export for debugging
-window.deck = deck;
