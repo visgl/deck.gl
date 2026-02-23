b900cf29bff557296222e407ac6c76c9ffb076e3
Felix Palmer <felixpalmer@gmail.com>
2026-02-23 13:44:34 +0100

    revert example

--- examples/website/google-3d-tiles/app.jsx
+++ examples/website/google-3d-tiles/app.jsx
@@ -24,12 +24,13 @@ export const COLORS = [
 const colorScale = scaleLinear().clamp(true).domain([0, 50, 100, 200, 300]).range(COLORS);
 
 const INITIAL_VIEW_STATE = {
-  latitude: 36.743,
-    longitude: -5.157,
-    zoom: 15,
-    bearing: 70,
-    pitch: 50,
-    height: 200,
+  latitude: 50.089,
+  longitude: 14.42,
+  zoom: 16,
+  minZoom: 14,
+  maxZoom: 16.5,
+  bearing: 90,
+  pitch: 60
 };
 
 const BUILDING_DATA =
