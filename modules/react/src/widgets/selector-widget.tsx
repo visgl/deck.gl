ea52f03f6222b50f0c04028e64cb1e62c5d3d1ad
Xiaoji Chen <cxiaoji@gmail.com>
2026-03-12 00:36:11 -0700

    lint

--- modules/react/src/widgets/selector-widget.tsx
+++ modules/react/src/widgets/selector-widget.tsx
@@ -7,6 +7,9 @@ import type {SelectorWidgetProps} from '@deck.gl/widgets';
 import {useWidget} from '../utils/use-widget';
 
 export const SelectorWidget = <ValueT = string,>(props: SelectorWidgetProps<ValueT>) => {
-  useWidget(_SelectorWidget as new (props_: SelectorWidgetProps<ValueT>) => _SelectorWidget<ValueT>, props);
+  useWidget(
+    _SelectorWidget as new (props_: SelectorWidgetProps<ValueT>) => _SelectorWidget<ValueT>,
+    props
+  );
   return null;
 };
