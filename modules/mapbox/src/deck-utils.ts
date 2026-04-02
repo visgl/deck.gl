ab72c2635c6d30c8139847537a8dec2270bfb839
Felix Palmer <felixpalmer@gmail.com>
2026-04-02 11:01:16 +0200

    fix(mapbox): onBefore|AfterRender fires without layers

--- modules/mapbox/src/deck-utils.ts
+++ modules/mapbox/src/deck-utils.ts
@@ -364,6 +364,12 @@ function afterRender(deck: Deck, map: Map): void {
         (params.viewport.id !== MAPBOX_VIEW_ID || !map.getLayer(params.layer.id)),
       clearCanvas: false
     });
+  } else {
+    // Even when there are no non-Mapbox layers to draw, fire lifecycle callbacks
+    // so that consumers can still track view state changes via onAfterRender
+    const {device, gl} = deck.layerManager!.context;
+    deck.props.onBeforeRender?.({device, gl});
+    deck.props.onAfterRender?.({device, gl});
   }
 
   // End of render cycle, clear generated viewport
