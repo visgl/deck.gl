47d203033b9651ab4b7efe028d61333a198db418
Xiaoji Chen <cxiaoji@gmail.com>
2026-04-17 17:23:30 -0700

    Add react tests

--- test/modules/react/deckgl.spec.ts
+++ test/modules/react/deckgl.spec.ts
@@ -3,15 +3,15 @@
 // Copyright (c) vis.gl contributors
 
 /* eslint-disable no-unused-vars */
-import {test, expect} from 'vitest';
-import {createElement, createRef, act} from 'react';
+import {test, expect, vi} from 'vitest';
+import {createElement, createRef, act, type RefObject} from 'react';
 import {createRoot} from 'react-dom/client';
 
 import {Layer, Widget} from '@deck.gl/core';
 import DeckGL, {type DeckGLRef} from '@deck.gl/react';
 import {type WidgetProps, type WidgetPlacement} from '@deck.gl/core';
 
-import {device, gl} from '@deck.gl/test-utils/vitest';
+import {gl} from '@deck.gl/test-utils/vitest';
 
 // Required by React 19
 // @ts-expect-error undefined global flag
@@ -27,7 +27,14 @@ const TEST_VIEW_STATE = {
 
 // If testing under node, provide a headless context
 /* global document */
-const getDeckProps = () => (globalThis.__JSDOM__ ? {gl} : {device});
+const getDeckProps = () => (globalThis.__JSDOM__ ? {gl} : {});
+
+function waitUntilReady(ref: RefObject<DeckGLRef | null>): Promise<boolean> {
+  return vi.waitUntil(() => {
+    const deck = ref.current?.deck;
+    return deck && deck.isInitialized && !deck.needsRedraw({clearRedrawFlags: false});
+  });
+}
 
 test('DeckGL#mount/unmount', async () => {
   const ref = createRef<DeckGLRef>();
@@ -35,26 +42,20 @@ test('DeckGL#mount/unmount', async () => {
   document.body.append(container);
   const root = createRoot(container);
 
-  const loadPromise = new Promise<void>(resolve => {
-    act(() => {
-      root.render(
-        createElement(DeckGL, {
-          initialViewState: TEST_VIEW_STATE,
-          ref,
-          width: 100,
-          height: 100,
-          ...getDeckProps(),
-          onLoad: () => resolve()
-        })
-      );
-    });
+  act(() => {
+    root.render(
+      createElement(DeckGL, {
+        initialViewState: TEST_VIEW_STATE,
+        ref,
+        width: 100,
+        height: 100,
+        ...getDeckProps()
+      })
+    );
   });
-  await loadPromise;
-
-  expect(ref.current, 'DeckGL overlay is rendered.').toBeTruthy();
+  await waitUntilReady(ref);
 
   const {deck} = ref.current!;
-  expect(deck).toBeTruthy();
   expect(deck, 'DeckGL is initialized').toBeTruthy();
   const viewport = deck!.getViewports()[0];
   expect(viewport && viewport.longitude, 'View state is set').toBe(TEST_VIEW_STATE.longitude);
@@ -69,28 +70,27 @@ test('DeckGL#mount/unmount', async () => {
 });
 
 test('DeckGL#render', async () => {
+  const ref = createRef<DeckGLRef>();
   const container = document.createElement('div');
   document.body.append(container);
   const root = createRoot(container);
 
-  const renderPromise = new Promise<void>(resolve => {
-    act(() => {
-      root.render(
-        createElement(
-          DeckGL,
-          {
-            initialViewState: TEST_VIEW_STATE,
-            width: 100,
-            height: 100,
-            ...getDeckProps(),
-            onAfterRender: () => resolve()
-          },
-          createElement('div', {className: 'child'}, 'Child')
-        )
-      );
-    });
+  act(() => {
+    root.render(
+      createElement(
+        DeckGL,
+        {
+          ref,
+          initialViewState: TEST_VIEW_STATE,
+          width: 100,
+          height: 100,
+          ...getDeckProps()
+        },
+        createElement('div', {className: 'child'}, 'Child')
+      )
+    );
   });
-  await renderPromise;
+  await waitUntilReady(ref);
 
   const child = container.querySelector('.child');
   expect(child, 'Child is rendered').toBeTruthy();
@@ -124,24 +124,21 @@ test('DeckGL#props omitted are reset', async () => {
   document.body.append(container);
   const root = createRoot(container);
 
-  let renderPromise = new Promise<void>(resolve => {
-    // Initialize widgets and layers on first render.
-    act(() => {
-      root.render(
-        createElement(DeckGL, {
-          initialViewState: TEST_VIEW_STATE,
-          ref,
-          width: 100,
-          height: 100,
-          ...getDeckProps(),
-          layers: LAYERS,
-          widgets: WIDGETS,
-          onLoad: () => resolve()
-        })
-      );
-    });
+  // Initialize widgets and layers on first render.
+  act(() => {
+    root.render(
+      createElement(DeckGL, {
+        initialViewState: TEST_VIEW_STATE,
+        ref,
+        width: 100,
+        height: 100,
+        ...getDeckProps(),
+        layers: LAYERS,
+        widgets: WIDGETS
+      })
+    );
   });
-  await renderPromise;
+  await waitUntilReady(ref);
 
   const {deck} = ref.current!;
   expect(deck, 'DeckGL is initialized').toBeTruthy();
@@ -149,19 +146,16 @@ test('DeckGL#props omitted are reset', async () => {
   expect(widgets && Array.isArray(widgets) && widgets.length, 'Widgets is set').toBe(1);
   expect(layers && Array.isArray(layers) && layers.length, 'Layers is set').toBe(1);
 
-  renderPromise = new Promise<void>(resolve => {
-    act(() => {
-      // Render deck a second time without setting widget or layer props.
-      root.render(
-        createElement(DeckGL, {
-          ref,
-          ...getDeckProps(),
-          onAfterRender: () => resolve()
-        })
-      );
-    });
+  act(() => {
+    // Render deck a second time without setting widget or layer props.
+    root.render(
+      createElement(DeckGL, {
+        ref,
+        ...getDeckProps()
+      })
+    );
   });
-  await renderPromise;
+  await waitUntilReady(ref);
 
   widgets = deck!.props.widgets;
   layers = deck!.props.layers;
