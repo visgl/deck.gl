420053187b82404c053eac559af5df939f853d3c
Charles Richardson <charlesrichardsonusa@gmail.com>
2026-06-29 22:10:14 -0400

    Make Map3D overlay demo visible by default

--- examples/website/google-map-3d-overlay-prototype/vite.config.js
+++ examples/website/google-map-3d-overlay-prototype/vite.config.js
@@ -2,9 +2,20 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) vis.gl contributors
 
+import {fileURLToPath, URL} from 'node:url';
+
+const fromRoot = path => fileURLToPath(new URL(`../../../${path}`, import.meta.url));
+
 export default {
   define: {
     'process.env.GoogleMapsAPIKey': JSON.stringify(process.env.GoogleMapsAPIKey),
     'process.env.GoogleMapsMapId': JSON.stringify(process.env.GoogleMapsMapId)
+  },
+  resolve: {
+    alias: [
+      {find: /^@deck\.gl\/core$/, replacement: fromRoot('modules/core/src/index.ts')},
+      {find: /^@deck\.gl\/google-maps$/, replacement: fromRoot('modules/google-maps/src/index.ts')},
+      {find: /^@deck\.gl\/layers$/, replacement: fromRoot('modules/layers/src/index.ts')}
+    ]
   }
 };
