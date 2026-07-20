1407af9a9f80a10794df6c1ac81f4beebeafc33c
Chris Gervang <2461547+chrisgervang@users.noreply.github.com>
2026-07-20 08:38:40 -0700

    test(pydeck): add lighting effect to minimal fixture

--- bindings/pydeck/tests/bindings/pydeck_examples/minimal.py
+++ bindings/pydeck/tests/bindings/pydeck_examples/minimal.py
@@ -1,7 +1,13 @@
-from pydeck import Deck, Layer, View, ViewState
+from pydeck import Deck, Effect, Layer, View, ViewState
 
 
 def create_minimal_test_object():
+    lighting = Effect(
+        "LightingEffect",
+        ambient=Effect("AmbientLight", intensity=0.4),
+        point_light_1=Effect("PointLight", position=[-0.144528, 49.739968, 8000], intensity=0.6),
+        point_light_2=Effect("PointLight", position=[-3.807751, 54.104682, 8000], intensity=0.6),
+    )
     layer = Layer(
         "HexagonLayer",
         "https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv",
@@ -21,4 +27,4 @@ def create_minimal_test_object():
         bearing=-27.396674584323023,
     )
     view = View(type="MapView", controller=True)
-    return Deck(layers=[layer], initial_view_state=view_state, views=[view])
+    return Deck(layers=[layer], effects=[lighting], initial_view_state=view_state, views=[view])
