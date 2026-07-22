e321f2c4ab390bd4e1275a2973344d95dfde3f6f
Ib Green <ib.green.home@gmail.com>
2026-07-22 11:15:22 -0400

    fix(layers): match ArcLayer WebGPU interpolation

--- modules/layers/src/arc-layer/arc-layer.wgsl.ts
+++ modules/layers/src/arc-layer/arc-layer.wgsl.ts
@@ -22,6 +22,7 @@ struct Varyings {
   @location(0) color: vec4<f32>,
   @location(1) uv: vec2<f32>,
   @location(2) pickingColor: vec3<f32>,
+  @location(3) isValid: f32,
 };
 
 fn paraboloid(
@@ -69,6 +70,52 @@ fn interpolateFlat(
   return vec3<f32>(mix(source.xy, targetPosition.xy, ratio) + tilt, z * cos(tiltAngle));
 }
 
+// Great circle interpolation
+// http://www.movable-type.co.uk/scripts/latlong.html
+fn getAngularDistance(source: vec2<f32>, targetPosition: vec2<f32>) -> f32 {
+  let sourceRadians = radians(source);
+  let targetRadians = radians(targetPosition);
+  let sinHalfDelta = sin((sourceRadians - targetRadians) / 2.0);
+  let sinHalfDeltaSquared = sinHalfDelta * sinHalfDelta;
+  let a = sinHalfDeltaSquared.y +
+    cos(sourceRadians.y) * cos(targetRadians.y) * sinHalfDeltaSquared.x;
+  return 2.0 * asin(sqrt(a));
+}
+
+fn interpolateGreatCircle(
+  source: vec3<f32>,
+  targetPosition: vec3<f32>,
+  source3D: vec3<f32>,
+  target3D: vec3<f32>,
+  angularDistance: f32,
+  ratio: f32,
+  height: f32
+) -> vec3<f32> {
+  var longitudeLatitude: vec2<f32>;
+
+  // If the angular distance is PI, use linear interpolation. Otherwise use spherical interpolation.
+  if (abs(angularDistance - PI) < 0.001) {
+    longitudeLatitude = (1.0 - ratio) * source.xy + ratio * targetPosition.xy;
+  } else {
+    let a = sin((1.0 - ratio) * angularDistance);
+    let b = sin(ratio * angularDistance);
+    let p = source3D.yxz * a + target3D.yxz * b;
+    longitudeLatitude = degrees(vec2<f32>(
+      atan2(p.y, -p.x),
+      atan2(p.z, length(p.xy))
+    ));
+  }
+
+  let z = paraboloid(
+    angularDistance * EARTH_RADIUS,
+    source.z,
+    targetPosition.z,
+    ratio,
+    height
+  );
+  return vec3<f32>(longitudeLatitude, z);
+}
+
 @vertex
 fn vertexMain(
   attributes: Attributes,
@@ -80,57 +127,170 @@ fn vertexMain(
 
   let segmentIndex = f32(vertexIndex / 2u);
   let segmentSide = select(-1.0, 1.0, vertexIndex % 2u == 1u);
-  let segmentRatio = getSegmentRatio(segmentIndex);
+  var segmentRatio = getSegmentRatio(segmentIndex);
   let previousRatio = getSegmentRatio(max(0.0, segmentIndex - 1.0));
-  let nextRatio = getSegmentRatio(min(arc.numSegments - 1.0, segmentIndex + 1.0));
+  var nextRatio = getSegmentRatio(min(arc.numSegments - 1.0, segmentIndex + 1.0));
+  // If this is the first point, use next - current as direction.
+  var indexDirection = select(-1.0, 1.0, segmentIndex <= 0.0);
+  var isValid = 1.0;
 
-  var sourceWorld = attributes.instanceSourcePositions;
-  var targetWorld = attributes.instanceTargetPositions;
-  if (arc.useShortestPath != 0.0) {
-    sourceWorld.x = ((sourceWorld.x + 180.0) % 360.0) - 180.0;
-    targetWorld.x = ((targetWorld.x + 180.0) % 360.0) - 180.0;
-    let deltaLongitude = targetWorld.x - sourceWorld.x;
-    if (deltaLongitude > 180.0) {
-      targetWorld.x -= 360.0;
+  var currentClip: vec4<f32>;
+  var nextClip: vec4<f32>;
+
+  if (
+    (arc.greatCircle != 0.0 || project.projectionMode == PROJECTION_MODE_GLOBE) &&
+    project.coordinateSystem == COORDINATE_SYSTEM_LNGLAT
+  ) {
+    let source = project_globe_(vec3<f32>(attributes.instanceSourcePositions.xy, 0.0));
+    let targetPosition = project_globe_(vec3<f32>(attributes.instanceTargetPositions.xy, 0.0));
+    let angularDistance = getAngularDistance(
+      attributes.instanceSourcePositions.xy,
+      attributes.instanceTargetPositions.xy
+    );
+
+    let previousPosition = interpolateGreatCircle(
+      attributes.instanceSourcePositions,
+      attributes.instanceTargetPositions,
+      source,
+      targetPosition,
+      angularDistance,
+      previousRatio,
+      attributes.instanceHeights
+    );
+    var currentPosition = interpolateGreatCircle(
+      attributes.instanceSourcePositions,
+      attributes.instanceTargetPositions,
+      source,
+      targetPosition,
+      angularDistance,
+      segmentRatio,
+      attributes.instanceHeights
+    );
+    var nextPosition = interpolateGreatCircle(
+      attributes.instanceSourcePositions,
+      attributes.instanceTargetPositions,
+      source,
+      targetPosition,
+      angularDistance,
+      nextRatio,
+      attributes.instanceHeights
+    );
+
+    if (abs(currentPosition.x - previousPosition.x) > 180.0) {
+      indexDirection = -1.0;
+      isValid = 0.0;
+    } else if (abs(currentPosition.x - nextPosition.x) > 180.0) {
+      indexDirection = 1.0;
+      isValid = 0.0;
     }
-    if (deltaLongitude < -180.0) {
-      sourceWorld.x -= 360.0;
+    nextPosition = select(nextPosition, previousPosition, indexDirection < 0.0);
+    nextRatio = select(nextRatio, previousRatio, indexDirection < 0.0);
+
+    if (isValid == 0.0) {
+      // Split at the antimeridian.
+      nextPosition.x += select(360.0, -360.0, nextPosition.x > 0.0);
+      let ratio = (
+        select(-180.0, 180.0, currentPosition.x > 0.0) - currentPosition.x
+      ) / (nextPosition.x - currentPosition.x);
+      currentPosition = mix(currentPosition, nextPosition, ratio);
+      segmentRatio = mix(segmentRatio, nextRatio, ratio);
     }
+
+    let currentPosition64Low = mix(
+      attributes.instanceSourcePositions64Low,
+      attributes.instanceTargetPositions64Low,
+      segmentRatio
+    );
+    let nextPosition64Low = mix(
+      attributes.instanceSourcePositions64Low,
+      attributes.instanceTargetPositions64Low,
+      nextRatio
+    );
+    let currentProjection = project_position_to_clipspace_and_commonspace(
+      currentPosition,
+      currentPosition64Low,
+      ZERO_OFFSET
+    );
+    currentClip = currentProjection.clipPosition;
+    nextClip = project_position_to_clipspace(nextPosition, nextPosition64Low, ZERO_OFFSET);
+    geometry.position = currentProjection.commonPosition;
+  } else {
+    var sourceWorld = attributes.instanceSourcePositions;
+    var targetWorld = attributes.instanceTargetPositions;
+    if (arc.useShortestPath != 0.0) {
+      sourceWorld.x = ((sourceWorld.x + 180.0) % 360.0) - 180.0;
+      targetWorld.x = ((targetWorld.x + 180.0) % 360.0) - 180.0;
+      let deltaLongitude = targetWorld.x - sourceWorld.x;
+      if (deltaLongitude > 180.0) {
+        targetWorld.x -= 360.0;
+      }
+      if (deltaLongitude < -180.0) {
+        sourceWorld.x -= 360.0;
+      }
+    }
+
+    let source = project_position_vec3_f64(
+      sourceWorld,
+      attributes.instanceSourcePositions64Low
+    );
+    let targetPosition = project_position_vec3_f64(
+      targetWorld,
+      attributes.instanceTargetPositions64Low
+    );
+
+    // Common x at longitude=-180.
+    var antimeridianX = 0.0;
+    if (arc.useShortestPath != 0.0) {
+      if (project.projectionMode == PROJECTION_MODE_WEB_MERCATOR_AUTO_OFFSET) {
+        antimeridianX = -(project.coordinateOrigin.x + 180.0) / 360.0 * TILE_SIZE;
+      }
+      let thresholdRatio = (antimeridianX - source.x) / (targetPosition.x - source.x);
+      if (previousRatio <= thresholdRatio && nextRatio > thresholdRatio) {
+        isValid = 0.0;
+        indexDirection = sign(segmentRatio - thresholdRatio);
+        segmentRatio = thresholdRatio;
+      }
+    }
+
+    nextRatio = select(nextRatio, previousRatio, indexDirection < 0.0);
+    var currentPosition = interpolateFlat(
+      source,
+      targetPosition,
+      segmentRatio,
+      attributes.instanceHeights,
+      attributes.instanceTilts
+    );
+    var nextPosition = interpolateFlat(
+      source,
+      targetPosition,
+      nextRatio,
+      attributes.instanceHeights,
+      attributes.instanceTilts
+    );
+
+    if (arc.useShortestPath != 0.0 && nextPosition.x < antimeridianX) {
+      currentPosition.x += TILE_SIZE;
+      nextPosition.x += TILE_SIZE;
+    }
+
+    currentClip = project_common_position_to_clipspace(vec4<f32>(currentPosition, 1.0));
+    nextClip = project_common_position_to_clipspace(vec4<f32>(nextPosition, 1.0));
+    geometry.position = vec4<f32>(currentPosition, 1.0);
   }
 
-  let source = project_position_vec3_f64(sourceWorld, attributes.instanceSourcePositions64Low);
-  let targetPosition = project_position_vec3_f64(
-    targetWorld,
-    attributes.instanceTargetPositions64Low
-  );
-  let currentPosition = interpolateFlat(
-    source,
-    targetPosition,
-    segmentRatio,
-    attributes.instanceHeights,
-    attributes.instanceTilts
-  );
-  let directionRatio = select(nextRatio, previousRatio, segmentIndex > 0.0);
-  let nextPosition = interpolateFlat(
-    source,
-    targetPosition,
-    directionRatio,
-    attributes.instanceHeights,
-    attributes.instanceTilts
-  );
-
-  geometry.position = vec4<f32>(currentPosition, 1.0);
   geometry.uv = vec2<f32>(segmentRatio, segmentSide);
   geometry.pickingColor = picking_getPickingColorFromIndex(instanceIndex);
 
-  let currentClip = project_common_position_to_clipspace(geometry.position);
-  let nextClip = project_common_position_to_clipspace(vec4<f32>(nextPosition, 1.0));
   let widthPixels = clamp(
     project_unit_size_to_pixel(attributes.instanceWidths * arc.widthScale, arc.widthUnits),
     arc.widthMinPixels,
     arc.widthMaxPixels
   );
-  let offset = getExtrusionOffset(nextClip.xy - currentClip.xy, segmentSide, widthPixels);
+  let offset = getExtrusionOffset(
+    (nextClip.xy - currentClip.xy) * indexDirection,
+    segmentSide,
+    widthPixels
+  );
 
   var output: Varyings;
   output.position = currentClip + vec4<f32>(project_pixel_size_to_clipspace(offset), 0.0, 0.0);
@@ -138,11 +298,16 @@ fn vertexMain(
   output.color = vec4<f32>(color.rgb, color.a * layer.opacity);
   output.uv = geometry.uv;
   output.pickingColor = geometry.pickingColor;
+  output.isValid = isValid;
   return output;
 }
 
 @fragment
 fn fragmentMain(varyings: Varyings) -> @location(0) vec4<f32> {
+  if (varyings.isValid == 0.0) {
+    discard;
+  }
+
   var color = varyings.color;
   if (picking.isActive > 0.5) {
     if (!picking_isColorValid(varyings.pickingColor)) {
