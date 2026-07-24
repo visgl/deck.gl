2e79d26f2ca4066c2b106fa594eb8d86873370ab
Ib Green <ib.green.home@gmail.com>
2026-07-24 09:28:37 -0400

    refactor(layers): use buffer groups in PathLayer WebGPU port

--- modules/layers/src/path-layer/path-layer.ts
+++ modules/layers/src/path-layer/path-layer.ts
@@ -3,7 +3,6 @@
 // Copyright (c) vis.gl contributors
 
 import {Layer, project32, color, picking, UNIT} from '@deck.gl/core';
-import {BufferLayout, Parameters} from '@luma.gl/core';
 import {Geometry} from '@luma.gl/engine';
 import {Model} from '@luma.gl/engine';
 import PathTesselator from './path-tesselator';
@@ -160,130 +159,99 @@ export default class PathLayer<DataT = any, ExtraPropsT extends {} = {}> extends
 
   initializeState() {
     const noAlloc = true;
+    const isWebGPU = this.context.device.type === 'webgpu';
     const attributeManager = this.getAttributeManager();
     /* eslint-disable max-len */
-    if (this.context.device.type === 'webgpu') {
-      attributeManager!.addInstanced({
-        instancePositions: {
-          size: 12,
-          type: 'float32',
-          // WebGPU keeps the fp64 path by uploading float32 high parts and reconstructing
-          // with the matching `instancePositions64Low` residuals in WGSL.
-          transition: false,
-          accessor: 'getPath',
-          // eslint-disable-next-line @typescript-eslint/unbound-method
-          update: this.calculateInstancePositions,
-          shaderAttributes: {
-            instanceLeftPositions: {size: 3, elementOffset: 0},
-            instanceStartPositions: {size: 3, elementOffset: 3},
-            instanceEndPositions: {size: 3, elementOffset: 6},
-            instanceRightPositions: {size: 3, elementOffset: 9}
-          },
-          noAlloc
-        },
-        instancePositions64Low: {
-          size: 12,
-          type: 'float32',
-          // This is the low-part companion to `instancePositions`, not a plain-fp32 downgrade.
-          transition: false,
-          accessor: 'getPath',
-          // eslint-disable-next-line @typescript-eslint/unbound-method
-          update: this.calculateInstancePositions64Low,
-          shaderAttributes: {
-            instanceLeftPositions64Low: {size: 3, elementOffset: 0},
-            instanceStartPositions64Low: {size: 3, elementOffset: 3},
-            instanceEndPositions64Low: {size: 3, elementOffset: 6},
-            instanceRightPositions64Low: {size: 3, elementOffset: 9}
-          },
-          noAlloc
-        },
-        instanceTypes: {
-          size: 1,
-          // eslint-disable-next-line @typescript-eslint/unbound-method
-          update: this.calculateSegmentTypes,
-          noAlloc
-        },
-        instanceStrokeWidths: {
-          size: 1,
-          accessor: 'getWidth',
-          transition: false,
-          defaultValue: 1
-        },
-        instanceColors: {
-          size: this.props.colorFormat.length,
-          type: 'unorm8',
-          accessor: 'getColor',
-          transition: false,
-          defaultValue: DEFAULT_COLOR
-        },
-        /** Source path row for each generated segment/joint instance. */
-        rowIndexes: {
-          size: 1,
-          type: 'uint32',
-          accessor: (object, {index}) => (object && object.__source ? object.__source.index : index)
-        }
-      });
-    } else {
-      attributeManager!.addInstanced({
-        vertexPositions: {
-          size: 3,
-          // Start filling buffer from 1 vertex in
-          vertexOffset: 1,
-          type: 'float64',
-          fp64: this.use64bitPositions(),
-          transition: ATTRIBUTE_TRANSITION,
-          accessor: 'getPath',
-          // eslint-disable-next-line @typescript-eslint/unbound-method
-          update: this.calculatePositions,
-          noAlloc,
-          shaderAttributes: {
-            instanceLeftPositions: {
-              vertexOffset: 0
-            },
-            instanceStartPositions: {
-              vertexOffset: 1
-            },
-            instanceEndPositions: {
-              vertexOffset: 2
-            },
-            instanceRightPositions: {
-              vertexOffset: 3
+    attributeManager!.addInstanced({
+      ...(isWebGPU
+        ? {
+            // WebGPU cannot express WebGL's vertexOffset window in one vertex buffer layout.
+            // Pack each segment's [left, start, end, right] high and low position parts instead.
+            instancePositions: {
+              size: 24,
+              type: 'float32',
+              transition: false,
+              accessor: 'getPath',
+              // eslint-disable-next-line @typescript-eslint/unbound-method
+              update: this.calculateWebGPUPositions,
+              shaderAttributes: {
+                instanceLeftPositions: {size: 3, elementOffset: 0},
+                instanceStartPositions: {size: 3, elementOffset: 3},
+                instanceEndPositions: {size: 3, elementOffset: 6},
+                instanceRightPositions: {size: 3, elementOffset: 9},
+                instanceLeftPositions64Low: {size: 3, elementOffset: 12},
+                instanceStartPositions64Low: {size: 3, elementOffset: 15},
+                instanceEndPositions64Low: {size: 3, elementOffset: 18},
+                instanceRightPositions64Low: {size: 3, elementOffset: 21}
+              },
+              noAlloc
             }
           }
-        },
-        instanceTypes: {
-          size: 1,
-          type: 'uint8',
-          // eslint-disable-next-line @typescript-eslint/unbound-method
-          update: this.calculateSegmentTypes,
-          noAlloc
-        },
-        instanceStrokeWidths: {
-          size: 1,
-          accessor: 'getWidth',
-          transition: ATTRIBUTE_TRANSITION,
-          defaultValue: 1
-        },
-        instanceColors: {
-          size: this.props.colorFormat.length,
-          type: 'unorm8',
-          accessor: 'getColor',
-          transition: ATTRIBUTE_TRANSITION,
-          defaultValue: DEFAULT_COLOR
-        },
-        /** Source path row for each generated segment/joint instance. */
-        rowIndexes: {
-          size: 1,
-          type: 'uint32',
-          accessor: (object, {index}) => (object && object.__source ? object.__source.index : index)
-        }
-      });
-    }
+        : {
+            vertexPositions: {
+              size: 3,
+              // Start filling buffer from 1 vertex in
+              vertexOffset: 1,
+              type: 'float64',
+              fp64: this.use64bitPositions(),
+              transition: ATTRIBUTE_TRANSITION,
+              accessor: 'getPath',
+              // eslint-disable-next-line @typescript-eslint/unbound-method
+              update: this.calculatePositions,
+              noAlloc,
+              shaderAttributes: {
+                instanceLeftPositions: {
+                  vertexOffset: 0
+                },
+                instanceStartPositions: {
+                  vertexOffset: 1
+                },
+                instanceEndPositions: {
+                  vertexOffset: 2
+                },
+                instanceRightPositions: {
+                  vertexOffset: 3
+                }
+              }
+            }
+          }),
+      instanceTypes: {
+        size: 1,
+        type: isWebGPU ? 'float32' : 'uint8',
+        // eslint-disable-next-line @typescript-eslint/unbound-method
+        update: this.calculateSegmentTypes,
+        noAlloc
+      },
+      instanceStrokeWidths: {
+        size: 1,
+        accessor: 'getWidth',
+        transition: isWebGPU ? false : ATTRIBUTE_TRANSITION,
+        defaultValue: 1,
+        bufferGroup: 'path-instance-data'
+      },
+      instanceColors: {
+        size: this.props.colorFormat.length,
+        type: 'unorm8',
+        accessor: 'getColor',
+        transition: isWebGPU ? false : ATTRIBUTE_TRANSITION,
+        defaultValue: DEFAULT_COLOR,
+        bufferGroup: 'path-instance-data'
+      },
+      /** Source path row for each generated segment/joint instance. */
+      rowIndexes: {
+        size: 1,
+        type: 'uint32',
+        accessor: (object, {index}) => (object && object.__source ? object.__source.index : index),
+        // AttributeManager only materializes buffer groups on WebGPU, so WebGL keeps its layout.
+        bufferGroup: 'path-instance-data'
+      }
+    });
     /* eslint-enable max-len */
 
     this.setState({
       pathTesselator: new PathTesselator({
-        fp64: this.use64bitPositions()
+        fp64: this.use64bitPositions(),
+        isWebGPU
       })
     });
   }
@@ -392,14 +360,6 @@ export default class PathLayer<DataT = any, ExtraPropsT extends {} = {}> extends
   }
 
   protected _getModel(): Model {
-    const parameters =
-      this.context.device.type === 'webgpu'
-        ? ({
-            depthWriteEnabled: true,
-            depthCompare: 'less-equal'
-          } satisfies Parameters)
-        : undefined;
-
     /*
      *       _
      *        "-_ 1                   3                       5
@@ -447,7 +407,7 @@ export default class PathLayer<DataT = any, ExtraPropsT extends {} = {}> extends
     return new Model(this.context.device, {
       ...this.getShaders(),
       id: this.props.id,
-      bufferLayout: this._getModelBufferLayouts(),
+      bufferLayout: this.getAttributeManager()!.getBufferLayouts(),
       geometry: new Geometry({
         topology: 'triangle-list',
         attributes: {
@@ -455,7 +415,6 @@ export default class PathLayer<DataT = any, ExtraPropsT extends {} = {}> extends
           positions: {value: new Float32Array(SEGMENT_POSITIONS), size: 2}
         }
       }),
-      parameters,
       isInstanced: true
     });
   }
@@ -467,14 +426,6 @@ export default class PathLayer<DataT = any, ExtraPropsT extends {} = {}> extends
     attribute.value = pathTesselator.get('positions');
   }
 
-  protected calculateInstancePositions(attribute) {
-    this._calculateInterleavedInstancePositions(attribute, false);
-  }
-
-  protected calculateInstancePositions64Low(attribute) {
-    this._calculateInterleavedInstancePositions(attribute, true);
-  }
-
   protected calculateSegmentTypes(attribute) {
     const {pathTesselator} = this.state;
 
@@ -482,7 +433,7 @@ export default class PathLayer<DataT = any, ExtraPropsT extends {} = {}> extends
     attribute.value = pathTesselator.get('segmentTypes');
   }
 
-  protected _calculateInterleavedInstancePositions(attribute, lowPart: boolean) {
+  protected calculateWebGPUPositions(attribute) {
     const {pathTesselator} = this.state;
     const value = pathTesselator.get('positions');
 
@@ -492,46 +443,27 @@ export default class PathLayer<DataT = any, ExtraPropsT extends {} = {}> extends
     }
 
     const numInstances = pathTesselator.instanceCount;
-    const result = new Float32Array(numInstances * 12);
+    const result = new Float32Array(numInstances * 24);
     // WebGL reads a padded neighbor window using `vertexOffset: 1`; this materializes
     // the same [-1, 0, 1, 2] access pattern explicitly for the WebGPU layout.
     const neighborOffsets = [-1, 0, 1, 2];
 
     for (let i = 0; i < numInstances; i++) {
-      const targetIndex = i * 12;
+      const targetIndex = i * 24;
       for (let vertexOffset = 0; vertexOffset < 4; vertexOffset++) {
         const sourceVertex = i + neighborOffsets[vertexOffset];
         const targetOffset = targetIndex + vertexOffset * 3;
         for (let j = 0; j < 3; j++) {
           const position =
             sourceVertex >= 0 && sourceVertex < numInstances ? value[sourceVertex * 3 + j] : 0;
-          result[targetOffset + j] = lowPart ? position - Math.fround(position) : position;
+          const highPart = Math.fround(position);
+          result[targetOffset + j] = highPart;
+          result[targetOffset + j + 12] = position - highPart;
         }
       }
     }
 
     attribute.startIndices = pathTesselator.vertexStarts;
     attribute.value = result;
-  }
-
-  protected _getModelBufferLayouts(): BufferLayout[] {
-    const bufferLayouts = this.getAttributeManager()!.getBufferLayouts();
-
-    if (this.context.device.type === 'webgpu') {
-      return bufferLayouts;
-    }
-
-    return bufferLayouts.map(layout =>
-      layout.name === 'vertexPositions'
-        ? {
-            ...layout,
-            attributes: (layout.attributes || []).filter(
-              attribute =>
-                attribute.attribute !== 'vertexPositions' &&
-                attribute.attribute !== 'vertexPositions64Low'
-            )
-          }
-        : layout
-    );
   }
 }
--- modules/layers/src/path-layer/path-tesselator.ts
+++ modules/layers/src/path-layer/path-tesselator.ts
@@ -22,6 +22,7 @@ export default class PathTesselator extends Tesselator<
     resolution?: number;
     wrapLongitude?: boolean;
     loop?: boolean;
+    isWebGPU?: boolean;
   }
 > {
   constructor(opts) {
@@ -36,9 +37,8 @@ export default class PathTesselator extends Tesselator<
           initialize: true,
           type: opts.fp64 ? Float64Array : Float32Array
         },
-        // WebGPU currently consumes `instanceTypes` as f32; using Float32Array keeps the
-        // upload and vertex layout 4-byte aligned without introducing a padded uint8 path.
-        segmentTypes: {size: 1, type: Float32Array}
+        // WebGPU vertex inputs use a 4-byte scalar; keep WebGL's compact uint8 buffer unchanged.
+        segmentTypes: {size: 1, type: opts.isWebGPU ? Float32Array : Uint8ClampedArray}
       }
     });
   }
