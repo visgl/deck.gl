88836fe30e27c10cfc2474acd435a2fc06f6fd9e
felixpalmer <felixpalmer@gmail.com>
2026-08-31 15:00:36 +0200

    CARTO: Remove dead code (moved to api-client) (#10617)

--- test/modules/carto/index.ts
+++ test/modules/carto/index.ts
@@ -4,8 +4,6 @@
 
 import './api/basemap.spec';
 import './api/fetch-map.spec';
-import './api/layer-map.spec';
-import './api/parse-map.spec';
 import './utils.spec';
 import './layers/h3-tile-layer.spec';
 import './layers/h3-tileset-2d.spec';
