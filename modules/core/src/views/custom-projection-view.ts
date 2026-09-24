3fa171597b80a7badea432ce76132bd6ace7f615
Xiaoji Chen <cxiaoji@gmail.com>
2026-09-23 18:19:25 -0700

    refactor(core): default custom projection scale estimation to lnglat

--- modules/core/src/views/custom-projection-view.ts
+++ modules/core/src/views/custom-projection-view.ts
@@ -37,7 +37,6 @@ export type CustomProjectionViewProps = CommonViewProps<CustomProjectionViewStat
     | 'projectionId'
     | 'resolution'
     | 'getUnitsPerMeter'
-    | 'inputUnits'
     | 'orthographic'
   >;
 
